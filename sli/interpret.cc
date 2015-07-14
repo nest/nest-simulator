45e513fa296d8c1b00af5f9a0cc987029121a599
Tammo Ippen <tammo.ippen@posteo.de>
2015-07-14 09:47:44 +0200

    Apply the described changes in PR #55

--- sli/interpret.cc
+++ sli/interpret.cc
@@ -880,7 +880,8 @@ SLIInterpreter::message( std::ostream& out,
   std::strftime( timestring, buflen, "%b %d %H:%M:%S", std::localtime( &tm ) );
 
   std::string msg = String::compose( "%1 %2 [%3]: ", timestring, from, levelname );
-  out << std::endl << msg << errorname;
+  out << std::endl
+      << msg << errorname;
 
   // Set the preferred line indentation.
   const size_t indent = 4;
@@ -907,7 +908,8 @@ SLIInterpreter::message( std::ostream& out,
   // Indent first message line
   if ( text_str.size() != 0 )
   {
-    std::cout << std::endl << std::string( indent, ' ' );
+    std::cout << std::endl
+              << std::string( indent, ' ' );
   }
 
   size_t pos = 0;
@@ -919,7 +921,8 @@ SLIInterpreter::message( std::ostream& out,
       // Print a lineshift followed by an indented whitespace
       // Manually inserted lineshift at the end of the message
       // are suppressed.
-      out << std::endl << std::string( indent, ' ' );
+      out << std::endl
+          << std::string( indent, ' ' );
       pos = 0;
     }
     else
@@ -942,7 +945,8 @@ SLIInterpreter::message( std::ostream& out,
       if ( i != 0 && text_str.at( i - 1 ) == ' '
         && static_cast< int >( space - i ) > static_cast< int >( width - pos ) )
       {
-        out << std::endl << std::string( indent, ' ' );
+        out << std::endl
+            << std::string( indent, ' ' );
         pos = 0;
       }
 

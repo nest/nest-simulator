74c4cbb9f3fd649b8dce0e3e312ad64c6312e554
hannah <h.bos@fz-juelich.de>
2015-08-11 17:00:40 +0200

    added full stop in documentation

--- pynest/nest/hl_api.py
+++ pynest/nest/hl_api.py
@@ -1063,7 +1063,7 @@ def Connect(pre, post, conn_spec=None, syn_spec=None, model=None):
     In the case of scalar parameters, all keys must be doubles except for 'receptor_type' which must be
     initialised with an integer. 
     Parameter arrays are only available for the rules 'one_to_one' and 'all_to_all'. For 'one_to_one' the
-    array has to be a one-dimensional NumPy array with length len(pre) For 'all_to_all' the array has
+    array has to be a one-dimensional NumPy array with length len(pre). For 'all_to_all' the array has
     to be a two-dimensional NumPy array with shape len(post) x len(pre).
     Any distributed parameter must be initialised with a further dictionary specifying the distribution 
     type ('distribution', e.g. 'normal') and any distribution-specific parameters (e.g. 'mu' and 'sigma').

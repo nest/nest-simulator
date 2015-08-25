e57694fda33ceca18064913996668b1c419fb872
Schumann Till <Till.Schumann@rwth-aachen.de>
2015-08-25 17:05:14 +0200

    typedef changed

--- nestkernel/H5Synapses/H5SynMEMPedictor.h
+++ nestkernel/H5Synapses/H5SynMEMPedictor.h
@@ -11,10 +11,10 @@
  * 
  */
 
-typedef __int32 int32_t;
-typedef __int64 int64_t;
-typedef unsigned __int32 uint32_t;
-typedef unsigned __int64 uint64_t;
+typedef int int32_t;
+typedef long int int64_t;
+typedef unsigned int uint32_t;
+typedef unsigned long int uint64_t;
 
 class H5SynMEMPredictor
 {

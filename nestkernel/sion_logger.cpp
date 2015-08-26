48f2bcd798954a6396c8b26def5a15b2c1fd80d2
Sebastian Billaudelle <sbillaudelle@gmail.com>
2015-08-26 18:34:02 +0200

    Added basic implementation of SIONlib logger.

---
+++ nestkernel/sion_logger.cpp
@@ -0,0 +1,222 @@
+#include "recording_device.h"
+
+#include "sion.h"
+
+#include "sion_logger.h"
+
+void
+nest::SIONLogger::enroll( const int virtual_process, RecordingDevice& device )
+{
+  const Node& node = device.get_node();
+  const int gid = node.get_gid();
+
+  // is virtual_process == virtual process we are in?
+  // FIXME: critical?
+
+  if ( files_.find( virtual_process ) == files_.end() )
+  {
+    files_.insert( std::make_pair( virtual_process, VirtualProcessEntry() ) );
+  }
+
+  if ( files_[ virtual_process ].devices.find( gid ) == files_[ virtual_process ].devices.end() )
+  {
+    DeviceEntry entry( device );
+    files_[ virtual_process ].devices.insert( std::make_pair( gid, entry ) );
+  }
+}
+
+void
+nest::SIONLogger::initialize()
+{
+  MPI_Comm local_comm;
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+#pragma omp parallel
+  {
+    Network& network = *( Node::network() );
+    thread t = network.get_thread_id();
+    int vp = network.thread_to_vp( t );
+
+    std::string tmp = "data.dat";
+    char* filename = strdup( tmp.c_str() );
+
+    // SIONlib parameters
+    int n_files = 1;
+    sion_int32 fs_block_size = -1;
+
+    sion_int64 sion_buffer_size = P_.sion_buffer_size_;
+
+    int sid = sion_paropen_ompi( filename,
+      "bw",
+      &n_files,
+      MPI_COMM_WORLD,
+      &local_comm, // FIXME: does it do anything when not on JUQUEEN?
+      &sion_buffer_size,
+      &fs_block_size,
+      &rank,
+      NULL,
+      NULL ); // FIXME: nullptr allowed here? Readback filename?
+
+    VirtualProcessEntry& vpe = files_[ vp ];
+	vpe.sid = sid;
+
+    // iterate over registed devices and set sid
+    for ( std::map< int, DeviceEntry >::iterator jj = vpe.devices.begin(); jj != vpe.devices.end(); ++jj )
+    {
+      int gid = jj->first;
+      DeviceEntry& entry = jj->second;
+      entry.sid = sid;
+    }
+	
+	// TODO: write header
+  }
+}
+
+void
+nest::SIONLogger::finalize()
+{
+#pragma omp parallel
+  {
+    Network& network = *( Node::network() );
+    thread t = network.get_thread_id();
+    int vp = network.thread_to_vp( t );
+
+    VirtualProcessEntry& vpe = files_[ vp ];
+	sion_parclose_ompi( vpe.sid );
+
+    // TODO: write header
+  }
+}
+
+void
+nest::SIONLogger::write( const RecordingDevice& device, const Event& event )
+{
+  const Node& node = device.get_node();
+  int vp = node.get_vp();
+  int gid = node.get_gid();
+
+  const index sender = event.get_sender_gid();
+  const Time stamp = event.get_stamp();
+  const double offset = event.get_offset();
+
+  VirtualProcessEntry& entry = files_[vp];
+  int& sid = entry.sid;
+
+  double time = stamp.get_ms() - offset;
+  int n_values = 0;
+
+  sion_fwrite( &gid, sizeof( int ), 1, sid );
+  sion_fwrite( &sender, sizeof( int ), 1, sid );
+  sion_fwrite( &time, sizeof( double ), 1, sid );
+  sion_fwrite( &n_values, sizeof( int ), 1, sid );
+}
+
+void
+nest::SIONLogger::write( const RecordingDevice& device,
+  const Event& event,
+  const std::vector< double_t >& values )
+{
+  const Node& node = device.get_node();
+  int vp = node.get_vp();
+  int gid = node.get_gid();
+
+  const index sender = event.get_sender_gid();
+  const Time stamp = event.get_stamp();
+  const double offset = event.get_offset();
+  
+  VirtualProcessEntry& entry = files_[vp];
+  int& sid = entry.sid;
+
+  double time = stamp.get_ms() - offset;
+  int n_values = values.size();
+
+  sion_fwrite( &gid, sizeof( int ), 1, sid );
+  sion_fwrite( &sender, sizeof( int ), 1, sid );
+  sion_fwrite( &time, sizeof( double ), 1, sid );
+  sion_fwrite( &n_values, sizeof( int ), 1, sid );
+  
+  for ( std::vector< double_t >::const_iterator val = values.begin(); val != values.end(); ++val )
+  {
+	  double value = *val;
+	  sion_fwrite( &value, sizeof( double ), 1, sid );
+  }
+}
+
+const std::string
+nest::SIONLogger::build_filename_( const RecordingDevice& device ) const
+{
+  const Node& node = device.get_node();
+
+  // number of digits in number of virtual processes
+  const int vpdigits = static_cast< int >(
+    std::floor( std::log10( static_cast< float >( Communicator::get_num_virtual_processes() ) ) )
+    + 1 );
+  const int gidigits = static_cast< int >(
+    std::floor( std::log10( static_cast< float >( Node::network()->size() ) ) ) + 1 );
+
+  std::ostringstream basename;
+  const std::string& path = Node::network()->get_data_path();
+  if ( !path.empty() )
+    basename << path << '/';
+  basename << Node::network()->get_data_prefix();
+
+  const std::string& label = device.get_label();
+  if ( !label.empty() )
+    basename << label;
+  else
+    basename << node.get_name();
+
+  int vp = node.get_vp();
+  int gid = node.get_gid();
+
+  basename << "-" << std::setfill( '0' ) << std::setw( gidigits ) << gid << "-"
+           << std::setfill( '0' ) << std::setw( vpdigits ) << vp;
+
+  return basename.str() + '.' + P_.file_ext_;
+}
+
+/* ----------------------------------------------------------------
+ * Parameter extraction and manipulation functions
+ * ---------------------------------------------------------------- */
+
+nest::SIONLogger::Parameters_::Parameters_()
+  : precision_( 3 )
+  , file_ext_( "dat" )
+  , fbuffer_size_( BUFSIZ ) // default buffer size as defined in <cstdio>
+  , close_after_simulate_( false )
+  , flush_after_simulate_( true )
+  , sion_buffer_size_( 2400 )
+{
+}
+
+void
+nest::SIONLogger::Parameters_::get( const SIONLogger& al, DictionaryDatum& d ) const
+{
+  ( *d )[ names::precision ] = precision_;
+  ( *d )[ names::file_extension ] = file_ext_;
+  ( *d )[ names::fbuffer_size ] = fbuffer_size_;
+  ( *d )[ names::close_after_simulate ] = close_after_simulate_;
+  ( *d )[ names::flush_after_simulate ] = flush_after_simulate_;
+}
+
+void
+nest::SIONLogger::Parameters_::set( const SIONLogger& al, const DictionaryDatum& d )
+{
+  updateValue< long >( d, names::precision, precision_ );
+  updateValue< std::string >( d, names::file_extension, file_ext_ );
+  updateValue< bool >( d, names::close_after_simulate, close_after_simulate_ );
+  updateValue< bool >( d, names::flush_after_simulate, flush_after_simulate_ );
+
+  long fbuffer_size;
+  if ( updateValue< long >( d, names::fbuffer_size, fbuffer_size ) )
+  {
+    if ( fbuffer_size < 0 )
+      throw BadProperty( "/fbuffer_size must be <= 0" );
+    else
+    {
+      fbuffer_size_old_ = fbuffer_size_;
+      fbuffer_size_ = fbuffer_size;
+    }
+  }
+}
---
+++ nestkernel/sion_logger.h
@@ -0,0 +1,203 @@
+#ifndef SION_LOGGER_H
+#define SION_LOGGER_H
+
+#include <fstream>
+#include <algorithm>
+
+#include "logger.h"
+
+#include "mpi.h"
+#include "sion.h"
+
+namespace nest
+{
+
+class SIONLogger : public Logger
+{
+public:
+  SIONLogger()
+    : files_()
+    , initialized_( false )
+  {
+  }
+
+  ~SIONLogger() throw()
+  {
+  }
+
+  void enroll( const int virtual_process, RecordingDevice& device );
+  void initialize();
+  void finalize();
+  void write( const RecordingDevice& device, const Event& event );
+  void write( const RecordingDevice& device, const Event& event, const std::vector< double_t >& );
+
+private:
+  const std::string build_filename_( const RecordingDevice& device ) const;
+
+  class SIONBuffer
+  {
+  private:
+    char* buffer;
+    int ptr;
+    int max_size;
+
+  public:
+    SIONBuffer()
+      : buffer( NULL )
+      , ptr( 0 )
+      , max_size( 0 )
+    {
+    }
+
+    SIONBuffer( int size )
+      : buffer( NULL )
+      , ptr( 0 )
+    {
+      if ( size > 0 )
+      {
+        buffer = new char[ size ];
+        max_size = size;
+      }
+      max_size = 0;
+    }
+
+    ~SIONBuffer()
+    {
+      if ( buffer != NULL )
+        delete[] buffer;
+    }
+
+    void
+    reserve( int size )
+    {
+      char* new_buffer = new char[ size ];
+
+      if ( buffer != NULL )
+      {
+        ptr = std::min( ptr, size );
+        memcpy( new_buffer, buffer, ptr );
+        delete[] buffer;
+      }
+          buffer = new_buffer;
+      max_size = size;
+    }
+
+    void
+    write( const char* v, long unsigned int n )
+    {
+      if ( ptr + n <= max_size )
+      {
+        memcpy( buffer + ptr, v, n );
+        ptr += n;
+      }
+      else
+      {
+        std::cerr << "SIONBuffer: buffer overflow: ptr=" << ptr << " n=" << n
+                  << " max_size=" << max_size << std::endl;
+      }
+    }
+
+    int
+    get_size()
+    {
+      return ptr;
+    }
+
+    int
+    get_capacity()
+    {
+      return max_size;
+    }
+
+    void
+    ensure_free_space( int size )
+    {
+      if ( !has_free_space( size ) )
+      {
+		// TODO: What? +10*size?
+        int new_max_size = max_size + size * 10;
+        reserve( max_size + size * 10 );
+      }
+    }
+
+    bool
+    has_free_space( int size )
+    {
+      return ( ptr + size < max_size );
+    }
+
+    void
+    clear()
+    {
+      ptr = 0;
+    }
+
+    char*
+    read()
+    {
+      return buffer;
+    }
+  };
+
+  struct DeviceHeader
+  {
+    int device_id;
+    int n_values;
+    std::vector< std::string > value_names;
+  };
+
+  struct FileHeader
+  {
+    int n_devices;
+    std::vector< DeviceHeader > device_headers;
+  };
+
+  struct DeviceEntry
+  {
+    DeviceEntry( RecordingDevice& device )
+      : device( device )
+    {
+    }
+    RecordingDevice& device;
+    SIONBuffer buffer;
+
+    int sid;
+  };
+  
+  struct VirtualProcessEntry
+  {
+    int sid;
+	std::map< int, DeviceEntry > devices;
+  };
+
+  struct Parameters_
+  {
+    long precision_;
+
+	long sion_buffer_size_;
+
+    std::string file_ext_;      //!< the file name extension to use, without .
+    long fbuffer_size_;         //!< the buffer size to use when writing to file
+    long fbuffer_size_old_;     //!< the buffer size to use when writing to file (old)
+    bool close_after_simulate_; //!< if true, finalize() shall close the stream
+    bool flush_after_simulate_; //!< if true, finalize() shall flush the stream
+
+    Parameters_();
+
+    void get( const SIONLogger&, DictionaryDatum& ) const;
+    void set( const SIONLogger&, const DictionaryDatum& );
+  };
+
+  Parameters_ P_;
+
+  // one map for each virtual process,
+  // in turn containing one ostream for everydevice
+  typedef std::map< int, VirtualProcessEntry > file_map;
+  file_map files_;
+
+  bool initialized_;
+};
+
+} // namespace
+
+#endif // SION_LOGGER_H

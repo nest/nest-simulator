478ba9ce87bcc7ebd49d9ce9f368dcaed05b34fb
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2015-09-27 00:50:06 +0200

    Adapted buildnest to use Git instead of SVN

--- releasetools/buildnest.sh
+++ releasetools/buildnest.sh
@@ -1,23 +1,9 @@
 #!/bin/csh -e
 
-#  Shell script to roll a NEST tarball from the repository
-#
-#  The script will:
-#
-#  - make a clean checkout to __NEST_SOURCE in the local directory
-#  - set SLI_PATCHLEVEL to current SVN revision in configure.ac.in
-#  - ask if to tag the release in the repository
-#  - run bootstrap
-#  - configure in __NEST_BUILD
-#  - roll a tarball
-#  - move the tarball to the local directory
-#  - remove __NEST_SOURCE and __NEST_BUILD
-#
-#  The script requires SVN access and releasetools in the sourcedir.
-#
+#  Shell script to roll a NEST tarball from the git repository
 
 # first, we check for autoconf >= 2.60, as tarballs built with older versions
-# may cause problems on many machines. See #122 for details.
+# may cause problems on many machines. See trac #122 for details.
 set ac_ver = `autoconf --version | head -n 1 | cut -d' ' -f 4`
 set ac_ver_major = `echo $ac_ver | cut -d\. -f 1`
 set ac_ver_minor = `echo $ac_ver | cut -d\. -f 2`
@@ -30,142 +16,85 @@ if ( ! ( ( $ac_ver_major == 2 ) & ( $ac_ver_minor >= 60 ) ) ) then
 endif
 
 set workdir = `pwd`
-
-# commandline parsing
-if ( $#argv != 0 ) then
-  if ( $#argv == 2 & $1 == "--branch" ) then
-    echo "Using branch '"$2"'"
-    set svndir="branches/"$2
-    set branch="_$2"
-  else
-    echo "buildnest.sh options:"
-    echo "  --branch <branch>    Make a tarball of <branch> instead of trunk"
-    echo "  --help               Print this help information and exit"
-    echo
-    exit 0
-  endif
-else
-  set svndir="branches/nest-2.6/2.6.1"
-  set branch=""
 endif
 
-# need to find releasetools path
-set scriptpath = `pwd`/$0
-set scriptdir = `echo $scriptpath | gawk '{ split($1, a, /\/buildnest.sh/); print a[1]; }'`
-
-# absolute pathnames give trouble with make
-set nest_srcdir = __NEST_SOURCE$branch
-set nest_blddir = __NEST_BUILD$branch
-
-# SVN ID to use
-set svn_id = https://svn.nest-initiative.org/nest/$svndir
-set svn_tags = https://svn.nest-initiative.org/nest/tags
+set git_url = "https://github.com/nest/nest-simulator.git"
+set nest_srcdir = __NEST_SOURCE
+set nest_blddir = __NEST_BUILD
 
 # set umask to 022 (gives 755/644 permission in tarball)
 umask 022
 
 # remove any existing checkout
-rm -rf $nest_srcdir
+#rm -rf $nest_srcdir
 rm -rf $nest_blddir
 
-# print a nice welcome screen
 echo ""
 echo "NEST Tarball Creation Script"
 echo "----------------------------"
 echo ""
-echo "You are about to create a tarball of the current SVN revision of trunk."
-echo "In order to have local changes included, you have to commit them first."
+echo "You are about to create a tarball of the master branch of NEST."
+echo "In order to include local changes, you have to commit them first."
 echo ""
-echo -n "Do you want to continue [y/N]? "
+echo -n "Do you want to continue [Y/n]? "
 set ans=`head -1`
-echo ""
 
-set label="-pre"
-
-if ( $ans =~ [yY]* ) then
-  echo -n "Do you want to tag this tarball in the SVN repository [y/N]? "
-  set ans=`head -1`
-  if ( $ans =~ [yY]* ) then
-    set label=""
-  endif
-else
+if ( $ans =~ [nN]* ) then
   exit 0
 endif
 
-set label="$branch$label"
+echo -n "Git tag for the tarball [empty to skip tagging]: "
+set tag=`head -1`
 
-# check out
+echo -n "Add git revision hash to version number [y/N]? "
+set add_rev=`head -1`
+
 echo ""
-echo "Creating fresh checkout ..."
-svn --quiet checkout $svn_id $nest_srcdir
-
-# add svn revision number to tarball name
-set svnver=`svnversion $nest_srcdir`
-set patchlevel = "1"
-
-# move to nest_srcdir
+echo "Creating clone ..."
+#git clone $git_url $nest_srcdir
 cd $nest_srcdir
 
-# edit configure.ac.in
-echo ""
-echo -n "Setting patch level in configure.ac"
-sed -i -e "s/SLI_PATCHLEVEL=svn/SLI_PATCHLEVEL=$patchlevel/" configure.ac.in
+set label = ""
+if ( $add_rev =~ [yY]* ) then
+  set label = "-"`git rev-parse --short HEAD`
+endif
 
-set nest_minor   = "6"
-set nest_major   = "2"
-set nest_version = "$nest_major.$nest_minor.$patchlevel"
-set nest_nprog   = "nest-$nest_version"
+set nest_major = `grep SLI_MAJOR= configure.ac | cut -d= -f2`
+set nest_minor = `grep SLI_MINOR= configure.ac | cut -d= -f2`
+set nest_patch = `grep SLI_PATCHLEVEL= configure.ac | cut -d= -f2`$label
+set nest_version = "$nest_major.$nest_minor.$nest_patch"
 
-sed -i -e "s/AC_INIT(\[nest\], \[$nest_major\.$nest_minor\.svn\]/AC_INIT([nest], [$nest_version]/" configure.ac.in
+sed -i'' -e "s/SLI_PATCHLEVEL=.*/SLI_PATCHLEVEL=$nest_patch/" configure.ac
+sed -i'' -e "s/AC_INIT.*/AC_INIT([nest], [$nest_version], [info@nest-initiative.org])/" configure.ac
 
-echo " to $nest_nprog"
+if ( "$tag" != "" ) then
+  git tag -a $tag -m "Release of $tag"
+  git push --tags
+endif
 
-# tag new version if real run
-#if ( "$label" == "" ) then
-#  echo ""
-#  echo "Tagging new version as $nest_nprog" 
-#  svn copy $svn_id $svn_tags/$nest_nprog -r $svnver -m "Tagging release as $nest_nprog"
-#endif
-
-# remove all candidate modules from configure.ac---this change MUST NOT be checked in
-gawk -f $scriptdir/clear_extra_modules.awk configure.ac.in > configure.ac.tmp \
-  && mv configure.ac.tmp configure.ac.in
-
-# bootstrap after modification of configure.ac.in
-echo ""
+echo "Bootstrapping clone ..."
 ./bootstrap.sh
 
-# now configure and roll in separate directory
-# build dir must be parallel to source dir!
 cd $workdir
 mkdir $nest_blddir
 cd $nest_blddir
 
-# must have relative path here to avoid spurious 
-# directories to be created on rolling tarball
-echo ""
 echo "Configuring build directory ..." 
 ../$nest_srcdir/configure --prefix=$HOME/opt/nest > /dev/null
 
-# pre-compile Cython code
+echo "Precompile Cython files ..."
 make -C pynest pynestkernel.cpp
 
-echo ""
 echo "Rolling tarball ..."
 make -s dist
 
-# leave build dir
 cd $workdir
+mv $nest_blddir/*.tar.gz $workdir
 
-# move/copy files to work directory
-mv $nest_blddir/$nest_nprog.tar.gz $workdir
-
+echo "Cleaning up ..."
+#rm -rf $nest_srcdir
+#rm -rf $nest_blddir
+	
 echo ""
 echo "Done."
 echo ""
-echo "NEST Tarball   : ${workdir}/${nest_nprog}.tar.gz"
-echo ""
-
-# clean up
-rm -rf $nest_srcdir
-rm -rf $nest_blddir

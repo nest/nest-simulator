3713f1724a56f7daf5e9d708a3d89e42a8a6748b
Alex Seeholzer <seeholzer@gmail.com>
2015-11-05 15:00:44 +0100

    correct spike_times generated in the test: parrot neurons now spike at exactly the intended spike times by accounting for delay between spike_generators and parrot_neurons.

--- pynest/nest/tests/test_stdp_triplet_synapse.py
+++ pynest/nest/tests/test_stdp_triplet_synapse.py
@@ -64,8 +64,9 @@ class STDPTripletConnectionTestCase(unittest.TestCase):
 
     def generateSpikes(self, neuron, times):
         """Trigger spike to given neuron at specified times."""
-        gen = nest.Create("spike_generator", 1, {"spike_times": times})
-        nest.Connect(gen, neuron)
+        delay = 1.
+        gen = nest.Create("spike_generator", 1, {"spike_times": [t-delay for t in times]})
+        nest.Connect(gen, neuron, syn_spec={"delay": delay})
 
     def status(self, which):
         """Get synapse parameter status."""

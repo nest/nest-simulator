ee17315828c6c7eb1d951875c4f9dd246377f211
Yansong Chua <y.chua@fz-juelich.de>
2015-12-07 17:07:36 +0100

    after_test

--- pynest/nest/tests/test_kinetics_multi_cmpt_neu.py
+++ pynest/nest/tests/test_kinetics_multi_cmpt_neu.py
@@ -31,6 +31,9 @@ from scipy import *
 from scipy.integrate import ode
 import unittest
 
+HAVE_GSL = nest.sli_func("statusdict/have_gsl ::")
+
+@unittest.skipIf(not HAVE_GSL, 'GSL is not available')
 @nest.check_stack
 
 class mc_neuron_ode():

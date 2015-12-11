4176f78e88058671f2dd2834001bf54b9da06150
Tammo Ippen <tammo.ippen@posteo.de>
2015-12-11 16:33:34 +0100

    Cleanup & Music & Make Python work again
    * have SET in lower case
    * group targets in base CMakeLists.txt
    * have WITH_DEBUG option and default values for WITH_OPTIMIZE
    * have the None build type as default
    * include and link libneurosim from base CMakeLists.txt
    * Find Music and build with Music
    * rm selfinclude from config.h and add copyright head
    * do not link against Python-lib, it breaks with OS X at least
    
    All tests run through.

---
+++ cmake/FindMusic.cmake
@@ -0,0 +1,51 @@
+# - Find MUSIC header and library
+#
+
+# This module defines
+#  MUSIC_FOUND, if false, do not try to use MUSIC.
+#  MUSIC_INCLUDE_DIR, where to find music.hh.
+#  MUSIC_LIBRARY, the libraries to link against to use MUSIC.
+#  MUSIC_EXECUTABLE, the music executable.
+#  MUSIC_VERSION, the library version
+#
+# As a hint allows MUSIC_ROOT_DIR.
+
+find_path(MUSIC_INCLUDE_DIR
+  NAMES music.hh
+  HINTS ${MUSIC_ROOT_DIR}/include
+)
+find_library(MUSIC_LIBRARY
+  NAMES music
+  HINTS ${MUSIC_ROOT_DIR}/lib
+)
+find_program(MUSIC_EXECUTABLE
+  NAMES music
+  HINTS ${MUSIC_ROOT_DIR}/bin
+)
+
+
+if( NOT MUSIC_EXECUTABLE STREQUAL "MUSIC_EXECUTABLE-NOTFOUND" )
+  execute_process(
+    COMMAND ${MUSIC_EXECUTABLE} --version
+    RESULT_VARIABLE RESULT
+    OUTPUT_VARIABLE MUSIC_VAR_OUTPUT
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if(RESULT EQUAL 0)
+    string( REGEX REPLACE "^MUSIC ([0-9].[0-9].[0-9]).*" "\\1" MUSIC_VERSION ${MUSIC_VAR_OUTPUT} )
+  endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Music
+  FOUND_VAR
+    MUSIC_FOUND
+  REQUIRED_VARS
+    MUSIC_LIBRARY
+    MUSIC_INCLUDE_DIR
+    MUSIC_EXECUTABLE
+  VERSION_VAR
+    MUSIC_VERSION
+  )
+
+mark_as_advanced(MUSIC_ROOT_DIR MUSIC_INCLUDE_DIR MUSIC_LIBRARY MUSIC_EXECUTABLE)

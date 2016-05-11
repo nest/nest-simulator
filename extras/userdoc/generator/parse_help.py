0b7e75796f705f1664f91b0fbba8e33b6445dba7
steffen <s.graber@fz-juelich.de>
2016-05-11 11:52:49 +0200

    initial commit

---
+++ extras/userdoc/generator/parse_help.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+#
+# parse_help.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Generate NEST help files
+========================
+
+Parse all NEST files for documentation and build the help.
+"""
+
+import os
+import re
+
+from modules.writers import coll_data, check_ifdef
+
+path = '../../../'
+path = os.path.abspath(path)
+allfiles = [os.path.join(dirpath, f) for dirpath, dirnames, files in
+            os.walk(path) for f in files if
+            f.endswith((".sli", ".cpp", ".cc", ".h", ".py"))]
+num = 0
+full_list = []
+sli_list = []
+
+
+def cut_it(trenner, text):
+    """
+    Cut it.
+
+    Cut text by trenner.
+    """
+    import re
+    return re.split(trenner, text)
+
+keywords = ["Name:", "Synopsis:", "Parameters:", "Description:",
+            "Options:", "Examples:", "Variants:", "Bugs:",
+            "Diagnostics:", "Author:", "FirstVersion:", "Remarks:",
+            "Availability:", "References:", "SeeAlso:", "Source:",
+            "Sends:", "Receives:", "Transmits:", "Requires:"]
+
+# file_docname_list = []
+# # List of all.sli files
+# for file in allfiles:
+#     if file.endswith('.sli'):
+#         docstring = r'\/\*[ *\n]?BeginDocumentation\n(.*?)\n*?\*\/'
+#         f = open(('%s' % (file,)), 'r')
+#         filetext = f.read()
+#         f.close()
+#
+#         # !!!!!!!NUR INNERHALB des DOC Blocks!!!!!
+#
+#         # Multiline matiching
+#         items = re.findall(docstring, filetext, re.DOTALL)
+#         namestring = r'([ *]?Name[ *]?\:[ *]?)(.*?)([ *]?\-)'
+#         docnames = re.findall(namestring, filetext, re.DOTALL)
+#         print '#################################################\n' + file
+#         for docname in docnames:
+#             print docname[1]
+#                 # docname = docname.lstrip(r'Name[ *]?\:')
+#                 # file_docname_list.append(docname[1])
+#             # print file_docname_list
+#             base = os.path.basename(file)
+#             sli_list.append(os.path.splitext(base)[0])
+
+for file in allfiles:
+    # .py is for future use
+    if not file.endswith('.py'):
+        docstring = r'\/\*[ *\n]?BeginDocumentation\n(.*?)\n*?\*\/'
+        f = open(('%s' % (file,)), 'r')
+        filetext = f.read()
+        f.close()
+        # Multiline matiching
+        items = re.findall(docstring, filetext, re.DOTALL)
+        for item in items:
+            alllines = []
+            s = " ###br###\n"
+            for line in item.splitlines():
+                line = re.sub(r"(\s\s)", '$$', line)
+                alllines.append(line)
+            item = s.join(alllines)
+            require = check_ifdef(item, filetext, docstring)
+            if require:
+                item = '\n\nRequire: ' + require + item
+            num = num + 1
+            documentation = {}
+            for token in item.split():
+                if token in keywords:
+                    keyword_curr = token
+                    documentation[keyword_curr] = ""
+                else:
+                    if keyword_curr in documentation:
+                        documentation[keyword_curr] += " " + token
+            all_data = coll_data(keywords, documentation, num, file, sli_list)
+
+            # name_list.append(all_data)

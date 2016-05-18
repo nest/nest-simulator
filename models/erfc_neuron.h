eb91509f336138e1a32beea730ac87002230e482
kuehn <t.kuehn@fz-juelich.de>
2016-05-18 18:16:37 +0200

    Add documentation for erfc_neuron.h

--- models/erfc_neuron.h
+++ models/erfc_neuron.h
@@ -30,32 +30,29 @@
 namespace nest
 {
 /* BeginDocumentation
-   Name: ginzburg_neuron - Binary stochastic neuron with sigmoidal activation function.
+   Name: erfc_neuron - Binary stochastic neuron with complementary error 
+   function as activation function.
 
    Description:
-   The ginzburg_neuron is an implementation of a binary neuron that
+   The erfc_neuron is an implementation of a binary neuron that
    is irregularly updated as Poisson time points. At each update
    point the total synaptic input h into the neuron is summed up,
    passed through a gain function g whose output is interpreted as
    the probability of the neuron to be in the active (1) state.
 
-   The gain function g used here is g(h) = c1*h + c2 * 0.5*(1 +
-   tanh(c3*(h-theta))) (output clipped to [0,1]). This allows to
-   obtain affin-linear (c1!=0, c2!=0, c3=0) or sigmoidal (c1=0,
-   c2=1, c3!=0) shaped gain functions.  The latter choice
-   corresponds to the definition in [1], giving the name to this
-   neuron model.
-   The choice c1=0, c2=1, c3=beta/2 corresponds to the Glauber
-   dynamics [2], g(h) = 1 / (1 + exp(-beta (h-theta))).
+   The gain function g used here is 
+   g(h) = 0.5 * erfc (( h - theta_ ) / ( sqrt( 2. ) * sigma)).
+   This corresponds to a McCulloch-Pitts neuron receiving additional
+   Gaussian noise with mean 0 and standard deviation sigma
    The time constant tau_m is defined as the mean
    inter-update-interval that is drawn from an exponential
-   distribution with this parameter. Using this neuron to reprodce
-   simulations with asynchronous update [1], the time constant needs
-   to be chosen as tau_m = dt*N, where dt is the simulation time
+   distribution with this parameter. Using this neuron to reproduce
+   simulations with asynchronous update (similar to [1,2]), the time 
+   constant needs to be chosen as tau_m = dt*N, where dt is the simulation time
    step and N the number of neurons in the original simulation with
    asynchronous update. This ensures that a neuron is updated on
-   average every tau_m ms. Since in the original paper [1] neurons
-   are coupled with zero delay, this implementation follows this
+   average every tau_m ms. Since in the original papers [1,2] neurons
+   are coupled with zero delay, this implementation follows that
    definition. It uses the update scheme described in [3] to
    maintain causality: The incoming events in time step t_i are
    taken into account at the beginning of the time step to calculate
@@ -80,16 +77,15 @@ namespace nest
 
 
    Parameters:
-   tau_m      double - Membrane time constant (mean inter-update-interval) in ms.
-   theta      double - threshold for sigmoidal activation function mV
-   c1         double - linear gain factor (probability/mV)
-   c2         double - prefactor of sigmoidal gain (probability)
-   c3         double - slope factor of sigmoidal gain (1/mV)
-
+   tau_m      double - Membrane time constant (mean inter-update-interval) (ms)
+   theta      double - threshold for sigmoidal activation function (mV)
+   sigma      double - 1/sqrt(2pi) x inverse of maximal slope (mV)
+   
    References:
    [1] Iris Ginzburg, Haim Sompolinsky. Theory of correlations in stochastic neural networks (1994).
    PRE 50(4) p. 3171
-   [2] Hertz Krogh, Palmer. Introduction to the theory of neural computation. Westview (1991).
+   [2] W. McCulloch und W. Pitts (1943). A logical calculus of the ideas immanent in nervous
+   activity. Bulletin of Mathematical Biophysics, 5:115-133.
    [3] Abigail Morrison, Markus Diesmann. Maintaining Causality in Discrete Time Neuronal
    Simulations.
    In: Lectures in Supercomputational Neuroscience, p. 267. Peter beim Graben, Changsong Zhou, Marco
@@ -97,9 +93,9 @@ namespace nest
 
    Sends: SpikeEvent
    Receives: SpikeEvent, PotentialRequest
-   FirstVersion: February 2016
+   FirstVersion: May 2016
    Authors: Jakob Jordan, Tobias Kuehn
-   SeeAlso: pp_psc_delta
+   SeeAlso: mcculloch_pitts_neuron, ginzburg_neuron
 */
 
 class gainfunction_erfc
@@ -108,7 +104,7 @@ private:
   /** threshold of activation function */
   double_t theta_;
 
-  /** inverse steepness of gain function */
+  /** 1/sqrt(2pi) x inverse of the maximal slope of gain function */
   double_t sigma_;
 
 public:
@@ -116,8 +112,7 @@ public:
 
   gainfunction_erfc()
     : theta_( 0.0 )
-    , // mV
-    sigma_( 1.0 )
+    , sigma_( 1.0 )
   {
   }
 
@@ -129,7 +124,7 @@ public:
 
 inline bool gainfunction_erfc::operator()( librandom::RngPtr rng, double_t h )
 {
-  return rng->drand() < 0.5 * erfc ( ( h - theta_ ) / ( sqrt( 2. ) * sigma_ ) );
+  return rng->drand() < 0.5 * erfc ( - ( h - theta_ ) / ( sqrt( 2. ) * sigma_ ) );
 }
 
 typedef binary_neuron< nest::gainfunction_erfc > erfc_neuron;

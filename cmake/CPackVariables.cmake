0ce01ad20d1386614192436546502045c285ad20
Tammo Ippen <tammo.ippen@posteo.de>
2016-05-18 17:01:24 +0200

    Refactor cmake includes into functions

--- cmake/CPackVariables.cmake
+++ cmake/CPackVariables.cmake
@@ -17,50 +17,53 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-# CPack stuff
-set( CPACK_GENERATOR TGZ )
-set( CPACK_SOURCE_GENERATOR TGZ )
+function( NEST_SET_CPACK_VARIABLES )
+  # CPack stuff
+  set( CPACK_GENERATOR TGZ )
+  set( CPACK_SOURCE_GENERATOR TGZ )
 
-set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "nest::The Neural Simulation Tool" )
-set( CPACK_PACKAGE_VENDOR "NEST Initiative (http://www.nest-initiative.org/)" )
-set( CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/extras/logos/nest.png" )
+  set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "nest::The Neural Simulation Tool" )
+  set( CPACK_PACKAGE_VENDOR "NEST Initiative (http://www.nest-initiative.org/)" )
+  set( CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/extras/logos/nest.png" )
 
-set( CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE" )
-set( CPACK_RESOURCE_FILE_README "${PROJECT_SOURCE_DIR}/README.md" )
+  set( CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE" )
+  set( CPACK_RESOURCE_FILE_README "${PROJECT_SOURCE_DIR}/README.md" )
 
-set( CPACK_PACKAGE_VERSION_MAJOR ${NEST_VERSION_MAJOR} )
-set( CPACK_PACKAGE_VERSION_MINOR ${NEST_VERSION_MINOR} )
-set( CPACK_PACKAGE_VERSION_PATCH ${NEST_VERSION_PATCHLEVEL} )
-set( CPACK_PACKAGE_VERSION ${NEST_VERSION_VERSION} )
-set( CPACK_PACKAGE_EXECUTABLES "nest" )
+  set( CPACK_PACKAGE_VERSION_MAJOR ${NEST_VERSION_MAJOR} )
+  set( CPACK_PACKAGE_VERSION_MINOR ${NEST_VERSION_MINOR} )
+  set( CPACK_PACKAGE_VERSION_PATCH ${NEST_VERSION_PATCHLEVEL} )
+  set( CPACK_PACKAGE_VERSION ${NEST_VERSION_VERSION} )
+  set( CPACK_PACKAGE_EXECUTABLES "nest" )
 
-set( CPACK_SOURCE_IGNORE_FILES
-    "\\\\.gitignore"
-    "\\\\.git/"
-    "\\\\.travis\\\\.yml"
+  set( CPACK_SOURCE_IGNORE_FILES
+      "\\\\.gitignore"
+      "\\\\.git/"
+      "\\\\.travis\\\\.yml"
 
-    # if we have in source builds
-    "/build/"
-    "/_CPack_Packages/"
-    "CMakeFiles/"
-    "cmake_install\\\\.cmake"
-    "Makefile.*"
-    "CMakeCache\\\\.txt"
-    "CPackConfig\\\\.cmake"
-    "CPackSourceConfig\\\\.cmake"
-    )
-set( CPACK_SOURCE_PACKAGE_FILE_NAME ${NEST_VERSION_PRGNAME} )
+      # if we have in source builds
+      "/build/"
+      "/_CPack_Packages/"
+      "CMakeFiles/"
+      "cmake_install\\\\.cmake"
+      "Makefile.*"
+      "CMakeCache\\\\.txt"
+      "CPackConfig\\\\.cmake"
+      "CPackSourceConfig\\\\.cmake"
+      )
+  set( CPACK_SOURCE_PACKAGE_FILE_NAME ${NEST_VERSION_PRGNAME} )
 
-set( CPACK_PACKAGE_INSTALL_DIRECTORY "NEST ${NEST_VERSION_VERSION}" )
-include( CPack )
+  set( CPACK_PACKAGE_INSTALL_DIRECTORY "NEST ${NEST_VERSION_VERSION}" )
+  include( CPack )
 
-# add make dist target
-add_custom_target( dist
-    # create current rcsinfo.sli
-    # target dir is PROJECT_SOURCE_DIR as this will be the `install dir` from cmake package_source
-    COMMAND ${PROJECT_SOURCE_DIR}/extras/create_rcsinfo.sh "${PROJECT_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}"
-    COMMAND ${CMAKE_MAKE_PROGRAM} package_source
-    # not sure about this... seems, that it will be removed before dist...
-    # DEPENDS doc
-    COMMENT "Creating a source distribution from NEST..."
-    )
+  # add make dist target
+  add_custom_target( dist
+      # create current rcsinfo.sli
+      # target dir is PROJECT_SOURCE_DIR as this will be the `install dir` from cmake package_source
+      COMMAND ${PROJECT_SOURCE_DIR}/extras/create_rcsinfo.sh "${PROJECT_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}"
+      COMMAND ${CMAKE_MAKE_PROGRAM} package_source
+      # not sure about this... seems, that it will be removed before dist...
+      # DEPENDS doc
+      COMMENT "Creating a source distribution from NEST..."
+      )
+
+endfunction()

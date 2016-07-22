86b3c30fbecd3f3d34e6496c53089cd83c956614
Alexander  Peyser <slns007@juqueen1.zam.kfa-juelich.de>
2016-07-22 14:34:23 +0200

    Allow overriding path to compiler for BGQ

--- cmake/Platform/BlueGeneQ_GCC.cmake
+++ cmake/Platform/BlueGeneQ_GCC.cmake
@@ -22,8 +22,8 @@ include( Platform/BlueGeneQ_Base )
 set( ENV{PATH} "/bgsys/drivers/ppcfloor/gnu-linux/bin:$ENV{PATH}" )
 
 # set the compiler
-set( CMAKE_C_COMPILER powerpc64-bgq-linux-gcc )
-set( CMAKE_CXX_COMPILER powerpc64-bgq-linux-g++ )
+set( CMAKE_C_COMPILER powerpc64-bgq-linux-gcc CACHE FILEPATH "cmake is so awesomely misdesigned")
+set( CMAKE_CXX_COMPILER powerpc64-bgq-linux-g++ CACHE FILEPATH "cmake is so awesomely misdesigned")
 
 if ( static-libraries )
   __bluegeneq_setup_static( GNU CXX )

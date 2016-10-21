fe32e74ffbd6aa0aff14954e42f3b8d698cc57ca
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2016-10-21 20:07:15 +0200

    Removed iaf_neuron

--- models/modelsmodule.cpp
+++ models/modelsmodule.cpp
@@ -57,7 +57,6 @@
 #include "iaf_cond_alpha_mc.h"
 #include "iaf_cond_exp.h"
 #include "iaf_cond_exp_sfa_rr.h"
-#include "iaf_neuron.h"
 #include "iaf_psc_alpha.h"
 #include "iaf_psc_alpha_multisynapse.h"
 #include "iaf_psc_delta.h"
@@ -171,7 +170,6 @@ ModelsModule::commandstring( void ) const
 void
 ModelsModule::init( SLIInterpreter* )
 {
-  kernel().model_manager.register_node_model< iaf_neuron >( "iaf_neuron" );
   kernel().model_manager.register_node_model< iaf_chs_2007 >( "iaf_chs_2007" );
   kernel().model_manager.register_node_model< iaf_psc_alpha >(
     "iaf_psc_alpha" );

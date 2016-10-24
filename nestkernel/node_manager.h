nest/nest-simulator

commit 438d81d728bfbd8b28d3762d7893bbc1af98285f

Better encapsulation of SparseNodeArray.

nestkernel/node_manager.h
@@ -48,8 +48,6 @@ class Model;
 
 class NodeManager : public ManagerInterface
 {
-  friend const SparseNodeArray& ConnBuilder::get_local_nodes();
-
 public:
   NodeManager();
   ~NodeManager();
@@ -86,7 +84,6 @@ public:
    * @throws nest::UnknownModelID
    */
   index add_node( index m, long n = 1 );
-
 
   /**
    * Restore nodes from an array of status dictionaries.
@@ -212,6 +209,21 @@ public:
    */
   bool any_node_uses_wfr() const;
 
+  /**
+   * Iterator pointing to beginning of process-local nodes.
+   */
+  SparseNodeArray::const_iterator local_nodes_begin() const;
+
+  /**
+   * Iterator pointing to end of process-local nodes.
+   */
+  SparseNodeArray::const_iterator local_nodes_end() const;
+
+  /**
+   * Number of process-local nodes.
+   */
+  size_t local_nodes_size() const;
+
 private:
   /**
    * Initialize the network data structures.
@@ -238,8 +250,6 @@ private:
    * @see prepare_nodes_()
    */
   void prepare_node_( Node* );
-
-  const SparseNodeArray& get_local_nodes_() const;
 
   /**
    * Returns the next local gid after curr_gid (in round robin fashion).
@@ -339,10 +349,22 @@ NodeManager::any_node_uses_wfr() const
   return any_node_uses_wfr_;
 }
 
-inline const SparseNodeArray&
-NodeManager::get_local_nodes_() const
+inline SparseNodeArray::const_iterator
+NodeManager::local_nodes_begin() const
 {
-  return local_nodes_;
+  return local_nodes_.begin();
+}
+
+inline SparseNodeArray::const_iterator
+NodeManager::local_nodes_end() const
+{
+  return local_nodes_.begin();
+}
+
+inline size_t
+NodeManager::local_nodes_size() const
+{
+  return local_nodes_.size();
 }
 
 } // namespace

nestkernel/conn_builder.cpp
@@ -568,13 +568,6 @@ nest::ConnBuilder::single_connect_( index sgid,
   }
 }
 
-const nest::SparseNodeArray&
-nest::ConnBuilder::get_local_nodes()
-{
-  return kernel().node_manager.get_local_nodes_();
-}
-
-
 void
 nest::ConnBuilder::set_pre_synaptic_element_name( std::string name )
 {
@@ -614,7 +607,7 @@ nest::OneToOneBuilder::connect_()
       // local nodes is smaller than the targets_-range, we will iterate over
       // the local nodes instead and check, whether the node is in the
       // targets_-range.
-      if ( targets_->size() < get_local_nodes().size()
+      if ( targets_->size() < kernel().node_manager.local_nodes_size()
         || not targets_->is_range()
         || parameters_requiring_skipping_.size() > 0 )
       {
@@ -650,13 +643,13 @@ nest::OneToOneBuilder::connect_()
       }
       else
       {
-        for ( SparseNodeArray::const_iterator it = get_local_nodes().begin();
-              it != get_local_nodes().end();
+        for ( SparseNodeArray::const_iterator it = kernel().node_manager.local_nodes_begin();
+              it != kernel().node_manager.local_nodes_end();
               ++it )
         {
-          Node* const target = ( *it ).node_;
+          Node* const target = ( *it ).get_node();
           const thread target_thread = target->get_thread();
-          const index tgid = ( *it ).gid_;
+          const index tgid = ( *it ).get_gid();
 
           int idx;
           // Is local node in target list?
@@ -882,7 +875,7 @@ nest::AllToAllBuilder::connect_()
       // local nodes is smaller than the targets_-range, we will iterate over
       // the local nodes instead and check, whether the node is in the
       // targets_-range.
-      if ( targets_->size() < get_local_nodes().size()
+      if ( targets_->size() < kernel().node_manager.local_nodes_size()
         || not targets_->is_range()
         || parameters_requiring_skipping_.size() > 0 )
       {
@@ -907,12 +900,12 @@ nest::AllToAllBuilder::connect_()
       }
       else
       {
-        for ( SparseNodeArray::const_iterator it = get_local_nodes().begin();
-              it != get_local_nodes().end();
-              ++it )
+        for ( SparseNodeArray::const_iterator it = kernel().node_manager.local_nodes_begin();
+                it != kernel().node_manager.local_nodes_end();
+                ++it )
         {
-          Node* const target = ( *it ).node_;
-          const index tgid = ( *it ).gid_;
+          Node* const target = ( *it ).get_node();
+          const index tgid = ( *it ).get_gid();
 
           // Is the local node in the targets list?
           if ( targets_->find( tgid ) < 0 )
@@ -1196,7 +1189,7 @@ nest::FixedInDegreeBuilder::connect_()
       // local nodes is smaller than the targets_-range, we will iterate over
       // the local nodes instead and check, whether the node is in the
       // targets_-range.
-      if ( targets_->size() < get_local_nodes().size()
+      if ( targets_->size() < kernel().node_manager.local_nodes_size()
         || not targets_->is_range() )
       {
         for ( GIDCollection::const_iterator tgid = targets_->begin();
@@ -1214,12 +1207,12 @@ nest::FixedInDegreeBuilder::connect_()
       }
       else
       {
-        for ( SparseNodeArray::const_iterator it = get_local_nodes().begin();
-              it != get_local_nodes().end();
-              ++it )
+        for ( SparseNodeArray::const_iterator it = kernel().node_manager.local_nodes_begin();
+                it != kernel().node_manager.local_nodes_end();
+                ++it )
         {
-          Node* const target = ( *it ).node_;
-          const index tgid = ( *it ).gid_;
+          Node* const target = ( *it ).get_node();
+          const index tgid = ( *it ).get_gid();
 
           // Is the local node in the targets list?
           if ( targets_->find( tgid ) < 0 )
@@ -1486,7 +1479,7 @@ nest::FixedTotalNumberBuilder::connect_()
     }
 
     sum_dist += static_cast< double >( number_of_targets_on_vp[ k ] );
-    sum_partitions += static_cast< uint >( num_conns_on_vp[ k ] );
+    sum_partitions += static_cast< unsigned int >( num_conns_on_vp[ k ] );
   }
 
 // end code adapted from gsl 1.8
@@ -1583,7 +1576,7 @@ nest::BernoulliBuilder::connect_()
       // local nodes is smaller than the targets_-range, we will iterate over
       // the local nodes instead and check, whether the node is in the
       // targets_-range.
-      if ( targets_->size() < get_local_nodes().size()
+      if ( targets_->size() < kernel().node_manager.local_nodes_size()
         || not targets_->is_range() )
       {
         for ( GIDCollection::const_iterator tgid = targets_->begin();
@@ -1602,12 +1595,12 @@ nest::BernoulliBuilder::connect_()
 
       else
       {
-        for ( SparseNodeArray::const_iterator it = get_local_nodes().begin();
-              it != get_local_nodes().end();
-              ++it )
+        for (  SparseNodeArray::const_iterator it = kernel().node_manager.local_nodes_begin();
+                it != kernel().node_manager.local_nodes_end();
+               ++it )
         {
-          Node* const target = ( *it ).node_;
-          const index tgid = ( *it ).gid_;
+          Node* const target = ( *it ).get_node();
+          const index tgid = ( *it ).get_gid();
 
           // Is the local node in the targets list?
           if ( targets_->find( tgid ) < 0 )

nestkernel/conn_builder.h
@@ -111,11 +111,6 @@ public:
     return false;
   }
 
-  /**
-   * This function gets the local nodes from the node manager for more efficient
-   */
-  const SparseNodeArray& get_local_nodes();
-
 protected:
   //! Implements the actual connection algorithm
   virtual void connect_() = 0;

nestkernel/connection_manager.cpp
@@ -84,7 +84,6 @@ nest::ConnectionManager::~ConnectionManager()
   // back to the system anyway. Hence, why bother cleaning up our highly
   // scattered connection infrastructure? They do not have any open files, which
   // need to be closed or similar.
-  // delete_connections_();
 }
 
 void

nestkernel/sparse_node_array.h
@@ -58,6 +58,14 @@ public:
   struct NodeEntry
   {
     NodeEntry( Node&, index );
+
+    // Accessor functions here are mostly in place to make things "look nice".
+    // Since SparseNodeArray only exposes access to const_interator, iterators
+    // could anyways not be used to change entry contents.
+    // TODO: But we may want to re-think this.
+    Node* get_node() const;
+    index get_gid() const;
+
     Node* node_;
     index gid_; //!< store gid locally for faster searching
   };
@@ -197,6 +205,18 @@ inline std::map< long, size_t >
 nest::SparseNodeArray::get_step_ctr() const
 {
   return step_ctr_;
+}
+
+inline nest::Node*
+nest::SparseNodeArray::NodeEntry::get_node() const
+{
+  return node_;
+}
+
+inline nest::index
+nest::SparseNodeArray::NodeEntry::get_gid() const
+{
+  return gid_;
 }
 
 #endif /* SPARSE_NODE_ARRAY_H */

commit 23e0c1405dfbd44da16f98b00071f8392adeaf1e

Fixed bug introduced in branch by 438d81d728

nestkernel/node_manager.h
@@ -358,7 +358,7 @@ NodeManager::local_nodes_begin() const
 inline SparseNodeArray::const_iterator
 NodeManager::local_nodes_end() const
 {
-  return local_nodes_.begin();
+  return local_nodes_.end();
 }
 
 inline size_t
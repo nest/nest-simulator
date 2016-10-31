1f7a3de275186ffda8fb5d43f893984a6879b76a
janhahne <jhahne@uni-wuppertal.de>
2016-10-31 11:05:23 +0100

    removed unused needs_prelim_update

--- nestkernel/nest_names.h
+++ nestkernel/nest_names.h
@@ -262,7 +262,6 @@ extern const Name N_channels; //!< Specific to correlomatrix_detector
 extern const Name n_events;   //!< Recorder parameter
 extern const Name
   n_proc; //!< Number of component processes of ppd_sup_/gamma_sup_generator
-extern const Name needs_prelim_update; //!< Node parameter
 extern const Name neuron;              //!< Node type
 extern const Name node_uses_wfr;       //!< Node parameter
 extern const Name noise;               //!< Specific to iaf_chs_2008 neuron

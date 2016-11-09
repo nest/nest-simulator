c1d9aedf1b3eeb89165ca07dd5d64877771b1686
hakonsbm <hakmork@nmbu.no>
2016-11-09 11:11:30 +0100

    Added slice test; Added conversion to lists.

--- pynest/nest/tests/test_GIDCollection.py
+++ pynest/nest/tests/test_GIDCollection.py
@@ -37,27 +37,36 @@ class GIDCollectionTestCase(unittest.TestCase):
     def test_GIDCollection_addition(self):
         """Addition of GIDCollections"""
 
-        a = nest.Create("iaf_neuron", 2)
-        b = nest.Create("iaf_neuron", 2)
-        c = a + b
-        
-        self.assertEqual(c, (1, 2, 3, 4))
+        nodes_a = nest.Create("iaf_neuron", 2)
+        nodes_b = nest.Create("iaf_neuron", 2)
+        all_nodes = nodes_a + nodes_b
+        all_nodes_list = [n for n in all_nodes]
+        test_list = [1, 2, 3, 4]
+        self.assertEqual(all_nodes_list, test_list)
     
     def test_GIDCollection_membership(self):
         """Membership in GIDCollections"""
         
-        a = nest.Create("iaf_neuron", 10)
+        nodes = nest.Create("iaf_neuron", 10)
         
-        self.assertTrue(5 in a)
-        self.assertTrue(10 in a)
-        self.assertFalse(12 in a)
+        self.assertTrue(5 in nodes)
+        self.assertTrue(10 in nodes)
+        self.assertFalse(11 in nodes)
+        
+    def test_GIDCollection_slices(self):
+        """Slices of GIDCollections"""
+
+        nodes = nest.Create("iaf_neuron", 50)
+        nodes_slice_a = nodes[:20]
+        slice_list = [i for i in nodes_slice_a]
+        test_list = [i for i in range(1, 21)]
+        self.assertEqual(slice_list, test_list)
+    
         
         
 
 def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(GIDCollectionTestCase('test_GIDCollection_addition'))
-    suite.addTest(GIDCollectionTestCase('test_GIDCollection_membership'))
+    suite = unittest.makeSuite(GIDCollectionTestCase, 'test')
     return suite
 
 

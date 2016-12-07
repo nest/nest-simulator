9a0353713e4454a733f4823e5ef2eb298dba0165
Stine Vennemo <stine.vennemo@gmail.com>
2016-12-07 10:38:39 +0100

    Deprecation changes in hl_api_subnets

--- pynest/nest/lib/hl_api_subnets.py
+++ pynest/nest/lib/hl_api_subnets.py
@@ -18,7 +18,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
-from hl_api_helper import deprecated
 
 """
 Functions for hierarchical networks
@@ -29,7 +28,7 @@ from .hl_api_nodes import Create
 from .hl_api_info import GetStatus, SetStatus
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def PrintNetwork(depth=1, subnet=None):
     """Print the network tree up to depth, starting at subnet.
@@ -57,7 +56,7 @@ def PrintNetwork(depth=1, subnet=None):
     sr("%i PrintNetwork" % depth)
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def CurrentSubnet():
     """Returns the global id of the current subnet.
@@ -72,7 +71,7 @@ def CurrentSubnet():
     return (spp(), )
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def ChangeSubnet(subnet):
     """Make given subnet the current.
@@ -94,7 +93,7 @@ def ChangeSubnet(subnet):
     sr("ChangeSubnet")
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def GetLeaves(subnets, properties=None, local_only=False):
     """Return the GIDs of the leaf nodes of the given subnets.
@@ -134,7 +133,7 @@ def GetLeaves(subnets, properties=None, local_only=False):
                     litconv=True)
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def GetNodes(subnets, properties=None, local_only=False):
     """Return the global ids of the all nodes of the given subnets.
@@ -172,7 +171,7 @@ def GetNodes(subnets, properties=None, local_only=False):
                     litconv=True)
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def GetChildren(subnets, properties=None, local_only=False):
     """Return the global ids of the immediate children of the given subnets.
@@ -210,7 +209,7 @@ def GetChildren(subnets, properties=None, local_only=False):
                     litconv=True)
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def GetNetwork(gid, depth):
     """Return a nested list with the children of subnet id at level
@@ -243,7 +242,7 @@ def GetNetwork(gid, depth):
     return spp()
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def BeginSubnet(label=None, params=None):
     """Create a new subnet and change into it.
@@ -264,7 +263,7 @@ def BeginSubnet(label=None, params=None):
     ChangeSubnet(sn)
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def EndSubnet():
     """Change to the parent subnet and return the gid of the current.
@@ -286,7 +285,7 @@ def EndSubnet():
             "Unexpected EndSubnet(). Cannot go higher than the root node.")
 
 
-@deprecated('', text='Subnets have been removed, see documentation.')
+@deprecated('', text='Subnets are to be removed, see documentation.')
 @check_stack
 def LayoutNetwork(model, dim, label=None, params=None):
     """Create a subnetwork of dimension dim with nodes of type model and

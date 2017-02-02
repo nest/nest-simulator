f4d3d079e4e4ddda2fdc49bb418be6b1b5ef5a04
Jakob Jordan <j.jordan@fz-juelich.de>
2017-02-02 18:21:52 +0100

    Remove global spike detector and related functions/variables

--- nestkernel/mpi_manager.h
+++ nestkernel/mpi_manager.h
@@ -91,30 +91,6 @@ public:
   thread get_rank() const;
 
   /**
-   * Get number of recording processes.
-   */
-  thread get_num_rec_processes() const;
-
-  /**
-   * Get number of simulating processes.
-   */
-  thread get_num_sim_processes() const;
-
-  /**
-   * Set number of recording processes, switches NEST to global
-   * spike detection mode.
-   *
-   * @param nrp  number of recording processes
-   * @param called_by_reset   pass true when calling from Scheduler::reset()
-   *
-   * @note The `called_by_reset` parameter is a cludge to avoid a
-   *       chicken-and-egg problem when resetting the kernel. It surpresses a
-   *       test for existing nodes, trusting that the kernel will immediately
-   *       afterwards delete all existing nodes.
-   */
-  void set_num_rec_processes( int nrp, bool called_by_reset );
-
-  /**
    * Return the process id for a given virtual process. The real process' id
    * of a virtual process is defined by the relation: p = (vp mod P), where
    * P is the total number of processes.
@@ -216,8 +192,6 @@ public:
 private:
   int num_processes_;    //!< number of MPI processes
   int rank_;             //!< rank of the MPI process
-  index n_rec_procs_;    //!< MPI processes dedicated for recording devices
-  index n_sim_procs_;    //!< MPI processes used for simulation
   int send_buffer_size_; //!< expected size of send buffer
   int recv_buffer_size_; //!< size of receive buffer
   bool use_mpi_;         //!< whether MPI is used
@@ -389,18 +363,6 @@ inline thread
 MPIManager::get_rank() const
 {
   return rank_;
-}
-
-inline thread
-MPIManager::get_num_rec_processes() const
-{
-  return n_rec_procs_;
-}
-
-inline thread
-MPIManager::get_num_sim_processes() const
-{
-  return n_sim_procs_;
 }
 
 inline int

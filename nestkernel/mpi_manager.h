821735fa2599b0d8930d6639fb2c4f9cce2673ea
Jakob Jordan <j.jordan@fz-juelich.de>
2017-02-12 21:44:25 +0100

    Fix bug in resizing MPI buffers

--- nestkernel/mpi_manager.h
+++ nestkernel/mpi_manager.h
@@ -505,7 +505,7 @@ inline bool
 MPIManager::increase_buffer_size_target_data()
 {
   assert( adaptive_target_buffers_ );
-  if ( buffer_size_target_data_ < max_buffer_size_target_data_ )
+  if ( buffer_size_target_data_ * growth_factor_buffer_target_data_ < max_buffer_size_target_data_ )
   {
     buffer_size_target_data_ = static_cast< size_t >( floor( buffer_size_target_data_ * growth_factor_buffer_target_data_ ) );
     return true;
@@ -520,7 +520,7 @@ inline bool
 MPIManager::increase_buffer_size_spike_data()
 {
   assert( adaptive_spike_buffers_ );
-  if ( buffer_size_spike_data_ < max_buffer_size_spike_data_ )
+  if ( buffer_size_spike_data_ * growth_factor_buffer_spike_data_ < max_buffer_size_spike_data_ )
   {
     buffer_size_spike_data_ = static_cast< size_t >( floor( buffer_size_spike_data_ * growth_factor_buffer_spike_data_ ) );
     return true;

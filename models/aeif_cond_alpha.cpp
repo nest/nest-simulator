74e1285aefaf4a047d0077e5ab4660ce3c053375
Dennis Terhorst <d.terhorst@fz-juelich.de>
2017-03-09 12:32:38 +0100

    fixed one indent

--- models/aeif_cond_alpha.cpp
+++ models/aeif_cond_alpha.cpp
@@ -417,9 +417,9 @@ nest::aeif_cond_alpha::init_buffers_()
   }
 
   if ( B_.c_ == 0 )
-    {
-      B_.c_ = gsl_odeiv_control_yp_new( P_.gsl_error_tol, P_.gsl_error_tol );
-    }
+  {
+    B_.c_ = gsl_odeiv_control_yp_new( P_.gsl_error_tol, P_.gsl_error_tol );
+  }
   else
   {
     gsl_odeiv_control_init(

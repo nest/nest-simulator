d5a021aff056f2d7ea78514d3ce83c45c4588fb3
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2017-04-06 15:27:02 +0200

    Fixes #658: Implements recording of current generated by stimulating devices

--- pynest/nest/tests/test_current_recording_generators.py
+++ pynest/nest/tests/test_current_recording_generators.py
@@ -151,13 +151,13 @@ class CurrentRecordingGeneratorTestCase(unittest.TestCase):
                 and numpy.all(i_ac[t_stop_ind:]) == 0), \
             "Current not zero when AC generator inactive"
         self.assertAlmostEqual(numpy.amax(i_ac[t_start_ind:t_stop_ind]),
-                                self.i_amp + self.i_off,
-                                msg=("Current not correct "
-                                     "when AC generator active"))
+                               self.i_amp + self.i_off,
+                               msg=("Current not correct "
+                                    "when AC generator active"))
         self.assertAlmostEqual(numpy.amin(i_ac[t_start_ind:t_stop_ind]),
-                                -self.i_amp + self.i_off,
-                                msg=("Current not correct "
-                                     "when AC generator active"))
+                               -self.i_amp + self.i_off,
+                               msg=("Current not correct "
+                                    "when AC generator active"))
 
         t_start_ind = numpy.where(t_dc == self.t_start+self.t_origin)[0][0]
         t_stop_ind = numpy.where(t_dc == self.t_stop+self.t_origin)[0][0]

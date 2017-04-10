4618fddb2a427c705eea0f02fc2e5e5dee32dd83
David Kappel <david@igi.tugraz.at>
2017-04-10 15:30:38 +0200

    applied patch for issue 696

--- nestkernel/connection_manager.cpp
+++ nestkernel/connection_manager.cpp
@@ -391,6 +391,13 @@ nest::ConnectionManager::connect( index sgid,
   }
   else if ( target->local_receiver() ) // target is a normal device
   {
+    if ( target->one_node_per_process() )
+    {
+      // music proxy connection or similar device with one node per process.
+      connect_( *source, *target, sgid, target_thread, syn, d, w );
+      return;
+    }
+
     // make sure source is on this MPI rank
     if ( source->is_proxy() )
     {
@@ -453,12 +460,19 @@ nest::ConnectionManager::connect( index sgid,
   }
   else if ( target->local_receiver() ) // target is a normal device
   {
+    if ( target->one_node_per_process() )
+    {
+      // music proxy connection or similar device with one node per process.
+      connect_( *source, *target, sgid, target_thread, syn, params, d, w );
+      return;
+    }
+
     // make sure source is on this MPI rank
     if ( source->is_proxy() )
     {
       return;
     }
-
+      
     // make sure connections are only created on the thread of the device
     if ( ( source->get_thread() != target_thread )
       && ( source->has_proxies() ) )
@@ -521,6 +535,13 @@ nest::ConnectionManager::connect( index sgid,
   }
   else if ( target->local_receiver() ) // target is a normal device
   {
+    if ( target->one_node_per_process() )
+    {
+      // music proxy connection or similar device with one node per process.
+      connect_( *source, *target, sgid, target_thread, syn, params );
+      return true;
+    }
+
     // make sure source is on this MPI rank
     if ( source->is_proxy() )
     {

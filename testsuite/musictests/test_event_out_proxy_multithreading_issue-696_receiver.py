ae7290992b9834db6146bd4207f3ef38ceb474e3
David Kappel <david@igi.tugraz.at>
2017-04-12 15:01:29 +0200

    fixed copyright header

--- testsuite/musictests/test_event_out_proxy_multithreading_issue-696_receiver.py
+++ testsuite/musictests/test_event_out_proxy_multithreading_issue-696_receiver.py
@@ -1,25 +1,24 @@
-#! /usr/bin/env python
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
-#  test_event_out_proxy_multithreading_issue-696_receiver.py
+# test_event_out_proxy_multithreading_issue-696_receiver.py
 #
-#  This file is part of NEST.
+# This file is part of NEST.
 #
-#  Copyright (C) 2004 The NEST Initiative
+# Copyright (C) 2004 The NEST Initiative
 #
-#  NEST is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #
-#  NEST is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with NEST.  If not, see <http://www.gnu.org/licenses/>.
-#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 import music
 import itertools

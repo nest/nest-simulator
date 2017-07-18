5684d0953d4946275a23e1321c26cef4569278aa
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2017-07-18 13:09:31 +0200

    removed the installation steps for cppcheck,explicitely defining the cmake options for python

--- build.sh
+++ build.sh
@@ -51,13 +51,19 @@ else
 fi
 
 if [ "$xPYTHON" = "1" ] ; then
-    CONFIGURE_PYTHON="-Dwith-python=ON"
+   if [ "$TRAVIS_PYTHON_VERSION" == "2.7.13" ]; then
+      CONFIGURE_PYTHON="-DPYTHON-LIBRARY=~/virtualenv/python2.7.13/lib/python2.7 -DPYTHON_INCLUDE_DIR=~/virtualenv/python2.7.13/include/python2.7"
+   elif [ "$TRAVIS_PYTHON_VERSION" == "3.4.4" ]; then
+       CONFIGURE_PYTHON="-DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.4m.so -DPYTHON_INCLUDE_DIR=~/virtualenv/python3.4/include/python3.4m"
+   fi
 else
     CONFIGURE_PYTHON="-Dwith-python=OFF"
 fi
 
 if [ "$xMUSIC" = "1" ] ; then
     CONFIGURE_MUSIC="-Dwith-music=$HOME/.cache/music.install"
+    chmod +x music_install.sh
+    ./music_install.sh
 else
     CONFIGURE_MUSIC="-Dwith-music=OFF"
 fi
@@ -87,98 +93,71 @@ NEST_RESULT=$(readlink -f $NEST_RESULT)
 mkdir "$NEST_VPATH" "$NEST_RESULT"
 mkdir "$NEST_VPATH/reports"
 
-if [ "$xSTATIC_ANALYSIS" = "1" ] ; then
-  echo
-  echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
-  echo "+               S T A T I C   C O D E   A N A L Y S I S                       +"
-  echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
+echo
+echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
+echo "+               S T A T I C   C O D E   A N A L Y S I S                       +"
+echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
 
-  echo "MSGBLD0010: Initializing VERA++ static code analysis."
-  # Add the NEST profile to the VERA++ profiles.
-  sudo cp ./extras/vera++.profile /usr/lib/vera++/profiles/nest
-  echo "MSGBLD0020: VERA++ initialization completed."  
+echo "MSGBLD0010: Initializing VERA++ static code analysis."
+ # Add the NEST profile to the VERA++ profiles.
+sudo cp ./extras/vera++.profile /usr/lib/vera++/profiles/nest
+echo "MSGBLD0020: VERA++ initialization completed."  
 
-  if [ ! -f "$HOME/.cache/bin/cppcheck" ]; then
-    echo "MSGBLD0030: Installing CPPCHECK version 1.69."
-    # Build cppcheck version 1.69.
-    git clone https://github.com/danmar/cppcheck.git
-    cd cppcheck
-    git checkout tags/1.69
-    mkdir -p install
-    make PREFIX=$HOME/.cache CFGDIR=$HOME/.cache/cfg HAVE_RULES=yes install
-    cd ..
-    echo "MSGBLD0040: CPPCHECK installation completed."
-
-    echo "MSGBLD0050: Installing CLANG-FORMAT."
-    wget --no-verbose http://llvm.org/releases/3.6.2/clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    tar xf clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    # Copy and not move because '.cache' may aleady contain other subdirectories and files.
-    cp -R clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04/* $HOME/.cache
-    echo "MSGBLD0060: CLANG-FORMAT installation completed."
-
-    # Remove these directories, otherwise the copyright-header check will complain.
-    rm -rf ./cppcheck
-    rm -rf ./clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04
-  fi
-
-  # Ensure that the cppcheck and clang-format installation can be found.
-  export PATH=$HOME/.cache/bin:$PATH
-  
-  echo "MSGBLD0070: Retrieving changed files."
+echo "MSGBLD0070: Retrieving changed files."
   # Note: BUG: Extracting the filenames may not work in all cases. 
   #            The commit range might not properly reflect the history.
   #            see https://github.com/travis-ci/travis-ci/issues/2668
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "MSGBLD0080: PULL REQUEST: Retrieving changed files using GitHub API."
-    file_names=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
-  else
-    echo "MSGBLD0090: Retrieving changed files using git diff."    
-    file_names=`(git diff --name-only $TRAVIS_COMMIT_RANGE || echo "") | tr '\n' ' '`
-  fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+   echo "MSGBLD0080: PULL REQUEST: Retrieving changed files using GitHub API."
+   file_names=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
+else
+   echo "MSGBLD0090: Retrieving changed files using git diff."    
+   file_names=`(git diff --name-only $TRAVIS_COMMIT_RANGE || echo "") | tr '\n' ' '`
+fi
 
-  printf '%s\n' "$file_names" | while IFS= read -r line
-  do
-    for single_file_name in $file_names
-    do
-      echo "MSGBLD0095: File changed: $single_file_name"
-    done
-  done
-  echo "MSGBLD0100: Retrieving changed files completed."
-  echo
+printf '%s\n' "$file_names" | while IFS= read -r line
+ do
+   for single_file_name in $file_names
+   do
+     echo "MSGBLD0095: File changed: $single_file_name"
+   done
+ done
+echo "MSGBLD0100: Retrieving changed files completed."
+echo
 
 
-  # Set the command line arguments for the static code analysis script and execute it.
+# Set the command line arguments for the static code analysis script and execute it.
 
-  # The names of the static code analysis tools executables.
-  VERA=vera++                   
-  CPPCHECK=cppcheck
-  CLANG_FORMAT=clang-format
-  PEP8=pep8
+# The names of the static code analysis tools executables.
+VERA=vera++                   
+CPPCHECK=cppcheck
+CLANG_FORMAT=clang-format
+PEP8=pep8
 
-  # Perform or skip a certain analysis.
-  PERFORM_VERA=true
-  PERFORM_CPPCHECK=true
-  PERFORM_CLANG_FORMAT=true
-  PERFORM_PEP8=true
+# Perform or skip a certain analysis.
+PERFORM_VERA=true
+PERFORM_CPPCHECK=true
+PERFORM_CLANG_FORMAT=true
+PERFORM_PEP8=true
 
-  # The following command line parameters indicate whether static code analysis error messages
-  # will cause the Travis CI build to fail or are ignored.
-  IGNORE_MSG_VERA=true
-  IGNORE_MSG_CPPCHECK=true
-  IGNORE_MSG_CLANG_FORMAT=false
-  IGNORE_MSG_PEP8=false
+# The following command line parameters indicate whether static code analysis error messages
+# will cause the Travis CI build to fail or are ignored.
+IGNORE_MSG_VERA=true
+IGNORE_MSG_CPPCHECK=true
+IGNORE_MSG_CLANG_FORMAT=false
+IGNORE_MSG_PEP8=false
 
-  # The script is called within the Travis CI environment and cannot be run incremental.
-  RUNS_ON_TRAVIS=true
-  INCREMENTAL=false
+# The script is called within the Travis CI environment and cannot be run incremental.
+RUNS_ON_TRAVIS=true
+INCREMENTAL=false
 
-  sudo chmod +x ./extras/static_code_analysis.sh
-  ./extras/static_code_analysis.sh "$RUNS_ON_TRAVIS" "$INCREMENTAL" "$file_names" "$NEST_VPATH" \
-  "$VERA" "$CPPCHECK" "$CLANG_FORMAT" "$PEP8" \
-  "$PERFORM_VERA" "$PERFORM_CPPCHECK" "$PERFORM_CLANG_FORMAT" "$PERFORM_PEP8" \
-  "$IGNORE_MSG_VERA" "$IGNORE_MSG_CPPCHECK" "$IGNORE_MSG_CLANG_FORMAT" "$IGNORE_MSG_PEP8"
+chmod +x ./extras/static_code_analysis.sh
+./extras/static_code_analysis.sh "$RUNS_ON_TRAVIS" "$INCREMENTAL" "$file_names" "$NEST_VPATH" \
+"$VERA" "$CPPCHECK" "$CLANG_FORMAT" "$PEP8" \
+"$PERFORM_VERA" "$PERFORM_CPPCHECK" "$PERFORM_CLANG_FORMAT" "$PERFORM_PEP8" \
+"$IGNORE_MSG_VERA" "$IGNORE_MSG_CPPCHECK" "$IGNORE_MSG_CLANG_FORMAT" "$IGNORE_MSG_PEP8"
 
-fi   # Static code analysis.
+# End of Static code analysis.
 
 
 cd "$NEST_VPATH"

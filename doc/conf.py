b027ea6b411bd3639cef807fa7bbf0c7dc43d46b
steffen <s.graber@fz-juelich.de>
2017-08-17 15:34:32 +0200

    Setting mathjax cdn

--- doc/conf.py
+++ doc/conf.py
@@ -75,6 +75,8 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
+mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 

94706d5dae3ac2f34d9ccd327f7da1d3fee4693d
steffen <s.graber@fz-juelich.de>
2017-08-18 11:09:29 +0200

    recommonmark issues

--- doc/conf.py
+++ doc/conf.py
@@ -38,6 +38,13 @@ source_parsers = {
 source_suffix = ['.rst', '.md']
 
 
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
+
 # -- Checking for pandoc --------------------------------------------------
 
 try:
@@ -201,10 +208,3 @@ texinfo_documents = [
 ]
 
 # -- Options for readthedocs ----------------------------------------------
-
-
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-        'enable_auto_doc_ref': True,
-    }, True)
-    app.add_transform(AutoStructify)

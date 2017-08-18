e11de6d230269959de9ce922a64f4aa3aad3c012
steffen <s.graber@fz-juelich.de>
2017-08-18 11:27:56 +0200

    More options for AutoStructify

--- doc/conf.py
+++ doc/conf.py
@@ -155,6 +155,8 @@ def setup(app):
     app.add_config_value('recommonmark_config', {
             #'url_resolver': lambda url: github_doc_root + url,
             'auto_toc_tree_section': 'Contents',
+            'enable_inline_math': True,
+            'enable_auto_doc_ref': True,
             'enable_eval_rst': True
             }, True)
     app.add_transform(AutoStructify)

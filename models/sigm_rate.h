894950a56800c84c1fba7ecaa401f55b31cb3a9f
MSenden <mario.senden@googlemail.com>
2017-11-21 10:22:31 +0100

    added documentation

--- models/sigm_rate.h
+++ models/sigm_rate.h
@@ -36,6 +36,68 @@
 namespace nest
 {
 
+/* BeginDocumentation
+Name: sigm_rate - rate model with sigmoidal gain function as defined
+in [1].
+
+Description:
+
+ sigm_rate is an implementation of a nonlinear rate model with input
+ function input(h) = ( g * h )^4 / ( .1^4 + ( g * h )^4 ).  
+ The boolean parameter linear_summation determines whether the input 
+ function is applied to the summed up incoming connections
+ (= True, default value) or to each input individually (= False).
+
+ The model supports connections to other rate models with either zero or
+ non-zero delay, and uses the secondary_event concept introduced with
+ the gap-junction framework.
+
+Parameters:
+
+ The following parameters can be set in the status dictionary.
+
+ rate                double - Rate (unitless)
+ tau                 double - Time constant of rate dynamics in ms.
+ mean                double - Mean of Gaussian white noise.
+ std                 double - Standard deviation of Gaussian white noise.
+ g                   double - Gain parameter.
+ linear_summation    bool   - Specifies type of non-linearity (see above).
+ rectify_output      bool   - Switch to restrict rate to values >= 0.
+
+Note:
+The boolean parameter linear_summation determines whether the
+input from different presynaptic neurons is first summed linearly and
+then transformed by a nonlinearity (true), or if the input from
+individual presynaptic neurons is first nonlinearly transformed and
+then summed up (false). Default is true.
+
+References:
+
+ [1] Gancarz, G., & Grossberg, S. (1998). 
+ A neural model of the saccade generator in the reticular formation.
+ Neural Networks, 11(7), 1159–1174. doi: 10.1016/S0893-6080(98)00096-3
+
+ [2] Hahne, J., Dahmen, D., Schuecker, J., Frommer, A.,
+ Bolten, M., Helias, M. and Diesmann, M. (2017).
+ Integration of Continuous-Time Dynamics in a
+ Spiking Neural Network Simulator.
+ Front. Neuroinform. 11:34. doi: 10.3389/fninf.2017.00034
+
+ [3] Hahne, J., Helias, M., Kunkel, S., Igarashi, J.,
+ Bolten, M., Frommer, A. and Diesmann, M. (2015).
+ A unified framework for spiking and gap-junction interactions
+ in distributed neuronal network simulations.
+ Front. Neuroinform. 9:22. doi: 10.3389/fninf.2015.00022
+
+Sends: InstantaneousRateConnectionEvent, DelayedRateConnectionEvent
+
+Receives: InstantaneousRateConnectionEvent, DelayedRateConnectionEvent,
+DataLoggingRequest
+
+Author: Mario Senden, Jan Hahne, Jannis Schuecker
+SeeAlso: rate_connection_instantaneous, rate_connection_delayed
+*/
+
 class nonlinearities_sigm_rate
 {
 private:

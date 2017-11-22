579bb7202b193ffcecce85211aea8f92933ada80
Stine Vennemo <stine.vennemo@gmail.com>
2017-11-22 11:43:57 +0100

    Connect needs GIDCollections

--- pynest/nest/tests/test_sp/test_disconnect.py
+++ pynest/nest/tests/test_sp/test_disconnect.py
@@ -73,18 +73,18 @@ class TestDisconnectSingle(unittest.TestCase):
                 neurons = nest.Create('iaf_psc_alpha', 4)
                 syn_dict = {'model': syn_model}
 
-                nest.Connect(neurons[0], neurons[2], "one_to_one", syn_dict)
-                nest.Connect(neurons[1], neurons[3], "one_to_one", syn_dict)
+                nest.Connect(neurons[:1], neurons[2:3], "one_to_one", syn_dict)
+                nest.Connect(neurons[1:2], neurons[3:4], "one_to_one", syn_dict)
                 # Delete existent connection
                 conns = nest.GetConnections(
-                    neurons[0], neurons[2], syn_model)
+                    neurons[:1], neurons[2:3], syn_model)
                 if mpi_test:
                     conns = self.comm.allgather(conns)
                     conns = filter(None, conns)
                 assert len(conns) == 1
-                nest.DisconnectOneToOne(neurons[0][0], neurons[2][0], syn_dict)
+                nest.DisconnectOneToOne(neurons[0], neurons[2], syn_dict)
                 conns = nest.GetConnections(
-                    neurons[0], neurons[2], syn_model)
+                    neurons[:1], neurons[2:3], syn_model)
                 if mpi_test:
                     conns = self.comm.allgather(conns)
                     conns = filter(None, conns)
@@ -92,7 +92,7 @@ class TestDisconnectSingle(unittest.TestCase):
 
                 # Assert that one can not delete a non existent connection
                 conns1 = nest.GetConnections(
-                    neurons[0], neurons[1], syn_model)
+                    neurons[:1], neurons[1:2], syn_model)
                 if mpi_test:
                     conns1 = self.comm.allgather(conns1)
                     conns1 = filter(None, conns1)

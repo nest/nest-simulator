1c010865364008951fa2cd19367e6b6098a4bf4b
steffen <s.graber@fz-juelich.de>
2017-12-14 07:53:34 +0100

    test js

--- doc/_static/js/custom.js
+++ doc/_static/js/custom.js
@@ -1,2 +1,2 @@
 var x = document.getElementsByClassName("icon icon-home");
-x.innerHTML = "<img src="http://www.nest-simulator.org/wp-content/uploads/2015/03/nest_logo.png">";
+x.innerHTML = '<img src="http://www.nest-simulator.org/wp-content/uploads/2015/03/nest_logo.png">';

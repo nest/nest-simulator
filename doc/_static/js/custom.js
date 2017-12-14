b2c8017cacac6607ca183ac36f544997fefce8cd
steffen <s.graber@fz-juelich.de>
2017-12-14 08:03:59 +0100

    logo

--- doc/_static/js/custom.js
+++ doc/_static/js/custom.js
@@ -1,2 +1,3 @@
-var x = document.getElementsByClassName("icon icon-home");
-x.innerHTML = '<img src="http://www.nest-simulator.org/wp-content/uploads/2015/03/nest_logo.png">';
+var x = document.getElementsByClassName("wy-side-nav-search")[0];
+var x.document.getElementsByClassName("icon-home")[0].style.backgroundImage = '<img src="http://www.nest-simulator.org/wp-content/uploads/2015/03/nest_logo.png">';
+console.log(x);

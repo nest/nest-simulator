c81abbbe18fb9d2aff23216e0b2f4d848be869da
steffen <s.graber@fz-juelich.de>
2017-12-14 09:19:19 +0100

    logo with css

--- doc/_static/js/custom.js
+++ doc/_static/js/custom.js
@@ -1,3 +1,3 @@
 var x = document.getElementsByClassName("wy-side-nav-search")[0];
-var x.document.getElementsByClassName("icon-home")[0].style.backgroundImage = '<img src="http://www.nest-simulator.org/wp-content/uploads/2015/03/nest_logo.png">';
+
 console.log(x);

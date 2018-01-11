74e66a721f1dce06d325270646b9fe9a10b41ed2
steffen <s.graber@fz-juelich.de>
2018-01-11 14:37:00 +0100

    add copyright

--- extras/docker/nest-2.12.0/entrypoint.sh
+++ extras/docker/nest-2.12.0/entrypoint.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+# check_code_style.sh
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+# This script performs a static code analysis and can be used to verify
+# if a source file fulfills the NEST coding style guidelines.
+# Run ./extras/check_code_style.sh --help for more detailed information.
+
 set -e
 
 # Activate Python3 environment

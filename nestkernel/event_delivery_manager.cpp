0e4fa9e915817706ff6623839bb13a4aafebc685
Jakob Jordan <jakobjordan@posteo.de>
2018-03-05 18:06:38 +0100

    Move SendBufferPosition to separate file

--- nestkernel/event_delivery_manager.cpp
+++ nestkernel/event_delivery_manager.cpp
@@ -38,6 +38,7 @@
 #include "connection_manager.h"
 #include "connection_manager_impl.h"
 #include "event_delivery_manager_impl.h"
+#include "send_buffer_position.h"
 #include "source.h"
 
 // Includes from sli:

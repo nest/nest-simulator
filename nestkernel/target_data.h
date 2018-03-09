63a44a81aba440e168adb096b5c13ea16e9c0991
Alexander Peyser <a.peyser@fz-juelich.de>
2018-03-09 11:14:45 +0100

    use variant for target_data

--- nestkernel/target_data.h
+++ nestkernel/target_data.h
@@ -32,6 +32,98 @@
 
 namespace nest
 {
+class TargetDataFields
+{
+private:
+  unsigned int lcid_ : 27;
+  unsigned int tid_ : 10;
+  unsigned int syn_id_ : 8;
+
+public:
+  // Members must be set explicitly -- no defaults
+  void set_lcid( const index lcid );
+  index get_lcid() const;
+  void set_tid( const thread tid );
+  thread get_tid() const;
+  void set_syn_id( const synindex syn_id );
+  synindex get_syn_id() const;
+};
+
+inline void
+TargetDataFields::set_lcid( const index lcid )
+{
+  lcid_ = lcid;
+}
+
+inline index
+TargetDataFields::get_lcid() const
+{
+  return lcid_;
+}
+
+inline void
+TargetDataFields::set_tid( const thread tid )
+{
+  tid_ = tid;
+}
+
+inline thread
+TargetDataFields::get_tid() const
+{
+  return tid_;
+}
+
+inline void
+TargetDataFields::set_syn_id( const synindex syn_id )
+{
+  syn_id_ = syn_id;
+}
+
+inline synindex
+TargetDataFields::get_syn_id() const
+{
+  return syn_id_;
+}
+
+class SecondaryTargetDataFields
+{
+private:
+  unsigned int send_buffer_pos_;
+  unsigned char syn_id_;
+
+public:
+  // Members must be set explicitly -- no defaults
+  void set_send_buffer_pos( const size_t pos );
+  size_t get_send_buffer_pos() const;
+  void set_syn_id( const synindex syn_id );
+  synindex get_syn_id() const;
+};
+
+inline void
+SecondaryTargetDataFields::set_send_buffer_pos( const size_t pos )
+{
+  assert( pos < std::numeric_limits< unsigned int >::max() );
+  send_buffer_pos_ = pos;
+}
+
+inline size_t
+SecondaryTargetDataFields::get_send_buffer_pos() const
+{
+  return send_buffer_pos_;
+}
+
+inline void
+SecondaryTargetDataFields::set_syn_id( const synindex syn_id )
+{
+  assert( syn_id < std::numeric_limits< unsigned char >::max() );
+  syn_id_ = syn_id;
+}
+
+inline synindex
+SecondaryTargetDataFields::get_syn_id() const
+{
+  return syn_id_;
+}
 
 /**
  * Used to communicate part of the connection infrastructure from
@@ -39,8 +131,12 @@ namespace nest
  * buffers.
  * SeeAlso: SpikeData
  */
-class TargetDataBase
+class TargetData
 {
+    // Members must be set explicitly -- no defaults
+    // Done this way to create large vector without preconstruction
+    // and to handle variant fields
+    
 private:
   static const unsigned int default_marker_ = 0;
   static const unsigned int complete_marker_ = 1;
@@ -50,13 +146,15 @@ private:
   unsigned int source_lid_ : 19; //!< local id of presynaptic neuron
   unsigned int source_tid_ : 10; //!< thread index of presynaptic neuron
   unsigned int marker_ : 2;
-  bool is_primary_ : 1;
-
-protected:
-  TargetDataBase();
-
+  bool is_primary_ : 1; //!< TargetData has TargetDataFields
+                        //!< else has SecondaryTargetDataFields
 public:
-  ~TargetDataBase();
+  //<! variant fields
+  union {
+      TargetDataFields target_data;
+      SecondaryTargetDataFields secondary_data;
+  };
+
   void reset_marker();
   void set_complete_marker();
   void set_end_marker();
@@ -72,191 +170,85 @@ public:
   bool is_primary() const;
 };
 
-// Just to set protected: members must always be
-// explicitly set
-inline TargetDataBase::TargetDataBase()
-{
-}
-
-inline TargetDataBase::~TargetDataBase()
-{
-}
-
 inline void
-TargetDataBase::reset_marker()
+TargetData::reset_marker()
 {
   marker_ = default_marker_;
 }
 
 inline void
-TargetDataBase::set_complete_marker()
+TargetData::set_complete_marker()
 {
   marker_ = complete_marker_;
 }
 
 inline void
-TargetDataBase::set_end_marker()
+TargetData::set_end_marker()
 {
   marker_ = end_marker_;
 }
 
 inline void
-TargetDataBase::set_invalid_marker()
+TargetData::set_invalid_marker()
 {
   marker_ = invalid_marker_;
 }
 
 inline bool
-TargetDataBase::is_complete_marker() const
+TargetData::is_complete_marker() const
 {
   return marker_ == complete_marker_;
 }
 
 inline bool
-TargetDataBase::is_end_marker() const
+TargetData::is_end_marker() const
 {
   return marker_ == end_marker_;
 }
 
 inline bool
-TargetDataBase::is_invalid_marker() const
+TargetData::is_invalid_marker() const
 {
   return marker_ == invalid_marker_;
 }
 
 inline void
-TargetDataBase::set_source_lid( const index source_lid )
+TargetData::set_source_lid( const index source_lid )
 {
   assert( source_lid < 1048576 );
   source_lid_ = source_lid;
 }
 
 inline void
-TargetDataBase::set_source_tid( const thread source_tid )
+TargetData::set_source_tid( const thread source_tid )
 {
   assert( source_tid < 1024 );
   source_tid_ = source_tid;
 }
 
 inline index
-TargetDataBase::get_source_lid() const
+TargetData::get_source_lid() const
 {
   return source_lid_;
 }
 
 inline thread
-TargetDataBase::get_source_tid() const
+TargetData::get_source_tid() const
 {
   return source_tid_;
 }
 
 inline void
-TargetDataBase::set_is_primary( const bool is_primary )
+TargetData::set_is_primary( const bool is_primary )
 {
   is_primary_ = is_primary;
 }
 
 inline bool
-TargetDataBase::is_primary() const
+TargetData::is_primary() const
 {
   return is_primary_;
 }
-
-class TargetData : public TargetDataBase
-{
-private:
-  unsigned int lcid_ : 27;
-  unsigned int tid_ : 10;
-  unsigned int syn_id_ : 8;
-
-public:
-  // Members must be set explicitly -- no defaults
-  // Done this way to create large vector without preconstruction
-  void set_lcid( const index lcid );
-  index get_lcid() const;
-  void set_tid( const thread tid );
-  thread get_tid() const;
-  void set_syn_id( const synindex syn_id );
-  synindex get_syn_id() const;
-};
-
-inline void
-TargetData::set_lcid( const index lcid )
-{
-  lcid_ = lcid;
-}
-
-inline index
-TargetData::get_lcid() const
-{
-  return lcid_;
-}
-
-inline void
-TargetData::set_tid( const thread tid )
-{
-  tid_ = tid;
-}
-
-inline thread
-TargetData::get_tid() const
-{
-  return tid_;
-}
-
-inline void
-TargetData::set_syn_id( const synindex syn_id )
-{
-  syn_id_ = syn_id;
-}
-
-inline synindex
-TargetData::get_syn_id() const
-{
-  return syn_id_;
-}
-
-class SecondaryTargetData : public TargetDataBase
-{
-private:
-  unsigned int send_buffer_pos_;
-  unsigned char syn_id_;
-
-public:
-  // Members must be set explicitly -- no defaults
-  // Done this way to create large vector without preconstruction
-  void set_send_buffer_pos( const size_t pos );
-  size_t get_send_buffer_pos() const;
-  void set_syn_id( const synindex syn_id );
-  synindex get_syn_id() const;
-};
-
-inline void
-SecondaryTargetData::set_send_buffer_pos( const size_t pos )
-{
-  assert( pos < std::numeric_limits< unsigned int >::max() );
-  send_buffer_pos_ = pos;
-}
-
-inline size_t
-SecondaryTargetData::get_send_buffer_pos() const
-{
-  return send_buffer_pos_;
-}
-
-inline void
-SecondaryTargetData::set_syn_id( const synindex syn_id )
-{
-  assert( syn_id < std::numeric_limits< unsigned char >::max() );
-  syn_id_ = syn_id;
-}
-
-inline synindex
-SecondaryTargetData::get_syn_id() const
-{
-  return syn_id_;
-}
-
 } // namespace nest
 
 #endif // TARGET_DATA_H

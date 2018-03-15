768119a315b6f2245e4187950081f8e8c919c00a
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2018-03-15 12:43:36 +0100

    added the python3.5 header file for music

--- music_install.sh
+++ music_install.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/INCF/MUSIC.git music
 cd music
 ./autogen.sh
-./configure --prefix=$HOME/.cache/music.install
+./configure --prefix=$HOME/.cache/music.install --PYTHON_INCLUDE=/opt/python/3.5-dev/include/python3.5m/Python.h
 make
 make install
 cd ..

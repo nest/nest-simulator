2bc5e7114a0408512094f476e9313aed8e179654
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2018-04-03 11:08:56 +0200

    Using the first release of csa/v0.1.8

--- csa_install.sh
+++ csa_install.sh
@@ -2,24 +2,11 @@
  # Install current csa version.                                                     
 # Install csa
   - git clone https://github.com/INCF/csa.git
-  - pushd csa
+  - cd csa
+  - git checkout tags/v0.1.8
   - ./autogen.sh
-  - ./configure --with-libneurosim=$HOME/.cache/libneurosim.install --prefix=$HOME/.c\
-ache/csa.install
+  - ./configure --with-libneurosim=$HOME/.cache/libneurosim.install --prefix=$HOME/.cache/csa.install
   - make
   - make install
-  - popd
-
-
-
-
-
-git clone https://github.com/INCF/MUSIC.git music
-cd music
-./autogen.sh
-./configure --prefix=$HOME/.cache/music.install
-make
-make install
-cd ..
-rm -rf music
+  - cd ..
 

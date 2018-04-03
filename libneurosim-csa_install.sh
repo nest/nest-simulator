d7cc680753bbe38cbdaa8c5197b66ddfcf9deb57
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2018-04-03 12:45:21 +0200

    corrected the version of csa

--- libneurosim-csa_install.sh
+++ libneurosim-csa_install.sh
@@ -12,9 +12,14 @@ rm -rf libneurosim
 # Install csa
 git clone https://github.com/INCF/csa.git csa
 cd csa
+git checkout tags/v0.1.8
 ./autogen.sh
 ./configure --with-libneurosim=$HOME/.cache/libneurosim.install --prefix=$HOME/.cache/csa.install
 make
 make install
 cd ..
 rm -rf csa
+
+
+
+

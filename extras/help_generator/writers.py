cf8e1ce6b67a6adcd483c5bc0702e6004c5a794c
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2018-04-05 03:38:13 +0200

    Fix formatting errors

--- extras/help_generator/writers.py
+++ extras/help_generator/writers.py
@@ -155,7 +155,7 @@ def write_helpindex(helpdir):
 
     if not os.path.exists(helpdir):
         return
-    
+
     filelist = glob.glob(os.path.join(helpdir, '*', '*.hlp'))
     html_list = []
     hlp_list = []

23743bcccb1dd19e24cda49b3a97731ada4af423
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2018-04-16 16:10:28 +0200

    Remove old versions of scripts and rename source directories in new ones

--- extras/install_music.sh
+++ extras/install_music.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
- # Install current MUSIC version.
-git clone https://github.com/INCF/MUSIC.git music
-cd music
+# Install current MUSIC version.
+git clone https://github.com/INCF/MUSIC.git music.src
+pushd music.src
 ./autogen.sh
 ./configure --prefix=$HOME/.cache/music.install
 make
 make install
-cd ..
-rm -rf music
-
+popd
+rm -rf music.src

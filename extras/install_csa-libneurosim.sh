aed3e2f6d235b943d7fc7864c1cb66f16a1c847a
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2018-04-16 12:13:08 +0200

    renamed the files for the installation of music,csa and libneurosim and moved them to extras dir

---
+++ extras/install_csa-libneurosim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash                                                                           
+# Install libneurosim
+git clone https://github.com/INCF/libneurosim.git libneurosim
+cd libneurosim
+./autogen.sh
+./configure --prefix=$HOME/.cache/libneurosim.install --with-mpi
+make
+make install
+cd ..
+rm -rf libneurosim
+
+# Install csa
+git clone https://github.com/INCF/csa.git csa
+cd csa
+git checkout tags/v0.1.8
+./autogen.sh
+./configure --with-libneurosim=$HOME/.cache/libneurosim.install --prefix=$HOME/.cache/csa.install
+make
+make install
+cd ..
+rm -rf csa
+
+
+
+
---
+++ extras/install_music.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ # Install current MUSIC version.
+git clone https://github.com/INCF/MUSIC.git music
+cd music
+./autogen.sh
+./configure --prefix=$HOME/.cache/music.install
+make
+make install
+cd ..
+rm -rf music
+

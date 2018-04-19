e82fca97804c6bd824990d8977a06eac453d8077
Stine Vennemo <stine.vennemo@gmail.com>
2018-04-19 15:14:41 +0200

    To test commit

--- pynest/nest/lib/hl_api_types.py
+++ pynest/nest/lib/hl_api_types.py
@@ -447,7 +447,7 @@ class GIDCollection(object):
         # This was added to ensure that the function is a nop (instead of,
         # for instance, raising an exception) when applied to an empty list,
         # which is an artifact of the API operating on lists, rather than
-        # relying on language idioms, such as comprehensions
+        # relying on language idioms, such as comprehensions.
         if self.__len__() == 0:
             return
 

0e535159a168d324f7082f7db7fb869045eb0751
steffengraber <s.graber@fz-juelich.de>
2018-05-29 13:05:07 +0200

    try apt install

--- setup.py
+++ setup.py
@@ -39,10 +39,26 @@ if os.environ.get('READTHEDOCS', None) == 'True':
     # So that we can build documentation using seaborn
     install_requires = ['seaborn']
 
-    build_line = "bash ./readthedocs-build.sh"
-    args = shlex.split(build_line)
+    build_line1 = "sudo add-apt-repository ppa:nest-simulator/nest"
+    args1 = shlex.split(build_line)
     print(args)
-    p = subprocess.Popen(args)
+    p = subprocess.Popen(args1)
+
+    build_line2 = "sudo apt-get update"
+    args2 = shlex.split(build_line2)
+    print(args2)
+    p = subprocess.Popen(args2)
+
+    build_line3 = "sudo apt-get install nest"
+    args3 = shlex.split(build_line3)
+    print(args3)
+    p = subprocess.Popen(args3)
+
+
+    # build_line = "bash ./readthedocs-build.sh"
+    # args = shlex.split(build_line)
+    # print(args)
+    # p = subprocess.Popen(args)
 
     # source_line = "source ./result/bin/nest_vars.sh"
     # args = shlex.split(source_line)

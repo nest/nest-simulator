524221d745ee9bf7bf17e3c4a0bc0eec5e01552a
steffengraber <s.graber@fz-juelich.de>
2018-06-08 09:13:12 +0200

    conda

--- setup.py
+++ setup.py
@@ -38,10 +38,10 @@ if os.environ.get('READTHEDOCS', None) == 'True':
     # So that we can build documentation using seaborn
     install_requires = ['seaborn']
 
-    build_line = "bash ./readthedocs-build.sh"
-    args = shlex.split(build_line)
-    print(args)
-    p = subprocess.Popen(args)
+    #build_line = "bash ./readthedocs-build.sh"
+    #args = shlex.split(build_line)
+    #print(args)
+    #p = subprocess.Popen(args)
 
     # source_line = "source ./result/bin/nest_vars.sh"
     # args = shlex.split(source_line)

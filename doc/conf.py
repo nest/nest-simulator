4bc2eff6c8d49b017b43c3b3ddcfa7fd42750add
steffengraber <s.graber@fz-juelich.de>
2018-06-22 13:15:27 +0200

    autodoc path

--- doc/conf.py
+++ doc/conf.py
@@ -50,9 +50,9 @@ from subprocess import check_output, CalledProcessError
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../topology'))
-sys.path.insert(0, os.path.abspath('../pynest/nest'))
+sys.path.insert(0, os.path.abspath('./..'))
+sys.path.insert(0, os.path.abspath('./../topology'))
+sys.path.insert(0, os.path.abspath('./../pynest/nest'))
 
 source_suffix = ['.rst', '.md']
 source_parsers = {

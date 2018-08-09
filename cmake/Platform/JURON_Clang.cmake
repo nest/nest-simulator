0644f58ae6a3638173363c881fd33f3d1e685745
Itaru Kitayama <kitayama@stair.center>
2018-08-09 05:20:33 +0200

    Add JURON support specific CMake files

---
+++ cmake/Platform/JURON_Clang.cmake
@@ -0,0 +1,35 @@
+# JUEON_Clang.cmake
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2018 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+include( Platform/JURON_Base )
+
+# set the compiler
+set( CMAKE_C_COMPILER clang CACHE FILEPATH "override C compiler" )
+set( CMAKE_CXX_COMPILER clang++ CACHE FILEPATH "override C++ compiler" )
+
+set( OpenMP_C_FLAGS "-fopenmp -fopenmp-targets=nvptx64-nvidia-cuda --cuda-path=$ENV{CUDA}" CACHE STRING "Compiler flag for OpenMP parallelization" FORCE )
+set( OpenMP_CXX_FLAGS "-fopenmp -fopenmp-targets=nvptx64-nvidia-cuda --cuda-path=$ENV{CUDA}" CACHE STRING "Compiler flag for OpenMP parallelization" FORCE )
+
+if ( static-libraries )
+  __juron_setup_static( LLVM CXX )
+  __juron_setup_static( LLVM C )
+else ()
+  __juron_setup_dynamic( LLVM CXX )
+  __juron_setup_dynamic( LLVM C )
+endif ()

bd0c5fbbe8372e7503e84b4cc328a9483c43ad1b
gtrensch <g.trensch@fz-juelich.de>
2018-08-19 18:10:43 +0200

    add license and description

--- examples/MyModule/soundclick_example.py
+++ examples/MyModule/soundclick_example.py
@@ -1,3 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# soundclick_example.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+soundclick recording backend example
+------------------------------------
+
+Example PyNest script to demonstrate the soundclick recording backend.
+
+Recorded spike events produce a clicking sound similar to that heard in 
+electrophysiological recordings. The recording backend makes use of the
+SFML (Simple and Fast Multimedia Library) audio module. 
+This requires lifsml-dev to be installed. To create the illusion of a 
+realistic sound from an electrophysiological recording, the recording 
+backend slows down the simulation to biological real time.
+'''
+
 import nest
 nest.Install("mymodule")
 

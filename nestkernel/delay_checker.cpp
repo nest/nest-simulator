2d80db3cdba59b4e1df9826501eb9f56378252dd
Alexander Peyser <a.peyser@fz-juelich.de>
2018-09-14 10:49:53 +0200

    Remove maybe unneeded min_delay freeze

--- nestkernel/delay_checker.cpp
+++ nestkernel/delay_checker.cpp
@@ -98,12 +98,7 @@ nest::DelayChecker::set_status( const DictionaryDatum& d )
 
   if ( min_delay_updated && max_delay_updated )
   {
-    if ( kernel().connection_manager.get_num_connections() > 0 )
-    {
-      throw BadProperty(
-        "Connections already exist. Please call ResetKernel first" );
-    }
-    else if ( new_min_delay < Time::get_resolution() )
+    if ( new_min_delay < Time::get_resolution() )
     {
       throw BadDelay( new_min_delay.get_ms(),
         "min_delay must be greater than or equal to resolution." );

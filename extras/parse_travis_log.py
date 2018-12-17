04ec06ecb3cb16710eebabd6749de08f94dea576
gtrensch <g.trensch@fz-juelich.de>
2018-12-17 18:45:24 +0100

    pep8 formatting

--- extras/parse_travis_log.py
+++ extras/parse_travis_log.py
@@ -897,15 +897,16 @@ def build_return_code(status_cmake_configure,
     -------
     0 (success) or 1.
     """
-    if ((status_cmake_configure) and
-        (status_make) and
-        (status_make_install) and
-        (skip_installcheck or status_tests) and
-        (skip_code_analysis or
-        ((ignore_vera or get_num_msgs(summary_vera) == 0) and
-         (ignore_cppcheck or get_num_msgs(summary_cppcheck) == 0) and
-         (ignore_format or get_num_msgs(summary_format) == 0) and
-         (ignore_pep8 or get_num_msgs(summary_pep8) == 0)))):
+    if ((status_cmake_configure) and                                                           # noqa
+        (status_make) and                                                                      # noqa
+        (status_make_install) and                                                              # noqa
+        (skip_installcheck or status_tests) and                                                # noqa
+        (skip_code_analysis or ((ignore_vera or get_num_msgs(summary_vera) == 0) and           # noqa
+                                (ignore_cppcheck or get_num_msgs(summary_cppcheck) == 0) and   # noqa
+                                (ignore_format or get_num_msgs(summary_format) == 0) and       # noqa
+                                (ignore_pep8 or get_num_msgs(summary_pep8) == 0))              # noqa
+        )                                                                                      # noqa
+       ):                                                                                      # noqa
 
         return 0
     else:

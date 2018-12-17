f35bbf94612a5c0e2c078c0d85bb6ea3d822782c
dnao <dnao@github.com>
2018-12-17 23:36:52 +1100

    Accidentally deleted constant

--- nestkernel/nest_names.h
+++ nestkernel/nest_names.h
@@ -423,6 +423,7 @@ extern const Name tau_c;
 extern const Name tau_Ca;
 extern const Name tau_D_KNa;
 extern const Name tau_decay;
+extern const Name tau_decay_AMPA;
 extern const Name tau_decay_ex;
 extern const Name tau_decay_in;
 extern const Name tau_decay_GABA_A;

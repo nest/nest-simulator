6570c58cfa13fbe7fc30495c54abd8bea9d1555b
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2019-02-20 19:17:16 +0100

    Remove MPI-linked modules from test script
    An MPI-enabled process (also when this enablement comes via loading of
    a dynamic module) is not allowed to Popen another process that also
    uses MPI. This commit makes the test script not include any MPI-using
    library (mpi4py and nest in this case) as that breaks Popen'ing the
    actual call to mpirun on at least OpenMPI 3+.

--- pynest/nest/tests/test_connect_all_patterns.py
+++ pynest/nest/tests/test_connect_all_patterns.py
@@ -20,41 +20,37 @@
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import subprocess as sp
+import subprocess
 import unittest
-import nest
 
-HAVE_MPI = nest.sli_func("statusdict/have_mpi ::")
+cmd = ["nest", "-c", "statusdict/have_mpi :: =only"]
+HAVE_MPI = subprocess.check_output(cmd) == "true"
 
 
 class TestConnectAllPatterns(unittest.TestCase):
 
     @unittest.skipIf(not HAVE_MPI, 'NEST was compiled without MPI')
     def testWithMPI(self):
-        # Check that we can import mpi4py
-        try:
-            from mpi4py import MPI
-        except ImportError:
-            raise unittest.SkipTest("mpi4py required")
         directory = os.path.dirname(os.path.realpath(__file__))
-        scripts = ["test_connect_all_to_all.py",
-                   "test_connect_one_to_one.py",
-                   "test_connect_fixed_indegree.py",
-                   "test_connect_fixed_outdegree.py",
-                   "test_connect_fixed_total_number.py",
-                   "test_connect_pairwise_bernoulli.py"
-                   ]
+        scripts = [
+            "test_connect_all_to_all.py",
+            "test_connect_one_to_one.py",
+            "test_connect_fixed_indegree.py",
+            "test_connect_fixed_outdegree.py",
+            "test_connect_fixed_total_number.py",
+            "test_connect_pairwise_bernoulli.py"
+        ]
+
         failing_tests = []
         for script in scripts:
-            test_script = os.path.join(directory, script)
-            command = nest.sli_func("mpirun", 2, "nosetests",
-                                    test_script)
-            command = command.split()
-            process = sp.Popen(command, stdout=sp.PIPE, stderr=sp.PIPE)
-            stdout, stderr = process.communicate()
-            retcode = process.returncode
-            if retcode != 0:
+            script_abs = os.path.join(directory, script)
+            cmd = ["nest", "-c", "2 (nosetests) (%s) mpirun =only" % script_abs]
+            test_cmd = subprocess.check_output(cmd)
+            process = subprocess.Popen(cmd)
+            process.communicate()
+            if process.returncode != 0:
                 failing_tests.append(script)
+
         self.assertTrue(not failing_tests, 'The following tests failed when ' +
                         'executing with "mpirun -np 2 nosetests [script]": ' +
                         ", ".join(failing_tests))

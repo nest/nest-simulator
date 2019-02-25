be0e6e4da6c64cdd9e1feb785adef9daa9e03c50
Håkon Mørk <hakon.mork@nmbu.no>
2019-02-25 16:25:16 +0100

    Adjust connection limit

--- nestkernel/connection_manager.cpp
+++ nestkernel/connection_manager.cpp
@@ -685,7 +685,7 @@ nest::ConnectionManager::increase_connection_count( const thread tid,
     num_connections_[ tid ].resize( syn_id + 1 );
   }
   ++num_connections_[ tid ][ syn_id ];
-  if ( num_connections_[ tid ][ syn_id ] >= 1 << 27 )
+  if ( num_connections_[ tid ][ syn_id ] >= ( 1 << 27 ) - 1 )
   {
     throw KernelException( "Too many connections per thread for synapse." );
   }

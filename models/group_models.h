2d6dbcd9205da7f2b31e74c23e07e9f515ff88ba
Jessica Mitchell <j.mitchell@fz-juelich.de>
2019-03-05 15:48:39 +0100

    create file to define groups of synapses, neurons and devices. These groups will be included in the h files, and will allow doxygen to extract them.

---
+++ models/group_models.h
@@ -0,0 +1,17 @@
+/** @defgroup Devices
+ *
+ *  This module is for the devices of NEST
+ *
+ */
+
+/** @defgroup Synapses
+ *
+ * This module is for the synapses of NEST
+ *
+ */
+
+/** @defgroup Neurons
+ *
+ * This module is for the neurons of NEST
+ *
+ */

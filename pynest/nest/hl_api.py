e8842f33a78b4a7a64f9216a35fcaa80e0642ebf
Alexander Peyser <a.peyser@fz-juelich.de>
2019-03-14 14:23:09 +0100

    WIP: generalize _load_module

--- pynest/nest/hl_api.py
+++ pynest/nest/hl_api.py
@@ -30,10 +30,10 @@ import os, sys
 # the global namespace. This makes the API functions of PyNEST itself
 # and those of extra modules available to the user.
 
-# return all package attributes that from pkg import * would put in the module namespace
+# return all package attributes that from $pkg import * would put in the module namespace $pkg
 # see https://docs.python.org/3/reference/simple_stmts.html#import
 def _pkg_attrs(pkg):
-    try: # if there's an "all", return that
+    try: # if there's an "__all__", return that
         return pkg.__all__
     except AttributeError: # otherwise, return everything at top level that doesn't start with `_`
         return (attr for attr in pkg.__dict__ if attr[0] != '_')
@@ -45,27 +45,31 @@ def _import_names(pkg, mod_dict):
 
 # from .$prefix.$x import * ; relative to mod_file which is the filename from which mod_dict was read
 # where $x.py is all files ./$path/*.py not starting with __
-# Ignores modules that have been previously (statically) loaded
-def _import_libs(mod_file, mod_dict, path, prefix):
+def _import_libs(mod_file, mod_dict, path, prefix, ignore_modules=frozenset()):
     libdir = os.path.join(os.path.dirname(mod_file), path)
     for name in os.listdir(libdir):
         if not name.endswith(".py") or name.startswith('__'):
             continue # not a regular python module
         
         pkg_name = "{}.{}".format(prefix, name[:-3])
-        if "nest." + pkg_name in sys.modules:
-            continue # has already been statically imported
-            
+        if pkg_name in ignore_modules:
+            continue # this package is not to be imported dynamically
+
         pkg = __import__(pkg_name, mod_dict, locals(), ['*'], 1)
         _import_names(pkg, mod_dict)
 
+#############################
 # insert static imports here
-#
-# from libs.$X import *
+# in the form:
+# from lib.$X import * ; _ignore_modules.add('lib.$X')
+############################
+_ignore_modules = set()
 
+############################
 # Then whatever is left over, load dynamically
 # then do `from .libs.$X import *` for every module in ./libs/$X.py that is left
-_import_libs(__file__, globals(), 'lib', 'lib')
+_import_libs(__file__, globals(), 'lib', 'lib', _ignore_modules)
+############################
 
 # With '__all__' we provide an explicit index of the package. Without any
 # imported submodules and any redundant functions we could minimize list.

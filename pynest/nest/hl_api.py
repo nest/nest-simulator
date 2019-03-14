0e5217dd8b8f8ef0ceabe243e2abdaafdf5fbde1
Alexander Peyser <a.peyser@fz-juelich.de>
2019-03-14 15:13:42 +0100

    WIP: make pep8 work

--- pynest/nest/hl_api.py
+++ pynest/nest/hl_api.py
@@ -23,37 +23,46 @@
 High-level API of PyNEST Module
 """
 
-import os, sys
+import os
+import sys
 
 # We search through the subdirectory "lib" of the "nest" module
 # directory and import the content of all Python files therein into
 # the global namespace. This makes the API functions of PyNEST itself
 # and those of extra modules available to the user.
 
-# return all package attributes that from $pkg import * would put in the module namespace $pkg
-# see https://docs.python.org/3/reference/simple_stmts.html#import
+
 def _pkg_attrs(pkg):
-    try: # if there's an "__all__", return that
+    # return all package attributes that
+    # from $pkg import *
+    # would put in the module namespace $pkg
+    # see https://docs.python.org/3/reference/simple_stmts.html#import
+    try:
+        # if there's an "__all__", return that
         return pkg.__all__
-    except AttributeError: # otherwise, return everything at top level that doesn't start with `_`
+    except AttributeError:
+        # otherwise, return everything at top level that doesn't start with `_`
         return (attr for attr in pkg.__dict__ if attr[0] != '_')
 
-# import * names from pkg to the dictionary of mod_dict
+
 def _import_names(pkg, mod_dict):
+    # import * names from pkg to the dictionary of mod_dict
     for attr in _pkg_attrs(pkg):
         mod_dict[attr] = getattr(pkg, attr)
 
-# from .$prefix.$x import * ; relative to mod_file which is the filename from which mod_dict was read
-# where $x.py is all files ./$path/*.py not starting with __
+
 def _import_libs(mod_file, mod_dict, path, prefix, ignore_modules=frozenset()):
+    # from .$prefix.$x import *
+    # relative to mod_file which is the filename from which mod_dict was read
+    # where $x.py is all files ./$path/*.py not starting with __
     libdir = os.path.join(os.path.dirname(mod_file), path)
     for name in os.listdir(libdir):
         if not name.endswith(".py") or name.startswith('__'):
-            continue # not a regular python module
-        
+            continue  # not a regular python module
+
         pkg_name = "{}.{}".format(prefix, name[:-3])
         if pkg_name in ignore_modules:
-            continue # this package is not to be imported dynamically
+            continue  # this package is not to be imported dynamically
 
         pkg = __import__(pkg_name, mod_dict, locals(), ['*'], 1)
         _import_names(pkg, mod_dict)
@@ -67,8 +76,12 @@ _ignore_modules = set()
 
 ############################
 # Then whatever is left over, load dynamically
-# then do `from .libs.$X import *` for every module in ./libs/$X.py that is left
-_import_libs(__file__, globals(), 'lib', 'lib', _ignore_modules)
+# then do
+#   `from .libs.$X import *`
+# for every module in ./libs/$X.py that is left
+_import_libs(__file__, globals(),
+             'lib', 'lib',
+             _ignore_modules)
 ############################
 
 # With '__all__' we provide an explicit index of the package. Without any

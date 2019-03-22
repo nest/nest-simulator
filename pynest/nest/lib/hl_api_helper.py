659738dc98169f257cd4c6b10821eb8e9e8cba99
Håkon Mørk <hakon.mork@nmbu.no>
2019-03-22 10:07:36 +0100

    PEP8

--- pynest/nest/lib/hl_api_helper.py
+++ pynest/nest/lib/hl_api_helper.py
@@ -77,6 +77,7 @@ def format_Warning(message, category, filename, lineno, line=None):
 
     return '%s:%s: %s:%s\n' % (filename, lineno, category.__name__, message)
 
+
 warnings.formatwarning = format_Warning
 
 

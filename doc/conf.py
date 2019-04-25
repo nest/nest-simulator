3f135d0a5ef1cefee4b4ce98f6bcf181750808af
Jessica Mitchell <j.mitchell@fz-juelich.de>
2019-04-25 16:04:03 +0200

    fix pep8

--- doc/conf.py
+++ doc/conf.py
@@ -83,7 +83,9 @@ for dirpath, dirnames, files in os.walk(os.path.dirname(__file__)):
 # -- General configuration ------------------------------------------------
 
 # import errors on libraries that depend on C modules
-# http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
+# http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-
+#compile-with-readthedocs-when-youre-using-numpy-and-scipy/
+
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):

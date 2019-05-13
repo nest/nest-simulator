d99d16534e258238fbf8c8a3c9f57b53d59a6871
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2019-05-13 19:01:12 +0200

    Combine all PyNEST MPI tests into a single test script

--- pynest/tests/mpi_tests.py
+++ pynest/tests/mpi_tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# test_mpitests.py
+# test_connect_all_patterns.py
 #
 # This file is part of NEST.
 #
@@ -27,28 +27,8 @@ import unittest
 import shlex
 
 
-# The lines below ensure that Popened subprocesses run in a minimal
-# environment that prevents mpirun from noticing that the parent is
-# already using MPI and the child application itself from interfering
-# with the MPI context of the parent.
-# This is a rather crude hack to Popen MPI-enabled PyNEST from this
-# MPI-enabled Python script (MPI is imported via NEST in the toplevel
-# testsuite runner). The reason for requiring this hack is that some
-# MPI implementations (e.g. OpenMPI > version 3) prevent running
-# applications in such a way.
-# A clean solution would require to run each PyNEST test as its own
-# process from a runner that itself does not import MPI (here: nest or
-# mpi4py). The clean solution requires a massive restructuring and
-# will probably only be implemented when we move to another test
-# framework for PyNEST altogether (see also https://git.io/fjUCg).
-required_env_vars = ("PATH", "PYTHONPATH", "HOME")
-env = {k: v for k, v in os.environ.items() if k in required_env_vars}
-
-
 def check_output(cmd):
-    cmd = shlex.split(cmd)
-    output = subprocess.check_output(cmd, env=env)
-    return output.decode("utf-8")
+    return subprocess.check_output(shlex.split(cmd)).decode("utf-8")
 
 
 def eprint(*args, **kwargs):
@@ -59,16 +39,23 @@ cmd = "nest -c 'statusdict/have_mpi :: =only'"
 HAVE_MPI = check_output(cmd) == "true"
 
 
-class TestSPwithMPI(unittest.TestCase):
+class TestConnectAllPatterns(unittest.TestCase):
 
     @unittest.skipIf(not HAVE_MPI, 'NEST was compiled without MPI')
     def testWithMPI(self):
         script_dir = os.path.dirname(os.path.realpath(__file__))
-        scripts = ["mpitest_issue_578_sp.py"]
+        scripts = [
+            "test_connect_all_to_all.py",
+            "test_connect_one_to_one.py",
+            "test_connect_fixed_indegree.py",
+            "test_connect_fixed_outdegree.py",
+            "test_connect_fixed_total_number.py",
+            "test_connect_pairwise_bernoulli.py",
+            "test_sp/mpitest_issue_578_sp.py",
+        ]
 
         failing = []
         for script_name in scripts:
-            print("")
             script = os.path.join(script_dir, script_name)
             cmd = "nest -c '2 (python) ({}) mpirun =only'".format(script)
             test_cmd = check_output(cmd)
@@ -87,7 +74,7 @@ class TestSPwithMPI(unittest.TestCase):
 
 
 def suite():
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestSPwithMPI)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestConnectAllPatterns)
     return suite
 
 

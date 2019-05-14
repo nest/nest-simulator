40089465b1f13b0e0ab45bea922a3aeeb3ba8926
Håkon Mørk <hakon.mork@nmbu.no>
2019-05-14 10:56:53 +0200

    Removed import of removed package

--- pynest/nest/spatial/__init__.py
+++ pynest/nest/spatial/__init__.py
@@ -19,5 +19,4 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-from . import pos  # noqa
 from .hl_api_spatial import *  # noqa

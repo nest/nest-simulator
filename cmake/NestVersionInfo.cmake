4e361c3cfdbc043fd3835fafd15160779052074e
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2019-06-07 11:13:36 +0200

    Replaced commented out messages by proper documentation

--- cmake/NestVersionInfo.cmake
+++ cmake/NestVersionInfo.cmake
@@ -1,3 +1,35 @@
+# NestVersionInfo.cmake
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+# Determine NEST version based on git branch
+#
+# This module defines
+#  NEST_VERSION_BRANCH, the current git branch
+#  NEST_VERSION_SUFFIX, set using -Dwith-version-suffix=<suffix>.
+#  NEST_VERSION, the numeric version number plus the suffix
+#  NEST_VERSION_GITHASH, the current git revision hash (empty for tarballs)
+#  NEST_VERSION_STRING, the full NEST version string
+#
+# In release branches, the string "UNKNOWN" below has to be replaced
+# with the proper version (e.g. "nest-2.18") in order to get the
+# correct version number if building from tarballs.
+
 
 macro(get_version_info)
    execute_process(

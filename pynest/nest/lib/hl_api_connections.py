faf6ce508ab9e9b683b455383d08b312fe505e87
Håkon Mørk <hakon.mork@nmbu.no>
2019-08-20 13:36:15 +0200

    Updated docstring to document new feature
    Specifying arrays of values in the synapse specification when connecting with fixed_total_number.

--- pynest/nest/lib/hl_api_connections.py
+++ pynest/nest/lib/hl_api_connections.py
@@ -194,12 +194,16 @@ def Connect(pre, post, conn_spec=None, syn_spec=None, model=None):
     except for 'receptor_type' which must be initialised with an integer.
 
     Parameter arrays are available for the rules 'one_to_one',
-    'all_to_all', 'fixed_indegree' and 'fixed_outdegree':
+    'all_to_all', 'fixed_total_number', 'fixed_indegree' and
+    'fixed_outdegree':
     - For 'one_to_one' the array has to be a one-dimensional
       NumPy array with length len(pre).
     - For 'all_to_all' the array has to be a two-dimensional NumPy array
       with shape (len(post), len(pre)), therefore the rows describe the
       target and the columns the source neurons.
+    - For 'fixed_total_number' the array has to be a one-dimensional
+      NumPy array with length len(N), where N is the number of connections
+      specified.
     - For 'fixed_indegree' the array has to be a two-dimensional NumPy array
       with shape (len(post), indegree), where indegree is the number of
       incoming connections per target neuron, therefore the rows describe the

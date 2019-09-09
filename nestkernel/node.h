d0926676b0020fc125186902e2efe9c0ecfe5e64
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2019-09-09 13:21:12 +0200

    Remove todo, as this is now handled.

--- nestkernel/node.h
+++ nestkernel/node.h
@@ -258,14 +258,6 @@ public:
    */
   void init_buffers();
 
-  // JME: we should call calibrate() at the end of set_status and at
-  // the end of the constructor call instead of during Prepare. This
-  // would make Prepare -> Run Run ... -> Cleanup much more
-  // robust. Wrt recording devices, this would enroll the devices at
-  // the end of SetStatus and then we need to prohibit the change of
-  // recorder backend properties between Runs, which requires a flag
-  // set in Prepare and unset in Cleanup.
-
   /**
    * Re-calculate dependent parameters of the node.
    * This function is called each time a simulation is begun/resumed.

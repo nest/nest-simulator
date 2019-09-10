a4b11923111f2645705801db738c80a53c69a127
Jessica Mitchell <j.mitchell@fz-juelich.de>
2019-09-10 13:49:07 +0200

    minor changes to language, format

--- nestkernel/recording_backend_ascii.h
+++ nestkernel/recording_backend_ascii.h
@@ -35,15 +35,15 @@ Write data to plain text files
 
 The `ascii` recording backend writes collected data persistently to a
 plain text ASCII file. It can be used for small to medium sized
-simulations, where the ease of a simple data format outweights the
+simulations, where the ease of a simple data format outweighs the
 benefits of high-performance output operations.
 
 This backend will open one file per recording device per thread on
-each MPI process. This can entail a very high load on the file system
-in large simulations. Especially on machines with distributed
-filesystems using this backend can become prohibitively inefficient.
-In case of experiencing such scaling problems, the :ref:`SIONlib
-backend <sionlib_backend>` can be a possible alternative.
+each MPI process. This can cause a high load on the file system
+in large simulations. This backend can become prohibitively inefficient,
+particularly on machines with distributed filesystems.
+In case you experience such scaling problems,  the :ref:`SIONlib
+backend <sionlib_backend>` may be a possible alternative.
 
 Filenames of data files are determined according to the following
 pattern:
@@ -52,26 +52,26 @@ pattern:
 
    data_path/data_prefix(label|model_name)-gid-vp.file_extension
 
-The properties `data_path` and `data_prefix` are global kernel
-properties. They can for example be set during repetitive simulation
-protocols to separate the data originating from indivitual runs. The
-`label` replaces the model name component if it is set to a non-empty
-string. `gid` and `vp` denote the zero-padded global ID and virtual
+The properties ``data_path`` and ``data_prefix`` are global kernel
+properties. They can, for example, be set during repetitive simulation
+protocols to separate the data originating from individual runs. The
+``label`` replaces the model name component if it is set to a non-empty
+string. ``gid`` and ``vp`` denote the zero-padded global ID and virtual
 process of the recorder writing the file. The filename ends in a dot
 and the `file_extension`.
 
 The life of a file starts with the call to ``Prepare`` and ends with
 the call to ``Cleanup``. Data that is produced during successive calls
-to ``Run`` inbetween a pair of ``Prepare`` and ``Cleanup`` calls will
+to ``Run`` in between a pair of ``Prepare`` and ``Cleanup`` calls will
 be written to the same file, while the call to ``Run`` will flush all
 data to the file, so it is available for immediate inspection.
 
-In case, a file of the designated name for a new recording already
+When creating a new recording, if the file name already
 exists, the ``Prepare`` call will fail with a corresponding error
 message. To instead overwrite the old file, the kernel property
-`overwrite_files` can be set to *true* using ``SetKernelStatus``.  An
+``overwrite_files`` can be set to *true* using ``SetKernelStatus``. An
 alternative way for avoiding name clashes is to re-set the kernel
-properties `data_path` or `data_prefix`, so that another filename is
+properties ``data_path`` or ``data_prefix``, so that another filename is
 chosen.
 
 Data format
@@ -82,47 +82,50 @@ containing field names for the different data columns. The header
 starts with a `#` character.
 
 The first field of each record written is the global id of the neuron
-the event originated from, i.e. the *source* of the event. This is
+the event originated from, i.e., the *source* of the event. This is
 followed by the time of the measurement, the recorded floating point
 values and the recorded integer values.
 
 The format of the time field depends on the value of the property
-`time_in_steps`. If set to *false* (which is the default), time is
+``time_in_steps``. If set to *false* (which is the default), time is
 written as a single floating point number representing the simulation
-time in ms. If `time_in_steps` is *true*, the time of the event is
+time in ms. If ``time_in_steps`` is *true*, the time of the event is
 written as a pair of values consisting of the integer simulation time
 step in units of the simulation resolution and the negative floating
 point offset in ms from the next integer grid point.
 
 .. note::
+
    The number of decimal places for all decimal numbers written can be
-   controlled using the recorder property `precision`.
+   controlled using the recorder property ``precision``.
 
 Parameter summary
 +++++++++++++++++
 
-`file_extension`
-  A string (default: *"dat"*) that specifies the file name extension,
-  without leading dot. The generic default was chosen, because the
-  exact type of data cannot be known a priori.
+.. glossary::
 
-`filenames`
-  A list of the filenames where data is recorded to. This list has one
-  entry per local thread and is a read-only property.
+ file_extension
+   A string (default: *"dat"*) that specifies the file name extension,
+   without leading dot. The generic default was chosen, because the
+   exact type of data cannot be known a priori.
 
-`label`
-  A string (default: *""*) that replaces the model name component in
-  the filename if it is set.
+ filenames
+   A list of the filenames where data is recorded to. This list has one
+   entry per local thread and is a read-only property.
 
-`precision`
-  An integer (default: *3*) that controls the number of decimal places
-  used to write decimal numbers to the output file.
+ label
+   A string (default: *""*) that replaces the model name component in
+   the filename if it is set.
 
-`time_in_steps`
-  A Boolean (default: *false*) specifying whether to write time in
-  steps, i.e. in integer multiples of the simulation resolution plus a
-  floating point number for the negative offset from the next grid
-  point in ms, or just the simulation time in ms.
+ precision
+   An integer (default: *3*) that controls the number of decimal places
+   used to write decimal numbers to the output file.
+
+ time_in_steps
+   A Boolean (default: *false*) specifying whether to write time in
+   steps, i.e., in integer multiples of the simulation resolution plus a
+   floating point number for the negative offset from the next grid
+   point in ms, or just the simulation time in ms.
 
 EndDocumentation */
 
--- nestkernel/recording_backend_screen.h
+++ nestkernel/recording_backend_screen.h
@@ -37,33 +37,35 @@ to check recordings in a more ad hoc fashion. The recording backend
 quick inspection.
 
 The first field of each record written is the global id of the neuron
-the event originated from, i.e. the *source* of the event. This is
+the event originated from, i.e., the *source* of the event. This is
 followed by the time of the measurement, the recorded floating point
-values and the recorded integer values.
+values, and the recorded integer values.
 
 The format of the time field depends on the value of the property
-`time_in_steps`. If set to *false* (which is the default), time is
+``time_in_steps``. If set to *false* (which is the default), time is
 written as one floating point number representing the simulation time
-in ms. If `time_in_steps` is *true*, the time of the event is written
+in ms. If ``time_in_steps`` is *true*, the time of the event is written
 as a value pair consisting of the integer simulation time step and the
 floating point offset in ms from the next grid point.
 
 .. note::
 
    Using this backend for production runs is not recommended, as it
-   may produce *huge* amounts of console output and thereby might slow
-   down the simulation *considerably*.
+   may produce *huge* amounts of console output and *considerably* slow
+   down the simulation.
 
 Parameter summary
 +++++++++++++++++
 
-`precision`
+.. glossary::
+
+ precision
   controls the number of decimal places used to write decimal numbers
   to the terminal.
 
-`time_in_steps`
+ time_in_step
   A boolean (default: false) specifying whether to print time in
-  steps, i.e. in integer multiples of the resolution and an offset,
+  steps, i.e., in integer multiples of the resolution and an offset,
   rather than just in ms.
 
 EndDocumentation */

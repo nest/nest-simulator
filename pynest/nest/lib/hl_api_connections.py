f39226028d03a034d32cfa2753e25f7cabd815dc
Johanna Senk <j.senk@fz-juelich.de>
2019-09-12 15:40:47 +0200

    Fix weights/delays in hl api

--- pynest/nest/lib/hl_api_connections.py
+++ pynest/nest/lib/hl_api_connections.py
@@ -250,9 +250,9 @@ def _process_spatial_projections(conn_spec, syn_spec):
                     "'{}' is not allowed in syn_spec when ".format(key) +
                     "connecting with mask or kernel".format(key))
         if 'weight' in syn_spec:
-            projections['weights'] = syn_spec['weight']
+            projections['weight'] = syn_spec['weight']
         if 'delay' in syn_spec:
-            projections['delays'] = syn_spec['delay']
+            projections['delay'] = syn_spec['delay']
         if 'synapse_model' in syn_spec:
             projections['synapse_model'] = syn_spec['synapse_model']
 

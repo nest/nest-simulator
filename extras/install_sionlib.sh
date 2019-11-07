9c4c2cc2514b3b9ebf1ee4753cc00ef3d4f276ba
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2019-11-07 13:53:52 +0100

    Update SIONlib installer to use newest version.

--- extras/install_sionlib.sh
+++ extras/install_sionlib.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+SIONLIBVERSION="1.7.4"
+
 # Install SIONlib
-wget --content-disposition http://apps.fz-juelich.de/jsc/sionlib/download.php?version=1.6.2
-tar xfz sionlib-1.6.2.tar.gz
+wget --content-disposition http://apps.fz-juelich.de/jsc/sionlib/download.php?version=${SIONLIBVERSION}
+tar xfz sionlib-${SIONLIBVERSION}.tar.gz
 mv sionlib sionlib.src
 pushd sionlib.src
-./configure --prefix=$HOME/.cache/sionlib.install --mpi=openmpi --enable-python=2 --disable-fortran --disable-parutils --disable-mic
+./configure --prefix=$HOME/.cache/sionlib.install --mpi=openmpi --enable-python=3 --disable-fortran --disable-parutils --disable-mic
 cd build-linux-gomp-openmpi
 make
 make install
 popd
-rm -rf sionlib-1.6.2.tar.gz sionlib.src
+rm -rf sionlib-${SIONLIBVERSION}.tar.gz sionlib.src

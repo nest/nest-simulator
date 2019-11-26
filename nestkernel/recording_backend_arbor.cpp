45f8ca5ed45e51afb54db6586ef26a040ea9e4eb
Wouter Klijn <w.klijn@fz-juelich.de>
2019-11-26 16:26:21 +0100

    Comment out the check for delay. Might be causing problems, but at least it runs now

--- nestkernel/recording_backend_arbor.cpp
+++ nestkernel/recording_backend_arbor.cpp
@@ -216,10 +216,10 @@ nest::RecordingBackendArbor::prepare()
     arb::shadow::broadcast( 0u, MPI_COMM_WORLD, arbor_->info.arbor_root ) + 1; // arbor has a pre-exchange
 
   // set min_delay
-  DictionaryDatum dict_in( new Dictionary );
-  ( *dict_in )[ "min_delay" ] = min_delay;
-  ( *dict_in )[ "max_delay" ] = ( *dict_out )[ "max_delay" ];
-  kernel().set_status( dict_in );
+  //DictionaryDatum dict_in( new Dictionary );
+  //( *dict_in )[ "min_delay" ] = min_delay;
+  //( *dict_in )[ "max_delay" ] = ( *dict_out )[ "max_delay" ];
+  //kernel().set_status( dict_in );
 
   // Arbor has an initial exchange before time 0
   std::vector< arb::shadow::spike > empty_spikes;

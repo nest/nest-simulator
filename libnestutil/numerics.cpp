9a6979eec185c78edfc9a9588ba43529f5960735
Philpp Weidel <philipp.weidel@aictx.ai>
2019-12-06 11:27:04 +0100

    code format

--- libnestutil/numerics.cpp
+++ libnestutil/numerics.cpp
@@ -110,19 +110,19 @@ dtruncate( double x )
   return ip;
 }
 
-bool 
+bool
 is_integer( double n )
 {
   double int_part;
   double frac_part = std::modf( n, &int_part );
 
-  // Since n > 0 and modf always rounds towards zero, a value of n just below an 
-  // integer will result in frac_part = 0.99999.... . We subtract from 1 in this case. 
+  // Since n > 0 and modf always rounds towards zero, a value of n just below an
+  // integer will result in frac_part = 0.99999.... . We subtract from 1 in this case.
   if ( frac_part > 0.5 )
   {
     frac_part = 1 - frac_part;
   }
-  
+
   // factor 4 allows for two bits of rounding error
-  return frac_part < 4 * n * std::numeric_limits<double>::epsilon();
+  return frac_part < 4 * n * std::numeric_limits< double >::epsilon();
 }

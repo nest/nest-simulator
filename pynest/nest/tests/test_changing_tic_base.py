89c3466b56c205aa97218aa28e68bb0205b73cdf
Håkon Mørk <hakon.mork@nmbu.no>
2020-01-13 10:41:56 +0100

    Added comments to test

--- pynest/nest/tests/test_changing_tic_base.py
+++ pynest/nest/tests/test_changing_tic_base.py
@@ -26,7 +26,11 @@ import numpy as np
 
 @nest.ll_api.check_stack
 class TestChangingTicBase(unittest.TestCase):
-    eps = 1e-7
+    eps = 1e-7  # Tolerance value
+    # The defaults of iaf_psc_exp_ps_lossless contains the time of the last spike, converted to ms from step=-1.
+    # As the initialized step-value is negative, there is no need to account for the change in tic-base.
+    # However, because the value in the defaults is converted to ms, it will differ from the reference value.
+    # The model is therefore ignored.
     ignored_models = ['iaf_psc_exp_ps_lossless']
 
     def setUp(self):
@@ -34,18 +38,25 @@ class TestChangingTicBase(unittest.TestCase):
 
     def test_models(self):
         """Time objects in models correctly updated"""
+        # Generate a dictionary of reference values for each model.
         reference = {}
         for model in nest.Models():
+            if model in self.ignored_models:
+                continue
             try:
                 reference[model] = nest.GetDefaults(model)
             except nest.kernel.NESTError:
                 # If we can't get the defaults, we ignore the model.
                 pass
+
+        # Change the tic-base.
         nest.SetKernelStatus({'tics_per_ms': 1500., 'resolution': 0.5})
+        # At this point, Time objects in models should have been updated to
+        # account for the new tic-base. Values in model defaults should therefore
+        # be equal (within a tolerance) to the reference values.
+
         failing_models = []
         for model in reference.keys():
-            if model in self.ignored_models:
-                continue
             model_reference = reference[model]
             model_defaults = nest.GetDefaults(model)
             # Remove entries where the item contains more than one value, as this causes issues when comparing.
@@ -57,16 +68,21 @@ class TestChangingTicBase(unittest.TestCase):
 
             keydiff = []
             for key, value in model_defaults.items():
+                # value may not be a number, so we test for equality first.
+                # If it's not equal to the reference value, we assume it is a number.
                 if value != model_reference[key] and abs(value - model_reference[key]) > self.eps:
                     print(value - model_reference[key])
                     keydiff.append([key, model_reference[key], value])
+            # If any keys have values different from the reference, the model fails.
             if len(keydiff) > 0:
                 print(model, keydiff)
                 failing_models.append(model)
 
+        # No models should fail for the test to pass.
         self.assertEqual([], failing_models)
 
     def _assert_ticbase_change_raises_and_reset(self, after_call):
+        """Assert that changing tic-base raises a NESTError, and reset the kernel"""
         with self.assertRaises(nest.kernel.NESTError, msg='after calling "{}"'.format(after_call)):
             nest.SetKernelStatus({'tics_per_ms': 1500., 'resolution': 0.5})
         nest.ResetKernel()

300112f8a33276cd0dd2e3bf9325588276683dc2
Sebastian Spreizer <spreizer@web.de>
2020-02-04 11:33:26 +0100

    Add web hl api for pyNEST and nest-web for bash

--- pynest/nest/__init__.py
+++ pynest/nest/__init__.py
@@ -34,6 +34,7 @@ from . import math                    # noqa
 from . import spatial_distributions   # noqa
 from . import logic                   # noqa
 from . import spatial                 # noqa needs to be imported last because of documentation generation
+from . import web                     # noqa
 
 
 def test():

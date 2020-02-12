a331e8624f22e46292536716b316ef50385c18cd
Johanna Senk <jhnnsnk@users.noreply.github.com>
2020-02-12 10:57:48 +0100

    Minor (capitalization)

--- pynest/examples/Potjans_2014/stimulus_params.py
+++ pynest/examples/Potjans_2014/stimulus_params.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-""" PyNEST Microcircuit Example: Stimulus parameters
+""" PyNEST Microcircuit Example: Stimulus Parameters
 -------------------------------------------------------
 
 External transient stimulation is optional. Thalamic input and DC input

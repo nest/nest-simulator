2e36fcf9147fcd9338edcd359226ff3aa9e11241
Susanne Kunkel <susanne.kunkel@nmbu.no>
2020-02-25 11:19:48 +0100

    same commit as 2890ca6a3cf125e0502b3e01744f5ad5e5a88a7f in PR #1442

---
+++ nestkernel/per_thread_bool_indicator.h
@@ -0,0 +1,105 @@
+/*
+ *  per_thread_bool_indicator.h
+ *
+ *  This file is part of NEST.
+ *
+ *  Copyright (C) 2004 The NEST Initiative
+ *
+ *  NEST is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NEST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PER_THREAD_BOOL_INDICATOR_H
+#define PER_THREAD_BOOL_INDICATOR_H
+
+// C++ includes:
+#include <cassert>
+#include <cstddef>
+
+// Includes from nestkernel:
+#include "nest_types.h"
+#include "vp_manager.h"
+
+// Includes from sli:
+#include "dictdatum.h"
+
+namespace nest
+{
+
+class BoolIndicatorUInt64
+{
+public:
+  BoolIndicatorUInt64();
+  BoolIndicatorUInt64( const bool status );
+
+  bool is_true() const;
+  bool is_false() const;
+
+  void set_true();
+  void set_false();
+
+  void logical_and( const bool status );
+
+private:
+  static const uint_fast64_t true_uint64 = true;
+  static const uint_fast64_t false_uint64 = false;
+  uint_fast64_t status_;
+
+  friend class PerThreadBoolIndicator;
+};
+
+/**
+ * A thread-safe vector to keep track of the status across threads,
+ * for example during gather operations. Uses a vector of integers
+ * instead of a vector of bools to guarantee thread safety.
+ */
+class PerThreadBoolIndicator
+{
+public:
+  PerThreadBoolIndicator(){};
+
+  BoolIndicatorUInt64& operator[]( const thread tid );
+
+  /**
+   * Resize to the given number of threads and set all elements to false.
+   */
+  void initialize( const thread num_threads, const bool status );
+
+  /**
+   * Waits for all threads and returns whether all elements are false.
+   */
+  bool all_false() const;
+
+  /**
+   * Waits for all threads and returns whether all elements are true.
+   */
+  bool all_true() const;
+
+  /**
+   * Waits for all threads and returns whether any elements are false.
+   */
+  bool any_false() const;
+
+  /**
+   * Waits for all threads and returns whether any elements are true.
+   */
+  bool any_true() const;
+
+private:
+  std::vector< BoolIndicatorUInt64 > per_thread_status_;
+};
+
+} // namespace nest
+
+#endif /* PER_THREAD_BOOL_INDICATOR_H */

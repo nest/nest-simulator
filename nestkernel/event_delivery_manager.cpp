167b0cd753b313ff1100a35f8a6909cea6b7bd52
Susanne Kunkel <susanne.kunkel@nmbu.no>
2020-02-27 13:04:52 +0100

    Remove extra thread barriers

--- nestkernel/event_delivery_manager.cpp
+++ nestkernel/event_delivery_manager.cpp
@@ -320,9 +320,8 @@ EventDeliveryManager::gather_spike_data_( const thread tid,
 
   while ( gather_completed_checker_.any_false() )
   {
-// Assume this is the last gather round and change to false
-// otherwise
-#pragma omp barrier
+    // Assume this is the last gather round and change to false
+    // otherwise
     gather_completed_checker_[ tid ].set_true();
 
 #pragma omp single
@@ -599,9 +598,8 @@ EventDeliveryManager::gather_target_data( const thread tid )
 
   while ( gather_completed_checker_.any_false() )
   {
-// assume this is the last gather round and change to false
-// otherwise
-#pragma omp barrier
+    // assume this is the last gather round and change to false
+    // otherwise
     gather_completed_checker_[ tid ].set_true();
 
 #pragma omp single

da362a22f2351d32fa9ca5a0ed9bced0e41879f3
steffengraber <s.graber@fz-juelich.de>
2020-03-19 16:15:48 +0100

    Split pager name and option into list

--- pynest/nest/lib/hl_api_helper.py
+++ pynest/nest/lib/hl_api_helper.py
@@ -501,7 +501,8 @@ def show_help_with_pager(hlpobj, pager=None):
         return
 
     try:
-        subprocess.check_call([pager, objf])
+        pagerl = pager.split(" ")
+        subprocess.check_call(pagerl + [objf])
     except (OSError, IOError, subprocess.CalledProcessError):
         print('Displaying help with pager "{}" failed. '
               'Please define a working parser in file .nestrc '
@@ -509,7 +510,7 @@ def show_help_with_pager(hlpobj, pager=None):
 
 
 def model_deprecation_warning(model):
-    """Checks whether the model is to be removed in a future verstion of NEST.
+    """Checks whether the model is to be removed in a future version of NEST.
     If so, a deprecation warning is issued.
 
     Parameters

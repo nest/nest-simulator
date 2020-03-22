876555057dfe8e72ce8c2fa72b81217d48f6901e
Nilton Liuji KAMIJI <nilton.kamiji@gmail.com>
2020-03-21 22:17:38 -0300

    Make Boost enabled by default (#1469) considering maximum version of 1.68.0 (#1239), and fix boost include path issue (#1238).

--- cmake/ProcessOptions.cmake
+++ cmake/ProcessOptions.cmake
@@ -587,13 +587,19 @@ function( NEST_PROCESS_WITH_BOOST )
     # Needs Boost version >=1.58.0 to use Boost sorting
     find_package( Boost 1.58.0 COMPONENTS unit_test_framework )
     if ( Boost_FOUND )
-      # export found variables to parent scope
-      set( HAVE_BOOST ON PARENT_SCOPE )
-      # Boost uses lower case in variable names
-      set( BOOST_FOUND "${Boost_FOUND}" PARENT_SCOPE )
-      set( BOOST_LIBRARIES "${Boost_LIBRARIES}" PARENT_SCOPE )
-      set( BOOST_INCLUDE_DIR "${Boost_INCLUDE_DIR}" PARENT_SCOPE )
-      set( BOOST_VERSION "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}" PARENT_SCOPE )
+      if ( "${Boost_VERSION}" VERSION_LESS "1.69" )
+        # export found variables to parent scope
+        set( HAVE_BOOST ON PARENT_SCOPE )
+        # Boost uses lower case in variable names
+        set( BOOST_FOUND "${Boost_FOUND}" PARENT_SCOPE )
+        set( BOOST_LIBRARIES "${Boost_LIBRARIES}" PARENT_SCOPE )
+        set( BOOST_INCLUDE_DIR "${Boost_INCLUDE_DIR}" PARENT_SCOPE )
+        set( BOOST_VERSION "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}" PARENT_SCOPE )
+        
+        include_directories( ${Boost_INCLUDE_DIR} )
+      else () 
+        set( BOOST_VERSION "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}" PARENT_SCOPE )
+      endif ()
     endif ()
   endif ()
 endfunction()

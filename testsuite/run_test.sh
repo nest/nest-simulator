1ffaa0b411b3b50dde2857e837f764cfa92d0ade
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2020-05-12 02:07:36 +0200

    Add copyright headers to testsuite shell scripts

--- testsuite/run_test.sh
+++ testsuite/run_test.sh
@@ -1,4 +1,23 @@
 
+# run_tests.sh
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
 
 #
 # run_test script_name codes_success codes_failure

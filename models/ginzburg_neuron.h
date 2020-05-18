a7f165ea3b8ca668b7aaf94e8a808258d6751057
Sara Konradi <s.konradi@fz-juelich.de>
2020-05-18 16:18:36 +0200

    Add subscripts in parameters section

--- models/ginzburg_neuron.h
+++ models/ginzburg_neuron.h
@@ -91,10 +91,10 @@ Parameters
 ====== ============= ===========================================================
  tau_m  ms           Membrane time constant (mean inter-update-interval)
  theta  mV           Threshold for sigmoidal activation function
- c1     probability/ Linear gain factor
+ c_1    probability/ Linear gain factor
         mV
- c2     probability  Prefactor of sigmoidal gain
- c3     1/mV         Slope factor of sigmoidal gain
+ c_2    probability  Prefactor of sigmoidal gain
+ c_3    1/mV         Slope factor of sigmoidal gain
 ====== ============= ===========================================================
 
 References

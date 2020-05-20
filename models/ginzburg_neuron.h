23ccdec0d1d6c9b9b1e7cf2a4eedbecfda5444ee
Sara Konradi <s.konradi@fz-juelich.de>
2020-05-20 14:07:31 +0200

    Fix :math: in description

--- models/ginzburg_neuron.h
+++ models/ginzburg_neuron.h
@@ -47,11 +47,11 @@ the probability of the neuron to be in the active (1) state.
 
 The gain function g used here is :math:`g(h) = c_1*h + c_2 * 0.5*(1 +
 \tanh(c_3*(h-\theta)))` (output clipped to [0,1]). This allows to
-obtain affin-linear (c_1!=0, c_2!=0, c_3=0) or sigmoidal (c_1=0,
-c_2=1, c_3!=0) shaped gain functions. The latter choice
+obtain affin-linear (:math:`c_1!=0, c_2!=0, c_3=0`) or sigmoidal (:math:`c_1=0,
+c_2=1, c_3!=0`) shaped gain functions. The latter choice
 corresponds to the definition in [1]_, giving the name to this
 neuron model.
-The choice c_1=0, c_2=1, c_3=beta/2 corresponds to the Glauber
+The choice :math:`c_1=0, c_2=1, c_3=beta/2` corresponds to the Glauber
 dynamics [2]_, :math:`g(h) = 1 / (1 + \exp(-\beta (h-\theta)))`.
 The time constant :math:`\tau_m` is defined as the mean
 inter-update-interval that is drawn from an exponential

1f71d352c407f712cf1081623d0b6ea6da178fb4
Stine Vennemo <stine.vennemo@gmail.com>
2020-06-16 12:10:06 +0200

    Fixed missing use of len

--- pynest/nest/lib/hl_api_types.py
+++ pynest/nest/lib/hl_api_types.py
@@ -708,8 +708,7 @@ class SynapseCollection(object):
 
         if (isinstance(params, (list, tuple)) and
                 self.__len__() != len(params)):
-            raise TypeError(
-                "status dict must be a dict, or a list of dicts of length len(nodes)")
+            raise TypeError("status dict must be a dict, or a list of dicts of length {}".format(self.__len__()))
 
         if kwargs and params is None:
             params = kwargs

7c741bf7e1fed3e5d265e49d639f0e74a54f2963
HanjiaJiang <bagheera1984@gmail.com>
2020-07-01 00:55:51 +0200

    Apply suggestions from code review
    Table formatting
    
    Co-authored-by: sarakonradi <58212853+sarakonradi@users.noreply.github.com>

--- models/tsodyks_connection_hom.h
+++ models/tsodyks_connection_hom.h
@@ -87,8 +87,9 @@ an arbitrary postsynaptic effect depending on y(t).
 Parameters
 ++++++++++
 
-========  ======  ======================================================
- U        real    Parameter determining the increase in u with each spike [0,1]
+========  ======  ========================================================
+ U        real    Parameter determining the increase in u with each spike
+                  [0,1]
  tau_psc  ms      Time constant of synaptic current
  tau_fac  ms      Time constant for facilitation
  tau_rec  ms      Time constant for depression

f574021720df8ede158a6be8d1ded3dbb0129331
Lekshmi Deepu <r.deepu@fz-juelich.de>
2020-07-23 02:41:19 +0200

    using consistent python version(3.8) for both linux and MacOS, using caching for MacOS for improving the speed

--- extras/travis_build.sh
+++ extras/travis_build.sh
@@ -46,13 +46,18 @@ else
 fi
 
 if [ "$xPYTHON" = "1" ] ; then
-   if [ "$TRAVIS_PYTHON_VERSION" = "3.6.10" ]; then
-      PYTHON_LIB_DIR=`find /usr/lib/x86_64-linux-gnu -name "libpython3*.so"`
-      PYTHON_INCLUDE_DIR=`python3 -c "import sysconfig; print(sysconfig.get_path('include'))"`
-      CONFIGURE_PYTHON="-DPYTHON_LIBRARY=$PYTHON_LIB_DIR -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR"
+   PYTHON_LIB_DIR=`python3 -c "import sysconfig; print(sysconfig.get_path('platstdlib'))"`
+   PYTHON_INCLUDE_DIR=`python3 -c "import sysconfig; print(sysconfig.get_path('include'))"` 
+   if [ "$TRAVIS_PYTHON_VERSION" = "3.8.2" ]; then
+       #PYTHON_LIB_DIR=`find /usr/lib/x86_64-linux-gnu -name "libpython3*.so"`
+      PYTHON_LIBRARY=`find $PYTHON_LIB_DIR -name "libpython3*.so"` 
+      CONFIGURE_PYTHON="\
+          -DPYTHON_LIBRARY=$PYTHON_LIBRARY
+          -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR"
    fi
    if [[ $OSTYPE = darwin* ]]; then
-      PYPREFIX="/usr/local/Cellar/python@3.8/3.8.3_2/Frameworks/Python.framework/Versions/3.8"
+      #PYPREFIX="/usr/local/Cellar/python@3.8/3.8.3_2/Frameworks/Python.framework/Versions/3.8"
+      PYTHON_LIBRARY=`find $PYTHON_LIB_DIR -name "libpython3.8.dylib"` 
       CONFIGURE_PYTHON="\
             -DPYTHON_LIBRARY=$PYPREFIX/lib/libpython3.8.dylib
             -DPYTHON_INCLUDE_DIR=$PYPREFIX/include/python3.8"

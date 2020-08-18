1e07f791913848674cefd3e1df74a88a701499ce
Stine Vennemo <stine.vennemo@gmail.com>
2020-08-18 11:00:29 +0200

    Line length

--- pynest/nest/lib/hl_api_info.py
+++ pynest/nest/lib/hl_api_info.py
@@ -277,10 +277,10 @@ def SetStatus(nodes, params, val=None):
     set_status_local_nodes = set_status_nodes and nodes.get('local')
 
     if (params_is_dict and set_status_local_nodes):
-        
+
         node_params = n0.get()
-        contains_list = [is_iterable(vals) and key in node_params and not is_iterable(node_params[key]) for key, vals in params.items()]
-        #contains_list = [is_iterable(vals) and not is_iterable(node_params[key]) for key, vals in params.items()]
+        contains_list = [is_iterable(vals) and key in node_params and not is_iterable(node_params[key]) for
+                         key, vals in params.items()]
 
         if any(contains_list):
             temp_param = [{} for _ in range(len(nodes))]
--- pynest/nest/lib/hl_api_types.py
+++ pynest/nest/lib/hl_api_types.py
@@ -402,7 +402,8 @@ class NodeCollection(object):
         if isinstance(params, dict) and self[0].get('local'):
 
             node_params = self[0].get()
-            contains_list = [is_iterable(vals) and key in node_params and not is_iterable(node_params[key]) for key, vals in params.items()]
+            contains_list = [is_iterable(vals) and key in node_params and not is_iterable(node_params[key]) for
+                             key, vals in params.items()]
 
             if any(contains_list):
                 temp_param = [{} for _ in range(self.__len__())]
@@ -736,7 +737,8 @@ class SynapseCollection(object):
 
         if isinstance(params, dict):
             node_params = self[0].get()
-            contains_list = [is_iterable(vals) and key in node_params and not is_iterable(node_params[key]) for key, vals in params.items()]
+            contains_list = [is_iterable(vals) and key in node_params and not is_iterable(node_params[key]) for
+                             key, vals in params.items()]
 
             if any(contains_list):
                 temp_param = [{} for _ in range(self.__len__())]

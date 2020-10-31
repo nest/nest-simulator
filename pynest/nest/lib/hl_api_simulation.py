c4cae3d04cdff1949f700e8686d1a91a0c513ad2
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2020-10-31 16:31:31 +0100

    add dump and load code

--- pynest/nest/lib/hl_api_simulation.py
+++ pynest/nest/lib/hl_api_simulation.py
@@ -27,13 +27,16 @@ from contextlib import contextmanager
 
 from ..ll_api import *
 from .hl_api_helper import *
+from typing import Dict, List
 
 __all__ = [
     'Cleanup',
+    'Dump',
     'DisableStructuralPlasticity',
     'EnableStructuralPlasticity',
     'GetKernelStatus',
     'Install',
+    'Load',
     'Prepare',
     'ResetKernel',
     'Run',
@@ -460,3 +463,109 @@ def DisableStructuralPlasticity():
 
     """
     sr('DisableStructuralPlasticity')
+
+@check_stack
+def Dump(selections: List = ("nodes", "synapses")) -> Dict:
+    """Returns a dictionary containing the current net in memory for serialization.
+
+    Only a subset of the data can be serialized.
+
+
+    Parameters
+    ----------
+    selections : List[strings], optional
+        Only obtain a subset of the net parameters. Options are "nodes" and/or "synapses"
+
+
+    Returns
+    -------
+    Dict:
+        Object containing the specified serialization sorted by dict key likes "nodes" or "synapses"
+
+
+    See Also
+    ---------
+
+    Load : Function to construct the net from the data.
+
+    """
+
+    sli_func("({}) M_WARNING message".format("Dumping is currently only supported for a single process."))
+    dumpdata = dict()
+    numnetwork = GetKernelStatus("network_size")
+
+    # nothing added
+    if numnetwork < 2:
+        return dumpdata
+
+    nodes = NodeCollection(range(1, numnetwork + 1))
+    if "nodes" in selections:
+        dumpdata["nodes"] = GetStatus(nodes)
+
+    syn_ids = GetConnections(source=nodes)
+    if "synapses" in selections:
+        dumpdata["synapses"] = GetStatus(syn_ids)
+    return dumpdata
+    
+@check_stack
+def Load(data: Dict) -> Dict:
+    """
+    Loads a dictionary obtained by the dump method.
+
+    Repeated loading will add to the network size. To overwrite clear with a call to resetnetwork.
+    To directly overwrite the binary memory state a lower level access needs to be developed in the future.
+
+    Parameters
+    ----------
+    data: Dictionary
+        the data to be loaded.
+
+    Returns
+    -------
+    Dict:
+        the created nest obejcts and all synapses
+
+    See Also
+    ---------
+
+    Dump : Function to obtain a structured memory dump.
+    """
+    created = dict()
+    sli_func("({}) M_WARNING message".format("Loading is currently only supported for a single process."))
+    if "nodes" in data:
+        dictmissbefore = GetKernelStatus({"dict_miss_is_error"})[0]
+        SetKernelStatus({"dict_miss_is_error": False})
+        verbose = get_verbosity()
+        set_verbosity("M_ERROR")
+        newnodes = NodeCollection()
+        try:
+            for d in data["nodes"]:
+                newnodes += Create(d["model"], d)
+        finally:
+            SetKernelStatus({"dict_miss_is_error": dictmissbefore})
+            # restore verbosity level
+            set_verbosity(verbose)
+
+        created["nodes"] = newnodes
+
+    if "synapses" in data:
+        try:
+            for conn in data["synapses"]:
+                source, target = conn["source"], conn["target"]
+                # remove unused, copy to prevent side effects
+                specs = copy.copy(conn)
+                specs.pop("port")
+                specs.pop("receptor")
+                specs.pop("synapse_id")
+                specs.pop("target_thread")
+                specs.pop("source")
+                specs.pop("target")
+                Connect([source], [target], syn_spec=specs)
+        except Exception as e:
+            print("Error during synapse loading:" + str(e))
+
+        #we only want the newly created connections, but Connect does not return a SynapseCollection object
+        created["synapses"] = GetConnections()
+
+    return created
+

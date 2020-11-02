175c534afafed75ce74cf2dcd5fea741e903a0d9
YounesBouhadjar <younes.bouhadjy@gmail.com>
2020-11-02 17:09:29 +0100

    minor changes: correction and improvement of documentation

--- pynest/examples/twoneurons.py
+++ pynest/examples/twoneurons.py
@@ -22,8 +22,9 @@
 """Two neuron example
 ----------------------------
 
-This script simulates two connected pre and postsynaptic neurons. The presynaptic neuron receives a constant external current
-and records its membrane potential.
+This script simulates two connected pre and postsynaptic neurons. 
+The presynaptic neuron receives a constant external current.
+And, the membrane potential of both neurons are recorded.
 
 See Also
 ~~~~~~~~
@@ -32,9 +33,8 @@ See Also
 
 """
 
-#######################################################################
-# First, we import all necessary modules for simulation, analysis and
-# plotting.
+###############################################################################
+# First, we import all necessary modules for simulation, analysis and plotting.
 
 import nest
 import nest.voltage_trace
@@ -43,21 +43,21 @@ import matplotlib.pyplot as plt
 nest.set_verbosity("M_WARNING")
 nest.ResetKernel()
 
-#######################################################################
-# Second, the nodes (the two neurons and devices) are created using ``Create``.
+###############################################################################
+# Second, we create the two neurons and the recording device.
 
 neuron_1 = nest.Create("iaf_psc_alpha")
 neuron_2 = nest.Create("iaf_psc_alpha")
 voltmeter = nest.Create("voltmeter")
 
-#######################################################################
-# Third, we set the external current of neuron 1
+###############################################################################
+# Third, we set the external current of neuron 1.
 
 neuron_1.I_e = 376.0
 
-#######################################################################
+###############################################################################
 # Fourth, we connect neuron 1 to neuron 2. 
-# And, then we connect a voltmeter to the two neurons. The command
+# And, then we connect a voltmeter to the two neurons.
 # To learn more about the previous steps, please check out the
 # :doc:`one neuron example <one_neuron>`.
 
@@ -68,13 +68,13 @@ nest.Connect(neuron_1, neuron_2, syn_spec={'weight': weight, 'delay': delay})
 nest.Connect(voltmeter, neuron_1)
 nest.Connect(voltmeter, neuron_2)
 
-#######################################################################
+###############################################################################
 # Now we simulate the network using ``Simulate``, which takes the
 # desired simulation time in milliseconds.
 
 nest.Simulate(1000.0)
 
-#######################################################################
+###############################################################################
 # Finally, we plot the neurons's membrane potential as a function of
 # time.
 

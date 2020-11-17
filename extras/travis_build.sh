fb076d1f5ff7742cade55b4fb04daf3e75235f09
lekshmideepu <lekshmideepu@users.noreply.github.com>
2020-11-17 21:43:47 +0100

    Update extras/travis_build.sh
    Co-authored-by: Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>

--- extras/travis_build.sh
+++ extras/travis_build.sh
@@ -36,7 +36,7 @@ if [[ $OSTYPE = darwin* ]]; then
     export CC=$(ls /usr/local/bin/gcc-* | grep '^/usr/local/bin/gcc-\d$')
     export CXX=$(ls /usr/local/bin/g++-* | grep '^/usr/local/bin/g++-\d$')
     #Ensure that nosetests path can be found
-    export PATH=$HOME/.local/bin:$PATH
+    export PATH=/Users/travis/Library/Python/3.9/bin:$PATH
 fi
 
 if [ "$xNEST_BUILD_COMPILER" = "CLANG" ]; then

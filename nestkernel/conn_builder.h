de2d77377bb1dcf7a03195fbc1c4891c1d75d5c3
Stine Vennemo <stine.vennemo@gmail.com>
2021-01-13 14:39:56 +0100

    Fixes for conngen

--- nestkernel/conn_builder.h
+++ nestkernel/conn_builder.h
@@ -211,6 +211,9 @@ protected:
 
   std::vector< index > synapse_model_id_;
 
+  //! dictionaries to pass to connect function, one per thread for every syn_spec
+  std::vector< std::vector< DictionaryDatum > > param_dicts_;
+
 private:
   typedef std::map< Name, ConnParameter* > ConnParameterMap;
 
@@ -229,9 +232,6 @@ private:
 
   //! all other parameters, mapping name to value representation
   std::vector< ConnParameterMap > synapse_params_;
-
-  //! dictionaries to pass to connect function, one per thread for every syn_spec
-  std::vector< std::vector< DictionaryDatum > > param_dicts_;
 
   //! synapse-specific parameters that should be skipped when we set default synapse parameters
   std::set< Name > skip_syn_params_;

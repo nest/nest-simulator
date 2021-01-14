16dc7a97e6ce4178fbc2c0794e3df0354d172ed1
Stine Vennemo <stine.vennemo@gmail.com>
2021-01-14 08:09:28 +0100

    create_param_dict_ has changed name

--- nestkernel/conn_builder_conngen.cpp
+++ nestkernel/conn_builder_conngen.cpp
@@ -118,7 +118,7 @@ ConnectionGeneratorBuilder::connect_()
       Node* target_node = kernel().node_manager.get_node_or_proxy( ( *targets_ )[ target ] );
       const thread target_thread = target_node->get_thread();
 
-      create_param_dict_( ( *sources_ )[ source ], *target_node, target_thread, rng, 0 );
+      update_param_dict_( ( *sources_ )[ source ], *target_node, target_thread, rng, 0 );
 
       // Use the low-level connect() here, as we need to pass a custom weight and delay
       kernel().connection_manager.connect( ( *sources_ )[ source ],

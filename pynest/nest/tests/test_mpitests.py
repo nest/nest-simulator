452e23e396937a95d5e32d79c38978bfb20877d4
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2021-02-03 00:20:33 +0100

    Local VPs can now be requested by GetLocalVPs(). Proper testing required some reorganisation of MPI-dependent Python tests.

--- pynest/nest/tests/test_mpitests.py
+++ pynest/nest/tests/test_mpitests.py
@@ -27,27 +27,28 @@ from subprocess import call
 HAVE_MPI = nest.ll_api.sli_func("statusdict/have_mpi ::")
 
 
-class TestStructuralPlasticityMPI(unittest.TestCase):
+class TestMPIDependentTests(unittest.TestCase):
 
     @unittest.skipIf(not HAVE_MPI, 'NEST was compiled without MPI')
-    def testWithMPI(self):
+    def testsWithMPI(self):
         if HAVE_MPI:
             failing_tests = []
-            mpitests = ["mpitest_issue_578_sp.py"]
+            mpitests = [('mpitest_get_local_vps.py', 4),
+                        ('test_sp/mpitest_issue_578_sp.py', 2)]
             path = os.path.dirname(__file__)
 
-            for test in mpitests:
+            for test, num_procs in mpitests:
                 test = os.path.join(path, test)
-                command = nest.ll_api.sli_func("mpirun", 2, "nosetests", test)
+                command = nest.ll_api.sli_func("mpirun", num_procs, "nosetests", test)
                 print("Executing test with command: " + command)
                 command = command.split()
                 my_env = os.environ.copy()
                 returncode = call(command, env=my_env)
                 if returncode != 0:  # call returns 0 for passing tests
-                    failing_tests.append(test)
+                    failing_tests.append((test, num_procs))
 
             self.assertTrue(not failing_tests, 'The following tests failed ' +
-                            'when executing with "mpirun -np 2 nosetests ' +
+                            'when executing with "mpirun -np N nosetests ' +
                             '[script]": {}'.format(failing_tests))
 
 

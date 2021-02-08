20942d0dbdde42db9e4883fe2e51423541b33b8d
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2021-02-08 16:10:32 +0100

    Fixed whitespace and figure file name

--- pynest/examples/store_restore_network.py
+++ pynest/examples/store_restore_network.py
@@ -239,7 +239,7 @@ class DemoPlot:
 if __name__ == '__main__':
 
     plt.ion()
-    
+
     T_sim = 1000
 
     dplot = DemoPlot()
@@ -291,7 +291,7 @@ if __name__ == '__main__':
     nest.Simulate(T_sim)
     dplot.add_to_plot(ein2.sr, lbl='Reloaded simulation (different seed)')
 
-    dplot.fig.savefig('store_load_demo.png')
+    dplot.fig.savefig('store_restore_network.png')
 
     input('Press ENTER to close figure!')
 

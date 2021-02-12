8bafeaa78566bd6c582cafef38e946fb0d0d0e85
Håkon Mørk <hakon.mork@nmbu.no>
2021-02-12 15:44:45 +0100

    Added test_random123 to test_all script

--- pynest/nest/tests/test_all.py
+++ pynest/nest/tests/test_all.py
@@ -54,6 +54,7 @@ from . import test_parrot_neuron
 from . import test_pp_psc_delta
 from . import test_pp_psc_delta_stdp
 from . import test_quantal_stp_synapse
+from . import test_random123
 from . import test_random_parameter
 from . import test_rate_copy_model
 from . import test_rate_instantaneous_and_delayed
@@ -109,6 +110,7 @@ def suite():
     suite.addTest(test_pp_psc_delta.suite())
     suite.addTest(test_pp_psc_delta_stdp.suite())
     suite.addTest(test_quantal_stp_synapse.suite())
+    suite.addTest(test_random123.suite())
     suite.addTest(test_random_parameter.suite())
     suite.addTest(test_rate_copy_model.suite())
     suite.addTest(test_rate_instantaneous_and_delayed.suite())

a415acf7a5fca35341281568f0fd373d13acafb7
Stine Vennemo <stine.vennemo@gmail.com>
2021-03-02 09:44:52 +0100

    Need to avoid rounding errors.

--- nestkernel/nest.cpp
+++ nestkernel/nest.cpp
@@ -302,7 +302,7 @@ connect_arrays( long* sources,
           {
             const auto int_cast_rtype = static_cast< size_t >( *param );
 
-            if ( int_cast_rtype != *param )
+            if ( int_cast_rtype - *param > 1e-20 ) // To avoid rounding errors
             {
               throw BadParameter( "Receptor types must be integers." );
             }

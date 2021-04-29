76ae82aef2a666abe77db84ac62bf9a77259e898
Jonas Stapmanns <j.stapmanns@fz-juelich.de>
2021-04-29 16:46:26 +0200

    Incorporated requested changes.

--- models/urbanczik_synapse.h
+++ models/urbanczik_synapse.h
@@ -56,10 +56,10 @@ urbanczik_synapse is a connector to create Urbanczik synapses as defined in
 depends on the postsynaptic dendritic potential, in addition to the pre- and
 postsynaptic spike timing.
 
-Urbanczik synapses require archiving of continuous quantities. Therefore they
-can only be connected to neuron models that are capable of doing this
-archiving. So far, the only compatible model is :doc:`pp_cond_exp_mc_urbanczik
-<pp_cond_exp_mc_urbanczik>`.
+Urbanczik synapses require the archiving of the dendritic membrane potential 
+which is continuous in time. Therefore they can only be connected to neuron
+models that are capable of doing this archiving. So far, the only compatible
+model is :doc:`pp_cond_exp_mc_urbanczik <pp_cond_exp_mc_urbanczik>`.
 
 Parameters
 ++++++++++

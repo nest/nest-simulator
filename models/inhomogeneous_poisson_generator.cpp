bf74873eb022cef20268cdeb5fbfa06cecaec120
lionel kusch <lionel.kusch@grenoble-inp.org>
2021-05-06 17:46:43 +0200

    Update models/inhomogeneous_poisson_generator.cpp
    modification comments
    
    Co-authored-by: Jochen Martin Eppler <jougs@gmx.net>

--- models/inhomogeneous_poisson_generator.cpp
+++ models/inhomogeneous_poisson_generator.cpp
@@ -304,8 +304,7 @@ nest::inhomogeneous_poisson_generator::set_data_from_stimulating_backend( std::v
     if ( rate_time_update.size() % 2 != 0 )
     {
       throw BadParameterValue(
-        "The size of the data for the inhomogeneous_poisson_generator needs to be even "
-        "[ sequence of the couple (time,rate) ]" );
+        "The size of the data for the inhomogeneous_poisson_generator needs to be even [(time,rate) pairs]" );
     }
     DictionaryDatum d = DictionaryDatum( new Dictionary );
     std::vector< double > times_ms;

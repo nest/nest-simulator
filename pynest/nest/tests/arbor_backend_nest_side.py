c1e92dda69952464b3f5e20c418cc37f3b29f9c8
Håkon Mørk <hakon.mork@nmbu.no>
2021-06-14 14:13:33 +0200

    Added copyright headers

--- pynest/nest/tests/arbor_backend_nest_side.py
+++ pynest/nest/tests/arbor_backend_nest_side.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# arbor_backend_nest_side.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
 # This script runs NEST and transmits spikes via the Arbor backend.
 # Spikes are also stored to file for testing.
 # It requires NESTIO + ARBOR-NESTIO.

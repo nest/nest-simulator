e2d4f13885f73fe691b6df5d41297e6dc75c2a79
Robin De Schepper <robin.deschepper93@gmail.com>
2021-06-16 18:10:39 +0200

    Tucked `PYEXECDIR` in a postprocessing function.

--- cmake/ProcessOptions.cmake
+++ cmake/ProcessOptions.cmake
@@ -438,6 +438,12 @@ function( NEST_PROCESS_WITH_PYTHON )
   endif ()
 endfunction()
 
+function( NEST_POST_PROCESS_WITH_PYTHON )
+  if ( Python_FOUND )
+    set( PYEXECDIR "${CMAKE_INSTALL_LIBDIR}/python${Python_VERSION_MAJOR}.${Python_VERSION_MINOR}/site-packages" PARENT_SCOPE )
+  endif()
+endfunction()
+
 function( NEST_PROCESS_WITH_OPENMP )
   # Find OPENMP
   if ( with-openmp )

724bf45a3c54d34ad57065eebb78c9b8dc8b4330
Mahdi Enan <mahdi.cie@gmail.com>
2021-06-28 15:04:23 +0200

    set glossary relative to base url. (fix attempt!)

--- doc/userdoc/_ext/HoverXTooltip.py
+++ doc/userdoc/_ext/HoverXTooltip.py
@@ -125,11 +125,11 @@ def hxt_role_ref(pattern):
 
         branch_name = base_url.split('/doc/')[0].split('/')[-1]
 
-        refuri = (f'/{language}/glossary.html#term-{term}')
+        # refuri = (f'/{language}/glossary.html#term-{term}')
 
         # the tag in which the term and description is defined.
         ref_tag = "<a class='reference external' " \
-                  "href='{refuri}'>" \
+                  "href='test'>" \
                   "<span class='hoverxtool hoverxtool_highlighter' " \
                   "data-toggle='tooltip' " \
                   "data-placement='top' title='{desc}'>" \

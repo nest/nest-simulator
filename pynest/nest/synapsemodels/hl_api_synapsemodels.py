dc0058fe21a9fd3894bbe8c18172fc8c1d669bf2
Stine Vennemo <stine.vennemo@gmail.com>
2021-07-06 08:49:14 +0200

    Add synapse models

--- pynest/nest/synapsemodels/hl_api_synapsemodels.py
+++ pynest/nest/synapsemodels/hl_api_synapsemodels.py
@@ -25,34 +25,80 @@ Synapse models
 
 __all__ = [
     'bernoulli',
+    'bernoulli_hpc',
+    'bernoulli_lbl',
     'clopath',
+    'clopath_hpc',
+    'clopath_lbl',
     'cont_delay',
+    'cont_delay_hpc',
+    'cont_delay_lbl',
     'copy_synapse_class',
     'diffusion',
+    'diffusion_lbl',
     'gap_junction',
+    'gap_junction_lbl',
     'ht',
+    'ht_hpc',
+    'ht_lbl',
     'jonke',
+    'jonke_hpc',
+    'jonke_lbl',
     'quantal_stp',
+    'quantal_stp_hpc',
+    'quantal_stp_lbl',
     'rate_connection_delayed',
+    'rate_connection_delayed_lbl',
     'rate_connection_instantaneous',
+    'rate_connection_instantaneous_lbl',
     'static',
     'static_hom_w',
+    'static_hom_w_hpc',
+    'static_hom_w_lbl',
     'static_hpc',
+    'static_lbl',
+    'stdp',
     'stdp_dopamine',
+    'stdp_dopamine_hpc',
+    'stdp_dopamine_lbl',
+    'stdp_facetshw_hom',
+    'stdp_facetshw_hom_hpc',
+    'stdp_facetshw_hom_lbl',
+    'stdp_hom',
+    'stdp_hom_hpc',
+    'stdp_hom_lbl',
+    'stdp_hpc',
+    'stdp_lbl',
     'stdp_nn_pre_centered',
+    'stdp_nn_pre_centered_hpc',
+    'stdp_nn_pre_centered_lbl',
     'stdp_nn_restr',
+    'stdp_nn_restr_hpc',
+    'stdp_nn_restr_lbl',
     'stdp_nn_symm',
+    'stdp_nn_symm_hpc',
+    'stdp_nn_symm_lbl',
     'stdp_pl_hom',
     'stdp_pl_hom_hpc',
-    'stdp',
-    'stdp_facetshw_hom',
-    'stdp_hom',
+    'stdp_pl_hom_lbl'
     'stdp_triplet',
+    'stdp_triplet_hpc',
+    'stdp_triplet_lbl',
     'tsodyks2',
+    'tsodyks2_hpc',
+    'tsodyks2_lbl'
     'tsodyks',
     'tsodyks_hom',
+    'tsodyks_hom_hpc',
+    'tsodyks_hom_lbl',
+    'tsodyks_hpc',
+    'tsodyks_lbl',
     'urbanczik',
+    'urbanczik_hpc',
+    'urbanczik_lbl',
     'vogels_sprekeler',
+    'vogels_sprekeler_hpc',
+    'vogels_sprekeler_lbl',
     'SynapseModel'
 ]
 
@@ -92,9 +138,29 @@ class bernoulli(SynapseModel):
         super().__init__('bernoulli_synapse', **kwargs)
 
 
+class bernoulli_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('bernoulli_synapse_hpc', **kwargs)
+
+
+class bernoulli_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('bernoulli_synapse_lbl', **kwargs)
+
+
 class clopath(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('clopath_synapse', **kwargs)
+
+
+class clopath_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('clopath_synapse_hpc', **kwargs)
+
+
+class clopath_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('clopath_synapse_lbl', **kwargs)
 
 
 class cont_delay(SynapseModel):
@@ -102,9 +168,24 @@ class cont_delay(SynapseModel):
         super().__init__('cont_delay_synapse', **kwargs)
 
 
+class cont_delay_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('cont_delay_synapse_hpc', **kwargs)
+
+
+class cont_delay_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('cont_delay_synapse_lbl', **kwargs)
+
+
 class diffusion(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('diffusion_connection', **kwargs)
+
+
+class diffusion_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('diffusion_connection_lbl', **kwargs)
 
 
 class gap_junction(SynapseModel):
@@ -112,9 +193,24 @@ class gap_junction(SynapseModel):
         super().__init__('gab_junction', **kwargs)
 
 
+class gap_junction_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('gab_junction_lbl', **kwargs)
+
+
 class ht(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('ht_synapse', **kwargs)
+
+
+class ht_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('ht_synapse_hpc', **kwargs)
+
+
+class ht_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('ht_synapse_lbl', **kwargs)
 
 
 class jonke(SynapseModel):
@@ -122,9 +218,29 @@ class jonke(SynapseModel):
         super().__init__('jonke_synapse', **kwargs)
 
 
+class jonke_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('jonke_synapse_hpc', **kwargs)
+
+
+class jonke_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('jonke_synapse_lbl', **kwargs)
+
+
 class quantal_stp(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('quantal_stp_synapse', **kwargs)
+
+
+class quantal_stp_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('quantal_stp_synapse_hpc', **kwargs)
+
+
+class quantal_stp_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('quantal_stp_synapse_lbl', **kwargs)
 
 
 class rate_connection_delayed(SynapseModel):
@@ -132,9 +248,19 @@ class rate_connection_delayed(SynapseModel):
         super().__init__('rate_connection_delayed', **kwargs)
 
 
+class rate_connection_delayed_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('rate_connection_delayed_lbl', **kwargs)
+
+
 class rate_connection_instantaneous(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('rate_connection_instantaneous', **kwargs)
+
+
+class rate_connection_instantaneous_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('rate_connection_instantaneous_lbl', **kwargs)
 
 
 class static(SynapseModel):
@@ -147,9 +273,29 @@ class static_hom_w(SynapseModel):
         super().__init__('static_synapse_hom_w', **kwargs)
 
 
+class static_hom_w_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('static_synapse_hom_w_hpc', **kwargs)
+
+
+class static_hom_w_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('static_synapse_hom_w_lbl', **kwargs)
+
+
 class static_hpc(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('static_synapse_hpc', **kwargs)
+
+
+class static_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('static_synapse_lbl', **kwargs)
+
+
+class stdp(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_synapse', **kwargs)
 
 
 class stdp_dopamine(SynapseModel):
@@ -157,9 +303,69 @@ class stdp_dopamine(SynapseModel):
         super().__init__('stdp_dopamine_synapse', **kwargs)
 
 
+class stdp_dopamine_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_dopamine_synapse_hpc', **kwargs)
+
+
+class stdp_dopamine_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_dopamine_synapse_lbl', **kwargs)
+
+
+class stdp_facetshw_hom(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_facetshw_synapse_hom', **kwargs)
+
+
+class stdp_facetshw_hom_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_facetshw_synapse_hom_hpc', **kwargs)
+
+
+class stdp_facetshw_hom_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_facetshw_synapse_hom_lbl', **kwargs)
+
+
+class stdp_hom(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_synapse_hom', **kwargs)
+
+
+class stdp_hom_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_synapse_hom_hpc', **kwargs)
+
+
+class stdp_hom_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_synapse_hom_lbl', **kwargs)
+
+
+class stdp_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_synapse_hpc', **kwargs)
+
+
+class stdp_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_synapse_lbl', **kwargs)
+
+
 class stdp_nn_pre_centered(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('stdp_nn_pre_centered_synapse', **kwargs)
+
+
+class stdp_nn_pre_centered_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_nn_pre_centered_synapse_hpc', **kwargs)
+
+
+class stdp_nn_pre_centered_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_nn_pre_centered_synapse_lbl', **kwargs)
 
 
 class stdp_nn_restr(SynapseModel):
@@ -167,9 +373,29 @@ class stdp_nn_restr(SynapseModel):
         super().__init__('stdp_nn_restr_synapse', **kwargs)
 
 
+class stdp_nn_restr_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_nn_restr_synapse_hpc', **kwargs)
+
+
+class stdp_nn_restr_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_nn_restr_synapse_lbl', **kwargs)
+
+
 class stdp_nn_symm(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('stdp_nn_symm_synapse', **kwargs)
+
+
+class stdp_nn_symm_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_nn_symm_synapse_hpc', **kwargs)
+
+
+class stdp_nn_symm_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_nn_symm_synapse_lbl', **kwargs)
 
 
 class stdp_pl_hom(SynapseModel):
@@ -182,19 +408,9 @@ class stdp_pl_hom_hpc(SynapseModel):
         super().__init__('stdp_pl_synapse_hom_hpc', **kwargs)
 
 
-class stdp(SynapseModel):
+class stdp_pl_hom_lbl(SynapseModel):
     def __init__(self, **kwargs):
-        super().__init__('stdp_synapse', **kwargs)
-
-
-class stdp_facetshw_hom(SynapseModel):
-    def __init__(self, **kwargs):
-        super().__init__('stdp_facetshw_synapse_hom', **kwargs)
-
-
-class stdp_hom(SynapseModel):
-    def __init__(self, **kwargs):
-        super().__init__('stdp_synapse_hom', **kwargs)
+        super().__init__('stdp_pl_synapse_hom_lbl', **kwargs)
 
 
 class stdp_triplet(SynapseModel):
@@ -202,9 +418,29 @@ class stdp_triplet(SynapseModel):
         super().__init__('stdp_triplet_synapse', **kwargs)
 
 
+class stdp_triplet_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_triplet_synapse_hpc', **kwargs)
+
+
+class stdp_triplet_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('stdp_triplet_synapse_lbl', **kwargs)
+
+
 class tsodyks2(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('tsodyks2_synapse', **kwargs)
+
+
+class tsodyks2_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('tsodyks2_synapse_hpc', **kwargs)
+
+
+class tsodyks2_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('tsodyks2_synapse_lbl', **kwargs)
 
 
 class tsodyks(SynapseModel):
@@ -217,11 +453,51 @@ class tsodyks_hom(SynapseModel):
         super().__init__('tsodyks_synapse_hom', **kwargs)
 
 
+class tsodyks_hom_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('tsodyks_synapse_hom_hpc', **kwargs)
+
+
+class tsodyks_hom_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('tsodyks_synapse_hom_lbl', **kwargs)
+
+
+class tsodyks_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('tsodyks_synapse_hpc', **kwargs)
+
+
+class tsodyks_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('tsodyks_synapse_lbl', **kwargs)
+
+
 class urbanczik(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('urbanczik_synapse', **kwargs)
 
 
+class urbanczik_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('urbanczik_synapse_hpc', **kwargs)
+
+
+class urbanczik_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('urbanczik_synapse_lbl', **kwargs)
+
+
 class vogels_sprekeler(SynapseModel):
     def __init__(self, **kwargs):
         super().__init__('vogels_sprekeler_synapse', **kwargs)
+
+
+class vogels_sprekeler_hpc(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('vogels_sprekeler_synapse_hpc', **kwargs)
+
+
+class vogels_sprekeler_lbl(SynapseModel):
+    def __init__(self, **kwargs):
+        super().__init__('vogels_sprekeler_synapse_lbl', **kwargs)

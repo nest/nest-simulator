a25e1b999950ce7b0e317d4669c2992caa0ff49f
Robin De Schepper <robin.deschepper93gmail.com>
2021-09-23 22:05:13 +0200

    Build wheel protocol to be executed by CI

---
+++ extras/wheelbuild/wheelbuild.sh
@@ -0,0 +1,9 @@
+BUILDPATH=build
+BUILDWHEELPATH=build_wheel_env
+mkdir $BUILDPATH
+cd $BUILDPATH
+cmake ..
+cd ..
+python extras/wheelbuild/prep_wheel_env.py $BUILDWHEELPATH
+export NEST_CMAKE_WHEELBUILD=wheelbuild
+cd $BUILDWHEELPATH

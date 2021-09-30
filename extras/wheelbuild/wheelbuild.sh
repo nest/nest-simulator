294712a50db6a0fde10e8b73f5aa4460eeb70fe5
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-30 10:38:43 +0200

    set env var before py script

--- extras/wheelbuild/wheelbuild.sh
+++ extras/wheelbuild/wheelbuild.sh
@@ -4,7 +4,7 @@ mkdir $BUILDPATH
 cd $BUILDPATH
 cmake ..
 cd ..
-python extras/wheelbuild/prep_wheel_env.py $BUILDWHEELPATH
 export NEST_CMAKE_BUILDWHEEL=ON
+python extras/wheelbuild/prep_wheel_env.py $BUILDWHEELPATH
 cd $BUILDWHEELPATH
 python setup.py bdist_wheel

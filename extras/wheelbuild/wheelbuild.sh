5495e5b96046cb61098287790f32cbf0027771c1
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-30 17:11:35 +0200

    Set install prefix for tmp wheel env

--- extras/wheelbuild/wheelbuild.sh
+++ extras/wheelbuild/wheelbuild.sh
@@ -2,9 +2,10 @@ BUILDPATH=build
 BUILDWHEELPATH=build_wheel_env
 mkdir $BUILDPATH
 cd $BUILDPATH
-cmake ..
+cmake .. -DCMAKE_INSTALL_PREFIX=tmp_build_wheel_env
 make install-nodoc
 cd ..
 export NEST_CMAKE_BUILDWHEEL=ON
 python extras/wheelbuild/prep_wheel_env.py $BUILDWHEELPATH
+rm -rf tmp_build_wheel_env
 cd $BUILDWHEELPATH

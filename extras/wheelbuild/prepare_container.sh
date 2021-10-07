ff5bf8f3e1ca5ebc9f7bb2f52f9aab4ede790dfa
Robin De Schepper <robin.deschepper93@gmail.com>
2021-10-07 14:55:36 +0200

    Only 64 bit wheels(No OpenMP on 32 bit manylinux image compilers?)

--- extras/wheelbuild/prepare_container.sh
+++
@@ -1,4 +0,0 @@
-echo "Running a build, can we figure out which one?"
-env
-echo "Yay, \`python\` is the one being built!"
-python -c "import sysconfig; import pprint; pprint.pprint(sysconfig.get_paths())"

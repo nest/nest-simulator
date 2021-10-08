f5808cc50056923374799f88f94b7aad500a72dc
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2021-10-08 18:48:59 +0200

    Tidy up startup, especially opening of sli-init.sli.

--- sli/slistartup.h
+++ sli/slistartup.h
@@ -59,18 +59,15 @@
 
 class SLIStartup : public SLIModule
 {
-  const std::string startupfilename;
-  const std::string slilibpath;
-  std::string slilibdir;
-  std::string slidocdir;
-  std::string sliprefix;
+  const std::string sliprefix;
+  const std::string slilibdir;
+  const std::string slidocdir;
+  const std::string startupfile;
 
-  std::string locateSLIInstallationPath( void );
-  bool checkpath( std::string const&, std::string& ) const;
+  std::string find_startup_file( const std::string& ) const;
   std::string getenv( const std::string& ) const;
-  std::string checkenvpath( std::string const&, SLIInterpreter*, std::string ) const;
 
-  Token targs;
+  Token commandline_args_;
   int verbosity_;
   bool debug_;
 

c38cf7ca6850ef85b82997c1b87479288f13c7c4
ayssar:wsl:ubuntu <med.ayssar@gmail.com>
2021-10-20 21:18:09 +0200

    remove unnecessary lines

--- cmake/ColorMessages.cmake
+++ cmake/ColorMessages.cmake
@@ -1,4 +1,4 @@
-# Utils.cmake
+# ColorMessages.cmake
 #
 # This file is part of NEST.
 #
@@ -23,22 +23,22 @@
 
 
 string(ASCII 27 Esc)
-set(Reset 		"${Esc}[m")
-set(Bold  		"${Esc}[1m")
-set(Red         	"${Esc}[31m")
-set(Green       	"${Esc}[32m")
-set(Blue        	"${Esc}[34m")
-set(Cyan        	"${Esc}[36m")
-set(Magenta     	"${Esc}[35m")
-set(Yellow      	"${Esc}[33m")
-set(White       	"${Esc}[37m")
-set(BoldRed     	"${Esc}[1;31m")
-set(BoldGreen   	"${Esc}[1;32m")
-set(BoldBlue    	"${Esc}[1;34m")
-set(BoldCyan    	"${Esc}[1;36m")
-set(BoldMagenta 	"${Esc}[1;35m")
-set(BoldYellow  	"${Esc}[1;33m")
-set(BoldWhite   	"${Esc}[1;37m")
+set(Reset "${Esc}[m")
+set(Bold "${Esc}[1m")
+set(Red "${Esc}[31m")
+set(Green "${Esc}[32m")
+set(Blue "${Esc}[34m")
+set(Cya "${Esc}[36m")
+set(Magenta "${Esc}[35m")
+set(Yellow "${Esc}[33m")
+set(White "${Esc}[37m")
+set(BoldRed "${Esc}[1;31m")
+set(BoldGreen "${Esc}[1;32m")
+set(BoldBlue "${Esc}[1;34m")
+set(BoldCyan "${Esc}[1;36m")
+set(BoldMagenta "${Esc}[1;35m")
+set(BoldYellow "${Esc}[1;33m")
+set(BoldWhite "${Esc}[1;37m")
 
 function(print)      
     set(option HAS_COLOR)

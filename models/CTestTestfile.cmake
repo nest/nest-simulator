133e199646498472f1fe90a7206186896a71439e
Willem Wybo <willem.a.m.wybo@gmail.com>
2021-11-26 17:23:43 +0100

    deleted superfluous file

--- models/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/wybo/Code/NEST/nest-simulator/models
-# Build directory: /Users/wybo/Code/NEST/nest-simulator/models
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

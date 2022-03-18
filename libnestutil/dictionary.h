b406fe6dbb93a86f4239758c99c73b80470ad0b4
Håkon Mørk <hakon.mork@nmbu.no>
2022-03-18 14:40:37 +0100

    Added documentation on the dictionary class

--- libnestutil/dictionary.h
+++ libnestutil/dictionary.h
@@ -26,11 +26,16 @@
 #include <boost/any.hpp>
 #include <map>
 #include <string>
-#include <unordered_set>
 #include <vector>
 
 #include "sliexceptions.h"
 
+/**
+ * @brief Get the typename of the operand.
+ *
+ * @param operand to get the typename of.
+ * @return std::string of the typename.
+ */
 std::string debug_type( const boost::any& operand );
 
 bool is_int( const boost::any& operand );
@@ -48,8 +53,20 @@ bool is_dict( const boost::any& operand );
 bool is_parameter( const boost::any& operand );
 bool is_nc( const boost::any& operand );
 
+/**
+ * @brief Check whether two boost::any values are equal.
+ *
+ * @param first The first value.
+ * @param second The other value.
+ * @return Whether the values are equal, both in type and value.
+ */
 bool value_equal( const boost::any first, const boost::any second );
 
+/**
+ * @brief A Python-like dictionary, based on std::map.
+ *
+ * Values are stored as boost::any objects, with std::string keys.
+ */
 class dictionary : public std::map< std::string, boost::any >
 {
   // TODO-PYNEST-NG: Meta-information about entries:
@@ -59,6 +76,15 @@ class dictionary : public std::map< std::string, boost::any >
 private:
   using maptype_ = std::map< std::string, boost::any >;
 
+  /**
+   * @brief Cast the specified value to the specified type.
+   *
+   * @tparam T Type of element. If the value is not of the specified type, a TypeMismatch error is thrown.
+   * @param value the any object to cast.
+   * @param key key where the value is located in the dictionary, for information upon cast errors.
+   * @throws TypeMismatch if the value is not of specified type T.
+   * @return value cast to the specified type.
+   */
   template < typename T >
   T
   cast_value_( const boost::any& value, const std::string& key ) const
@@ -76,6 +102,14 @@ private:
     }
   }
 
+  /**
+   * @brief Cast the specified value to an integer.
+   *
+   * @param value the any object to cast.
+   * @param key key where the value is located in the dictionary, for information upon cast errors.
+   * @throws TypeMismatch if the value is not an integer.
+   * @return value cast to an integer.
+   */
   size_t // TODO: or template?
   cast_to_integer_( const boost::any& value, const std::string& key ) const
   {
@@ -99,6 +133,14 @@ private:
   }
 
 public:
+  /**
+   * @brief Get the value at key in the specified type.
+   *
+   * @tparam T Type of the value. If the value is not of the specified type, a TypeMismatch error is thrown.
+   * @param key key where the value is located in the dictionary.
+   * @throws TypeMismatch if the value is not of specified type T.
+   * @return the value at key cast to the specified type.
+   */
   template < typename T >
   T
   get( const std::string& key ) const
@@ -106,12 +148,27 @@ public:
     return cast_value_< T >( at( key ), key );
   }
 
+  /**
+   * @brief Get the value at key as an integer.
+   *
+   * @param key key where the value is located in the dictionary.
+   * @throws TypeMismatch if the value is not an integer.
+   * @return the value at key cast to the specified type.
+   */
   size_t // TODO: or template?
   get_integer( const std::string& key ) const
   {
     return cast_to_integer_( at( key ), key );
   }
 
+  /**
+   * @brief Update the specified value if there exists a value at key.
+   *
+   * @param key key where the value may be located in the dictionary.
+   * @param value object to update if there exists a value at key.
+   * @throws TypeMismatch if the value at key is not the same type as the value argument.
+   * @return Whether value was updated.
+   */
   template < typename T >
   bool
   update_value( const std::string& key, T& value ) const
@@ -125,6 +182,14 @@ public:
     return false;
   }
 
+  /**
+   * @brief Update the specified value if there exists an integer value at key.
+   *
+   * @param key key where the value may be located in the dictionary.
+   * @param value object to update if there exists a value at key.
+   * @throws TypeMismatch if the value at key is not an integer.
+   * @return Whether the value was updated.
+   */
   template < typename T >
   bool
   update_integer_value( const std::string& key, T& value ) const
@@ -138,6 +203,12 @@ public:
     return false;
   }
 
+  /**
+   * @brief Check whether there exists a value with specified key in the dictionary.
+   *
+   * @param key key where the value may be located in the dictionary.
+   * @return true if there is a value with the specified key, false if not.
+   */
   bool
   known( const std::string& key ) const
   {
@@ -145,22 +216,41 @@ public:
     return maptype_::find( key ) != end();
   }
 
+  /**
+   * @brief Check whether the dictionary is equal to another dictionary.
+   *
+   * Two dictionaries are equal only if they contain the exact same entries with the same values.
+   *
+   * @param other dictionary to check against.
+   * @return true if the dictionary is equal to the other dictionary, false if not.
+   */
   bool operator==( const dictionary& other ) const;
 
+  /**
+   * @brief Check whether the dictionary is unequal to another dictionary.
+   *
+   * Two dictionaries are unequal if they do not contain the exact same entries with the same values.
+   *
+   * @param other dictionary to check against.
+   * @return true if the dictionary is unequal to the other dictionary, false if not.
+   */
   bool
   operator!=( const dictionary& other ) const
   {
     return not( *this == other );
   }
 
+  /**
+   * @brief Initializes or resets access flags for the current dictionary.
+   */
   void init_access_flags() const;
 
   /**
    * @brief Check that all elements in the dictionary have been accessed.
    *
-   * @param where Which function the error occurs in
-   * @param what Which parameter triggers the error
-   *
+   * @param where Which function the error occurs in.
+   * @param what Which parameter triggers the error.
+   * @throws UnaccessedDictionaryEntry if there are unaccessed dictionary entries.
    */
   void all_entries_accessed( const std::string where, const std::string what ) const;
 

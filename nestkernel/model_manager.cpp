c10e47f263ca1619db1d84492c46f54e1a498366
ayssar:ubuntu_pc <med.ayssar@gmail.com>
2022-04-24 14:10:22 +0200

    Fix copied model id

--- nestkernel/model_manager.cpp
+++ nestkernel/model_manager.cpp
@@ -250,12 +250,18 @@ ModelManager::copy_node_model_( index old_id, Name new_name )
   node_models_.push_back( new_model );
 
   index new_id = node_models_.size() - 1;
+  new_model->set_model_id( new_id );
+
   modeldict_->insert( new_name, new_id );
 
 #pragma omp parallel
   {
     const thread t = kernel().vp_manager.get_thread_id();
     const int model_id = new_model->get_model_id();
+    
+    assert(model_id > 0);
+    assert(model_id > old_id);
+
     proxy_nodes_[ t ].push_back( create_proxynode_( t, model_id ) );
   }
 

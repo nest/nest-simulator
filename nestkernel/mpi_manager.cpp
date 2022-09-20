4321e6f88353c1848a416109668a7fe687e74989
Håkon Mørk <hakon.mork@nmbu.no>
2022-09-20 14:04:30 +0200

    Refactored any_true() function to use MPI_Allreduce

--- nestkernel/mpi_manager.cpp
+++ nestkernel/mpi_manager.cpp
@@ -799,21 +799,10 @@ nest::MPIManager::any_true( const bool my_bool )
     return my_bool;
   }
 
-  // since there is no MPI_BOOL we first convert to int
-  int my_int = my_bool;
-
-  std::vector< int > all_int( get_num_processes() );
-  MPI_Allgather( &my_int, 1, MPI_INT, &all_int[ 0 ], 1, MPI_INT, comm );
-  // check if any MPI process sent a "true"
-  for ( unsigned int i = 0; i < all_int.size(); ++i )
-  {
-    if ( all_int[ i ] != 0 )
-    {
-      return true;
-    }
-  }
-
-  return false;
+  const int my_int = my_bool;
+  int global_int;
+  MPI_Allreduce( &my_int, &global_int, 1, MPI_INT, MPI_LOR, comm );
+  return global_int == 1;
 }
 
 // average communication time for a packet size of num_bytes using Allgather

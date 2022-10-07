df41a129f779575cace6fa685db7327cfe62d8c9
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2022-10-07 16:45:53 +0200

    Fix code style

--- nestkernel/secondary_event.h
+++ nestkernel/secondary_event.h
@@ -167,7 +167,8 @@ private:
   static std::vector< synindex > supported_syn_ids_;
   static size_t coeff_length_; // length of coeffarray
 
-  union CoeffarrayBegin {
+  union CoeffarrayBegin
+  {
     std::vector< unsigned int >::iterator as_uint;
     typename std::vector< DataType >::iterator as_d;
 
@@ -175,7 +176,8 @@ private:
                           // non-trivial constructors of iterators
   } coeffarray_begin_;
 
-  union CoeffarrayEnd {
+  union CoeffarrayEnd
+  {
     std::vector< unsigned int >::iterator as_uint;
     typename std::vector< DataType >::iterator as_d;
 

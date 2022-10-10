7e40b3eeced860da3fc8934bbe9034a11d2b39b1
clinssen <c.linssen@fz-juelich.de>
2022-10-10 10:03:23 +0200

    update docstring note about PYNEST_QUIET based on review
    Co-authored-by: Jochen Martin Eppler <jougs@gmx.net>

--- pynest/nest/lib/hl_api_info.py
+++ pynest/nest/lib/hl_api_info.py
@@ -203,9 +203,8 @@ def set_verbosity(level):
     .. note::
 
        To suppress the usual output when NEST starts up (e.g. the welcome message and
-       version information) you can set the environment variable ``PYNEST_QUIET`` by
-       running ``export PYNEST_QUIET = 1`` on the command line before executing your
-       simulation script. 
+       version information) you can run ``export PYNEST_QUIET=1`` on the command
+       line before executing your simulation script. 
 
     Parameters
     ----------

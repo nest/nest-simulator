8e379af71b2b33b5a5b94b1f03b765c790e38825
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2022-11-30 10:38:16 +0100

    Check properly for non-empty strings

--- testsuite/do_tests.sh
+++ testsuite/do_tests.sh
@@ -177,7 +177,7 @@ echo
 NEST_VERSION="$(sli -c "statusdict/version :: =only")"
 echo "  NEST executable .... $NEST (version $NEST_VERSION)"
 echo "  PREFIX ............. $PREFIX"
-if test "${PYTHON}"; then
+if test -n "${PYTHON}"; then
     PYTHON_VERSION="$("${PYTHON}" --version | cut -d' ' -f2)"
     echo "  Python executable .. $PYTHON (version $PYTHON_VERSION)"
     echo "  PYTHONPATH ......... `print_paths ${PYTHONPATH:-}`"
@@ -191,7 +191,7 @@ if test "${HAVE_MPI}" = "true"; then
 else
     echo "  Running MPI tests .. no (compiled without MPI support)"
 fi
-if test "${MUSIC}"; then
+if test -n "${MUSIC}"; then
     MUSIC_VERSION="$("${MUSIC}" --version | head -n1 | cut -d' ' -f2)"
     echo "  MUSIC executable ... $MUSIC (version $MUSIC_VERSION)"
 fi

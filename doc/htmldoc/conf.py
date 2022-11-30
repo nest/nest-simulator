2a0d95cd8a2b5c6af3ed6733034e5e96c869fcdd
Dennis Terhorst <d.terhorst@fz-juelich.de>
2022-11-30 14:38:07 +0100

    fix patch call

--- doc/htmldoc/conf.py
+++ doc/htmldoc/conf.py
@@ -353,8 +353,7 @@ try:
     print(f"  retrieving {patch_url}")
     urlretrieve(patch_url, patch_file)
     print(f"apply {patch_file}")
-    patch = subprocess.run('patch -p3', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    (stdout, stderr) = patch.communicate(open(patch_file, 'r'))
+    check_output('patch -p3', stdin=open(patch_file, 'r'), shell=True)
 
     print("patch result:")
     print(result)

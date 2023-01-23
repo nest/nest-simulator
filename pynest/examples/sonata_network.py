ed6b5f1fb2f699b69b1b5c1cca1dcdad5525c069
Nicolai Haug <nicolaihaug@yahoo.com>
2023-01-23 16:28:12 +0100

    Update NEST SONATA network example

--- pynest/examples/sonata_network.py
+++ pynest/examples/sonata_network.py
@@ -19,43 +19,121 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+NEST SONATA network
+-------------------
+
+This script builds and simulates a network of point neurons described by
+the SONATA format [1]_. The network model consists of 300 internal nodes 
+(explicitly simulated) and 100 external nodes (only provide inputs to the 
+simulated system). The SONATA files can be found in the 
+`pynest/examples/300_pointneurons <https://github.com/nest/nest-simulator/tree/master/pynest/examples/300_pointneurons>`_ 
+directory.
+
+See :ref:`nest_sonata` for details on the NEST support of the SONATA format. 
+
+References
+~~~~~~~~~~
+
+.. [1] Dai K, Hernando J, Billeh YN, Gratiy SL, Planas J, et al. (2020). 
+       The SONATA data format for efficient description of large-scale network models. 
+       PLOS Computational Biology 16(2): e1007696. https://doi.org/10.1371/journal.pcbi.1007696
+
+"""
+
+###############################################################################
+# Import all necessary modules for simulation, analysis and plotting.
+
 import nest
 import matplotlib.pyplot as plt
-import os
 from pathlib import Path
 
 nest.ResetKernel()
+
+###############################################################################
+# Set the number of MPI processes and threads to use in the building and
+# simulation of the network model.
 
 n_procs = 1
 n_threads = 1
 nest.set(total_num_virtual_procs=n_procs * n_threads,
          print_time=True)
 
-# Specify path to the SONATA .json configuration file(s)
-root_path = Path(__file__).resolve().parent
-sonata_path = root_path.joinpath("300_pointneurons")
+###############################################################################
+# Specify the path to the SONATA .json configuration file(s).
+# The `300_pointneurons` model has two configuration files: one network and
+# one simulation config.
+
+base_path = Path(__file__).resolve().parent
+sonata_path = base_path.joinpath("300_pointneurons")
 net_config = sonata_path.joinpath("circuit_config.json")
 sim_config = sonata_path.joinpath("simulation_config.json")
 
-# Instantiate SonataNetwork
+###############################################################################
+# SONATA networks are built and simulated through the :py:class:`.SonataNetwork`
+# class. SONATA config files are passed to the class constructor. Passing a
+# second config is optional and only relevant if the network and simulation
+# configurations are specified separately.
+#
+# First, we instantiate the class.
+
 sonata_net = nest.SonataNetwork(net_config, sim_config=sim_config)
 
-# Create and connect nodes
+###############################################################################
+# Next, we build the network. The network nodes are created by the membership
+# function :py:meth:`~SonataNetwork.Create()` and their connections by the
+# membership function :py:meth:`~SonataNetwork.Connect()`. For convenience,
+# we only need to call the membership function
+# :py:meth:`~SonataNetwork.BuildNetwork`() which internally calls
+# :py:meth:`~SonataNetwork.Create()` and :py:meth:`~SonataNetwork.Connect()`
+#
+# For large networks, the edges HDF5 files might not fit into memory in their
+# entirety. In the NEST kernel, the edges HDF5 datasets are therefore
+# read sequentially in chunks. The chunk size is modifiable through the
+# keyword argument `chunk_size` so that the user is able to achieve a balance
+# between the number of read operations and memory overhead.
+#
+# :py:meth:`~SonataNetwork.BuildNetwork()` returns a dictionary containing
+# the created :py:class:`.NodeCollection`s. The population names are the
+# dictionary keys.
+
 node_collections = sonata_net.BuildNetwork(chunk_size=2**20)
 
-# Connect spike recorder to a population
-s_rec = nest.Create("spike_recorder")
-pop_name = "internal"
-nest.Connect(node_collections[pop_name], s_rec)
+###############################################################################
+# We can now verify whether the built network has the expected number of
+# nodes and connections.
 
-# Simulate the network
-sonata_net.Simulate()
-
-# Results from the built and simulated network
 kernel_status = nest.GetKernelStatus()
 print(f"number of nodes: {kernel_status['network_size']:,}")
 print(f"number of connections: {kernel_status['num_connections']:,}")
-print(f"number of spikes: {kernel_status['local_spike_counter']:,}")
+
+###############################################################################
+# NEST does not currently support SONATA Spike Train Reports or utilize other
+# `output` components in the SONATA config. This means that recording devices
+# must be created and connected manually.
+#
+# Here, we create a `spike_recorder` to record the spiking events of neurons.
+# We wish to connect the spike recorder to the internal population and only
+# record from a subset of the neurons in the population. We extract the
+# internal population's `NodeCollection` from the `node_collections` dictionary
+# by using the internal population's name. Then we slice the `NodeCollection`
+# with a list specifying the node ids of the neurons we wish to record from.
+
+s_rec = nest.Create("spike_recorder")
+pop_name = "internal"
+record_node_ids = [1, 80, 160, 240, 270]
+nest.Connect(node_collections[pop_name][record_node_ids], s_rec)
+
+###############################################################################
+# Finally, we call the membership function :py:meth:`~SonataNetwork.Simulate()`
+# to simulate the network. Note that the simulation time and resolution are
+# expected to be provided in the SONATA config.
+
+sonata_net.Simulate()
+
+###############################################################################
+# After the simulation has finished, we can obtain the data recorded by the
+# spike recorder for further analysis.
 
 nest.raster_plot.from_device(s_rec)
 plt.show()

06c9cc967f1ee738c83e39455231400d477330f5
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2023-02-01 14:26:19 +0100

    Make retrieval of git hash also work for out-of-tree builds

--- doc/htmldoc/conf.py
+++ doc/htmldoc/conf.py
@@ -377,7 +377,8 @@ def patch_documentation(patch_url):
 
     print("Preparing patch...")
     try:
-        git_hash = check_output("git rev-parse HEAD", shell=True, encoding='utf8').strip()
+        git_dir = str(source_dir / ".git")
+        git_hash = check_output(f"GIT_DIR={git_dir} git rev-parse HEAD", shell=True, encoding='utf8').strip()
         print(f"  current git hash: {git_hash}")
         patch_file = f'{git_hash}_doc.patch'
         patch_url = f'{patch_url}/{patch_file}'

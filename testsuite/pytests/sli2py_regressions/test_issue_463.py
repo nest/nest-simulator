29675d715e716649d51261c9c767f07e5613756c
Catherine Schöfmann <c.schoefmann@fz-juelich.de>
2023-04-25 09:16:51 +0200

    Fix wrong copyright header

--- testsuite/pytests/sli2py_regressions/test_issue_463.py
+++ testsuite/pytests/sli2py_regressions/test_issue_463.py
@@ -4,7 +4,7 @@
 #
 # This file is part of NEST.
 #
-# Copyright (C) 2023 The NEST Initiative
+# Copyright (C) 2004 The NEST Initiative
 #
 # NEST is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

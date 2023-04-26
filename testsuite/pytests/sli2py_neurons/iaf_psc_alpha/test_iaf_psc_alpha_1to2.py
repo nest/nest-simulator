8a5fbbc9bd7f407e93eca348e892f407160dcd81
Robin De Schepper <robin.deschepper93@gmail.com>
2023-04-26 14:43:38 +0200

    more conversions

--- testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha_1to2.py
+++ testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha_1to2.py
@@ -4,7 +4,9 @@ import nest
 
 
 @pytest.fixture
-def setup_simulation(simulation, resolution, weight, delay, syn_spec, n1_params, n2_params):
+def setup_simulation(
+    simulation, resolution, weight, delay, syn_spec, n1_params, n2_params
+):
     def setup_simulation_fixture():
         _syn_spec = {}
         if weight is not None:
@@ -35,7 +37,7 @@ def setup_simulation(simulation, resolution, weight, delay, syn_spec, n1_params,
 class TestIAFPSCAlpha1to2WithMultiRes:
     def test_1to2(self, simulate):
         _, results = simulate()
-        self._utils.assert_expected_table(results, expect_default)
+        self._utils.get_comparable_timesamples(results, expect_default)
 
     # Set the `delay` parameter to `None` so that it isn't set by the `iaf_1to2` fixture
     @pytest.mark.parametrize("delay", [None])
@@ -43,11 +45,12 @@ class TestIAFPSCAlpha1to2WithMultiRes:
         # Instead, set the default delay here.
         nest.SetDefaults("static_synapse", {"delay": 1.0})
         _, results = simulate()
-        self._utils.assert_expected_table(results, expect_default)
+        self._utils.get_comparable_timesamples(results, expect_default)
 
     @pytest.fixture(autouse=True)
     def use_utils(self, testutils):
         self._utils = testutils
+
 
 @pytest.mark.parametrize("delay,resolution", [(2.0, 0.1)])
 @pytest.mark.parametrize("min_delay", [0.1, 0.5, 2.0])
@@ -55,69 +58,74 @@ def test_mindelay_invariance(simulate, min_delay, delay, testutils):
     assert min_delay <= delay
     nest.set(min_delay=min_delay, max_delay=delay)
     _, results = simulate()
-    testutils.assert_expected_table(results, expect_inv)
+    testutils.get_comparable_timesamples(results, expect_inv)
 
-expect_default = np.array([
-    [2.5, -70],  #          <-- Voltage trace of the postsynaptic neuron
-    [2.6, -70],  #               (neuron2), at rest until a spike arrives.
-    [2.7, -70],  #
-    [2.8, -70],  #
-    [2.9, -70],  #
-    [3.0, -70],  #
-    [3.1, -70],  #              spike at t=3.0 ms.
-    [3.2, -70],  #
-    [3.3, -70],  #
-    [3.4, -70],  #
-    [3.5, -70],  #          <--  Synaptic delay of 1.0 ms.
-    [3.6, -70],  #
-    [3.7, -70],  #
-    [3.8, -70],  #
-    [3.9, -70],  #
-    [4.0, -70],  #  <------  Spike arrives at the postsynaptic neuron
-    [4.1, -69.9974],  #     <-    (neuron2) and changes the state vector of
-    [4.2, -69.9899],  #       |   the neuron, not visible in voltage because
-    [4.3, -69.9781],  #       |   voltage of PSP initial condition is 0.
-    [4.4, -69.9624],  #       |
-    [4.5, -69.9434],  #        -  Arbitrarily close to the time of impact
-    [4.6, -69.9213],  #           (t=4.0 ms) the effect of the spike (PSP)
-    [4.7, -69.8967],  #           is visible in the voltage trace.
-    [4.8, -69.8699],  #
-    [4.9, -69.8411],  #
-    [5.0, -69.8108],  #
-    [5.1, -69.779],  #
-    [5.2, -69.7463],  #     <---  The voltage trace is independent
-    [5.3, -69.7126],  #           of the computation step size h.
-    [5.4, -69.6783],  #           Larger step sizes only have fewer
-    [5.5, -69.6435],  #           sample points.
-    [5.6, -69.6084],  #
-    [5.7, -69.5732],  #
-])
 
-expect_inv = np.array([
-     [ .1           , -70          ], #
-     [ .2           , -70          ], #
-     [ .3           , -70          ], #
-     [ .4           , -70          ], #
-     [ .5           , -70          ], #
-     [ 2.8          , -70          ], #
-     [ 2.9          , -70          ], #
-     [ 3.0          , -70          ], #
-     [ 3.1          , -70          ], #
-     [ 3.2          , -70          ], #
-     [ 3.3          , -70          ], #
-     [ 3.4          , -70          ], #
-     [ 3.5          , -70          ], #
-     [ 4.8          , -70          ], #
-     [ 4.9          , -70          ], #
-     [ 5.0          , -70          ], #
-     [ 5.1          , -69.9974     ], #
-     [ 5.2          , -69.9899     ], #
-     [ 5.3          , -69.9781     ], #
-     [ 5.4          , -69.9624     ], #
-     [ 5.5          , -69.9434     ], #
-     [ 5.6          , -69.9213     ], #
-     [ 5.7          , -69.8967     ], #
-     [ 5.8          , -69.8699     ], #
-     [ 5.9          , -69.8411     ], #
-     [ 6.0          , -69.8108     ], #
- ])
+expect_default = np.array(
+    [
+        [2.5, -70],  #          <-- Voltage trace of the postsynaptic neuron
+        [2.6, -70],  #               (neuron2), at rest until a spike arrives.
+        [2.7, -70],  #
+        [2.8, -70],  #
+        [2.9, -70],  #
+        [3.0, -70],  #
+        [3.1, -70],  #              spike at t=3.0 ms.
+        [3.2, -70],  #
+        [3.3, -70],  #
+        [3.4, -70],  #
+        [3.5, -70],  #          <--  Synaptic delay of 1.0 ms.
+        [3.6, -70],  #
+        [3.7, -70],  #
+        [3.8, -70],  #
+        [3.9, -70],  #
+        [4.0, -70],  #  <------  Spike arrives at the postsynaptic neuron
+        [4.1, -69.9974],  #     <-    (neuron2) and changes the state vector of
+        [4.2, -69.9899],  #       |   the neuron, not visible in voltage because
+        [4.3, -69.9781],  #       |   voltage of PSP initial condition is 0.
+        [4.4, -69.9624],  #       |
+        [4.5, -69.9434],  #        -  Arbitrarily close to the time of impact
+        [4.6, -69.9213],  #           (t=4.0 ms) the effect of the spike (PSP)
+        [4.7, -69.8967],  #           is visible in the voltage trace.
+        [4.8, -69.8699],  #
+        [4.9, -69.8411],  #
+        [5.0, -69.8108],  #
+        [5.1, -69.779],  #
+        [5.2, -69.7463],  #     <---  The voltage trace is independent
+        [5.3, -69.7126],  #           of the computation step size h.
+        [5.4, -69.6783],  #           Larger step sizes only have fewer
+        [5.5, -69.6435],  #           sample points.
+        [5.6, -69.6084],  #
+        [5.7, -69.5732],  #
+    ]
+)
+
+expect_inv = np.array(
+    [
+        [0.1, -70],  #
+        [0.2, -70],  #
+        [0.3, -70],  #
+        [0.4, -70],  #
+        [0.5, -70],  #
+        [2.8, -70],  #
+        [2.9, -70],  #
+        [3.0, -70],  #
+        [3.1, -70],  #
+        [3.2, -70],  #
+        [3.3, -70],  #
+        [3.4, -70],  #
+        [3.5, -70],  #
+        [4.8, -70],  #
+        [4.9, -70],  #
+        [5.0, -70],  #
+        [5.1, -69.9974],  #
+        [5.2, -69.9899],  #
+        [5.3, -69.9781],  #
+        [5.4, -69.9624],  #
+        [5.5, -69.9434],  #
+        [5.6, -69.9213],  #
+        [5.7, -69.8967],  #
+        [5.8, -69.8699],  #
+        [5.9, -69.8411],  #
+        [6.0, -69.8108],  #
+    ]
+)
--- testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha_dc.py
+++ testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha_dc.py
@@ -1,118 +1,244 @@
+import dataclasses
+
 import numpy as np
 import pytest
 import nest
+import testutil, testsimulation
 
-@pytest.fixture
-def setup_simulation(simulation, weight, delay, syn_spec, n1_params, amplitude, resolution):
-    def setup_simulation_fixture():
-        _syn_spec = {}
-        if weight is not None:
-            _syn_spec["weight"] = weight
-        if delay is not None:
-            _syn_spec["delay"] = delay
-        _syn_spec.update(syn_spec)
-        _n1_params = {}
-        _n1_params.update(n1_params)
-        n1 = simulation.neuron = nest.Create("iaf_psc_alpha")
-        n1.set(_n1_params)
-        dc = simulation.dc_generator = nest.Create("dc_generator")
-        dc.amplitude = amplitude
-        vm = simulation.voltmeter = nest.Create("voltmeter")
-        vm.interval = resolution
-        nest.Connect(dc, n1, syn_spec=_syn_spec)
+
+@dataclasses.dataclass
+class IAFPSCAlphaDCSimulation(testsimulation.Simulation):
+    amplitude: float = 1000.0
+    origin: float = 0.0
+    arrival: float = 3.0
+    dc_delay: float = 1.0
+    dc_visible: float = 3.0
+    dc_duration: float = 2.0
+
+    def __post_init__(self):
+        self.dc_on = self.dc_visible - self.dc_delay
+        self.dc_off = self.dc_on + self.dc_duration
+
+    def setup(self):
+        super().setup()
+        n1 = self.neuron = nest.Create("iaf_psc_alpha")
+        dc = self.dc_generator = nest.Create("dc_generator")
+        dc.amplitude = self.amplitude
+        vm = self.voltmeter = nest.Create("voltmeter")
+        vm.interval = self.resolution
         nest.Connect(vm, n1)
-
-    return setup_simulation_fixture
-
-
-@pytest.fixture
-def modify_dc_gen(simulation, amplitude, origin, arrival, resolution):
-    simulation.dc_generator.set(
-        amplitude=amplitude,
-        origin=origin,
-        start=arrival - resolution,
-    )
 
 
 @pytest.mark.parametrize("weight", [1.0])
+@pytest.mark.parametrize("simulation_class", [IAFPSCAlphaDCSimulation])
 class TestIAFPSCAlphaDC:
     @pytest.mark.parametrize("delay", [0.1])
-    def test_dc(self, simulate, testutils):
-        _, results = simulate()
-        testutils.assert_expected_table(results, expect_default)
+    def test_dc(self, simulation):
+        simulation.setup()
 
-    @pytest.mark.parametrize("resolution,delay", [(.1, .1), (.2, .2), (.5, .5), (1., 1.)])
-    @pytest.mark.parametrize("post_setup_simulation", [modify_dc_gen])
-    def test_dc_aligned(self, simulate, delay, testutils):
-        _, results = simulate()
-        testutils.assert_expected_table(results, expect_aligned)
+        dc_gen_spec = {"delay": simulation.delay, "weight": simulation.weight}
+        nest.Connect(simulation.dc_generator, simulation.neuron, syn_spec=dc_gen_spec)
 
-    @pytest.fixture(autouse=True)
-    def use_utils(self, testutils):
-        self._utils = testutils
+        results = simulation.simulate()
 
-expect_default = np.array([
-    [0.1, -70],  #   <--------   is at rest (initial condition).
-    [0.2, -70],  # <-------   |
+        actual, expected = testutil.get_comparable_timesamples(results, expect_default)
+        assert actual == expected
+
+    @pytest.mark.parametrize(
+        "resolution,delay", [(0.1, 0.1), (0.2, 0.2), (0.5, 0.5), (1.0, 1.0)]
+    )
+    def test_dc_aligned(self, simulation):
+        simulation.setup()
+
+        simulation.dc_generator.set(
+            amplitude=simulation.amplitude,
+            origin=simulation.origin,
+            start=simulation.arrival - simulation.resolution,
+        )
+        nest.Connect(
+            simulation.dc_generator,
+            simulation.neuron,
+            syn_spec={"delay": simulation.delay},
+        )
+
+        results = simulation.simulate()
+
+        actual, expected = testutil.get_comparable_timesamples(results, expect_aligned)
+        assert actual == expected
+
+    @pytest.mark.parametrize(
+        "resolution,delay", [(0.1, 0.1), (0.2, 0.2), (0.5, 0.5), (1.0, 1.0)]
+    )
+    def test_dc_aligned_auto(self, simulation):
+        simulation.setup()
+
+        simulation.dc_generator.set(
+            amplitude=simulation.amplitude,
+            origin=simulation.origin,
+            start=simulation.dc_on,
+        )
+        dc_gen_spec = {"delay": simulation.dc_delay, "weight": simulation.weight}
+        nest.Connect(simulation.dc_generator, simulation.neuron, syn_spec=dc_gen_spec)
+
+        results = simulation.simulate()
+        actual, expected = testutil.get_comparable_timesamples(results, expect_auto)
+        assert actual == expected
+
+    @pytest.mark.parametrize(
+        "resolution,delay", [(0.1, 0.1), (0.2, 0.2), (0.5, 0.5), (1.0, 1.0)]
+    )
+    @pytest.mark.parametrize("duration", [10.0])
+    def test_dc_aligned_stop(self, simulation):
+        simulation.setup()
+
+        simulation.dc_generator.set(
+            amplitude=simulation.amplitude,
+            origin=simulation.origin,
+            start=simulation.dc_on,
+            stop=simulation.dc_off,
+        )
+        dc_gen_spec = {"delay": simulation.dc_delay, "weight": simulation.weight}
+        nest.Connect(simulation.dc_generator, simulation.neuron, syn_spec=dc_gen_spec)
+
+        results = simulation.simulate()
+        actual, expected = testutil.get_comparable_timesamples(results, expect_stop)
+        assert actual == expected
+
+
+expect_default = np.array(
     [
-        0.3,
-        -69.602,
-    ],  # <-  |   - In the first update step 0ms  -> 0.1 ms, i.e. at
+        [0.1, -70],  #   <--------   is at rest (initial condition).
+        [0.2, -70],  # <-------   |
+        [
+            0.3,
+            -69.602,
+        ],  # <-  |   - In the first update step 0ms  -> 0.1 ms, i.e. at
+        [
+            0.4,
+            -69.2079,
+        ],  #  | |     the earliest possible time, the current generator
+        [
+            0.5,
+            -68.8178,
+        ],  #  | |     is switched on and emits a current event with time
+        [0.6, -68.4316],  #  | |     stamp 0.1 ms.
+        [0.7, -68.0492],  #  | |
+        [
+            0.8,
+            -67.6706,
+        ],  #  |  ---- After the minimal delay of 1 computation time step,
+        [
+            0.9,
+            -67.2958,
+        ],  #  |       the current affects the state of the neuron. This is
+        [
+            1.0,
+            -66.9247,
+        ],  #  |       reflected in the neuron's state variable y0 (initial
+        [
+            1.1,
+            -66.5572,
+        ],  #  |       condition) but has not yet affected the membrane
+        [1.2, -66.1935],  #  |       potential.
+        [1.3, -65.8334],  #  |
+        [
+            1.4,
+            -65.4768,
+        ],  #   ------ The effect of the DC current, influencing the neuron
+        [
+            1.5,
+            -65.1238,
+        ],  #          for 0.1 ms now, becomes visible in the membrane potential.
+        [1.6, -64.7743],  #
+    ]
+)
+
+
+expect_aligned = np.array(
     [
-        0.4,
-        -69.2079,
-    ],  #  | |     the earliest possible time, the current generator
+        [2.5, -70],  #
+        [2.6, -70],  #
+        [2.7, -70],  #
+        [2.8, -70],  #
+        [2.9, -70],  #
+        [3.0, -70],  #  %  <---- Current starts to affect
+        [3.1, -69.602],  #   %   neuron (visible in state variable
+        [3.2, -69.2079],  #  %   y0). This is the desired onset of
+        [3.3, -68.8178],  #  %    t= 3.0 ms.
+        [3.4, -68.4316],  #
+        [3.5, -68.0492],  #
+        [3.6, -67.6706],  #
+        [3.7, -67.2958],  #
+        [3.8, -66.9247],  #
+        [3.9, -66.5572],  #
+        [4.0, -66.1935],  #
+        [4.1, -65.8334],  #
+        [4.2, -65.4768],  #
+    ]
+)
+
+
+expect_auto = np.array(
     [
-        0.5,
-        -68.8178,
-    ],  #  | |     is switched on and emits a current event with time
-    [0.6, -68.4316],  #  | |     stamp 0.1 ms.
-    [0.7, -68.0492],  #  | |
+        [2.5, -70],  #
+        [2.6, -70],  #
+        [2.7, -70],  #
+        [2.8, -70],  #
+        [2.9, -70],  #
+        [3.0, -70],  #  %  <---- Current starts to affect
+        [3.1, -69.602],  #   %   neuron (visible in state variable
+        [3.2, -69.2079],  #  %   y0). This is the desired onset of
+        [3.3, -68.8178],  #  %    t= 3.0 ms.
+        [3.4, -68.4316],  #
+        [3.5, -68.0492],  #
+        [3.6, -67.6706],  #
+        [3.7, -67.2958],  #
+        [3.8, -66.9247],  #
+        [3.9, -66.5572],  #
+        [4.0, -66.1935],  #
+        [4.1, -65.8334],  #
+        [4.2, -65.4768],  #
+    ]
+)
+
+
+expect_stop = np.array(
     [
-        0.8,
-        -67.6706,
-    ],  #  |  ---- After the minimal delay of 1 computation time step,
-    [
-        0.9,
-        -67.2958,
-    ],  #  |       the current affects the state of the neuron. This is
-    [
-        1.0,
-        -66.9247,
-    ],  #  |       reflected in the neuron's state variable y0 (initial
-    [
-        1.1,
-        -66.5572,
-    ],  #  |       condition) but has not yet affected the membrane
-    [1.2, -66.1935],  #  |       potential.
-    [1.3, -65.8334],  #  |
-    [
-        1.4,
-        -65.4768,
-    ],  #   ------ The effect of the DC current, influencing the neuron
-    [
-        1.5,
-        -65.1238,
-    ],  #          for 0.1 ms now, becomes visible in the membrane potential.
-    [1.6, -64.7743],  #
-])
-expect_aligned = np.array([
-     [ 2.5          , -70          ], #
-     [ 2.6          , -70          ], #
-     [ 2.7          , -70          ], #
-     [ 2.8          , -70          ], #
-     [ 2.9          , -70          ], #
-     [ 3.0          , -70          ], #  %  <---- Current starts to affect
-     [ 3.1          , -69.602      ], #   %   neuron (visible in state variable
-     [ 3.2          , -69.2079     ], #  %   y0). This is the desired onset of
-     [ 3.3          , -68.8178     ], #  %    t= 3.0 ms.
-     [ 3.4          , -68.4316     ], #
-     [ 3.5          , -68.0492     ], #
-     [ 3.6          , -67.6706     ], #
-     [ 3.7          , -67.2958     ], #
-     [ 3.8          , -66.9247     ], #
-     [ 3.9          , -66.5572     ], #
-     [ 4.0          , -66.1935     ], #
-     [ 4.1          , -65.8334     ], #
-     [ 4.2          , -65.4768     ], #
- ])
+        [2.5, -70],  #
+        [2.6, -70],  #
+        [2.7, -70],  #
+        [2.8, -70],  #
+        [2.9, -70],  #
+        [3.0, -70],  #          <-- current starts to affect
+        [3.1, -69.602],  #  %         neuron (visible in state variable
+        [3.2, -69.2079],  #  %       y0). This is the desired onset of
+        [3.3, -68.8178],  #  %       t= 3.0 ms.
+        [3.4, -68.4316],  #
+        [3.5, -68.0492],  #
+        [3.6, -67.6706],  #
+        [3.7, -67.2958],  #
+        [3.8, -66.9247],  #
+        [3.9, -66.5572],  #
+        [4.0, -66.1935],  #
+        [4.1, -65.8334],  #
+        [4.2, -65.4768],  #
+        [4.3, -65.1238],  #
+        [4.4, -64.7743],  #
+        [4.5, -64.4283],  #
+        [4.6, -64.0858],  #
+        [4.7, -63.7466],  #
+        [4.8, -63.4108],  #
+        [4.9, -63.0784],  #
+        [5.0, -62.7492],  #    <-- current ends to affect neuron
+        [5.1, -62.8214],  #        (visible in state variable y0),
+        [5.2, -62.8928],  #        the highest voltage is observed.
+        [5.3, -62.9635],  #        The current was applied for the desired
+        [5.4, -63.0335],  #        duration (2ms).
+        [5.5, -63.1029],  #
+        [5.6, -63.1715],  #
+        [5.7, -63.2394],  #
+        [5.8, -63.3067],  #
+        [5.9, -63.3733],  #
+        [6.0, -63.4392],  #
+    ]
+)

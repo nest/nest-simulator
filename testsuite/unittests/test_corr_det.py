570739cf556b92bf81f85b40e85d3d27dc2e42ad
Ayssar_Juelich <med.ayssar@gmail.com>
2023-04-26 10:26:08 +0200

    Port test_corr_det

--- testsuite/unittests/test_corr_det.py
+++ testsuite/unittests/test_corr_det.py
@@ -21,22 +21,16 @@
 
 
 """
-Name: testsuite::test_corr_det - minimal test of correlation detector
-
-Synopsis: (test_corr_det) run -> dies if assertion fails
-
-Description:
-  Feeds correlation detector with two hand-crafted spike trains with
-  known correlation. Correlation detector parameters are set in model.
+Feeds correlation detector with two hand-crafted spike trains with
+known correlation. Correlation detector parameters are set in model.
 
 Remarks:
   The test does not test weighted correlations.
 
-Author: July 2008, Plesser
-SeeAlso: correlation_detector
 """
 import nest
 import pytest
+import numpy as np
 
 
 @pytest.fixture(autouse=True)
@@ -44,8 +38,12 @@ def prepare():
     nest.ResetKernel()
 
 
+def _slice(dict, keys):
+    return {a: dict[a] for a in keys if a in dict}
+
+
 def test_changing_params():
-    new_params = {"d_tau": 2.0, "tau_max": 20.0}
+    new_params = {"delta_tau": 2.0, "tau_max": 20.0}
 
     original_model_instance = nest.Create("correlation_detector")
     original_model_instance.set(new_params)
@@ -53,4 +51,125 @@ def test_changing_params():
     nest.SetDefaults("correlation_detector", new_params)
     modified_model_instance = nest.Create("correlation_detector")
 
-    assert modified_model_instance.get() == original_model_instance.get()
+    original = _slice(original_model_instance.get(), new_params.keys())
+    modified = _slice(modified_model_instance.get(), new_params.keys())
+
+    assert modified == original
+
+
+def test_setting_invalid_delta_tau():
+    nest.resolution = 0.1
+    with pytest.raises(Exception):
+        nest.SetDefaults("correlation_detector", {"delta_tau": 0.25})
+
+
+def test_setting_invalid_tau_max():
+    nest.resolution = 0.1
+    with pytest.raises(Exception):
+        nest.SetDefaults("correlation_detector", {"delta_tau": 1.0, "tau_max": 2.5})
+
+
+def test_setting_invalid_resolution():
+    nest.resolution = 0.1
+    nest.SetDefaults("correlation_detector", {"delta_tau": 0.1})
+    with pytest.raises(Exception):
+        nest.resolution = 1.0
+        detector = nest.Create('correlation_detector')
+
+
+def test_setting_num_of_histogram_bins():
+    nest.resolution = 0.2
+    nest.SetDefaults("correlation_detector", {"delta_tau": 1.0, "tau_max": 5.0})
+
+    detector = nest.Create('correlation_detector')
+
+    nest.Simulate(1)
+
+    histogram_size = len(detector.get("histogram"))
+
+    assert histogram_size == 11
+
+
+def prepare_correlation_detector(spike_times_array):
+    detector = nest.Create('correlation_detector')
+
+    sg1 = nest.Create('spike_generator')
+    sg1.set(precise_times=False, spike_times=spike_times_array[0])
+
+    nest.SetDefaults("static_synapse", {"receptor_type": 0})
+
+    nest.Connect(sg1, detector)
+
+    sg2 = nest.Create('spike_generator')
+    sg2.set(precise_times=False, spike_times=spike_times_array[1])
+
+    nest.SetDefaults("static_synapse", {"receptor_type": 1})
+
+    nest.Connect(sg2, detector)
+
+    all_spike_times = []
+    all_spike_times.extend(spike_times_array[0])
+    all_spike_times.extend(spike_times_array[1])
+    max_value = np.max(all_spike_times)
+
+    min_delay = nest.GetKernelStatus()["min_delay"]
+    t_sim = min_delay * 2 + max_value
+
+    nest.Simulate(t_sim)
+
+    return detector
+
+
+def diff_at_center():
+    spikes_times = [[1.0, 2.0,  6.0], [2.0, 4.0]]
+    histogram = [0, 1, 0, 1, 0, 1,  1, 1, 1, 0, 0]
+    return (spikes_times, histogram)
+
+
+def diff_at_edge():
+    spikes_times = [[6.0], [0.5, 5.4, 5.5, 5.6, 6.4, 6.5, 6.6, 11.5]]
+    histogram = [1, 0, 0, 0, 1, 3, 2, 0, 0, 0, 0]
+    return (spikes_times, histogram)
+
+
+@pytest.mark.parametrize("spikes_times, histogram", [diff_at_center(), diff_at_edge()])
+def test_histogram_correlation(spikes_times, histogram):
+
+    nest.resolution = 0.1
+    nest.SetDefaults("correlation_detector", {"delta_tau": 1.0, "tau_max": 5.0})
+
+    detector = prepare_correlation_detector(spikes_times)
+
+    n_events = detector.get("n_events")
+    spikes_times_size = list(map(lambda x: len(x), spikes_times))
+    assert (n_events == spikes_times_size).all()
+
+    detector_histogram = detector.get("histogram")
+    assert (detector_histogram == histogram).all()
+
+
+def test_setting_invalid_n_events():
+    """
+    test to ensure [1 1] not allowed for /n_events
+    """
+    detector = nest.Create("correlation_detector")
+    with pytest.raises(Exception):
+        detector.set(n_events=[1, 1])
+
+
+def test_reset():
+
+    nest.resolution = 0.1
+    nest.SetDefaults("correlation_detector", {"delta_tau": 1.0, "tau_max": 5.0})
+
+    spikes_times = [[1.0, 2.0, 6.0], [2.0, 4.0]]
+    detector = prepare_correlation_detector(spikes_times)
+
+    n_events = detector.get("n_events")
+
+    has_zero_entries = np.any(n_events == 0)
+
+    if not has_zero_entries:
+        detector.set(n_events=[0, 0])
+        assert np.all(detector.get("n_events") == 0)
+        assert np.all(detector.get("histogram") == 0)

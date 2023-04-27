08af6fd09417073287464b5ce0e83df351747709
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2023-04-27 14:01:11 +0200

    Refactored add-freeze-thaw test to single test to ensure correct ordering

--- testsuite/pytests/neurons/test_add_freeze_thaw.py
+++ testsuite/pytests/neurons/test_add_freeze_thaw.py
@@ -19,85 +19,57 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-r"""
+"""
 Test that per-thread nodes vectors are updated.
-
-1. Create nodes driven by internal current, simulate, check V_m changes.
-2. Add more nodes, reset old nodes, simulate again, check V_m changes for all.
-3. Freeze half the nodes, simulate, check their V_m is constant.
-4. Thaw nodes, simulate, check V_m changes again.
-
 """
 
 import numpy as np
 import pytest
 import nest
 
-pytestmark = pytest.mark.skipif_missing_threads()
 
-NUM_THREADS = 4
-BLOCKSIZE = 11   # number of neurons per Create, not multiple of NUM_THREADS
-SIMTIME = 10
-I_E = -50
-VM_REF = nest.GetDefaults('iaf_psc_alpha', 'V_m')
+@pytest.mark.skipif_missing_threads
+def test_add_freeze_thaw():
+    """    
+    The following simulation steps and checks are performed in order:
 
+      1. Create nodes driven by internal current, simulate, check V_m changes.
+      2. Add more nodes, reset old nodes, simulate again, check V_m changes for all.
+      3. Freeze half the nodes, simulate, check their V_m is constant.
+      4. Thaw nodes, simulate, check V_m changes again.
 
-@pytest.fixture(scope="module")
-def prepare_kernel():
+    Because the order of simulation steps and tests is important, implement as single test.
+    """
+
     nest.ResetKernel()
     nest.local_num_threads = 4
 
+    neuron_model = 'iaf_psc_alpha'
+    neurons_per_pop = 11   # not multiple of number of threads
+    neuron_params = {'I_e': -50.}
+    sim_time = 10.
 
-@pytest.fixture(scope="module")
-def neurons_a(prepare_kernel):
-    """
-    Set up network and simulate it step by step.
-    """
+    vm_ref = nest.GetDefaults(neuron_model, 'V_m')
 
-    return nest.Create('iaf_psc_alpha', n=BLOCKSIZE, params={'I_e': I_E})
+    # Test that neurons in population a hyperpolarize
+    pop_a = nest.Create(neuron_model, n=neurons_per_pop, params=neuron_params)
+    nest.Simulate(sim_time)
+    assert all(np.array(pop_a.V_m) < vm_ref)
 
+    # Test that neurons in population a and b hyperpolarize
+    pop_b = nest.Create(neuron_model, n=neurons_per_pop, params=neuron_params)
+    (pop_a + pop_b).V_m = vm_ref
+    nest.Simulate(sim_time)
+    assert all(np.array((pop_a + pop_b).V_m) < vm_ref)
 
-@pytest.fixture(scope="module")
-def neurons_b(neurons_a):
-    return nest.Create('iaf_psc_alpha', n=BLOCKSIZE, params={'I_e': I_E})
+    # Test that neurons in population a do not hyperpolarize when frozen
+    (pop_a + pop_b).V_m = vm_ref
+    pop_a.frozen = True
+    nest.Simulate(sim_time)
+    assert all(np.array(pop_a.V_m) == vm_ref) and all(np.array(pop_b.V_m) < vm_ref)
 
-
-@pytest.fixture(scope="module")
-def sim_a(neurons_a):
-    nest.Simulate(SIMTIME)
-
-
-@pytest.fixture(scope="module")
-def sim_a_b(neurons_a, neurons_b, sim_a):
-    (neurons_a + neurons_b).V_m = VM_REF
-    nest.Simulate(SIMTIME)
-
-
-@pytest.fixture(scope="module")
-def sim_frozen(neurons_a, neurons_b, sim_a_b):
-    (neurons_a + neurons_b).V_m = VM_REF
-    neurons_a.frozen = True
-    nest.Simulate(SIMTIME)
-
-
-@pytest.fixture(scope="module")
-def sim_thawed(neurons_a, neurons_b, sim_frozen):
-    (neurons_a + neurons_b).V_m = VM_REF
-    neurons_a.frozen = False
-    nest.Simulate(SIMTIME)
-
-
-def test_vm_changes(neurons_a, sim_a):
-    assert all(np.array(neurons_a.V_m) < VM_REF)
-
-
-def test_vm_changes_more_neurons(neurons_a, neurons_b, sim_a_b):
-    assert all(np.array((neurons_a + neurons_b).V_m) < VM_REF)
-
-
-def test_vm_frozen(neurons_a, neurons_b, sim_frozen):
-    assert all(np.array(neurons_a.V_m) == VM_REF) and all(np.array(neurons_b.V_m) < VM_REF)
-
-
-def test_vm_thawed(neurons_a, neurons_b, sim_thawed):
-    assert all(np.array((neurons_a + neurons_b).V_m) < VM_REF)
+    # Test that neurons in population a hyperpolarize when no longer frozen
+    (pop_a + pop_b).V_m = vm_ref
+    pop_a.frozen = False
+    nest.Simulate(sim_time)
+    assert all(np.array((pop_a + pop_b).V_m) < vm_ref)

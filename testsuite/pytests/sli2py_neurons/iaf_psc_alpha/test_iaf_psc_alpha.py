6012b1937ba2328276fe0291adf376fa3663d676
Robin De Schepper <robin.deschepper93@gmail.com>
2023-04-27 15:26:17 +0200

    converted `test_iaf_psc_alpha_dc_accuracy`

--- testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha.py
+++ testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha.py
@@ -8,6 +8,11 @@ import testsimulation, testutil
 from scipy.special import lambertw
 
 
+# Notes:
+# * test min delay stuff tests sim indep of mindelay, move out?
+# * `test_iaf_ps_dc_accuracy` tests kernel precision, move out?
+
+
 @dataclasses.dataclass
 class IAFPSCAlphaSimulation(testsimulation.Simulation):
     def setup(self):
@@ -180,6 +185,48 @@ class TestMinDelayUsingIAFPSCAlpha:
         assert actual == expected
 
 
+def test_kernel_precision():
+    nest.ResetKernel()
+    nest.set(tics_per_ms=2**14, resolution=2**0)
+    assert math.frexp(nest.ms_per_tic) == (0.5, -13)
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        "iaf_psc_alpha_ps",
+        "iaf_psc_delta_ps",
+        "iaf_psc_exp_ps",
+        "iaf_psc_exp_ps_lossless",
+    ],
+)
+@pytest.mark.parametrize("resolution", [2 ** i for i in range(0, -14, -1)])
+@pytest.mark.parametrize("duration, tolerance", [(5, 1e-13), (500, 1e-9)])
+class TestIAFPSDCAccuracy:
+    def test_iaf_pc_ps_dc_accuracy(self, model, duration, tolerance, resolution):
+        I_e = 1000.0
+        tau_m = 10.0
+        C_m = 250.0
+        nest.ResetKernel()
+        nest.set(tics_per_ms=2 ** 14, resolution=resolution)
+        neuron = nest.Create(
+            model,
+            params={
+                "E_L": 0.0,  # resting potential in mV
+                "V_m": 0.0,  # initial membrane potential in mV
+                "V_th": 2000.0,  # spike threshold in mV
+                "I_e": 1000.0,  # DC current in pA
+                "tau_m": 10.0,  # membrane time constant in ms
+                "C_m": 250.0,  # membrane capacity in pF
+            },
+        )
+        nest.Simulate(duration)
+        # Analytical solution
+        V = I_e * tau_m / C_m * (1.0 - math.exp(-duration / tau_m))
+        # Check that membrane potential is within tolerance of analytical solution.
+        assert neuron.V_m - V < tolerance
+
+
 expected_default = np.array(
     [
         [0.1, -70],  # <----- The earliest time dc_gen can be switched on.
@@ -238,211 +285,6 @@ expected_default = np.array(
         #       problem.One workaround is to end the simulation script
         #       with "h Simulate", thereby making the script resolution
         #       dependent.
-    ]
-)
-
-
-sli = np.array(
-    [
-        -7.000000e01,
-        -7.000000e01,
-        -6.998033e01,
-        -6.993104e01,
-        -6.986370e01,
-        -6.978665e01,
-        -6.970581e01,
-        -6.962526e01,
-        -6.954774e01,
-        -6.947497e01,
-        -6.940795e01,
-        -6.934717e01,
-        -6.929274e01,
-        -6.924454e01,
-        -6.920227e01,
-        -6.916553e01,
-        -6.913389e01,
-        -6.910686e01,
-        -6.908400e01,
-        -6.906485e01,
-        -6.904898e01,
-        -6.903600e01,
-        -6.902556e01,
-        -6.901732e01,
-        -6.901100e01,
-        -6.900634e01,
-        -6.900310e01,
-        -6.900109e01,
-        -6.900014e01,
-        -6.900008e01,
-        -6.900078e01,
-        -6.900214e01,
-        -6.900404e01,
-        -6.900641e01,
-        -6.900917e01,
-        -6.901226e01,
-        -6.901563e01,
-        -6.901922e01,
-        -6.902300e01,
-        -6.902695e01,
-        -6.903102e01,
-        -6.903519e01,
-        -6.903945e01,
-        -6.904378e01,
-        -6.904816e01,
-        -6.905258e01,
-        -6.905703e01,
-        -6.906151e01,
-        -6.906600e01,
-        -6.907050e01,
-        -6.907500e01,
-        -6.907950e01,
-        -6.908400e01,
-        -6.908849e01,
-        -6.909297e01,
-        -6.909744e01,
-        -6.910190e01,
-        -6.910634e01,
-        -6.911076e01,
-        -6.911517e01,
-        -6.911956e01,
-        -6.912394e01,
-        -6.912829e01,
-        -6.913263e01,
-        -6.913694e01,
-        -6.914124e01,
-        -6.914551e01,
-        -6.914977e01,
-        -6.915401e01,
-        -6.915822e01,
-        -6.916242e01,
-        -6.916659e01,
-        -6.917074e01,
-        -6.917488e01,
-        -6.917899e01,
-        -6.918309e01,
-        -6.918716e01,
-        -6.919121e01,
-        -6.919525e01,
-        -6.919926e01,
-        -6.920325e01,
-        -6.920722e01,
-        -6.921118e01,
-        -6.921511e01,
-        -6.921903e01,
-        -6.922292e01,
-        -6.922680e01,
-        -6.923065e01,
-        -6.923449e01,
-        -6.923831e01,
-        -6.924211e01,
-        -6.924589e01,
-        -6.924965e01,
-        -6.925339e01,
-        -6.925711e01,
-        -6.926082e01,
-        -6.926451e01,
-        -6.926817e01,
-        -6.927182e01,
-        -6.927546e01,
-        -6.927907e01,
-        -6.928267e01,
-        -6.928624e01,
-        -6.928980e01,
-        -6.929335e01,
-        -6.929687e01,
-        -6.930038e01,
-        -6.930387e01,
-        -6.930734e01,
-        -6.931079e01,
-        -6.931423e01,
-        -6.931765e01,
-        -6.932105e01,
-        -6.932444e01,
-        -6.932781e01,
-        -6.933116e01,
-        -6.933450e01,
-        -6.933782e01,
-        -6.934112e01,
-        -6.934441e01,
-        -6.934768e01,
-        -6.935093e01,
-        -6.935417e01,
-        -6.935739e01,
-        -6.936059e01,
-        -6.936378e01,
-        -6.936695e01,
-        -6.937011e01,
-        -6.937325e01,
-        -6.937638e01,
-        -6.937949e01,
-        -6.938258e01,
-        -6.938566e01,
-        -6.938873e01,
-        -6.939178e01,
-        -6.939481e01,
-        -6.939783e01,
-        -6.940083e01,
-        -6.940382e01,
-        -6.940679e01,
-        -6.940975e01,
-        -6.941270e01,
-        -6.941563e01,
-        -6.941854e01,
-        -6.942144e01,
-        -6.942433e01,
-        -6.942720e01,
-        -6.943005e01,
-        -6.943290e01,
-        -6.943572e01,
-        -6.943854e01,
-        -6.944134e01,
-        -6.944413e01,
-        -6.944690e01,
-        -6.944966e01,
-        -6.945240e01,
-        -6.945513e01,
-        -6.945785e01,
-        -6.946055e01,
-        -6.946324e01,
-        -6.946592e01,
-        -6.946859e01,
-        -6.947124e01,
-        -6.947387e01,
-        -6.947650e01,
-        -6.947911e01,
-        -6.948171e01,
-        -6.948429e01,
-        -6.948686e01,
-        -6.948942e01,
-        -6.949197e01,
-        -6.949450e01,
-        -6.949702e01,
-        -6.949953e01,
-        -6.950203e01,
-        -6.950451e01,
-        -6.950698e01,
-        -6.950944e01,
-        -6.951189e01,
-        -6.951432e01,
-        -6.951675e01,
-        -6.951916e01,
-        -6.952155e01,
-        -6.952394e01,
-        -6.952632e01,
-        -6.952868e01,
-        -6.953103e01,
-        -6.953337e01,
-        -6.953569e01,
-        -6.953801e01,
-        -6.954031e01,
-        -6.954261e01,
-        -6.954489e01,
-        -6.954716e01,
-        -6.954942e01,
-        -6.955166e01,
-        -6.955390e01,
-        -6.955613e01,
-        -6.955834e01,
     ]
 )
 

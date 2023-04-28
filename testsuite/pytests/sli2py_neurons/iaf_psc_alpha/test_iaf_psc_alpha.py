7cd8a9ddf7d7c90424f89ba6350b232ddbd59ca0
Robin De Schepper <robin.deschepper93@gmail.com>
2023-04-28 10:01:25 +0200

    fixed formatting and toml error

--- testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha.py
+++ testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha.py
@@ -25,7 +25,8 @@ import math
 import numpy as np
 import pytest
 import nest
-import testsimulation, testutil
+import testsimulation
+import testutil
 from scipy.special import lambertw
 
 
@@ -287,163 +288,152 @@ class TestIAFPSDCAccuracy:
 
 expected_default = np.array(
     [
-        [0.1, -70],  # <----- The earliest time dc_gen can be switched on.
-        [0.2, -70],  # <----- The DC current arrives at the neuron, it is
-        [0.3, -69.602],  # <-     reflected in the neuron's state variable y0,
-        [0.4, -69.2079],  #   |    (initial condition) but has not yet affected
-        [0.5, -68.8178],  #   |    the membrane potential.
-        [0.6, -68.4316],  #   |
-        [0.7, -68.0492],  #    --- the effect of the DC current is visible in the
-        [0.8, -67.6706],  #        membrane potential
-        [0.9, -67.2958],  #
-        [1.0, -66.9247],  #
-        [4.5, -56.0204],  #
-        [4.6, -55.7615],  #
-        [4.7, -55.5051],  #
-        [4.8, -55.2513],  #
-        [4.9, -55.0001],  #
-        [5.0, -70],  #  <---- The membrane potential crossed threshold in the
-        [5.1, -70],  #        step 4.9 ms -> 5.0 ms. The membrane potential is
-        [5.2, -70],  #        reset (no super-threshold values can be observed).
-        [5.3, -70],  #        The spike is reported at 5.0 ms
-        [5.4, -70],  #
-        [5.5, -70],  #
-        [5.6, -70],  #
-        [5.7, -70],  #
-        [5.8, -70],  #
-        [5.9, -70],  #
-        [6.0, -70],  #
-        [6.1, -70],  #
-        [6.2, -70],  #
-        [6.3, -70],  #
-        [6.4, -70],  #
-        [6.5, -70],  #
-        [6.6, -70],  #
-        [6.7, -70],  #
-        [6.8, -70],  #
-        [6.9, -70],  #
-        [7.0, -70],  #  <---- The last point in time at which the membrane potential
-        [7.1, -69.602],  #  <-    is clamped. The fact that the neuron is not refractory
-        [7.2, -69.2079],  #    |   anymore is reflected in the state variable r==0.
-        [7.3, -68.8178],  #    |   The neuron was refractory for 2.0 ms.
-        [7.4, -68.4316],  #    |
-        [7.5, -68.0492],  #    --- The membrane potential starts to increase
-        [7.6, -67.6706],  #        immediately afterwards and the neuron can generate
-        [7.7, -67.2958],  #        spikes again (at this resolution reported with time
-        [7.8, -66.9247],  #        stamp 7.1 ms on the grid)
-        [7.9, -66.5572],  #  <--
-        #     |
-        #     |
-        #     - The simulation was run for 8.0 ms.However, in the step
-        #       7.9 ms -> 8.0 ms the voltmeter necessarily receives the
-        #       voltages that occurred at time 7.9 ms (delay h).This
-        #       results in different end times of the recorded voltage
-        #       traces at different resolutions.In the current
-        #       simulation kernel there is no general cure for this
-        #       problem.One workaround is to end the simulation script
-        #       with "h Simulate", thereby making the script resolution
-        #       dependent.
+        [0.1, -70],
+        [0.2, -70],
+        [0.3, -69.602],
+        [0.4, -69.2079],
+        [0.5, -68.8178],
+        [0.6, -68.4316],
+        [0.7, -68.0492],
+        [0.8, -67.6706],
+        [0.9, -67.2958],
+        [1.0, -66.9247],
+        [4.5, -56.0204],
+        [4.6, -55.7615],
+        [4.7, -55.5051],
+        [4.8, -55.2513],
+        [4.9, -55.0001],
+        [5.0, -70],
+        [5.1, -70],
+        [5.2, -70],
+        [5.3, -70],
+        [5.4, -70],
+        [5.5, -70],
+        [5.6, -70],
+        [5.7, -70],
+        [5.8, -70],
+        [5.9, -70],
+        [6.0, -70],
+        [6.1, -70],
+        [6.2, -70],
+        [6.3, -70],
+        [6.4, -70],
+        [6.5, -70],
+        [6.6, -70],
+        [6.7, -70],
+        [6.8, -70],
+        [6.9, -70],
+        [7.0, -70],
+        [7.1, -69.602],
+        [7.2, -69.2079],
+        [7.3, -68.8178],
+        [7.4, -68.4316],
+        [7.5, -68.0492],
+        [7.6, -67.6706],
+        [7.7, -67.2958],
+        [7.8, -66.9247],
+        [7.9, -66.5572],
     ]
 )
 
 expected_i0 = np.array(
     [
-        [0.1, -69.602],  #  % <-      because the current has not yet had the
-        [0.2, -69.2079],  #  %   |     chance to influence the membrane potential.
-        [0.3, -68.8178],  #  %   |     However, the current is reflected in state
-        [0.4, -68.4316],  #  %   |     variable I0 in this initial condition of the
-        [0.5, -68.0492],  #  %   |     system.
-        [4.3, -56.0204],  #  %         affected the membrane potential.
-        [4.4, -55.7615],  #  %
-        [4.5, -55.5051],  #  %
-        [4.6, -55.2513],  #  %
-        [4.7, -55.0001],  #  %
-        [4.8, -70],  #  %
-        [4.9, -70],  #  %
-        [5.0, -70],  #  %
+        [0.1, -69.602],
+        [0.2, -69.2079],
+        [0.3, -68.8178],
+        [0.4, -68.4316],
+        [0.5, -68.0492],
+        [4.3, -56.0204],
+        [4.4, -55.7615],
+        [4.5, -55.5051],
+        [4.6, -55.2513],
+        [4.7, -55.0001],
+        [4.8, -70],
+        [4.9, -70],
+        [5.0, -70],
     ]
 )
 
 expected_i0_t = np.array(
     [
-        [1, 4.8],  # %       <-- the neuron emits a spike with time
+        [1, 4.8],
     ]
 )
 
 expected_i0_refr = np.array(
     [
-        [0.1, -69.4229],  #  %              because the current has not yet had the
-        [0.2, -68.8515],  #  %              chance to influence the membrane
-        [0.3, -68.2858],  #  %              potential (initial conditions).
-        [0.4, -67.7258],  #
-        [0.5, -67.1713],  #
-        [0.6, -66.6223],  #
-        [0.7, -66.0788],  #
-        [0.8, -65.5407],  #
-        [0.9, -65.008],  #
-        [1.0, -64.4806],  #
-        [1.1, -63.9584],  #
-        [1.2, -63.4414],  #
-        [1.3, -62.9295],  #
-        [1.4, -62.4228],  #
-        [1.5, -61.9211],  #
-        [1.6, -61.4243],  #
-        [1.7, -60.9326],  #
-        [1.8, -60.4457],  #
-        [1.9, -59.9636],  #
-        [2.0, -59.4864],  #
-        [2.1, -59.0139],  #
-        [2.2, -58.5461],  #
-        [2.3, -58.0829],  #
-        [2.4, -57.6244],  #
-        [2.5, -57.1704],  #
-        [2.6, -56.721],  #
-        [2.7, -56.276],  #
-        [2.8, -55.8355],  #
-        [2.9, -55.3993],  #
-        [3.0, -70],  #  %       <- The membrane potential crossed threshold in the
-        [3.1, -70],  #  %  |       reset (no super-threshold values can be observed).
-        [3.2, -70],  #  %  |
-        [3.3, -70],  #  %   ------ The spike is reported at 3.0 ms
-        [3.4, -70],  #
-        [3.5, -70],  #
-        [3.6, -70],  #
-        [3.7, -70],  #
-        [3.8, -70],  #
-        [3.9, -70],  #
-        [4.0, -70],  #
-        [4.1, -70],  #
-        [4.2, -70],  #
-        [4.3, -70],  #
-        [4.4, -70],  #
-        [4.5, -70],  #
-        [4.6, -70],  #
-        [4.7, -70],  #
-        [4.8, -70],  #
-        [4.9, -70],  #
-        [5.0, -70],  #  %   <- The last point in time at which the membrane potential
+        [0.1, -69.4229],
+        [0.2, -68.8515],
+        [0.3, -68.2858],
+        [0.4, -67.7258],
+        [0.5, -67.1713],
+        [0.6, -66.6223],
+        [0.7, -66.0788],
+        [0.8, -65.5407],
+        [0.9, -65.008],
+        [1.0, -64.4806],
+        [1.1, -63.9584],
+        [1.2, -63.4414],
+        [1.3, -62.9295],
+        [1.4, -62.4228],
+        [1.5, -61.9211],
+        [1.6, -61.4243],
+        [1.7, -60.9326],
+        [1.8, -60.4457],
+        [1.9, -59.9636],
+        [2.0, -59.4864],
+        [2.1, -59.0139],
+        [2.2, -58.5461],
+        [2.3, -58.0829],
+        [2.4, -57.6244],
+        [2.5, -57.1704],
+        [2.6, -56.721],
+        [2.7, -56.276],
+        [2.8, -55.8355],
+        [2.9, -55.3993],
+        [3.0, -70],
+        [3.1, -70],
+        [3.2, -70],
+        [3.3, -70],
+        [3.4, -70],
+        [3.5, -70],
+        [3.6, -70],
+        [3.7, -70],
+        [3.8, -70],
+        [3.9, -70],
+        [4.0, -70],
+        [4.1, -70],
+        [4.2, -70],
+        [4.3, -70],
+        [4.4, -70],
+        [4.5, -70],
+        [4.6, -70],
+        [4.7, -70],
+        [4.8, -70],
+        [4.9, -70],
+        [5.0, -70],
         [
             5.1,
             -69.4229,
-        ],  #  % <-   is clamped. The fact that the neuron is not refractory
-        [5.2, -68.8515],  #  %   |  anymore is reflected in the state variable r==0.
-        [5.3, -68.2858],  #  %   |  The neuron was refractory for 2.0 ms.
-        [5.4, -67.7258],  #  %   |
-        [5.5, -67.1713],  #  %    -- The membrane potential starts to increase
-        [5.6, -66.6223],  #  %       immediately afterwards (step 5ms -> 5ms + h),
-        [5.7, -66.0788],  #  %       and the neuron can generate spikes again
-        [5.8, -65.5407],  #  %       (with time stamp 5ms + h).
-        [5.9, -65.008],  #  %       The membrane trace is independent of the resolution.
-        [6.0, -64.4806],  #
-        [6.1, -63.9584],  #
-        [6.2, -63.4414],  #
-        [6.3, -62.9295],  #
-        [6.4, -62.4228],  #
-        [6.5, -61.9211],  #
-        [6.6, -61.4243],  #
-        [6.7, -60.9326],  #
-        [6.8, -60.4457],  #
-        [6.9, -59.9636],  #
+        ],
+        [5.2, -68.8515],
+        [5.3, -68.2858],
+        [5.4, -67.7258],
+        [5.5, -67.1713],
+        [5.6, -66.6223],
+        [5.7, -66.0788],
+        [5.8, -65.5407],
+        [5.9, -65.008],
+        [6.0, -64.4806],
+        [6.1, -63.9584],
+        [6.2, -63.4414],
+        [6.3, -62.9295],
+        [6.4, -62.4228],
+        [6.5, -61.9211],
+        [6.6, -61.4243],
+        [6.7, -60.9326],
+        [6.8, -60.4457],
+        [6.9, -59.9636],
     ]
 )
 
--- testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha_dc.py
+++ testsuite/pytests/sli2py_neurons/iaf_psc_alpha/test_iaf_psc_alpha_dc.py
@@ -24,7 +24,8 @@ import dataclasses
 import numpy as np
 import pytest
 import nest
-import testutil, testsimulation
+import testutil
+import testsimulation
 
 
 @dataclasses.dataclass
@@ -102,7 +103,7 @@ class TestIAFPSCAlphaDC:
         nest.Connect(simulation.dc_generator, simulation.neuron, syn_spec=dc_gen_spec)
 
         results = simulation.simulate()
-        actual, expected = testutil.get_comparable_timesamples(results, expect_auto)
+        actual, expected = testutil.get_comparable_timesamples(results, expect_aligned)
         assert actual == expected
 
     @pytest.mark.parametrize(
@@ -128,138 +129,87 @@ class TestIAFPSCAlphaDC:
 
 expect_default = np.array(
     [
-        [0.1, -70],  #   <--------   is at rest (initial condition).
-        [0.2, -70],  # <-------   |
-        [
-            0.3,
-            -69.602,
-        ],  # <-  |   - In the first update step 0ms  -> 0.1 ms, i.e. at
-        [
-            0.4,
-            -69.2079,
-        ],  #  | |     the earliest possible time, the current generator
-        [
-            0.5,
-            -68.8178,
-        ],  #  | |     is switched on and emits a current event with time
-        [0.6, -68.4316],  #  | |     stamp 0.1 ms.
-        [0.7, -68.0492],  #  | |
-        [
-            0.8,
-            -67.6706,
-        ],  #  |  ---- After the minimal delay of 1 computation time step,
-        [
-            0.9,
-            -67.2958,
-        ],  #  |       the current affects the state of the neuron. This is
-        [
-            1.0,
-            -66.9247,
-        ],  #  |       reflected in the neuron's state variable y0 (initial
-        [
-            1.1,
-            -66.5572,
-        ],  #  |       condition) but has not yet affected the membrane
-        [1.2, -66.1935],  #  |       potential.
-        [1.3, -65.8334],  #  |
-        [
-            1.4,
-            -65.4768,
-        ],  #   ------ The effect of the DC current, influencing the neuron
-        [
-            1.5,
-            -65.1238,
-        ],  #          for 0.1 ms now, becomes visible in the membrane potential.
-        [1.6, -64.7743],  #
+        [0.1, -70],
+        [0.2, -70],
+        [0.3, -69.602],
+        [0.4, -69.2079],
+        [0.5, -68.8178],
+        [0.6, -68.4316],
+        [0.7, -68.0492],
+        [0.8, -67.6706],
+        [0.9, -67.2958],
+        [1.0, -66.9247],
+        [1.1, -66.5572],
+        [1.2, -66.1935],
+        [1.3, -65.8334],
+        [1.4, -65.4768],
+        [1.5, -65.1238],
+        [1.6, -64.7743],
     ]
 )
 
 
 expect_aligned = np.array(
     [
-        [2.5, -70],  #
-        [2.6, -70],  #
-        [2.7, -70],  #
-        [2.8, -70],  #
-        [2.9, -70],  #
-        [3.0, -70],  #  %  <---- Current starts to affect
-        [3.1, -69.602],  #   %   neuron (visible in state variable
-        [3.2, -69.2079],  #  %   y0). This is the desired onset of
-        [3.3, -68.8178],  #  %    t= 3.0 ms.
-        [3.4, -68.4316],  #
-        [3.5, -68.0492],  #
-        [3.6, -67.6706],  #
-        [3.7, -67.2958],  #
-        [3.8, -66.9247],  #
-        [3.9, -66.5572],  #
-        [4.0, -66.1935],  #
-        [4.1, -65.8334],  #
-        [4.2, -65.4768],  #
-    ]
-)
-
-
-expect_auto = np.array(
-    [
-        [2.5, -70],  #
-        [2.6, -70],  #
-        [2.7, -70],  #
-        [2.8, -70],  #
-        [2.9, -70],  #
-        [3.0, -70],  #  %  <---- Current starts to affect
-        [3.1, -69.602],  #   %   neuron (visible in state variable
-        [3.2, -69.2079],  #  %   y0). This is the desired onset of
-        [3.3, -68.8178],  #  %    t= 3.0 ms.
-        [3.4, -68.4316],  #
-        [3.5, -68.0492],  #
-        [3.6, -67.6706],  #
-        [3.7, -67.2958],  #
-        [3.8, -66.9247],  #
-        [3.9, -66.5572],  #
-        [4.0, -66.1935],  #
-        [4.1, -65.8334],  #
-        [4.2, -65.4768],  #
+        [2.5, -70],
+        [2.6, -70],
+        [2.7, -70],
+        [2.8, -70],
+        [2.9, -70],
+        [3.0, -70],
+        [3.1, -69.602],
+        [3.2, -69.2079],
+        [3.3, -68.8178],
+        [3.4, -68.4316],
+        [3.5, -68.0492],
+        [3.6, -67.6706],
+        [3.7, -67.2958],
+        [3.8, -66.9247],
+        [3.9, -66.5572],
+        [4.0, -66.1935],
+        [4.1, -65.8334],
+        [4.2, -65.4768],
     ]
 )
 
 
 expect_stop = np.array(
     [
-        [2.5, -70],  #
-        [2.6, -70],  #
-        [2.7, -70],  #
-        [2.8, -70],  #
-        [2.9, -70],  #
-        [3.0, -70],  #          <-- current starts to affect
-        [3.1, -69.602],  #  %         neuron (visible in state variable
-        [3.2, -69.2079],  #  %       y0). This is the desired onset of
-        [3.3, -68.8178],  #  %       t= 3.0 ms.
-        [3.4, -68.4316],  #
-        [3.5, -68.0492],  #
-        [3.6, -67.6706],  #
-        [3.7, -67.2958],  #
-        [3.8, -66.9247],  #
-        [3.9, -66.5572],  #
-        [4.0, -66.1935],  #
-        [4.1, -65.8334],  #
-        [4.2, -65.4768],  #
-        [4.3, -65.1238],  #
-        [4.4, -64.7743],  #
-        [4.5, -64.4283],  #
-        [4.6, -64.0858],  #
-        [4.7, -63.7466],  #
-        [4.8, -63.4108],  #
-        [4.9, -63.0784],  #
-        [5.0, -62.7492],  #    <-- current ends to affect neuron
-        [5.1, -62.8214],  #        (visible in state variable y0),
-        [5.2, -62.8928],  #        the highest voltage is observed.
-        [5.3, -62.9635],  #        The current was applied for the desired
-        [5.4, -63.0335],  #        duration (2ms).
-        [5.5, -63.1029],  #
-        [5.6, -63.1715],  #
-        [5.7, -63.2394],  #
-        [5.8, -63.3067],  #
-        [5.9, -63.3733],  #
-        [6.0, -63.4392],  #
+        [2.5, -70],
+        [2.6, -70],
+        [2.7, -70],
+        [2.8, -70],
+        [2.9, -70],
+        [3.0, -70],
+        [3.1, -69.602],
+        [3.2, -69.2079],
+        [3.3, -68.8178],
+        [3.4, -68.4316],
+        [3.5, -68.0492],
+        [3.6, -67.6706],
+        [3.7, -67.2958],
+        [3.8, -66.9247],
+        [3.9, -66.5572],
+        [4.0, -66.1935],
+        [4.1, -65.8334],
+        [4.2, -65.4768],
+        [4.3, -65.1238],
+        [4.4, -64.7743],
+        [4.5, -64.4283],
+        [4.6, -64.0858],
+        [4.7, -63.7466],
+        [4.8, -63.4108],
+        [4.9, -63.0784],
+        [5.0, -62.7492],
+        [5.1, -62.8214],
+        [5.2, -62.8928],
+        [5.3, -62.9635],
+        [5.4, -63.0335],
+        [5.5, -63.1029],
+        [5.6, -63.1715],
+        [5.7, -63.2394],
+        [5.8, -63.3067],
+        [5.9, -63.3733],
+        [6.0, -63.4392],
     ]
 )

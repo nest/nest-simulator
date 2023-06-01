537a08722ddcb0e5f9cbe74f9f20e4d248b2d76b
Anthony Lee <anthony8lee@gmail.com>
2023-06-01 11:23:14 -0600

    Update glif_cond.h - Latex formatting

--- models/glif_cond.h
+++ models/glif_cond.h
@@ -103,8 +103,8 @@ parameter setting of voltage_reset_fraction and voltage_reset_add may lead to th
 situation that voltage is bigger than threshold after reset. In this case, the neuron
 will continue to spike until the end of the simulation regardless the stimulated inputs.
 We recommend the setting of the parameters of these three models to follow the
-condition of :math:`(E_L + voltage_reset_fraction * ( V_th - E_L ) + voltage_reset_add)
-< (V_th + th_spike_add)`.
+condition of :math:`(E_L + \text{voltage_reset_fraction} * ( V_{th} - E_L ) + \text{voltage_reset_add})
+< (V_{th} + \text{th_spike_add})`.
 
 Parameters
 ++++++++++

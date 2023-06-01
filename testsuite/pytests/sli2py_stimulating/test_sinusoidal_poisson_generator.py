d88242f51fae5ce57ddd0cb935fdf9efec21c8b0
Nicolai Haug <nicolaihaug@yahoo.com>
2023-06-01 02:35:00 +0200

    Port test_sinusoidal_poisson_generator from SLI-2-Py

--- testsuite/pytests/sli2py_stimulating/test_sinusoidal_poisson_generator.py
+++ testsuite/pytests/sli2py_stimulating/test_sinusoidal_poisson_generator.py
@@ -23,6 +23,8 @@
 Test basic properties of `sinusoidal_poisson_generator`.
 """
 
+import numpy as np
+import numpy.testing as nptest
 import pytest
 
 import nest
@@ -83,7 +85,7 @@ def test_set_individual_spike_trains_on_creation():
 
 def test_set_individual_spike_trains_on_copy_model():
     """
-    Test whether the set `individual_spike_trains` is passed on `CopyModel`.
+    Test whether the set `individual_spike_trains` is inherited on `CopyModel`.
     """
 
     nest.CopyModel(
@@ -105,3 +107,152 @@ def test_set_individual_spike_trains_on_instance():
 
     with pytest.raises(nest.kernel.NESTErrors.BadProperty):
         sspg.individual_spike_trains = False
+
+
+@pytest.mark.skipif_missing_threads()
+@pytest.mark.parametrize("individual_spike_trains", [False, True])
+@pytest.mark.parametrize("num_threads", [1, 2])
+def test_sinusoidal_poisson_generator_with_spike_recorder(
+    num_threads, individual_spike_trains
+):
+    """
+    Test spike recording with both `False` and `True` `individual_spike_trains`.
+
+    The test builds a network with `num_threads x 4` parrot neurons that
+    receives spikes from a `sinusoidal_poisson_generator`. A spike recorder is
+    connected to each parrot neuron. The test ensures that different targets
+    (on the same or different threads) receives identical spike trains if
+    `individual_spike_trains` is false and different spike trains otherwise.
+    """
+
+    nest.local_num_threads = num_threads
+
+    nest.SetDefaults(
+        "sinusoidal_poisson_generator",
+        {
+            "rate": 100,
+            "amplitude": 50.0,
+            "frequency": 10.0,
+            "individual_spike_trains": individual_spike_trains,
+        },
+    )
+
+    nrns_per_thread = 4
+    total_num_nrns = num_threads * nrns_per_thread
+
+    parrots = nest.Create("parrot_neuron", total_num_nrns)
+    sspg = nest.Create("sinusoidal_poisson_generator")
+    srecs = nest.Create("spike_recorder", total_num_nrns)
+
+    nest.Connect(sspg, parrots)
+    nest.Connect(parrots, srecs, "one_to_one")
+
+    nest.Simulate(500.0)
+
+    # Nested list of recorded spike times from each sender
+    spikes_all_nrns = [sender_event["times"] for sender_event in srecs.events]
+
+    # Check that we actually obtained a spike times array for each neuron
+    assert len(spikes_all_nrns) == total_num_nrns
+
+    # The below will be true if the spike times from all neurons are identical
+    # and false otherwise
+    actual = all(
+        [
+            np.array_equal(spikes_nrn_i, spikes_nrn_j)
+            for spikes_nrn_i, spikes_nrn_j in zip(spikes_all_nrns, spikes_all_nrns[1:])
+        ]
+    )
+
+    excpected = not individual_spike_trains
+
+    assert actual == excpected
+
+
+@pytest.mark.skipif_missing_threads()
+@pytest.mark.parametrize("individual_spike_trains", [False, True])
+@pytest.mark.parametrize("num_threads", [1, 2])
+def test_sinusoidal_poisson_generator_with_multimeter(
+    num_threads, individual_spike_trains
+):
+    """
+    Test multimeter recording with both `False` and `True` `individual_spike_trains`.
+
+    The test builds a network with `num_threads x 4` parrot neurons that
+    receives spikes from a `sinusoidal_poisson_generator`. A `multimeter` is
+    then connected to the generator and the system simulated. The test ensures
+    that a `multimeter` can be connected to record the rate, independent of
+    `individual_spike_tains`. Since only a single trace should be returned,
+    the test checks that the multimeter recording contains the expected number
+    of data points.
+    """
+
+    nest.local_num_threads = num_threads
+    T_sim = 100.0
+
+    nest.SetDefaults(
+        "sinusoidal_poisson_generator",
+        {
+            "rate": 100,
+            "amplitude": 50.0,
+            "frequency": 10.0,
+            "individual_spike_trains": individual_spike_trains,
+        },
+    )
+
+    nrns_per_thread = 4
+    total_num_nrns = num_threads * nrns_per_thread
+
+    parrots = nest.Create("parrot_neuron", total_num_nrns)
+    sspg = nest.Create("sinusoidal_poisson_generator")
+    mm = nest.Create("multimeter", {"record_from": ["rate"]})
+
+    nest.Connect(sspg, parrots)
+    nest.Connect(mm, sspg)
+
+    nest.Simulate(T_sim)
+
+    times = mm.get("events")["times"]
+    rates = mm.get("events")["rate"]
+
+    # Check that times and rates contain ndata points
+    expected_ndata = T_sim - 1
+    assert len(times) == expected_ndata
+    assert len(rates) == expected_ndata
+
+
+def test_sinusoidal_poisson_generator_rate_profile():
+    """
+    Test recorded rate of `sinusoidal_poisson_generator` against expectation.
+
+    The test checks that the recorded rate profile with `multimeter` is the
+    same as the analytical expectation.
+    """
+
+    dc = 1.0
+    ac = 0.5
+    freq = 10.0
+    phi = 2.0
+
+    nest.SetDefaults(
+        "sinusoidal_poisson_generator",
+        {"rate": dc, "amplitude": ac, "frequency": freq, "phase": phi / np.pi * 180},
+    )
+
+    parrots = nest.Create("parrot_neuron")
+    sspg = nest.Create("sinusoidal_poisson_generator")
+    mm = nest.Create("multimeter", {"record_from": ["rate"]})
+
+    nest.Connect(sspg, parrots)
+    nest.Connect(mm, sspg)
+
+    nest.Simulate(100.0)
+
+    times = mm.get("events")["times"]
+    actual_rates = mm.get("events")["rate"]
+
+    scaled_times = times * 2 * np.pi * freq / 1000
+    shifted_times = scaled_times + phi
+    expected_rates = np.sin(shifted_times) * ac + dc
+
+    nptest.assert_allclose(actual_rates, expected_rates)

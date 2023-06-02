26636d0f9a55a5393bd914295591c9bdf3dcf911
Nicolai Haug <nicolaihaug@yahoo.com>
2023-06-02 13:41:11 +0200

    WIP [skip ci]

---
+++ testsuite/pytests/sli2py_regressions/test_issue_410.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# test_issue_410.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Regression test for Issue #410 (GitHub).
+
+This test ensures that models with precise timing handle input spikes
+arriving at exactly the same times correctly.
+"""
+
+import pytest
+
+import nest
+
+
+@pytest.fixture(autouse=True)
+def reset():
+    nest.ResetKernel()

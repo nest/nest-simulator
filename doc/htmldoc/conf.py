51f73c3492445953a27c7a067b1a689b2b6df4bf
Steffen Graber <s.graber@fz-juelich.de>
2023-06-13 05:45:17 +0000

    Fix git dir
    (cherry picked from commit 275484694eb57682e2616d77e5290a670e0e8761)

--- doc/htmldoc/conf.py
+++ doc/htmldoc/conf.py
@@ -393,7 +393,7 @@ def patch_documentation(patch_url):
 
     print("Preparing patch...")
     try:
-        git_dir = f"{repo_root_dir} /.git"
+        git_dir = f"{repo_root_dir}/.git"
         print(f"  Git dir: {git_dir}")
 
         git_hash = subprocess.check_output(

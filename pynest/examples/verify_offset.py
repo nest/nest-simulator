7d65f8a221095da3aa18e3c9159d19cc425538af
Nicolai Haug <nicolaihaug@yahoo.com>
2023-06-29 11:06:50 +0200

    Fix copyright headers

--- pynest/examples/verify_offset.py
+++ pynest/examples/verify_offset.py
@@ -1,7 +1,30 @@
-import nest
+# -*- coding: utf-8 -*-
+#
+# verify_offset.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
 from pprint import pprint
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+import nest
 
 # TODO: This is just a test script, remove from repository before PR
 
@@ -10,29 +33,29 @@ nest.ResetKernel()
 weight = 250.0
 delay = 0.1
 
-nest.SetDefaults('iaf_tsodyks', {'I_e': 0.})
+nest.SetDefaults("iaf_tsodyks", {"I_e": 0.0})
 
-spikegen = nest.Create('spike_generator', 1)
-nest.SetStatus(spikegen, {"spike_times": [1.]})
+spikegen = nest.Create("spike_generator", 1)
+nest.SetStatus(spikegen, {"spike_times": [1.0]})
 
 # iaf_tsodyks with static synapse
-#nrn_pre = nest.Create("iaf_psc_exp_ps", 1)
+# nrn_pre = nest.Create("iaf_psc_exp_ps", 1)
 nrn_pre = nest.Create("iaf_tsodyks", 1)
 nrn_post = nest.Create("iaf_tsodyks", 1)
 
 
 # Set huge synaptic weight, control spike releases by refractory period
-nest.SetStatus(nrn_pre, {'t_ref': 10000.})
+nest.SetStatus(nrn_pre, {"t_ref": 10000.0})
 
-#m = nest.Create('multimeter', 1)
-#nest.SetStatus(m, {"record_from": ["V_m", "I_syn_ex"]})
+# m = nest.Create('multimeter', 1)
+# nest.SetStatus(m, {"record_from": ["V_m", "I_syn_ex"]})
 
 nest.CopyModel("static_synapse", "syn_static", {"weight": weight, "delay": delay})
-nest.CopyModel("static_synapse", "spike_forcing_syn", {"weight": 10000000., "delay": delay})
+nest.CopyModel("static_synapse", "spike_forcing_syn", {"weight": 10000000.0, "delay": delay})
 
 nest.Connect(spikegen, nrn_pre, syn_spec="spike_forcing_syn")
 nest.Connect(nrn_pre, nrn_post, syn_spec="syn_static")
 
-#nest.Connect(m, nrn_post)
+# nest.Connect(m, nrn_post)
 
 nest.Simulate(10)

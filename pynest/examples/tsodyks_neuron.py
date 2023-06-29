6b25d26dd57f99804249486b34c7d0c5025a65cb
Nicolai Haug <nicolaihaug@yahoo.com>
2023-06-29 11:03:01 +0200

    Fix formatting

--- pynest/examples/tsodyks_neuron.py
+++ pynest/examples/tsodyks_neuron.py
@@ -19,20 +19,22 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-import nest
 from pprint import pprint
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+import nest
 
 nest.ResetKernel()
 
 weight = 250.0
 delay = 0.1
 
-nest.SetDefaults('iaf_tsodyks', {'I_e': 0.})
+nest.SetDefaults("iaf_tsodyks", {"I_e": 0.0})
 
-spikegen = nest.Create('spike_generator', 1)
-nest.SetStatus(spikegen, {"spike_times": [1., 5.]})
+spikegen = nest.Create("spike_generator", 1)
+nest.SetStatus(spikegen, {"spike_times": [1.0, 5.0]})
 
 # iaf_tsodyks with static synapse
 nrn_tsodyks_pre = nest.Create("iaf_tsodyks", 1)
@@ -40,13 +42,13 @@ nrn_tsodyks_post = nest.Create("iaf_tsodyks", 1)
 
 
 # Set huge synaptic weight, control spike releases by refractory period
-#nest.SetStatus(nrn_tsodyks_pre, {'t_ref': 10000.})
+# nest.SetStatus(nrn_tsodyks_pre, {'t_ref': 10000.})
 
-m_tsodyks = nest.Create('multimeter', 1)
+m_tsodyks = nest.Create("multimeter", 1)
 nest.SetStatus(m_tsodyks, {"record_from": ["V_m", "I_syn_ex"]})
 
 nest.CopyModel("static_synapse", "syn_static", {"weight": weight, "delay": delay})
-nest.CopyModel("static_synapse", "spike_forcing_syn", {"weight": 10000000., "delay": delay})
+nest.CopyModel("static_synapse", "spike_forcing_syn", {"weight": 10000000.0, "delay": delay})
 
 nest.Connect(spikegen, nrn_tsodyks_pre, syn_spec="spike_forcing_syn")
 nest.Connect(nrn_tsodyks_pre, nrn_tsodyks_post, syn_spec="syn_static")
@@ -57,20 +59,21 @@ nest.Connect(m_tsodyks, nrn_tsodyks_post)
 # iaf_psc_exp with tsodyks_synapse
 nrn_exp_pre = nest.Create("iaf_psc_exp", 1)
 nrn_exp_post = nest.Create("iaf_psc_exp", 1)
-#nest.SetStatus(nrn_exp_pre, {'t_ref': 10000.})
+# nest.SetStatus(nrn_exp_pre, {'t_ref': 10000.})
 
-m_exp = nest.Create('multimeter', 1)
+m_exp = nest.Create("multimeter", 1)
 nest.SetStatus(m_exp, {"record_from": ["V_m", "I_syn_ex"]})
-syn_param = {"tau_psc": 3.0,
-             "tau_rec": 400.0,
-             "tau_fac": 1000.0,
-             "U": 0.5,
-             "delay": delay,
-             "weight": weight,
-             "u": 0.0,
-             "x": 0.0,
-             "y": 0.0,
-             }
+syn_param = {
+    "tau_psc": 3.0,
+    "tau_rec": 400.0,
+    "tau_fac": 1000.0,
+    "U": 0.5,
+    "delay": delay,
+    "weight": weight,
+    "u": 0.0,
+    "x": 0.0,
+    "y": 0.0,
+}
 nest.CopyModel("tsodyks_synapse", "syn_tsodyks", syn_param)
 nest.Connect(spikegen, nrn_exp_pre, syn_spec="spike_forcing_syn")
 nest.Connect(nrn_exp_pre, nrn_exp_post, syn_spec="syn_tsodyks")
@@ -89,15 +92,15 @@ pprint(r_exp)
 
 fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8, 6), tight_layout=True)
 
-axes[0].plot(r_tsodyks['V_m'], label='iaf_tsodyks / static_syn')
-axes[0].plot(r_exp['V_m'], label='iaf_psc_exp / tsodyks_syn')
-axes[0].set(xlabel='time', ylabel='V_m')
-axes[0].legend(loc='upper right')
+axes[0].plot(r_tsodyks["V_m"], label="iaf_tsodyks / static_syn")
+axes[0].plot(r_exp["V_m"], label="iaf_psc_exp / tsodyks_syn")
+axes[0].set(xlabel="time", ylabel="V_m")
+axes[0].legend(loc="upper right")
 
-axes[1].plot(r_tsodyks['I_syn_ex'], label='iaf_tsodyks / static_syn')
-axes[1].plot(r_exp['I_syn_ex'], label='iaf_psc_exp / tsodyks_syn')
-axes[1].set(xlabel='time', ylabel='I_syn_ex')
-axes[1].legend(loc='upper right')
+axes[1].plot(r_tsodyks["I_syn_ex"], label="iaf_tsodyks / static_syn")
+axes[1].plot(r_exp["I_syn_ex"], label="iaf_psc_exp / tsodyks_syn")
+axes[1].set(xlabel="time", ylabel="I_syn_ex")
+axes[1].legend(loc="upper right")
 
 
 plt.show()

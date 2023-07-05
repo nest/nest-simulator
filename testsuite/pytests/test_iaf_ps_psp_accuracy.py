ac5a513d60631f10313aa93d7eebd43a57d442af
janeirik <>
2023-07-05 15:34:26 +0200

    fix copyright formatting

--- testsuite/pytests/test_iaf_ps_psp_accuracy.py
+++ testsuite/pytests/test_iaf_ps_psp_accuracy.py
@@ -5,6 +5,7 @@
 # This file is part of NEST.
 #
 # Copyright (C) 2004 The NEST Initiative
+#
 # NEST is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or

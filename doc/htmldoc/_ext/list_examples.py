961e55deb664125e3b5d395197cbd67992d32a3f
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-07-12 23:26:20 +0200

    create extension to add examples to models

---
+++ doc/htmldoc/_ext/list_examples.py
@@ -0,0 +1,53 @@
+from docutils import nodes
+from docutils.parsers.rst import Directive
+import json
+from sphinx.locale import _
+#from sphinx.util.docutils import SphinxDirective
+#from sphinx.util.nodes import nested_parse_with_titles
+
+
+class listExamplesDirective(Directive):
+# Create a directive that requires one argument
+# (.. HelloWorld:: argument), which is the model name
+
+    has_content = False
+    required_arguments = 1
+    option_spec = {}
+
+    def run(self):
+        my_arg = self.arguments[0]
+
+        # TODO: Add script to generate this list for this extension
+        with open("_ext/model_match_examples.json") as json_file:
+            examples_data = json.load(json_file)
+
+        content = []
+        # bullet list equivalent to HTML ul
+        bullet_list = nodes.bullet_list()
+        for key, values in examples_data.items():
+            if key == my_arg:
+                for value in values:
+                    link_text = value.split("/")[-1]
+                    list_item = nodes.list_item()
+                    newnode = nodes.reference(value, link_text)
+                    newnode["refdocname"] = "auto_examples/"
+                    newnode["refuri"] = value + ".html"
+                    para = nodes.paragraph()
+                    para.append(newnode)
+                    list_item.append(para)
+                    bullet_list += list_item
+
+
+        return [bullet_list]
+
+
+
+def setup(app):
+    app.add_directive("listExamples", listExamplesDirective)
+
+    return {
+        "version": "0.1",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
+
--- models/iaf_psc_alpha.h
+++ models/iaf_psc_alpha.h
@@ -149,6 +149,12 @@ See also
 
 iaf_psc_delta, iaf_psc_exp, iaf_cond_exp
 
+
+Examples using this model
++++++++++++++++++++++++++
+
+.. listExamples:: iaf_psc_alpha
+
 EndUserDocs */
 
 class iaf_psc_alpha : public ArchivingNode

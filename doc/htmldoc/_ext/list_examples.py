2472ee4827b911d7f50eb82249ff003cd1c2f115
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-07-13 09:01:42 +0200

    fix ci errors, remove unneedd imports

--- doc/htmldoc/_ext/list_examples.py
+++ doc/htmldoc/_ext/list_examples.py
@@ -1,14 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# list_examples.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
 from docutils import nodes
 from docutils.parsers.rst import Directive
 import json
-from sphinx.locale import _
-from sphinx.util.docutils import SphinxDirective
-from sphinx.util.nodes import nested_parse_with_titles
 
 
 class ListExamplesDirective(Directive):
-# Create a directive that requires one argument
-# (.. HelloWorld:: argument), which is the model name
+    # Create a directive that requires one argument
+    # (.. helloworld:: argument), which is the model name
 
     has_content = False
     required_arguments = 1
@@ -40,7 +58,6 @@ class ListExamplesDirective(Directive):
                     list_item.append(para)
                     bullet_list += list_item
 
-
         return [bullet_list]
 
 
@@ -53,4 +70,3 @@ def setup(app):
         "parallel_read_safe": True,
         "parallel_write_safe": True,
     }
-

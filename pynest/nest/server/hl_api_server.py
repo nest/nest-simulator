e38128b995c9f7ec018be5e441c7c344743e5d9d
Sebastian Spreizer <spreizer@web.de>
2023-07-18 12:57:40 +0200

    Resolve merge conflicts

--- pynest/nest/server/hl_api_server.py
+++ pynest/nest/server/hl_api_server.py
@@ -63,11 +63,11 @@ RESTRICTION_DISABLED = get_boolean_environ('NEST_SERVER_DISABLE_RESTRICTION')
 EXCEPTION_ERROR_STATUS = 400
 
 __all__ = [
-    "app",
-    "do_exec",
-    "set_mpi_comm",
-    "run_mpi_app",
-    "nestify",
+    'app',
+    'do_exec',
+    'set_mpi_comm',
+    'run_mpi_app',
+    'nestify',
 ]
 
 app = Flask(__name__)
@@ -120,9 +120,6 @@ def setup_auth():
         import hashlib  # noqa
         import hmac  # noqa
 
-        if ACCESS_TOKEN:
-            self._hash = ACCESS_TOKEN
-
         # Find our reference to the current function in the garbage collector.
         frame = inspect.currentframe()
         code = frame.f_code
@@ -137,6 +134,10 @@ def setup_auth():
                         if len(funcs) > 1:
                             return ("Unauthorized", 403)
         self = funcs[0]
+
+        if ACCESS_TOKEN:
+            self._hash = ACCESS_TOKEN
+
         # Use the salted hash (unless `PYTHONHASHSEED` is fixed) of the location of this
         # function in the Python heap and the current timestamp to create a SHA512 hash.
         if not hasattr(self, "_hash"):
@@ -178,78 +179,6 @@ del setup_auth
 print( 80 * '*')
 
 
-@app.before_request
-def setup_auth():
-    """
-    Authentication function that generates and validates the Authorization header with a
-    bearer token.
-
-    Cleans up references to itself and the running `app` from this module, as it may be
-    accessible when the code execution sandbox fails.
-    """
-    try:
-        # Import the modules inside of the auth function, so that if they fail the auth
-        # returns a forbidden error.
-        import inspect  # noqa
-        import gc  # noqa
-        import time  # noqa
-        import hashlib  # noqa
-        import hmac  # noqa
-
-        # Find our reference to the current function in the garbage collector.
-        frame = inspect.currentframe()
-        code = frame.f_code
-        globs = frame.f_globals
-        functype = type(lambda: 0)
-        funcs = []
-        for func in gc.get_referrers(code):
-            if type(func) is functype:
-                if getattr(func, "__code__", None) is code:
-                    if getattr(func, "__globals__", None) is globs:
-                        funcs.append(func)
-                        if len(funcs) > 1:
-                            return ("Unauthorized", 403)
-        self = funcs[0]
-        # Use the salted hash (unless `PYTHONHASHSEED` is fixed) of the location of this
-        # function in the Python heap and the current timestamp to create a SHA512 hash.
-        if not hasattr(self, "_hash"):
-            hasher = hashlib.sha512()
-            hasher.update(str(hash(id(self))).encode("utf-8"))
-            hasher.update(str(time.perf_counter()).encode("utf-8"))
-            self._hash = hasher.hexdigest()
-            if not DISABLE_AUTHENTICATION:
-                print("")
-                print("    Bearer token to login to the NEST server with: ", self._hash)
-                print("")
-        # The first time we hit the line below is when below the function definition we
-        # call `setup_auth` without any Flask request existing yet, so the function errors
-        # and exits here after generating and storing the auth hash.
-        auth = request.headers.get("Authorization", None)
-        # We continue here the next time this function is called, before the Flask app
-        # handles the first request. At that point we also remove this module's reference
-        # to the running app.
-        try:
-            del globals()["app"]
-        except KeyError:
-            pass
-        # Things get more straightforward here: Every time a request is handled, compare
-        # the Authorization header to the hash, with a constant-time algorithm to avoid
-        # timing attacks.
-        if not (
-            DISABLE_AUTHENTICATION
-            or hmac.compare_digest(auth, f"Bearer {self._hash}")
-        ):
-            return ("Unauthorized", 403)
-    # DON'T LINT! Intentional bare except clause! Even `KeyboardInterrupt` and
-    # `SystemExit` exceptions should not bypass authentication!
-    except:  # noqa
-        return ("Unauthorized", 403)
-
-
-setup_auth()
-del setup_auth
-
-
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({
@@ -260,7 +189,7 @@ def index():
 
 def do_exec(args, kwargs):
     try:
-        source_code = kwargs.get("source", "")
+        source_code = kwargs.get('source', '')
         source_cleaned = clean_code(source_code)
 
         locals_ = dict()
@@ -271,7 +200,7 @@ def do_exec(args, kwargs):
                 globals_.update(get_modules_from_env())
                 exec(source_cleaned, globals_, locals_)
             if len(stdout) > 0:
-                response["stdout"] = "\n".join(stdout)
+                response['stdout'] = '\n'.join(stdout)
         else:
             code = RestrictedPython.compile_restricted(source_cleaned, "<inline>", "exec")  # noqa
             globals_ = get_restricted_globals()
@@ -280,10 +209,10 @@ def do_exec(args, kwargs):
             if "_print" in locals_:
                 response["stdout"] = "".join(locals_["_print"].txt)
 
-        if "return" in kwargs:
-            if isinstance(kwargs["return"], list):
+        if 'return' in kwargs:
+            if isinstance(kwargs['return'], list):
                 data = dict()
-                for variable in kwargs["return"]:
+                for variable in kwargs['return']:
                     data[variable] = locals_.get(variable, None)
             else:
                 data = locals_.get(kwargs["return"], None)
@@ -297,7 +226,7 @@ def do_exec(args, kwargs):
 
 
 def log(call_name, msg):
-    msg = f"==> MASTER 0/{time.time():.7f} ({call_name}): {msg}"
+    msg = f'==> MASTER 0/{time.time():.7f} ({call_name}): {msg}'
     print(msg, flush=True)
 
 
@@ -325,31 +254,32 @@ def do_call(call_name, args=[], kwargs={}):
         assert mpi_comm.Get_rank() == 0
 
     if mpi_comm is not None:
-        log(call_name, "sending call bcast")
+        log(call_name, 'sending call bcast')
         mpi_comm.bcast(call_name, root=0)
         data = (args, kwargs)
-        log(call_name, f"sending data bcast, data={data}")
+        log(call_name, f'sending data bcast, data={data}')
         mpi_comm.bcast(data, root=0)
 
-    if call_name == "exec":
+    if call_name == 'exec':
         master_response = do_exec(args, kwargs)
     else:
         call, args, kwargs = nestify(call_name, args, kwargs)
-        log(call_name, f"local call, args={args}, kwargs={kwargs}")
+        log(call_name, f'local call, args={args}, kwargs={kwargs}')
         master_response = call(*args, **kwargs)
 
     response = [master_response]
     if mpi_comm is not None:
-        log(call_name, "waiting for response gather")
+        log(call_name, 'waiting for response gather')
         response = mpi_comm.gather(response[0], root=0)
-        log(call_name, f"received response gather, data={response}")
+        log(call_name, f'received response gather, data={response}')
 
     return combine(call_name, response)
 
 
 @app.route('/exec', methods=['GET', 'POST'])
 def route_exec():
-    """Route to execute script in Python."""
+    """ Route to execute script in Python.
+    """
 
     if EXEC_CALL_ENABLED:
         args, kwargs = get_arguments(request)
@@ -373,13 +303,15 @@ nest_calls.sort()
 
 @app.route('/api', methods=['GET'])
 def route_api():
-    """Route to list call functions in NEST."""
+    """ Route to list call functions in NEST.
+    """
     return jsonify(nest_calls)
 
 
 @app.route('/api/<call>', methods=['GET', 'POST'])
 def route_api_call(call):
-    """Route to call function in NEST."""
+    """ Route to call function in NEST.
+    """
     print(f"\n{'='*40}\n", flush=True)
     args, kwargs = get_arguments(request)
     log("route_api_call", f"call={call}, args={args}, kwargs={kwargs}")
@@ -391,7 +323,6 @@ def route_api_call(call):
 # Helpers for the server
 # ----------------------
 
-
 class Capturing(list):
     """ Monitor stdout contents i.e. print. """
 
@@ -402,18 +333,19 @@ class Capturing(list):
 
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio  # free up some memory
+        del self._stringio    # free up some memory
         sys.stdout = self._stdout
 
 
 def clean_code(source):
-    codes = source.split("\n")
-    code_cleaned = filter(lambda code: not (code.startswith("import") or code.startswith("from")), codes)  # noqa
-    return "\n".join(code_cleaned)
+    codes = source.split('\n')
+    code_cleaned = filter(lambda code: not (code.startswith('import') or code.startswith('from')), codes)  # noqa
+    return '\n'.join(code_cleaned)
 
 
 def get_arguments(request):
-    """Get arguments from the request."""
+    """ Get arguments from the request.
+    """
     args, kwargs = [], {}
     if request.is_json:
         json = request.get_json()
@@ -423,16 +355,16 @@ def get_arguments(request):
             args = json
         elif isinstance(json, dict):
             kwargs = json
-            if "args" in kwargs:
-                args = kwargs.pop("args")
+            if 'args' in kwargs:
+                args = kwargs.pop('args')
     elif len(request.form) > 0:
-        if "args" in request.form:
-            args = request.form.getlist("args")
+        if 'args' in request.form:
+            args = request.form.getlist('args')
         else:
             kwargs = request.form.to_dict()
     elif len(request.args) > 0:
-        if "args" in request.args:
-            args = request.args.getlist("args")
+        if 'args' in request.args:
+            args = request.args.getlist('args')
         else:
             kwargs = request.args.to_dict()
     return list(args), kwargs
@@ -465,8 +397,8 @@ def get_modules_from_env():
 
 
 def get_or_error(func):
-    """Wrapper to get data and status."""
-
+    """ Wrapper to get data and status.
+    """
     def func_wrapper(call, args, kwargs):
         try:
             return func(call, args, kwargs)
@@ -478,8 +410,8 @@ def get_or_error(func):
 
 
 def get_restricted_globals():
-    """Get restricted globals for exec function."""
-
+    """ Get restricted globals for exec function.
+    """
     def getitem(obj, index):
         typelist = (list, tuple, dict, nest.NodeCollection)
         if obj is not None and type(obj) in typelist:
@@ -490,14 +422,12 @@ def get_restricted_globals():
     restricted_builtins = RestrictedPython.safe_builtins.copy()
     restricted_builtins.update(RestrictedPython.limited_builtins)
     restricted_builtins.update(RestrictedPython.utility_builtins)
-    restricted_builtins.update(
-        dict(
-            max=max,
-            min=min,
-            sum=sum,
-            time=time,
-        )
-    )
+    restricted_builtins.update(dict(
+        max=max,
+        min=min,
+        sum=sum,
+        time=time,
+    ))
 
     restricted_globals = dict(
         __builtins__=restricted_builtins,
@@ -513,13 +443,15 @@ def get_restricted_globals():
 
 
 def nestify(call_name, args, kwargs):
-    """Get the NEST API call and convert arguments if neccessary."""
+    """ Get the NEST API call and convert arguments if neccessary.
+    """
 
     call = getattr(nest, call_name)
-    objectnames = ["nodes", "source", "target", "pre", "post"]
+    objectnames = ['nodes', 'source', 'target', 'pre', 'post']
     paramKeys = list(inspect.signature(call).parameters.keys())
-    args = [nest.NodeCollection(arg) if paramKeys[idx] in objectnames else arg for (idx, arg) in enumerate(args)]
-    for key, value in kwargs.items():
+    args = [nest.NodeCollection(arg) if paramKeys[idx] in objectnames
+            else arg for (idx, arg) in enumerate(args)]
+    for (key, value) in kwargs.items():
         if key in objectnames:
             kwargs[key] = nest.NodeCollection(value)
 
@@ -528,13 +460,16 @@ def nestify(call_name, args, kwargs):
 
 @get_or_error
 def api_client(call_name, args, kwargs):
-    """API Client to call function in NEST."""
+    """ API Client to call function in NEST.
+    """
 
     call = getattr(nest, call_name)
 
     if callable(call):
-        if "inspect" in kwargs:
-            response = {"data": getattr(inspect, kwargs["inspect"])(call)}
+        if 'inspect' in kwargs:
+            response = {
+                'data': getattr(inspect, kwargs['inspect'])(call)
+            }
         else:
             response = do_call(call_name, args, kwargs)
     else:
@@ -556,7 +491,7 @@ def run_mpi_app(host="127.0.0.1", port=52425):
 
 
 def combine(call_name, response):
-    """Combine responses from different MPI processes.
+    """ Combine responses from different MPI processes.
 
     In a distributed scenario, each MPI process creates its own share
     of the response from the data available locally. To present a
@@ -600,7 +535,8 @@ def combine(call_name, response):
         return None
 
     # return the master response if all responses are known to be the same
-    if call_name in ("exec", "Create", "GetDefaults", "GetKernelStatus", "SetKernelStatus", "SetStatus"):
+    if call_name in ('exec', 'Create', 'GetDefaults', 'GetKernelStatus',
+                     'SetKernelStatus', 'SetStatus'):
         return response[0]
 
     # return a single response if there is only one which is not None
@@ -618,7 +554,7 @@ def combine(call_name, response):
 
     log("combine()", f"ERROR: cannot combine response={response}")
     msg = "Cannot combine data because of unknown reason"
-    raise Exception(msg)  # pylint: disable=W0719
+    raise Exception(msg)
 
 
 def merge_dicts(response):
@@ -638,49 +574,50 @@ def merge_dicts(response):
     result = []
 
     for device_dicts in zip(*response):
+
         # TODO: either stip fields like thread, vp, thread_local_id,
         # and local or make them lists that contain the values from
         # all dicts.
 
-        element_type = device_dicts[0]["element_type"]
+        element_type = device_dicts[0]['element_type']
 
-        if element_type not in ("neuron", "recorder", "stimulator"):
+        if element_type not in ('neuron', 'recorder', 'stimulator'):
             msg = f'Cannot combine data of element with type "{element_type}".'
-            raise Exception(msg)  # pylint: disable=W0719
+            raise Exception(msg)
 
-        if element_type == "neuron":
-            tmp = list(filter(lambda status: status["local"], device_dicts))
+        if element_type == 'neuron':
+            tmp = list(filter(lambda status: status['local'], device_dicts))
             assert len(tmp) == 1
             result.append(tmp[0])
 
-        if element_type == "recorder":
+        if element_type == 'recorder':
             tmp = deepcopy(device_dicts[0])
-            tmp["n_events"] = 0
+            tmp['n_events'] = 0
 
             for device_dict in device_dicts:
-                tmp["n_events"] += device_dict["n_events"]
+                tmp['n_events'] += device_dict['n_events']
 
-            record_to = tmp["record_to"]
-            if record_to not in ("ascii", "memory"):
+            record_to = tmp['record_to']
+            if record_to not in ('ascii', 'memory'):
                 msg = f'Cannot combine data when recording to "{record_to}".'
-                raise Exception(msg)  # pylint: disable=W0719
+                raise Exception(msg)
 
-            if record_to == "memory":
-                event_keys = tmp["events"].keys()
+            if record_to == 'memory':
+                event_keys = tmp['events'].keys()
                 for key in event_keys:
-                    tmp["events"][key] = []
+                    tmp['events'][key] = []
                 for device_dict in device_dicts:
                     for key in event_keys:
-                        tmp["events"][key].extend(device_dict["events"][key])
+                        tmp['events'][key].extend(device_dict['events'][key])
 
-            if record_to == "ascii":
-                tmp["filenames"] = []
+            if record_to == 'ascii':
+                tmp['filenames'] = []
                 for device_dict in device_dicts:
-                    tmp["filenames"].extend(device_dict["filenames"])
+                    tmp['filenames'].extend(device_dict['filenames'])
 
             result.append(tmp)
 
-        if element_type == "stimulator":
+        if element_type == 'stimulator':
             result.append(device_dicts[0])
 
     return result

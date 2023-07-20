f7a01bfba3a872b8f287911ece53fea2b3a4576b
Jan Vogelsang <47158055+JanVogelsang@users.noreply.github.com>
2023-07-20 15:06:48 +0200

    Update test_stdp_pl_synapse_hom.py

--- testsuite/pytests/test_stdp_pl_synapse_hom.py
+++ testsuite/pytests/test_stdp_pl_synapse_hom.py
@@ -377,7 +377,3 @@ class TestSTDPPlSynapse:
                             fname_snip += "_[dend_delay=" + str(self.dendritic_delay) + "]"
                             fname_snip += "_[t_ref=" + str(self.neuron_parameters["t_ref"]) + "]"
                             self.do_nest_simulation_and_compare_to_reproduced_weight(fname_snip=fname_snip)
-
-
-if __name__ == "__main__":
-    TestSTDPPlSynapse().test_stdp_synapse()

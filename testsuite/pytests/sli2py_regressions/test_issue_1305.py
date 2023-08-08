32f111fcd83991a2297ca3323519bf1e77a7873b
Nicolai Haug <nicolaihaug@yahoo.com>
2023-08-08 11:46:06 +0200

    Assert with pytest approx

--- testsuite/pytests/sli2py_regressions/test_issue_1305.py
+++ testsuite/pytests/sli2py_regressions/test_issue_1305.py
@@ -42,8 +42,7 @@ def test_resolution_rounding_valid():
     target_resolution = 0.102
     nest.set(resolution=target_resolution, tics_per_ms=1000.0)
 
-    abs_diff = abs(nest.resolution - target_resolution)
-    assert abs_diff < 1e-15
+    assert nest.resolution == pytest.approx(target_resolution)
 
 
 def test_resolution_rounding_invalid():

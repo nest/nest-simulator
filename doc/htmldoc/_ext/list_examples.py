3f127683af0ddd3d90791ba4918dbad0fea28917
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-08-08 19:28:43 +0200

    test getting title

--- doc/htmldoc/_ext/list_examples.py
+++ doc/htmldoc/_ext/list_examples.py
@@ -20,8 +20,10 @@
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 from docutils import nodes
-from docutils.parsers.rst import Directive
+from docutils.parsers.rst import Directive, Parser
+from sphinx.application import Sphinx
 import json
+import re
 
 
 class ListExamplesDirective(Directive):
@@ -41,29 +43,51 @@ class ListExamplesDirective(Directive):
         # for using file. The script is still WIP
         with open("_ext/model_match_examples.json") as json_file:
             examples_data = json.load(json_file)
-
-        content = []
-        # bullet list equivalent to HTML ul
-        bullet_list = nodes.bullet_list()
-        for key, values in examples_data.items():
-            if key == my_arg:
-                for value in values:
-                    link_text = value.split("/")[-1]
-                    list_item = nodes.list_item()
-                    newnode = nodes.reference(value, link_text)
-                    newnode["refdocname"] = "auto_examples/"
-                    newnode["refuri"] = value + ".html"
-                    para = nodes.paragraph()
-                    para.append(newnode)
-                    list_item.append(para)
-                    bullet_list += list_item
+            # bullet list equivalent to HTML <ul>
+            bullet_list = nodes.bullet_list()
+            for key, values in examples_data.items():
+                if key == my_arg:
+                    for value in values:
+                        path = value.split("/", 1)[1] + ".rst"
+                        # UPDATE need to figure out how to add output from one class to another, considerieng we have? to use setup(app)
+                        link_text = get_first_heading(path)
+                        #link_text = value.split("/")[-1]
+                        # equivalen to HTML <li>
+                        list_item = nodes.list_item()
+                        newnode = nodes.reference(value, link_text)
+                        newnode["refuri"] = value + ".html"
+                        para = nodes.paragraph()
+                        para.append(newnode)
+                        list_item.append(para)
+                        bullet_list += list_item
 
         return [bullet_list]
+
+
+
+
+from sphinx.environment import BuildEnvironment
+from sphinx.environment.collectors import EnvironmentCollector
+
+# Trying to figure out how to get the title from a file in Sphinx - using fmtlib github repo as example
+# I have not  run antying to test it
+# maybe I dont need this exact class, but can use the code( the travers part)  in "doctree resolved" or something?
+class TitleCollector(EnvironmentCollector):
+     def process_doc(self, app: Sphinx, doctree: nodes.document) -> None:
+
+        titlenode = nodes.title()
+        longtitlenode = titlenode
+        for node in doctree.traverse(nodes.section):
+            visitor = SphinxContentsFilter(doctree)
+            node[0].walkabout(visitor)
+            titlenode += visitor.get_entry_text()
+
 
 
 def setup(app):
     # Note that the directive names need to be all lower case
     app.add_directive("listexamples", ListExamplesDirective)
+    app.add_env_collector(TitleCollector)
 
     return {
         "version": "0.1",

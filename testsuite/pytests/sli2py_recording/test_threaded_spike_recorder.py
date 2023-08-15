0c62345897a420f0073372b81823960634b7032c
Nicolai Haug <nicolaihaug@yahoo.com>
2023-08-15 16:46:56 +0200

    Port test_threaded_spike_recorder from sli2py

--- testsuite/pytests/sli2py_recording/test_threaded_spike_recorder.py
+++ testsuite/pytests/sli2py_recording/test_threaded_spike_recorder.py
@@ -24,7 +24,44 @@ Test that ``spike_recorder`` behaves as expected in multithreaded simulations.
 """
 
 import nest
-import numpy.testing as nptest
+import pandas as pd
+import pandas.testing as pdtest
 import pytest
 
 pytestmark = pytest.mark.skipif_missing_threads
+
+
+def simulator(num_threads):
+    """
+    Simulate the system with provided number of threads and return recorded events.
+    """
+
+    nest.ResetKernel()
+    nest.local_num_threads = num_threads
+
+    num_nrns = 10  # should not be divisble by thread number
+
+    nrns = nest.Create("iaf_psc_alpha", num_nrns, params={"I_e": 376.0})
+    srec = nest.Create("spike_recorder")
+
+    nest.Connect(nrns, srec)
+
+    nest.Simulate(200.0)
+
+    df = pd.DataFrame(srec.get("events"))
+
+    return df
+
+
+def test_threaded_spike_recorder():
+    """
+    Test that recorded spike times are independent of number of threads.
+    """
+
+    df_t1 = simulator(1)
+    df_t3 = simulator(3)
+
+    df_t1.sort_values(by=["times", "senders"], inplace=True, ignore_index=True)
+    df_t3.sort_values(by=["times", "senders"], inplace=True, ignore_index=True)
+
+    pdtest.assert_frame_equal(df_t1, df_t3)

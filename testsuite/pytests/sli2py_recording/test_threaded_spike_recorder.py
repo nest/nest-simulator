db0b064b15ebbeb9a63e9c237d71ebc87f252fa8
Nicolai Haug <nicolaihaug@yahoo.com>
2023-08-15 16:02:21 +0200

    WIP: port test_threaded_spike_recorder [skip ci]

---
+++ testsuite/pytests/sli2py_recording/test_threaded_spike_recorder.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# test_threaded_spike_recorder.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Test that ``spike_recorder`` behaves as expected in multithreaded simulations.
+"""
+
+import nest
+import numpy.testing as nptest
+import pytest
+
+pytestmark = pytest.mark.skipif_missing_threads

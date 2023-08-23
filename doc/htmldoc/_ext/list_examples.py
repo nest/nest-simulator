7b877ed412e947fa387f220303b63546f6755e48
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-08-23 14:04:10 +0200

    improve code

--- doc/htmldoc/_ext/list_examples.py
+++ doc/htmldoc/_ext/list_examples.py
@@ -40,19 +40,17 @@ def ModelMatchExamples():
             model_files.append(os.path.splitext(filename)[0])
 
     matches = {}
-    for root, dirs, files in os.walk(filepath_examples):
-        for filename in files:
-            if filename.endswith(".rst"):
-                full_path = os.path.join(root, filename)
-                if "auto_examples/index" in full_path:
-                    continue
-                with open(full_path, "r", errors="ignore") as file:
-                    content = file.read()
-                    for model_file in model_files:
-                        if model_file in content:
-                            if model_file not in matches:
-                                matches[model_file] = []
-                            matches[model_file].append(full_path)
+    files = glob.glob(filepath_examples + "**/*.rst", recursive=True)
+    for filename in files:
+        if "auto_examples/index" in filename:
+            continue
+        with open(filename, "r", errors="ignore") as file:
+            content = file.read()
+            for model_file in model_files:
+                if model_file in content:
+                    if model_file not in matches:
+                        matches[model_file] = []
+                    matches[model_file].append(full_path)
 
     return matches
 
@@ -74,30 +72,31 @@ class ListExamplesDirective(Directive):
         examples_list = ModelMatchExamples()
 
         sorted_examples = {key: sorted(values) for key, values in examples_list.items()}
-        for key, values in sorted_examples.items():
-            if key == my_arg:
-                for value in values:
-                    # A leading '/'  is added to the example path to take advantage of the
-                    # std:label (ref role) syntax (see also objects.inv doc for intersphinx)
-                    # we also need the names to be all lower case
-                    value_label = "/" + value.lower()
-                    link_text = value.split("/")[-1]
-                    # equivalent to HTML <li>
-                    list_item = nodes.list_item()
-                    link_node = pending_xref(
-                        "",
-                        reftype="ref",
-                        refdomain="std",
-                        refexplicit=False,
-                        reftarget=value_label,
-                        refwarn=True,
-                        classes=["xref", "std", "std-ref"],
-                    )
-                    link_node += nodes.inline(text=link_text)
-                    para = nodes.paragraph()
-                    para.append(link_node)
-                    list_item.append(para)
-                    bullet_list += list_item
+        # for key, values in sorted_examples.items():
+        if key == my_arg:
+            values = example_list[key]
+            for value in values:
+                # A leading '/'  is added to the example path to take advantage of the
+                # std:label (ref role) syntax (see also objects.inv doc for intersphinx)
+                # we also need the names to be all lower case
+                value_label = "/" + value.lower()
+                link_text = value.split("/")[-1]
+                # equivalent to HTML <li>
+                list_item = nodes.list_item()
+                link_node = pending_xref(
+                    "",
+                    reftype="ref",
+                    refdomain="std",
+                    refexplicit=False,
+                    reftarget=value_label,
+                    refwarn=True,
+                    classes=["xref", "std", "std-ref"],
+                )
+                link_node += nodes.inline(text=link_text)
+                para = nodes.paragraph()
+                para.append(link_node)
+                list_item.append(para)
+                bullet_list += list_item
 
         return [bullet_list]
 

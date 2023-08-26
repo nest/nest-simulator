571dbc3b4d1030373f6bad766f1b6f5c7f19d7f2
shinya.ito <shinya.ito@alleninstitute.org>
2023-08-25 18:35:13 -0700

    Making double-alpha description clearer

--- models/glif_psc.h
+++ models/glif_psc.h
@@ -165,7 +165,7 @@ th_voltage_decay           double         Voltage-induced threshold time
                                           voltage-dependent component of the
                                           threshold in 1/ms (bv in Equation
                                           (4) in [1]_)
-tau_syn                    double vector  Rise time constants of the synaptic
+tau_syn                    double vector  Time constants of the synaptic
                                           alpha function in ms
 E_rev                      double vector  Reversal potential in mV
 spike_dependent_threshold  bool           flag whether the neuron has

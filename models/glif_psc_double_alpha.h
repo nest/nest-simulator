4309902685b0b8cc00514392e700e9bea041dd50
Shinya Ito <shixnya@gmail.com>
2023-08-30 13:29:06 -0700

    Update models/glif_psc_double_alpha.h
    Co-authored-by: clinssen <c.linssen@fz-juelich.de>

--- models/glif_psc_double_alpha.h
+++ models/glif_psc_double_alpha.h
@@ -53,7 +53,7 @@ of the fast component of the alpha function to be 1 pA at
 The relative peak current of the slow component is given as amp_slow, at
 :math:`t = \tau_\text{syn_slow}`. Namely,
 :math:`I_\text{syn} = \text{alpha_function}(\tau_\text{syn} = \tau_\text{syn_fast}) +
-\text{amp_slow} * \text{alpha\_function}(\tau_\text{syn} = \tau_\text{syn\_slow})`.
+\text{amp_slow} * \text{alpha_function}(\tau_\text{syn} = \tau_\text{syn_slow})`.
 Therefore if amp_slow is not 0, the peak current of the total synaptic current is larger
 than the specified weight. By default, glif_psc_double_alpha has a single synapse that
 is accessible through receptor_port 1. An arbitrary number of synapses with different

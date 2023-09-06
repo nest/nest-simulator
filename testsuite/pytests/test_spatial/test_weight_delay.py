20ae7cdf483c8ab9597ee6e6a010d3792d9e22ae
Nicolai Haug <nicolaihaug@yahoo.com>
2023-09-07 00:30:24 +0200

    Port both grid and free test_weight_delay using pytest parametrization

--- testsuite/pytests/test_spatial/test_weight_delay.py
+++ testsuite/pytests/test_spatial/test_weight_delay.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# test_weight_delay_grid.py
+# test_weight_delay.py
 #
 # This file is part of NEST.
 #
@@ -20,79 +20,104 @@
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Test that weights and delays can be set to arbitrary values within the limits set by resolution.
+Test setting weights and delays to arbitrary values limited by resolution.
 
-The test creates a grid with a single row and uses linear functions for weights and delays.
-Expected delays with resolution 0.04 are 1.0 0.96 0.96 0.92 0.92 0.88 0.84 and expected weights
-are 1.0, 0.98, 0.96, 0.94, 0.92, 0.90, 0.88, 0.86, 0.84, 0.82.
+The test checks that weights and delays in a spatial network can be set to
+arbitrary values within the limits set by resolution. The test creates a layer
+with a single row and uses linear functions for weights and delays. Expected
+delays with resolution 0.04 are 1.0 0.96 0.96 0.92 0.92 0.88 0.84 and expected
+weights are 1.0, 0.98, 0.96, 0.94, 0.92, 0.90, 0.88, 0.86, 0.84, 0.82.
 """
 
+import numpy as np
 import pytest
 
 import nest
 
 
-@pytest.fixture(scope="module")
-def network():
+def build_network(layer_type):
+    """Build spatial network with specified layer type."""
+
+    nest.ResetKernel()
     nest.resolution = 0.04
     nest.use_compressed_spikes = False
     nest.sort_connections_by_source = False
-
-    grid = nest.spatial.grid(shape=[10, 1], extent=[10.0, 1.0], center=[0.0, 0.0], edge_wrap=True)
-    src_layer = nest.Create("iaf_psc_alpha", positions=grid)
-    tgt_layer = nest.Create("iaf_psc_alpha", positions=grid)
 
     p1 = nest.CreateParameter("constant", {"value": 1.0})
     p2 = nest.CreateParameter("constant", {"value": -0.02})
     p3 = nest.CreateParameter("distance", {})
     linear_parameter = p1 + p2 * p3
 
-    conn_spec = {
-        "rule": "pairwise_bernoulli",
-        "use_on_source": False,
-        "mask": {"grid": {"shape": [10, 1]}, "anchor": [0, 0]},
-    }
-
     syn_spec = {
         "weight": linear_parameter,
         "delay": linear_parameter,
     }
+
+    if layer_type == "grid":
+        pos = nest.spatial.grid(
+            shape=[10, 1],
+            extent=[10.0, 1.0],
+            center=[0.0, 0.0],
+            edge_wrap=True,
+        )
+        conn_spec = {
+            "rule": "pairwise_bernoulli",
+            "use_on_source": False,
+            "mask": {"grid": {"shape": [10, 1]}, "anchor": [0, 0]},
+        }
+    elif layer_type == "free":
+        pos = nest.spatial.free(
+            [[x, 0.0] for x in np.linspace(-4.5, 4.5, 10)],
+            extent=[10.0, 1.0],
+            edge_wrap=True,
+        )
+        conn_spec = {
+            "rule": "pairwise_bernoulli",
+            "use_on_source": False,
+            "mask": {"rectangular": {"lower_left": [-0.5, -0.5], "upper_right": [9.5, 0.5]}},
+        }
+
+    src_layer = nest.Create("iaf_psc_alpha", positions=pos)
+    tgt_layer = nest.Create("iaf_psc_alpha", positions=pos)
 
     nest.Connect(src_layer, tgt_layer, conn_spec=conn_spec, syn_spec=syn_spec)
 
     return src_layer, tgt_layer
 
 
-def test_source_layer_nodes_dump(tmp_path, network, expected_source_nodes_dump):
+@pytest.mark.parametrize("layer_type", ["grid", "free"])
+def test_source_layer_nodes_dump(tmp_path, expected_source_nodes_dump, layer_type):
     """Test that source layer nodes dump mathces expectation."""
 
-    src_layer, _ = network
+    src_layer, _ = build_network(layer_type)
 
-    fname = tmp_path / "source_nodes.txt"
+    fname = tmp_path / f"{layer_type}_source_nodes.txt"
     nest.DumpLayerNodes(src_layer, fname)
 
     actual_source_nodes_dump = fname.read_text().splitlines()
     assert actual_source_nodes_dump == expected_source_nodes_dump
 
 
-def test_target_layer_nodes_dump(tmp_path, network, expected_target_nodes_dump):
+@pytest.mark.parametrize("layer_type", ["grid", "free"])
+def test_target_layer_nodes_dump(tmp_path, expected_target_nodes_dump, layer_type):
     """Test that target layer nodes dump mathces expectation."""
 
-    _, tgt_layer = network
+    _, tgt_layer = build_network(layer_type)
 
-    fname = tmp_path / "target_nodes.txt"
+    fname = tmp_path / f"{layer_type}_target_nodes.txt"
     nest.DumpLayerNodes(tgt_layer, fname)
 
     actual_target_nodes_dump = fname.read_text().splitlines()
     assert actual_target_nodes_dump == expected_target_nodes_dump
 
 
-def test_layer_connections_dump(tmp_path, network, expected_conn_dump):
+@pytest.mark.parametrize("layer_type", ["grid", "free"])
+def test_layer_connections_dump(tmp_path, expected_conn_dump, layer_type):
     """Test that layer connections dump mathces expectation."""
 
-    src_layer, tgt_layer = network
+    src_layer, tgt_layer = build_network(layer_type)
 
-    fname = tmp_path / "layer_conns.txt"
+    fname = tmp_path / f"{layer_type}_layer_conns.txt"
     nest.DumpLayerConnections(src_layer, tgt_layer, "static_synapse", fname)
 
     actual_conn_dump = fname.read_text().splitlines()

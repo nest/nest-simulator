b1ccd186d668789b8fad5c82ed470734f3e77022
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2023-09-07 23:59:52 +0200

    Avoid technical problems in test

--- testsuite/pytests/test_status.py
+++ testsuite/pytests/test_status.py
@@ -36,7 +36,13 @@ class StatusTestCase(unittest.TestCase):
 
         nest.ResetKernel()
 
-        self.assertEqual(nest.GetKernelStatus(), nest.kernel_status)
+        # Remove entry containing numpy arrays from status dicts since they do not compare well
+        gks_result = nest.GetKernelStatus()
+        ks_result = nest.kernel_status
+        del gks_result["buffer_resize_log"]
+        del ks_result["buffer_resize_log"]
+        self.assertEqual(gks_result, ks_result)
+
         self.assertEqual(nest.GetKernelStatus("resolution"), nest.resolution)
 
         nest.resolution = 0.4

06869304319a11c69774c4b79b5ff039d0026287
Nicolai Haug <nicolaihaug@yahoo.com>
2023-09-11 14:05:39 +0200

    WIP

---
+++ pynest/examples/iaf_tum_2000_short_term_plasticity.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+#
+# iaf_tum_2000_short_term_plasticity.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Short-term plasticity example
+-----------------------------
+
+This example simulates two connected ``iaf_tum_2000`` neurons [1]_, a neuron
+model with short-term synaptic plasticity. For details on the model, see
+:doc:`../models/iaf_tum_2000`.
+
+The ``iaf_tum_2000`` neuron model incorporates the `tsodyks_synapse <../models/tsodyks_synapse>`
+computations directly in `iaf_psc_exp <../models/iaf_psc_exp>` on the presynaptic
+side, which makes ``iaf_tum_2000`` very computationally efficient, see
+:doc:`../auto_examples/iaf_tum_2000_benchmark`.
+
+In this example, one neuron is driven by DC input and the voltage trace of the
+other neuron is recorded. 
+
+The example illustrates the short-term plasticity reproduces Figure 1A and 1B in [2]_, which show the short-term .
+
+to :doc:`../auto_examples/tsodyks_facilitating`, except that different synaptic
+parameters are used. Here, a large facilitation parameter ``U`` causes a fast
+saturation of the synaptic efficacy (Eq. 4 or Eq. 2.2 in [1]_ or [2]_, respectively),
+disabling a facilitating behavior.
+
+Here, a small facilitation
+parameter ``U`` causes a slow saturation of the synaptic efficacy
+(Eq. 2.2), enabling a facilitating behavior.
+
+
+Short-term plasticity (STP) (Stevens 95, Markram 96, Abbott 97, Zucker 02, Abbott 04), also called dynamical synapses,
+refers to a phenomenon in which synaptic efficacy changes over time in a way that reflects the history of presynaptic
+activity. Two types of STP, with opposite effects on synaptic efficacy, have been observed in experiments. They are
+known as Short-Term Depression (STD) and Short-Term Facilitation (STF). STD is caused by depletion of neurotransmitters
+consumed during the synaptic signaling process at the axon terminal of a pre-synaptic neuron, whereas STF is caused by
+influx of calcium into the axon terminal after spike generation, which increases the release probability of
+neurotransmitters.
+
+References
+~~~~~~~~~~
+
+.. [1] Tsodyks M, Uziel A, Markram H (2000). Synchrony generation in recurrent
+       networks with frequency-dependent synapses. The Journal of Neuroscience,
+       20,RC50:1-5. URL: https://infoscience.epfl.ch/record/183402
+
+.. [2] Tsodyks M, Pawelzik K, Markram H (1998). Neural networks with dynamic synapses. Neural
+       computation, http://dx.doi.org/10.1162/089976698300017502
+
+See Also
+~~~~~~~~
+
+:doc:`tsodyks_facilitating`, doc:`iaf_tum_2000_benchmark`
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import nest
+
+###############################################################################
+# First we make sure that the NEST kernel is reset and the resolution of the
+# simulation is 0.1 ms.
+
+nest.ResetKernel()
+nest.resolution = 0.1  # simulation step size (ms)
+
+###############################################################################
+# First we set the neuronal membrane and synaptic parameters according to [2]_.
+
+tau_m = 20.0  # membrane time constant [ms]
+R_m = 0.1  # membrane input resistance [GΩ]
+C_m = tau_m / R_m  # membrane capacitance [pF]
+V_th = 15.0  # threshold potential [mV]
+E_L = 0.0  # reset potential [mV]
+t_ref = 2.0  # refractory period [ms]
+
+
+tau_psc = 3.0  # PSC time constant (tau_inact in [2]) [ms]
+Tau_psc = 3.0  # time constant of PSC (= Tau_inact)
+Tau_rec = 800.0  # recovery time
+Tau_fac = 0.0  # facilitation time
+
+
+sim_time = 1200.0  # simulation time [ms]
+stim_start = 50.0  # start time of DC input [ms]
+stim_end = 1050.0  # end time of DC input [ms]
+
+
+f = 20.0 / 1000.0  # frequency [mHz]
+dc_amp = V_th * C_m / tau_m / (1 - np.exp(-(1 / f - t_ref) / tau_m))  # dc amplitude
+
+params = {
+    "tau_m": 40.0,  # membrane time constant [ms]
+    "t_ref": 2.0,  # refractory period [ms]
+    "E_L": 0.0,  # reset potential [mV]
+    "V_th": 15.0,  # threshold potential [mV]
+}
+
+neuron_params = (
+    {  # Set parameters for iaf_psc_delta
+        "tau_m": 20,  # membrance time constant [ms]
+        "t_ref": 2.0,  # refractory period [ms]
+        "C_m": 250.0,  # membrane capacitance [pF]
+        "E_L": 0.0,  # resting membrane potential [mV]
+        "V_th": 20.0,  # threshold potential [mV]
+        "V_reset": 0.0,  # reset potential [mV]
+        "V_m": 9.5,  # mean membrane potential [mV]
+    },
+)
+
+
+"""
+ ``V_m``         mV       :math:`V_{\text{m}}`           Membrane potential
+ ``E_L``         mV       :math:`E_\text{L}`             Resting membrane potential
+ ``C_m``         pF       :math:`C_{\text{m}}`           Capacity of the membrane
+ ``tau_m``       ms       :math:`\tau_{\text{m}}`        Membrane time constant
+ ``t_ref``       ms       :math:`t_{\text{ref}}`         Duration of refractory period
+ ``V_th``        mV       :math:`V_{\text{th}}`          Spike threshold
+ ``V_reset``     mV       :math:`V_{\text{reset}}`       Reset potential of the membrane
+ ``tau_syn_ex``  ms       :math:`\tau_{\text{syn, ex}}`  Rise time of the excitatory synaptic alpha function
+ ``tau_syn_in``  ms       :math:`\tau_{\text{syn, in}}`  Rise time of the inhibitory synaptic alpha function
+ ``I_e``         pA       :math:`I_\text{e}`             Constant input current
+ ``V_min``       mV       :math:`V_{\text{min}}`         Absolute lower value for the membrane potenial (default :math:`-\infty`)
+"""
+
+###############################################################################
+# Second, the simulation parameters are assigned to variables. The neuron
+# and synapse parameters are stored into a dictionary.
+
+# resolution = 0.1  # simulation step size (ms)
+# Tau = 40.0  # membrane time constant
+# Theta = 15.0  # threshold
+# E_L = 0.0  # reset potential of membrane potential
+# R = 0.1  # 100 M Ohm
+# C = Tau / R  # Tau (ms)/R in NEST units
+TauR = 2.0  # refractory time
+Tau_psc = 3.0  # time constant of PSC (= Tau_inact)
+Tau_rec = 800.0  # recovery time
+Tau_fac = 0.0  # facilitation time
+U = 0.5  # facilitation parameter U
+A = 250.0  # PSC weight in pA
+f = 20.0 / 1000.0  # frequency in Hz converted to 1/ms
+Tend = 1200.0  # simulation time
+TIstart = 50.0  # start time of dc
+TIend = 1050.0  # end time of dc
+I0 = Theta * C / Tau / (1 - np.exp(-(1 / f - TauR) / Tau))  # dc amplitude
+
+neuron_param = {
+    "tau_m": Tau,
+    "t_ref": TauR,
+    "tau_syn_ex": Tau_psc,
+    "tau_syn_in": Tau_psc,
+    "C_m": C,
+    "V_reset": E_L,
+    "E_L": E_L,
+    "V_m": E_L,
+    "V_th": Theta,
+}
+
+syn_param = {
+    "tau_psc": Tau_psc,
+    "tau_rec": Tau_rec,
+    "tau_fac": Tau_fac,
+    "U": U,
+    "delay": 0.1,
+    "weight": A,
+    "u": 0.0,
+    "x": 1.0,
+}
+
+###############################################################################
+# Third, we reset the kernel and set the resolution using the corresponding
+# kernel attribute.
+
+nest.ResetKernel()
+nest.resolution = resolution
+
+###############################################################################
+# Fourth, the nodes are created using ``Create``. We store the returned
+# handles in variables for later reference.
+
+neurons = nest.Create("iaf_psc_exp", 2)
+dc_gen = nest.Create("dc_generator")
+volts = nest.Create("voltmeter")
+
+################################################################################
+# Fifth, the ``iaf_psc_exp`` neurons, the ``dc_generator`` and the ``voltmeter``
+# are configured using ``SetStatus``, which expects a list of node handles and
+# a parameter dictionary or a list of parameter dictionaries.
+
+neurons.set(neuron_param)
+dc_gen.set(amplitude=I0, start=TIstart, stop=TIend)
+volts.set(label="voltmeter", interval=1.0)
+
+###############################################################################
+# Sixth, the ``dc_generator`` is connected to the first neuron
+# (`neurons[0]`) and the ``voltmeter`` is connected to the second neuron
+# (`neurons[1]`). The command ``Connect`` has different variants. Plain
+# ``Connect`` just takes the handles of pre- and postsynaptic nodes and uses
+# the default values for weight and delay. Note that the connection
+# direction for the ``voltmeter`` reflects the signal flow in the simulation
+# kernel, because it observes the neuron instead of receiving events from it.
+
+nest.Connect(dc_gen, neurons[0])
+nest.Connect(volts, neurons[1])
+
+###############################################################################
+# Seventh, the first neuron (`neurons[0]`) is connected to the second
+# neuron (`neurons[1]`).  The command ``CopyModel`` copies the
+# ``tsodyks_synapse`` model to the new name ``syn`` with parameters
+# ``syn_param``.  The manually defined model ``syn`` is used in the
+# connection routine via the ``syn_spec`` parameter.
+
+nest.CopyModel("tsodyks_synapse", "syn", syn_param)
+nest.Connect(neurons[0], neurons[1], syn_spec="syn")
+
+###############################################################################
+# Finally, we simulate the configuration using the command ``Simulate``,
+# where the simulation time `Tend` is passed as the argument.  We plot the
+# target neuron's membrane potential as a function of time.
+
+nest.Simulate(Tend)
+nest.voltage_trace.from_device(volts)
+plt.show()

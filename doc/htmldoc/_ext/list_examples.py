0312d0d7b2aa2f55aba4751f8595d4ec0ce8fc50
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-09-12 15:46:38 +0200

    improve naming, fix script when no examples exist, add logger

--- doc/htmldoc/_ext/list_examples.py
+++ doc/htmldoc/_ext/list_examples.py
@@ -27,6 +27,11 @@ from sphinx.util.docutils import SphinxDirective
 import os
 import glob
 
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+log = logging.getLogger(__name__)
+
 
 class listnode(nodes.General, nodes.Element):
     pass
@@ -37,39 +42,48 @@ def ProcessExamples(app, doctree, docname):
     # given by the model name in the directive argument
 
     env = app.builder.env
-    mydict = {}
+    examples_titles_map = {}
 
-    dict_match = ModelMatchExamples()
+    try:
+        models_to_examples_map = ModelMatchExamples()
 
-    for node in doctree.findall(listnode):
-        for item in env.all_examples:
-            # compare current location with attribute location to get
-            # correct model name
-            if item["name"] == docname:
-                list_of_examples = dict_match[item["model_name"]]
-                for value in list_of_examples:
-                    mydocname = os.path.splitext(value)[0]
-                    mydict[mydocname] = str(env.titles[mydocname].children[0])
+        for node in doctree.findall(listnode):
+            for requested_list in env.list_examples_directive_requests:
+                # compare current location with attribute location to get
+                # correct model name
+                if requested_list["name"] == docname:
+                    if requested_list["model_name"] not in models_to_examples_map:
+                        log.info("No examples found for Model: " + requested_list["model_name"])
+                        bullet_list = nodes.Text("None")
+                        break
 
-            sorted_examples = dict(sorted(mydict.items(), key=lambda x: x[1]))
+                    list_of_examples = models_to_examples_map[requested_list["model_name"]]
+                    for value in list_of_examples:
+                        mydocname = os.path.splitext(value)[0]
+                        examples_titles_map[mydocname] = str(env.titles[mydocname].children[0])
+                sorted_examples = dict(sorted(examples_titles_map.items(), key=lambda kv: kv[1]))
+                bullet_list = nodes.bullet_list()
 
-            bullet_list = nodes.bullet_list()
+                for filename, title in sorted_examples.items():
+                    # Create a reference node that links to the example
+                    # equivalent to HTML <li>
+                    list_item = nodes.list_item()
+                    newnode = nodes.reference("", "")
+                    newnode["internal"] = True
+                    newnode["refdocname"] = filename
+                    newnode["refuri"] = app.builder.get_relative_uri(docname, filename)
+                    newnode.append(nodes.Text(title))
+                    para = nodes.paragraph()
+                    para += newnode
+                    list_item.append(para)
+                    bullet_list += list_item
 
-            for filename, title in sorted_examples.items():
-                # Create a reference node that links to the example
-                # equivalent to HTML <li>
-                list_item = nodes.list_item()
-                newnode = nodes.reference("", "")
-                newnode["internal"] = True
-                newnode["refdocname"] = filename
-                newnode["refuri"] = app.builder.get_relative_uri(docname, filename)
-                newnode.append(nodes.Text(title))
-                para = nodes.paragraph()
-                para += newnode
-                list_item.append(para)
-                bullet_list += list_item
+            node.replace_self(bullet_list)
 
-        node.replace_self(bullet_list)
+    except Exception as e:
+        log.warning(repr(e))
+        log.warning("exception of class: %s", e.__class__)
+        raise
 
 
 def ModelMatchExamples():
@@ -92,9 +106,7 @@ def ModelMatchExamples():
             content = file.read()
             for model_file in model_files:
                 if model_file in content:
-                    if model_file not in matches:
-                        matches[model_file] = []
-                    matches[model_file].append(filename)
+                    matches.setdefault(model_file, []).append(filename)
 
     return matches
 
@@ -111,13 +123,13 @@ class ListExamplesDirective(SphinxDirective):
     def run(self):
         model_arg = self.arguments[0]
 
-        if not hasattr(self.env, "all_examples"):
-            self.env.all_examples = []
+        if not hasattr(self.env, "list_examples_directive_requests"):
+            self.env.list_examples_directive_requests = []
 
         # See TODO tutorial in Sphinx for more info.
-        # Using environment attribute all_examples to store argument and
+        # Using environment attribute list_examples_directive_requests to store argument and
         # its location (docname)
-        self.env.all_examples.append({"model_name": model_arg, "name": self.env.docname})
+        self.env.list_examples_directive_requests.append({"model_name": model_arg, "name": self.env.docname})
 
         return [listnode("")]
 

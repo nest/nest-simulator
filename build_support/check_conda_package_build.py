a7d2e47ffe905d6528f2b9f214a7dcfd29f1982e
Steffen Graber <s.graber@fz-juelich.de>
2023-09-14 14:06:44 +0000

    Reformatting

--- build_support/check_conda_package_build.py
+++ build_support/check_conda_package_build.py
@@ -42,46 +42,76 @@ CI_PR_NUM = os.environ.get("CI_PR_NUM")
 GITHUB_SHA = os.environ.get("GITHUB_SHA")
 
 url = "https://api.github.com/graphql"
-headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {pat}"
-}
+headers = {"Content-Type": "application/json", "Authorization": f"Bearer {pat}"}
 
 
 def findOID():
-    payload = "{\"query\":\"query findOID {  repository(owner: \\\"" + str(CI_NAME)  + "\\\", name: \\\"" + str(CI_REPO)  + "\\\") { refs(first: 1, refPrefix: \\\"refs/heads/\\\", query: \\\"dev\\\") {nodes {name \\n target {\\n ... on Commit {history(first: 1) {nodes {oid}}}}}}}}\",\"variables\":{}}"
+    payload = (
+        '{"query":"query findOID {  repository(owner: \\"'
+        + str(CI_NAME)
+        + '\\", name: \\"'
+        + str(CI_REPO)
+        + '\\") { refs(first: 1, refPrefix: \\"refs/heads/\\", query: \\"dev\\") {nodes {name \\n target {\\n ... on Commit {history(first: 1) {nodes {oid}}}}}}}}","variables":{}}'
+    )
     response = requests.request("POST", url, headers=headers, data=payload)
-    oid = json.loads(response.text)['data']['repository']['refs']['nodes'][0]['target']['history']['nodes'][0]['oid']
+    oid = json.loads(response.text)["data"]["repository"]["refs"]["nodes"][0]["target"]["history"]["nodes"][0]["oid"]
     return oid
 
 
 def listWorkflowStatus():
-    payload = "{\"query\":\"query listWorkflowsRun {\\n  repository(owner: \\\"" + CI_MAIN_NAME  + "\\\", name: \\\"" + CI_REPO  + "\\\") {\\n    pullRequest(number: " + CI_PR_NUM  + ") {\\n      commits(last: 1) {\\n edges {\\n node {\\n commit {\\n checkSuites(first: 1) {\\n nodes {\\n status\\n checkRuns(last: 16) {\\n nodes {\\n conclusion\\n completedAt\\n detailsUrl\\n status\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n}\",\"variables\":{}}"
+    payload = (
+        '{"query":"query listWorkflowsRun {\\n  repository(owner: \\"'
+        + CI_MAIN_NAME
+        + '\\", name: \\"'
+        + CI_REPO
+        + '\\") {\\n    pullRequest(number: '
+        + CI_PR_NUM
+        + ') {\\n      commits(last: 1) {\\n edges {\\n node {\\n commit {\\n checkSuites(first: 1) {\\n nodes {\\n status\\n checkRuns(last: 16) {\\n nodes {\\n conclusion\\n completedAt\\n detailsUrl\\n status\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n }\\n}","variables":{}}'
+    )
     status = requests.request("POST", url, headers=headers, data=payload)
     return status
 
 
 def createCommitOnBranch():
     oid = findOID()
-    payload = "{\"query\":\"mutation createCommit($input: CreateCommitOnBranchInput!) {  createCommitOnBranch(input: $input) {    commit {      url    }  }}\",\"variables\":{\"input\":{\"branch\":{\"repositoryNameWithOwner\":\"" + CI_NAME  + "/" + CI_REPO  + "\",\"branchName\":\"dev\"},\"message\":{\"headline\":\"Build from " +  GITHUB_SHA + "!\"},\"fileChanges\":{\"additions\":[{\"path\":\"recipe/conda_build_log.txt\",\"contents\":\"" +  GITHUB_SHA + "\"}]},\"expectedHeadOid\":\"" + oid + "\"}}}"
+    payload = (
+        '{"query":"mutation createCommit($input: CreateCommitOnBranchInput!) {  createCommitOnBranch(input: $input) {    commit {      url    }  }}","variables":{"input":{"branch":{"repositoryNameWithOwner":"'
+        + CI_NAME
+        + "/"
+        + CI_REPO
+        + '","branchName":"dev"},"message":{"headline":"Build from '
+        + GITHUB_SHA
+        + '!"},"fileChanges":{"additions":[{"path":"recipe/conda_build_log.txt","contents":"'
+        + GITHUB_SHA
+        + '"}]},"expectedHeadOid":"'
+        + oid
+        + '"}}}'
+    )
     response = requests.request("POST", url, headers=headers, data=payload)
     status = ""
-    while status!="COMPLETED":
+    while status != "COMPLETED":
         fullstatus = listWorkflowStatus()
-        status = json.loads(fullstatus.text)['data']['repository']['pullRequest']['commits']['edges'][0]['node']['commit']['checkSuites']['nodes'][0]['status']
-        checknodes = json.loads(fullstatus.text)['data']['repository']['pullRequest']['commits']['edges'][0]['node']['commit']['checkSuites']['nodes'][0]['checkRuns']
-        # First give time to start the CI and then poll  
-        time.sleep(60)  # time to start could be long  
-        for stat in checknodes['nodes']: 
-            if stat['conclusion']=="FAILURE" or \
-                stat['conclusion']=="CANCELLED" or \
-                stat['conclusion']=="TIMED_OUT" or \
-                stat['conclusion']=="STARTUP_FAILURE" or \
-                stat['conclusion']=="ACTION_REQUIRED":
-                print(f"SOMETHING WENT WRONG!\n")   
-                print(f"{stat['conclusion']} - {(stat['status'])}")      
+        status = json.loads(fullstatus.text)["data"]["repository"]["pullRequest"]["commits"]["edges"][0]["node"][
+            "commit"
+        ]["checkSuites"]["nodes"][0]["status"]
+        checknodes = json.loads(fullstatus.text)["data"]["repository"]["pullRequest"]["commits"]["edges"][0]["node"][
+            "commit"
+        ]["checkSuites"]["nodes"][0]["checkRuns"]
+        # First give time to start the CI and then poll
+        time.sleep(60)  # time to start could be long
+        for stat in checknodes["nodes"]:
+            if (
+                stat["conclusion"] == "FAILURE"
+                or stat["conclusion"] == "CANCELLED"
+                or stat["conclusion"] == "TIMED_OUT"
+                or stat["conclusion"] == "STARTUP_FAILURE"
+                or stat["conclusion"] == "ACTION_REQUIRED"
+            ):
+                print(f"SOMETHING WENT WRONG!\n")
+                print(f"{stat['conclusion']} - {(stat['status'])}")
     else:
         print(f"\n COMPLETED! \n")
     print(f"\n COMMIT RESPONSE \n -------------- \n{response.text}")
+
 
 createCommitOnBranch()

32a0b66ea4b0eccbbc817ead39134b7d07129850
Nicolai Haug <nicolaihaug@yahoo.com>
2023-09-23 01:40:59 +0200

    Update for pynest-ng

--- testsuite/pytests/test_visualization.py
+++ testsuite/pytests/test_visualization.py
@@ -108,7 +108,7 @@ class TestVisualization:
         device = nest.Create("voltmeter")
         nest.Connect(pg, nodes)
         nest.Connect(device, nodes)
-        nest.Simulate(100)
+        nest.Simulate(100.0)
 
         # Test with data from device
         plt.close("all")
@@ -131,6 +131,7 @@ class TestVisualization:
 
     def spike_recorder_data_setup(self, to_file=False):
         nest.ResetKernel()
+        nest.overwrite_files = True
         pg = nest.Create("poisson_generator", params={"rate": 1000.0})
         sr = nest.Create("spike_recorder")
         if to_file:
@@ -140,10 +141,10 @@ class TestVisualization:
             nest.Connect(pg, parrot)
             nest.Connect(parrot, sr)
             nest.Connect(parrot, sr_to_file)
-            nest.Simulate(100)
+            nest.Simulate(100.0)
             return sr, sr_to_file
         else:
-            nest.Simulate(100)
+            nest.Simulate(100.0)
             return sr
 
     def spike_recorder_raster_verify(self, sr_ref):
@@ -162,34 +163,34 @@ class TestVisualization:
         import nest.raster_plot
 
         sr, sr_to_file = self.spike_recorder_data_setup(to_file=True)
-        spikes = sr.get("events")
-        sr_ref = spikes["times"]
+        spikes = np.array(sr.events["times"])
+        senders = np.array(sr.events["senders"])
 
         # Test from_device
         nest.raster_plot.from_device(sr)
-        self.spike_recorder_raster_verify(sr_ref)
+        self.spike_recorder_raster_verify(spikes)
 
         # Test from_data
-        data = np.zeros([len(spikes["senders"]), 2])
-        data[:, 0] = spikes["senders"]
-        data[:, 1] = spikes["times"]
+        data = np.zeros([len(senders), 2])
+        data[:, 0] = senders
+        data[:, 1] = spikes
         nest.raster_plot.from_data(data)
-        self.spike_recorder_raster_verify(sr_ref)
+        self.spike_recorder_raster_verify(spikes)
 
         # Test from_file
         filename = sr_to_file.filenames[0]
         self.filenames.append(filename)
         nest.raster_plot.from_file(filename)
-        self.spike_recorder_raster_verify(sr_ref)
+        self.spike_recorder_raster_verify(spikes)
 
         # Test from_file_numpy
         nest.raster_plot.from_file_numpy([filename])
-        self.spike_recorder_raster_verify(sr_ref)
+        self.spike_recorder_raster_verify(spikes)
 
         if HAVE_PANDAS:
             # Test from_file_pandas
             nest.raster_plot.from_file_pandas([filename])
-            self.spike_recorder_raster_verify(sr_ref)
+            self.spike_recorder_raster_verify(spikes)
 
         # Test extract_events
         all_extracted = nest.raster_plot.extract_events(data)

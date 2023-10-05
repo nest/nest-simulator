899ceaebb695e2e0d891924bc957f8e0325f5405
Nicolai Haug <nicolaihaug@yahoo.com>
2023-10-05 11:52:49 +0200

    Import matplotlib unconditionally

--- testsuite/pytests/test_stdp_synapse.py
+++ testsuite/pytests/test_stdp_synapse.py
@@ -21,21 +21,12 @@
 
 from math import exp
 
+import matplotlib.pyplot as plt
 import nest
 import numpy as np
 import pytest
 
 DEBUG_PLOTS = False
-
-if DEBUG_PLOTS:
-    try:
-        import matplotlib as mpl  # noqa: F401
-        import matplotlib.pyplot as plt
-
-        DEBUG_PLOTS = True
-    except Exception:
-        DEBUG_PLOTS = False
-
 
 # Defined here so we can use it in init_params() and in parametrization
 RESOLUTION = 0.1  # [ms]

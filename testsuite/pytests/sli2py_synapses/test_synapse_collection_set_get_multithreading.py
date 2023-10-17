a52ec411c57b3eb2531313ca741e63094e426731
Nicolai Haug <nicolaihaug@yahoo.com>
2023-10-17 15:25:30 +0200

    Simplify, add docs and rename test

--- testsuite/pytests/sli2py_synapses/test_synapse_collection_set_get_multithreading.py
+++ testsuite/pytests/sli2py_synapses/test_synapse_collection_set_get_multithreading.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# test_setconnections_threads.py
+# test_synapse_collection_set_get_multithreading.py
 #
 # This file is part of NEST.
 #
@@ -19,25 +19,24 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
+
 import nest
 import pytest
 
 
-pytestmark = pytest.mark.skipif_missing_threads
+@pytest.mark.skipif_missing_threads
+def test_syn_collection_set_get_with_threads():
+    """
+    Test ``SynapseCollection`` setter and getter in multithreaded context.
+    """
 
-
-@pytest.fixture(autouse=True)
-def prepare(have_threads):
     nest.ResetKernel()
-    threads = 2 if have_threads else 1
-    nest.local_num_threads = threads
+    nest.local_num_threads = 2
 
-
-def test_connect_with_threads():
     source = nest.Create("iaf_psc_alpha")
     target = nest.Create("iaf_psc_alpha")
-
     nest.Connect(source, target, syn_spec={"synapse_model": "stdp_synapse"})
+
     conn = nest.GetConnections(source=source, synapse_model="stdp_synapse")
     conn_dict = conn.get()
 
@@ -46,4 +45,4 @@ def test_connect_with_threads():
 
     conn.set(weight=2.0)
 
-    assert conn.get()["weight"] == 2.0
+    assert conn.weight == 2.0

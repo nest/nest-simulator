f766cb331c55ab1b03dc8911742b5fa5d0e19ea6
Nicolai Haug <39106781+nicolossus@users.noreply.github.com>
2023-12-01 11:57:36 +0100

    WIP: MPI testing framework

---
+++ testsuite/pytests/utilities/mpi_wrapper.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+#
+# mpi_wrapper.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+import ast
+import functools
+import inspect
+import os
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+
+import numpy as np
+import pytest
+from decorator import decorator
+
+# from mpi4py import MPI
+
+
+class DecoratorParserBase:
+    """
+    Base class that parses the test module to retrieve imports, test code and
+    test parametrization.
+    """
+
+    def __init__(self):
+        pass
+
+    def _parse_import_statements(self, caller_fname):
+        with open(caller_fname, "r") as f:
+            tree = ast.parse(f.read(), caller_fname)
+
+        modules = []
+        for node in ast.walk(tree):
+            if isinstance(node, (ast.Import, ast.ImportFrom)):
+                modules.append(ast.unparse(node).encode())
+
+        return modules
+
+    def _parse_func_source(self, func):
+        lines, _ = inspect.getsourcelines(func)
+        # remove decorators and encode
+        func_src = [line.encode() for line in lines if not line.startswith("@")]
+        return func_src
+
+    def _params_as_str(self, *args, **kwargs):
+        params = ""
+        if args:
+            params += ", ".join(f"{arg}" for arg in args)
+        if kwargs:
+            if args:
+                params += ", "
+            params += ", ".join(f"{key}={value}" for key, value in kwargs.items())
+        return params
+
+
+class mpi_assert_equal_df(DecoratorParserBase):
+    """
+    docs
+    """
+
+    def __init__(self, procs_lst):
+        if not isinstance(procs_lst, list):
+            # TODO: Instead of passing the number of MPI procs to run explicitly, i.e., [1, 2, 4], another
+            # option is to pass the max number, e.g., 4, and handle the range internally
+            msg = "'mpi_assert_equal_df' decorator requires the number of MPI procs to test to be passed as a list"
+            raise TypeError(msg)
+
+        # TODO: check if len(procs_lst) >= 2 (not yet implemented for debugging purposes)
+
+        self._procs_lst = procs_lst
+        self._caller_fname = inspect.stack()[1].filename
+
+    def __call__(self, func):
+        def wrapper(func, *args, **kwargs):
+            # TODO: replace path setup below with the following:
+            # with tempfile.TemporaryDirectory() as tmpdir:
+            self._path = Path("./tmpdir")
+            self._path.mkdir(parents=True, exist_ok=True)
+
+            # Write the relevant code from test module to a new, temporary (TODO)
+            # runner script
+            with open(self._path / "runner.py", "wb") as fp:
+                self._write_runner(fp, func, *args, **kwargs)
+
+            for procs in self._procs_lst:
+                # TODO: MPI and subprocess does not seem to play well together
+                """
+                subprocess.run(['python', fp.name],
+                               capture_output=True,
+                               check=True
+                               )
+                """
+
+                """
+                res = subprocess.run(
+                    ["mpirun", "-np", str(procs), "python", fp.name], capture_output=True, check=True, env=os.environ
+                )
+
+                """
+                res = subprocess.run(
+                    # ["mpiexec", "-n", str(procs), sys.executable, fp.name],
+                    ["mpirun", "-np", str(procs), "python", fp.name],
+                    capture_output=True,
+                    check=True,
+                    shell=True,
+                    env=os.environ,
+                )
+
+                print(res)
+
+        # Here we need to assert that dfs from all runs are equal
+
+        return decorator(wrapper, func)
+
+    def _main_block_equal_df(self, func, *args, **kwargs):
+        main_block = "\n"
+        main_block += "if __name__ == '__main__':"
+        main_block += "\n\t"
+        main_block += f"df = {func.__name__}({self._params_as_str(*args, **kwargs)})"
+        main_block += "\n\t"
+
+        main_block += f"path = '{self._path}'"
+        main_block += "\n\t"
+
+        # Write output df to csv (will be compared later)
+        # main_block += "df.to_csv(f'{path}/df_{nest.NumProcesses()}-{nest.Rank()}.csv', index=False)"
+
+        return main_block.encode()
+
+    def _write_runner(self, fp, func, *args, **kwargs):
+        # TODO: most of this can probably be in base class
+        fp.write(b"\n".join(self._parse_import_statements(self._caller_fname)))
+        fp.write(b"\n\n")
+        fp.write(b"".join(self._parse_func_source(func)))
+
+        # TODO: only the main block needs to be changed between runner runs
+        fp.write(self._main_block_equal_df(func, *args, **kwargs))
---
+++ testsuite/pytests/utilities/test_brunel2000_mpi.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+#
+# test_brunel2000_mpi.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+import nest
+import numpy as np
+import pandas as pd
+import pytest
+from mpi_wrapper import mpi_assert_equal_df
+
+# from mpi4py import MPI
+
+
+@mpi_assert_equal_df([1, 2])
+def test_brunel2000():
+    """Implementation of the sparsely connected recurrent network described by
+    Brunel (2000).
+
+    References
+    ----------
+    Brunel N, Dynamics of Sparsely Connected Networks of Excitatory and
+    Inhibitory Spiking Neurons, Journal of Computational Neuroscience 8,
+    183-208 (2000).
+    """
+
+    nest.ResetKernel()
+
+    nest.set(total_num_virtual_procs=2)
+
+    # Model parameters
+    NE = 10_000  # number of excitatory neurons
+    NI = 2_500  # number of inhibitory neurons
+    CE = 1_000  # number of excitatory synapses per neuron
+    CI = 250  # number of inhibitory synapses per neuron
+    N_rec = 10  # number of (excitatory) neurons to record
+    D = 1.5  # synaptic delay, all connections [ms]
+    JE = 0.1  # peak of EPSP [mV]
+    eta = 2.0  # external rate relative to threshold rate
+    g = 5.0  # ratio inhibitory weight/excitatory weight
+    JI = -g * JE  # peak of IPSP [mV]
+
+    neuron_params = {
+        "tau_m": 20,  # membrance time constant [ms]
+        "t_ref": 2.0,  # refractory period [ms]
+        "C_m": 250.0,  # membrane capacitance [pF]
+        "E_L": 0.0,  # resting membrane potential [mV]
+        "V_th": 20.0,  # threshold potential [mV]
+        "V_reset": 0.0,  # reset potential [mV]
+    }
+
+    # Threshold rate; the external rate needed for a neuron to reach
+    # threshold in absence of feedback
+    nu_thresh = neuron_params["V_th"] / (JE * CE * neuron_params["tau_m"])
+
+    # External firing rate; firing rate of a neuron in the external population
+    nu_ext = eta * nu_thresh
+
+    # Build network
+    enodes = nest.Create("iaf_psc_delta", NE, params=neuron_params)
+    inodes = nest.Create("iaf_psc_delta", NI, params=neuron_params)
+    ext = nest.Create("poisson_generator", 1, params={"rate": nu_ext * CE * 1000.0})
+    srec = nest.Create("spike_recorder", 1)
+
+    nest.CopyModel("static_synapse", "esyn", params={"weight": JE, "delay": D})
+    nest.CopyModel("static_synapse", "isyn", params={"weight": JI, "delay": D})
+
+    nest.Connect(ext, enodes, conn_spec="all_to_all", syn_spec="esyn")
+    nest.Connect(ext, inodes, conn_spec="all_to_all", syn_spec="esyn")
+    nest.Connect(enodes[:N_rec], srec)
+    nest.Connect(
+        enodes,
+        enodes + inodes,
+        conn_spec={"rule": "fixed_indegree", "indegree": CE, "allow_autapses": False, "allow_multapses": True},
+        syn_spec="esyn",
+    )
+    nest.Connect(
+        inodes,
+        enodes + inodes,
+        conn_spec={"rule": "fixed_indegree", "indegree": CI, "allow_autapses": False, "allow_multapses": True},
+        syn_spec="isyn",
+    )
+
+    # Simulate network
+    nest.Simulate(200.0)
+
+    return pd.DataFrame.from_records(srec.events)

2ee7c5bd9827c3cbafc3862371a664cabacb0e90
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2023-12-11 22:42:22 +0100

    Re-instate doxygen comment

--- nestkernel/nest.h
+++ nestkernel/nest.h
@@ -53,6 +53,9 @@ void enable_dryrun_mode( const size_t n_procs );
 
 void register_logger_client( const deliver_logging_event_ptr client_callback );
 
+/**
+ * Register connection model (i.e. an instance of a class inheriting from `Connection`).
+ */
 template < template < typename > class ConnectorModelT >
 void register_connection_model( const std::string& name );
 

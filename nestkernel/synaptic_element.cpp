f754dbeea66c092dc57b1851b6d53ae7f07d609e
ackurth <anno.kurth@posteo.de>
2024-01-02 20:15:26 +0100

    can be greater than or equal to zero

--- nestkernel/synaptic_element.cpp
+++ nestkernel/synaptic_element.cpp
@@ -128,7 +128,8 @@ nest::SynapticElement::set( const DictionaryDatum& d )
 
   if ( new_decay_vacant < 0.0 )
   {
-    throw BadProperty( "Decay constant must be strictly positive." );
+    throw BadProperty( "Decay constant must be greater than or equal to zero." );
+
   }
   decay_vacant_ = new_decay_vacant;
 }

04139d4e55283849392b22495961cfcd5cc49c52
janeirik <>
2024-01-19 19:56:14 +0100

    reproduces dynamics frfom wang 2002 paper

--- pynest/examples/wang_decision_making.py
+++ pynest/examples/wang_decision_making.py
@@ -3,21 +3,28 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import numpy as np
 
-np.random.seed(123)
+np.random.seed(1234)
 rng = np.random.default_rng()
 
 dt = 0.1
 nest.set(resolution=dt, print_time=True)
 
+g_AMPA_ex = 0.05
+g_AMPA_ext_ex = 2.1
+g_NMDA_ex = 0.165
+g_GABA_ex = 1.3
+
+g_AMPA_in = 0.04
+g_AMPA_ext_in = 1.62
+g_NMDA_in = 0.13
+g_GABA_in = 1.0
+
 
 # Parameters from paper
-epop_params = {"g_AMPA": 0.05,
-               "g_AMPA_ext": 2.1,
-               "g_NMDA": 0.165,
-               "g_GABA": 1.3,
-               "tau_GABA": 5.0,
+epop_params = {"tau_GABA": 5.0,
                "tau_AMPA": 2.0,
                "tau_decay_NMDA": 100.0,
+               "tau_rise_NMDA": 2.0,
                "alpha": 0.5,
                "conc_Mg2": 1.0,
                "g_L": 25.,               # leak conductance
@@ -31,13 +38,10 @@ epop_params = {"g_AMPA": 0.05,
                }
 
 
-ipop_params = {"g_AMPA": 0.04,
-               "g_AMPA_ext": 1.62,
-               "g_NMDA": 0.13,
-               "g_GABA": 1.0,
-               "tau_GABA": 5.0,
+ipop_params = {"tau_GABA": 5.0,
                "tau_AMPA": 2.0,
                "tau_decay_NMDA": 100.0,
+               "tau_rise_NMDA": 2.0,
                "alpha": 0.5,
                "conc_Mg2": 1.0,
                "g_L": 20.,               # leak conductance
@@ -57,8 +61,7 @@ signal_update_interval = 50.
 f = 0.15 # proportion of neurons receiving signal inputs
 w_plus = 1.7
 w_minus = 1 - f * (w_plus - 1) / (1 - f)
-delay = 0.1
-
+delay = 0.5
 
 NE = 1600
 NI = 400
@@ -98,11 +101,22 @@ poisson_b = nest.Create("inhomogeneous_poisson_generator",
 
 poisson_0 = nest.Create("poisson_generator", params={"rate": 2400.})
 
-syn_spec_pot = {"synapse_model": "static_synapse", "weight":w_plus, "delay":delay, "receptor_type": 0}
-syn_spec_default = {"synapse_model": "static_synapse", "weight":1.0, "delay":delay, "receptor_type": 0}
-syn_spec_dep = {"synapse_model": "static_synapse", "weight":w_minus, "delay":delay, "receptor_type": 0}
-syn_spec_inhibitory = {"synapse_model": "static_synapse", "weight":-1.0, "delay":delay, "receptor_type": 0}
-syn_spec_ext = {"synapse_model": "static_synapse", "weight":1., "delay":0.1, "receptor_type": 1}
+syn_spec_pot_AMPA = {"synapse_model": "static_synapse", "weight":w_plus * g_AMPA_ex, "delay":delay, "receptor_type": 1}
+syn_spec_pot_NMDA = {"synapse_model": "static_synapse", "weight":w_plus * g_NMDA_ex, "delay":delay, "receptor_type": 3}
+
+syn_spec_dep_AMPA = {"synapse_model": "static_synapse", "weight":w_minus * g_AMPA_ex, "delay":delay, "receptor_type": 1}
+syn_spec_dep_NMDA = {"synapse_model": "static_synapse", "weight":w_minus * g_NMDA_ex, "delay":delay, "receptor_type": 3}
+
+ie_syn_spec = {"synapse_model": "static_synapse", "weight": -1.0 * g_GABA_ex, "delay":delay, "receptor_type": 2}
+ii_syn_spec = {"synapse_model": "static_synapse", "weight": -1.0 * g_GABA_in, "delay":delay, "receptor_type": 2}
+
+ei_syn_spec_AMPA = {"synapse_model": "static_synapse", "weight": 1.0 * g_AMPA_in, "delay":delay, "receptor_type": 1}
+ei_syn_spec_NMDA = {"synapse_model": "static_synapse", "weight": 1.0 * g_NMDA_in, "delay":delay, "receptor_type": 3}
+ee_syn_spec_AMPA = {"synapse_model": "static_synapse", "weight": 1.0 * g_AMPA_ex, "delay":delay, "receptor_type": 1}
+ee_syn_spec_NMDA = {"synapse_model": "static_synapse", "weight": 1.0 * g_NMDA_ex, "delay":delay, "receptor_type": 3}
+
+exte_syn_spec = {"synapse_model": "static_synapse", "weight":g_AMPA_ext_ex, "delay":0.1, "receptor_type": 1}
+exti_syn_spec = {"synapse_model": "static_synapse", "weight":g_AMPA_ext_in, "delay":0.1, "receptor_type": 1}
 
 sr_nonselective = nest.Create("spike_recorder")
 sr_selective1 = nest.Create("spike_recorder")
@@ -114,67 +128,75 @@ mm_selective2 = nest.Create("multimeter", {"record_from": ["V_m", "s_NMDA", "s_A
 mm_nonselective = nest.Create("multimeter", {"record_from": ["V_m", "s_NMDA", "s_AMPA", "s_GABA"]})
 mm_inhibitory = nest.Create("multimeter", {"record_from": ["V_m", "s_NMDA", "s_AMPA", "s_GABA"]})
 
-nest.Connect(poisson_0,
-             nonselective_pop + selective_pop1 + selective_pop2 + inhibitory_pop,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_ext)
 
-nest.Connect(nonselective_pop,
-             selective_pop1 + selective_pop2,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_dep)
 
-nest.Connect(nonselective_pop,
-             nonselective_pop + inhibitory_pop,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_default)
+# # # Create connections
 
-nest.Connect(selective_pop1,
-             selective_pop2,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_dep)
+# from external
+nest.Connect(poisson_0, nonselective_pop + selective_pop1 + selective_pop2, conn_spec="all_to_all", syn_spec=exte_syn_spec)
+nest.Connect(poisson_0, inhibitory_pop, conn_spec="all_to_all", syn_spec=exti_syn_spec)
 
-nest.Connect(selective_pop2,
-             selective_pop1,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_dep)
+nest.Connect(poisson_a, selective_pop1, conn_spec="all_to_all", syn_spec=exte_syn_spec)
+nest.Connect(poisson_b, selective_pop2, conn_spec="all_to_all", syn_spec=exte_syn_spec)
 
-nest.Connect(selective_pop1 + selective_pop2,
-             nonselective_pop + inhibitory_pop,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_default)
+# from nonselective pop
+nest.Connect(nonselective_pop, selective_pop1 + selective_pop2, conn_spec="all_to_all", syn_spec=syn_spec_dep_AMPA)
+nest.Connect(nonselective_pop, selective_pop1 + selective_pop2, conn_spec="all_to_all", syn_spec=syn_spec_dep_NMDA)
 
-nest.Connect(inhibitory_pop,
-             selective_pop1 + selective_pop2 + nonselective_pop + inhibitory_pop,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_inhibitory)
+nest.Connect(nonselective_pop, nonselective_pop, conn_spec="all_to_all", syn_spec=ee_syn_spec_AMPA)
+nest.Connect(nonselective_pop, nonselective_pop, conn_spec="all_to_all", syn_spec=ee_syn_spec_NMDA)
 
-nest.Connect(poisson_a,
-             selective_pop1,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_ext)
-
-nest.Connect(poisson_b,
-             selective_pop2,
-             conn_spec="all_to_all",
-             syn_spec=syn_spec_ext)
+nest.Connect(nonselective_pop, inhibitory_pop, conn_spec="all_to_all", syn_spec=ei_syn_spec_AMPA)
+nest.Connect(nonselective_pop, inhibitory_pop, conn_spec="all_to_all", syn_spec=ei_syn_spec_NMDA)
 
 nest.Connect(nonselective_pop, sr_nonselective)
+
+# from selective pops
+nest.Connect(selective_pop1, selective_pop1, conn_spec="all_to_all", syn_spec=syn_spec_pot_AMPA)
+nest.Connect(selective_pop1, selective_pop1, conn_spec="all_to_all", syn_spec=syn_spec_pot_NMDA)
+
+nest.Connect(selective_pop2, selective_pop2, conn_spec="all_to_all", syn_spec=syn_spec_pot_AMPA)
+nest.Connect(selective_pop2, selective_pop2, conn_spec="all_to_all", syn_spec=syn_spec_pot_NMDA)
+
+nest.Connect(selective_pop1, selective_pop2, conn_spec="all_to_all", syn_spec=syn_spec_dep_AMPA)
+nest.Connect(selective_pop1, selective_pop2, conn_spec="all_to_all", syn_spec=syn_spec_dep_NMDA)
+
+nest.Connect(selective_pop2, selective_pop1, conn_spec="all_to_all", syn_spec=syn_spec_dep_AMPA)
+nest.Connect(selective_pop2, selective_pop1, conn_spec="all_to_all", syn_spec=syn_spec_dep_NMDA)
+
+nest.Connect(selective_pop1 + selective_pop2, nonselective_pop, conn_spec="all_to_all", syn_spec=ee_syn_spec_AMPA)
+nest.Connect(selective_pop1 + selective_pop2, nonselective_pop, conn_spec="all_to_all", syn_spec=ee_syn_spec_NMDA)
+
+nest.Connect(selective_pop1 + selective_pop2, inhibitory_pop, conn_spec="all_to_all", syn_spec=ei_syn_spec_AMPA)
+nest.Connect(selective_pop1 + selective_pop2, inhibitory_pop, conn_spec="all_to_all", syn_spec=ei_syn_spec_NMDA)
+
 nest.Connect(selective_pop1, sr_selective1)
 nest.Connect(selective_pop2, sr_selective2)
+
+# from inhibitory pop
+nest.Connect(inhibitory_pop, selective_pop1 + selective_pop2 + nonselective_pop, conn_spec="all_to_all", syn_spec=ie_syn_spec)
+nest.Connect(inhibitory_pop, inhibitory_pop, conn_spec="all_to_all", syn_spec=ii_syn_spec)
+
 nest.Connect(inhibitory_pop, sr_inhibitory)
 
+
+# multimeters
 nest.Connect(mm_selective1, selective_pop1[0])
 nest.Connect(mm_selective2, selective_pop2[0])
 nest.Connect(mm_nonselective, nonselective_pop[0])
 nest.Connect(mm_inhibitory, inhibitory_pop[0])
 
-nest.Simulate(4000.)
+nest.Simulate(5000.)
 
 spikes_nonselective = sr_nonselective.get("events", "times")
 spikes_selective1 = sr_selective1.get("events", "times")
 spikes_selective2 = sr_selective2.get("events", "times")
 spikes_inhibitory = sr_inhibitory.get("events", "times")
+
+vm_nonselective = mm_nonselective.get("events", "V_m")
+s_AMPA_nonselective = mm_nonselective.get("events", "s_AMPA")
+s_GABA_nonselective = mm_nonselective.get("events", "s_GABA")
+s_NMDA_nonselective = mm_nonselective.get("events", "s_NMDA")
 
 vm_selective1 = mm_selective1.get("events", "V_m")
 s_AMPA_selective1 = mm_selective1.get("events", "s_AMPA")
@@ -191,12 +213,74 @@ s_AMPA_inhibitory = mm_inhibitory.get("events", "s_AMPA")
 s_GABA_inhibitory = mm_inhibitory.get("events", "s_GABA")
 s_NMDA_inhibitory = mm_inhibitory.get("events", "s_NMDA")
 
-gs = GridSpec(5,5)
 
-bins = np.arange(0, 4001, 5) - 0.001
-plt.hist(spikes_selective1, bins=bins, histtype="step")
-plt.hist(spikes_selective2, bins=bins, histtype="step")
+res = 1.0
+bins = np.arange(0, 4001, res) - 0.001
+
+fig, ax = plt.subplots(ncols=2, nrows=2, sharex=True, sharey=True)
+fig.tight_layout()
+d = NE * f * (res / 1000)
+hist1, _ = np.histogram(spikes_selective1, bins=bins)
+hist2, _ = np.histogram(spikes_selective2, bins=bins)
+
+ax[0,0].plot(hist1 / d)
+ax[0,0].set_title("Selective pop A")
+ax[0,1].plot(hist2 / d)
+ax[0,1].set_title("Selective pop B")
+
+d = NE * (1 - 2*f) * res / 1000
+hist, _ = np.histogram(spikes_nonselective, bins=bins)
+ax[1,0].plot(hist / d)
+ax[1,0].set_title("Nonselective pop")
+
+d = NI * res / 1000
+hist, _ = np.histogram(spikes_inhibitory, bins=bins)
+ax[1,1].plot(hist / d)
+ax[1,1].set_title("Inhibitory pop")
+
+
+
+fig, ax = plt.subplots(ncols=4, nrows=4, sharex=True, sharey="row")
+fig.tight_layout()
+
+
+ax[0,0].plot(s_AMPA_selective1)
+ax[0,1].plot(s_AMPA_selective2)
+ax[0,2].plot(s_AMPA_nonselective)
+ax[0,3].plot(s_AMPA_inhibitory)
+
+ax[1,0].plot(s_NMDA_selective1)
+ax[1,1].plot(s_NMDA_selective2)
+ax[1,2].plot(s_NMDA_nonselective)
+ax[1,3].plot(s_NMDA_inhibitory)
+
+ax[2,0].plot(s_GABA_selective1)
+ax[2,1].plot(s_GABA_selective2)
+ax[2,2].plot(s_GABA_nonselective)
+ax[2,3].plot(s_GABA_inhibitory)
+
+ax[3,0].plot(vm_selective1)
+ax[3,1].plot(vm_selective2)
+ax[3,2].plot(vm_nonselective)
+ax[3,3].plot(vm_inhibitory)
+
+
+ax[0,0].set_ylabel("S_AMPA")
+ax[1,0].set_ylabel("S_NMDA")
+ax[2,0].set_ylabel("S_GABA")
+ax[3,0].set_ylabel("V_m")
+
+ax[0,0].set_title("Selective pop1")
+ax[0,1].set_title("Selective pop2")
+ax[0,2].set_title("Nonselective pop")
+ax[0,3].set_title("Inhibitory pop")
+
+ax[0,0].set_title("Selective pop1")
+ax[0,1].set_title("Selective pop2")
+ax[0,2].set_title("Nonselective pop")
+ax[0,3].set_title("Inhibitory pop")
+
+
 
 plt.show()
-
 

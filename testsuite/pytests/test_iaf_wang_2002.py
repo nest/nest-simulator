3743147ceb23d26aabbc2561758a259000f30fb1
janeirik <>
2024-02-07 13:21:36 +0100

    flake8

--- testsuite/pytests/test_iaf_wang_2002.py
+++ testsuite/pytests/test_iaf_wang_2002.py
@@ -23,7 +23,7 @@
 Tests synaptic dynamics of the approximate model iaf_wang_2002.
 
 Since the neuron is conductance based, it is impossible to analytically
-confirm the membrane potential, but all the synaptic currents can be 
+confirm the membrane potential, but all the synaptic currents can be
 computed analytically (for the simplified implementation we use).
 The integration of the membrane potential is not tested here.
 """

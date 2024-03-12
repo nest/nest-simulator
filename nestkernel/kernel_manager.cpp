4d6990a104853b3a81976ac073e9d7ee9d85c475
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2024-03-12 16:01:36 +0100

    Initialize IOManager before ModelManager so *Backend infrastructure is up before device models are registered

--- nestkernel/kernel_manager.cpp
+++ nestkernel/kernel_manager.cpp
@@ -57,9 +57,9 @@ nest::KernelManager::KernelManager()
   , connection_manager()
   , sp_manager()
   , event_delivery_manager()
+  , io_manager()
   , model_manager()
   , music_manager()
-  , io_manager()
   , node_manager()
   , managers( { &logging_manager,
       &mpi_manager,
@@ -71,9 +71,9 @@ nest::KernelManager::KernelManager()
       &connection_manager,
       &sp_manager,
       &event_delivery_manager,
+      &io_manager,
       &model_manager,
       &music_manager,
-      &io_manager,
       &node_manager } )
   , initialized_( false )
 {

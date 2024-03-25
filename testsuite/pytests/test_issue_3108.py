590ee52562fbe43475c1d3daa99597fcfaf2b191
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2024-03-25 01:32:30 +0100

    Extended tests for slicing

--- testsuite/pytests/test_issue_3108.py
+++ testsuite/pytests/test_issue_3108.py
@@ -34,8 +34,13 @@ The spatial tests test that NodeCollection::rank_local_begin() works.
 The connect tests test that NodeCollection::thread_local_begin() works.
 """
 
+if nest.ll_api.sli_func("is_threaded"):
+    num_threads = [1, 2, 3, 4]
+else:
+    num_threads = [1]
 
-# Parametrization over the number of nodes here only so show hat it works
+
+@pytest.mark.parametrize("n_threads", num_threads)
 @pytest.mark.parametrize(
     "transform",
     [
@@ -50,10 +55,11 @@ The connect tests test that NodeCollection::thread_local_begin() works.
         lambda nc: (nc[:5] + nc[8:])[7::3],
     ],
 )
-def test_slice_node_collections(transform):
+def test_slice_node_collections(n_threads, transform):
     nest.ResetKernel()
-    nest.total_num_virtual_procs = 12
-    n_orig = nest.Create("parrot_neuron", 31)
+    nest.local_num_threads = n_threads
+
+    n_orig = nest.Create("parrot_neuron", 128)
     n_orig_gids = n_orig._to_array()["All"]
 
     n_sliced = transform(n_orig)
@@ -77,8 +83,78 @@ def test_slice_node_collections(transform):
             assert node.thread == thread
 
 
+@pytest.mark.parametrize("n_threads", num_threads)
+@pytest.mark.parametrize("stride", [1, 2, 3, 7, 12])
+def test_slice_single_element_parts(n_threads, stride):
+    """Same test as above, but on NC with single-element parts to check stepping over parts"""
+
+    nest.ResetKernel()
+    nest.local_num_threads = n_threads
+
+    # Use different models to get single-element node collections
+    mod_names = [f"pn{n:02d}" for n in range(91)]
+    for mod in mod_names:
+        nest.CopyModel("parrot_neuron", mod)
+
+    n_orig = sum(nest.Create(mod) for mod in mod_names)
+    n_orig_gids = n_orig._to_array()["All"]
+
+    n_sliced = n_orig[::stride]
+    n_pyslice_gids = n_orig_gids[::stride]
+
+    n_pyslice_gids_on_rank = set(
+        (n.global_id for n in nest.NodeCollection(n_pyslice_gids) if n.vp % nest.NumProcesses() == nest.Rank())
+    )
+
+    assert n_sliced._to_array()["All"] == n_pyslice_gids
+
+    n_sliced_gids_by_rank = n_sliced._to_array("rank")
+    assert len(n_sliced_gids_by_rank) == 1
+    assert set(next(iter(n_sliced_gids_by_rank.values()))) == n_pyslice_gids_on_rank
+
+    n_sliced_gids_by_thread = n_sliced._to_array("thread")
+    assert set(itertools.chain(*n_sliced_gids_by_thread.values())) == n_pyslice_gids_on_rank
+
+    for thread, tgids in n_sliced_gids_by_thread.items():
+        for node in nest.NodeCollection(tgids):
+            assert node.thread == thread
+
+
+@pytest.mark.parametrize("n_threads", num_threads)
+@pytest.mark.parametrize("stride", [1, 2, 3, 7, 12])
+def test_multi_parts_slicing(n_threads, stride):
+    """Same test as above, but on NC from parts of different size with different gaps"""
+
+    nest.ResetKernel()
+    nest.local_num_threads = n_threads
+
+    n_orig = nest.Create("parrot_neuron", 97)
+    n_orig_gids = n_orig._to_array()["All"]
+
+    n_sliced = n_orig[:5] + n_orig[6] + n_orig[8:18] + n_orig[23:34] + n_orig[41:]
+    n_pyslice_gids = nn_orig_gids[:5] + nn_orig_gids[6] + nn_orig_gids[8:18] + nn_orig_gids[23:34] + nn_orig_gids[41:]
+
+    n_pyslice_gids_on_rank = set(
+        (n.global_id for n in nest.NodeCollection(n_pyslice_gids) if n.vp % nest.NumProcesses() == nest.Rank())
+    )
+
+    assert n_sliced._to_array()["All"] == n_pyslice_gids
+
+    n_sliced_gids_by_rank = n_sliced._to_array("rank")
+    assert len(n_sliced_gids_by_rank) == 1
+    assert set(next(iter(n_sliced_gids_by_rank.values()))) == n_pyslice_gids_on_rank
+
+    n_sliced_gids_by_thread = n_sliced._to_array("thread")
+    assert set(itertools.chain(*n_sliced_gids_by_thread.values())) == n_pyslice_gids_on_rank
+
+    for thread, tgids in n_sliced_gids_by_thread.items():
+        for node in nest.NodeCollection(tgids):
+            assert node.thread == thread
+
+
+@pytest.mark.parametrize("n_threads", num_threads)
 @pytest.mark.parametrize("start, step", ([[0, 1], [0, 3]] + [[2, n] for n in range(1, 9)]))
-def test_get_positions_with_mpi(start, step):
+def test_get_positions_with_mpi(n_threads, start, step):
     """
     Test that correct positions can be obtained from sliced node collections.
 
@@ -88,9 +164,10 @@ def test_get_positions_with_mpi(start, step):
     processes.
     """
 
-    num_neurons = 23
+    num_neurons = 128
 
     nest.ResetKernel()
+    nest.local_num_threads = n_threads
 
     # Need floats because NEST returns positions as floats
     node_pos = [(float(x), 0.0) for x in range(num_neurons)]
@@ -113,8 +190,9 @@ def test_get_positions_with_mpi(start, step):
     assert pos == expected_pos
 
 
+@pytest.mark.parametrize("n_threads", num_threads)
 @pytest.mark.parametrize("pick", range(0, 7))
-def test_get_spatial_for_single_element_and_mpi(pick):
+def test_get_spatial_for_single_element_and_mpi(n_threads, pick):
     """
     Test that spatial information can be collected from a single layer element.
 
@@ -124,6 +202,7 @@ def test_get_spatial_for_single_element_and_mpi(pick):
     num_neurons = 7
 
     nest.ResetKernel()
+    nest.local_num_threads = n_threads
 
     node_pos = [(float(x), 0.0) for x in range(num_neurons)]
 
@@ -143,8 +222,9 @@ def test_get_spatial_for_single_element_and_mpi(pick):
         assert len(sp) == 0
 
 
+@pytest.mark.parametrize("n_threads", num_threads)
 @pytest.mark.parametrize("pick", range(0, 5))
-def test_connect_with_single_element_slice_and_mpi(pick):
+def test_connect_with_single_element_slice_and_mpi(n_threads, pick):
     """
     Test that connection with single-element sliced layer is possible on multiple mpi processes.
 
@@ -154,6 +234,8 @@ def test_connect_with_single_element_slice_and_mpi(pick):
     num_neurons = 5
 
     nest.ResetKernel()
+    nest.local_num_threads = n_threads
+
     layer = nest.Create(
         model="parrot_neuron",
         n=num_neurons,
@@ -172,18 +254,21 @@ def test_connect_with_single_element_slice_and_mpi(pick):
     assert tgt == local_nodes
 
 
+@pytest.mark.parametrize("n_threads", num_threads)
 @pytest.mark.parametrize("sstep", [2, 3, 4, 6])
 @pytest.mark.parametrize("tstep", [2, 3, 4, 6])
-def test_connect_slice_to_slice_and_mpi(sstep, tstep):
+def test_connect_slice_to_slice_and_mpi(n_threads, sstep, tstep):
     """
     Test that connection with stepped source and target layers is possible on multiple mpi processes.
 
     This was an original minimal reproducer for #3108.
     """
 
-    num_neurons = 23
+    num_neurons = 128
 
     nest.ResetKernel()
+    nest.local_num_threads = n_threads
+
     layer = nest.Create(
         model="parrot_neuron",
         n=num_neurons,

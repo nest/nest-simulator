47685f941d11d2c76aa9b7c02fd502faac11b641
akorgor <40828647+akorgor@users.noreply.github.com>
2024-04-09 13:19:06 +0200

    Remove reset and update interval kernel settings since not used in online tasks

--- pynest/examples/eprop_plasticity/online_eprop_supervised_classification_evidence-accumulation.py
+++ pynest/examples/eprop_plasticity/online_eprop_supervised_classification_evidence-accumulation.py
@@ -157,11 +157,9 @@ duration.update({key: value * duration["step"] for key, value in steps.items()})
 # Set up simulation
 # .................
 # As last step of the setup, we reset the NEST kernel to remove all existing NEST simulation settings and
-# objects and set some NEST kernel parameters, some of which are e-prop-related.
+# objects and set some NEST kernel parameters.
 
 params_setup = {
-    "eprop_reset_neurons_on_update": True,  # if True, reset dynamic variables at start of each update interval
-    "eprop_update_interval": duration["sequence"],  # ms, time interval for updating the synaptic weights
     "print_time": False,  # if True, print time progress bar during simulation, set False if run as code cell
     "resolution": duration["step"],
     "total_num_virtual_procs": 1,  # number of virtual processes, set in case of distributed computing

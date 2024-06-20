e5d78737ee449505f42007aca10e617141d07cf5
Yehor Pishyi <egorpeshiy@gmail.com>
2024-06-20 12:30:40 +0200

    Minor fixes

--- models/izhikevich.h
+++ models/izhikevich.h
@@ -55,17 +55,17 @@ Implementation of the simple spiking neuron model introduced by Izhikevich
 
    &\text{if}\;\;\; V_m \geq V_{th}:\\
    &\;\;\;\; V_m \text{ is set to } c\\
-   &\;\;\;\; u \text{ is incremented by } d\\
+   &\;\;\;\; U_m \text{ is incremented by } d\\
    & \, \\
-   &v \text{ jumps on each spike arrival by the weight of the spike}
+   &V_m \text{ jumps on each spike arrival by the weight of the spike}
 
 As published in [1]_, the numerics differs from the standard forward Euler
 technique in two ways:
 
-1) the new value of :math:`u` is calculated based on the new value of
+1) the new value of :math:`U_m` is calculated based on the new value of
    :math:`V_m`, rather than the previous value
 2) the variable :math:`V_m` is updated using a time step half the size of that
-   used to update variable :math:`u`.
+   used to update variable :math:`U_m`.
 
 This model offers both forms of integration, they can be selected using the
 boolean parameter ``consistent_integration``. To reproduce some results

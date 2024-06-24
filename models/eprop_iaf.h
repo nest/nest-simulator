c588cd246cbc0679b3e580e1e42a92577e167213
Agnes Korcsak-Gorzo <40828647+akorgor@users.noreply.github.com>
2024-06-24 07:17:22 +0200

    Add period in et al.

--- models/eprop_iaf.h
+++ models/eprop_iaf.h
@@ -291,7 +291,7 @@ void register_eprop_iaf( const std::string& name );
 
 /**
  * Class implementing a current-based leaky integrate-and-fire neuron model with delta-shaped postsynaptic currents for
- * e-prop plasticity according to Bellec et al (2020) with additional biological features described in
+ * e-prop plasticity according to Bellec et al. (2020) with additional biological features described in
  * Korcsak-Gorzo, Stapmanns, and Espinoza Valverde et al. (in preparation).
  */
 class eprop_iaf : public EpropArchivingNodeRecurrent

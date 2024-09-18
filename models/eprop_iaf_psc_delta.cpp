dac9f59ef272a2ed809c28f9d12dddd6f36f3a91
Agnes Korcsak-Gorzo <40828647+akorgor@users.noreply.github.com>
2024-09-18 12:23:03 +0200

    Refactor `handle` function of `CurrentEvent`

--- models/eprop_iaf_psc_delta.cpp
+++ models/eprop_iaf_psc_delta.cpp
@@ -411,11 +411,8 @@ eprop_iaf_psc_delta::handle( CurrentEvent& e )
 {
   assert( e.get_delay_steps() > 0 );
 
-  const double c = e.get_current();
-  const double w = e.get_weight();
-
-  // add weighted current; HEP 2002-10-04
-  B_.currents_.add_value( e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ), w * c );
+  B_.currents_.add_value(
+    e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ), e.get_weight() * e.get_current() );
 }
 
 void

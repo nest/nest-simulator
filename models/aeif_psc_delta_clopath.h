14515159bacc4619cbc3d101fd7fe89b7a1bddf3
Jessica Mitchell <j.mitchell@fz-juelich.de>
2024-10-28 09:30:09 +0100

    update text based on review

--- models/aeif_psc_delta_clopath.h
+++ models/aeif_psc_delta_clopath.h
@@ -96,11 +96,10 @@ See also [2]_.
 .. note::
 
     The default refractory period for ``aeif`` models is zero, consistent with the model definition in
-    Brette & Gerstner [4]_.
-    This means if the refractory period is zero, the ``aeif`` neurons can fire multiple spikes in a single time step,
-    which can lead to exploding spike numbers and extreme slow-down of simulations.
+    Brette & Gerstner [4]_.  Thus, an ``aeif`` neuron with default parameters can fire multiple spikes in a single
+    time step, which can lead to exploding spike numbers and extreme slow-down of simulations.
 
-    We recommend users set ``t_ref > 0``.
+    To avoid such unphysiological behavior, you should set a refractory time ``t_ref > 0``.
 
 Parameters
 ++++++++++

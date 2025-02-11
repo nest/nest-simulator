902c18b24bbdf722511bf50b8aef0a62e85121ce
med-ayssar <med.ayssar@gmail.com>
2025-02-11 11:48:46 +0100

    Revert changes from do_tests

--- testsuite/do_tests.sh
+++ testsuite/do_tests.sh
@@ -39,16 +39,17 @@ set -euo pipefail
 # usage 2 <option>
 #   Use this variant in case of a missing required option <option>
 #
-usage() {
-  if test $1 = 1; then
-    echo "Error: Unknown option '$2'"
-  fi
+usage ()
+{
+    if test $1 = 1; then
+        echo "Error: Unknown option '$2'"
+    fi
 
-  if test $1 = 2; then
-    echo "Error: Missing required option '$2'"
-  fi
+    if test $1 = 2; then
+        echo "Error: Missing required option '$2'"
+    fi
 
-  cat <<EOF
+    cat <<EOF
 Usage: $0 --prefix=<path> [options]
 
 Required arguments:
@@ -60,50 +61,50 @@ Options:
     --help                 Print program options and exit
 EOF
 
-  exit $1
+    exit $1
 }
 
 PREFIX=""
 PYTHON=""
 MUSIC=""
-while test $# -gt 0; do
-  case "$1" in
-  --help)
-    usage 0
-    ;;
-  --prefix=*)
-    PREFIX="$(echo "$1" | sed 's/^--prefix=//')"
-    ;;
-  --with-python=*)
-    PYTHON="$(echo "$1" | sed 's/^--with-python=//')"
-    ;;
-  --with-music=*)
-    MUSIC="$(echo "$1" | sed 's/^--with-music=//')"
-    ;;
-  *)
-    usage 1 "$1"
-    ;;
-  esac
-  shift
+while test $# -gt 0 ; do
+    case "$1" in
+        --help)
+            usage 0
+            ;;
+        --prefix=*)
+            PREFIX="$( echo "$1" | sed 's/^--prefix=//' )"
+            ;;
+        --with-python=*)
+            PYTHON="$( echo "$1" | sed 's/^--with-python=//' )"
+            ;;
+        --with-music=*)
+            MUSIC="$( echo "$1" | sed 's/^--with-music=//' )"
+            ;;
+        *)
+            usage 1 "$1"
+            ;;
+    esac
+    shift
 done
 
 if test ! "${PREFIX:-}"; then
-  usage 2 "--prefix"
+    usage 2 "--prefix";
 fi
 
 if test "${PYTHON}"; then
-  TIME_LIMIT=120 # seconds, for each of the Python tests
-  PYTEST_VERSION="$(${PYTHON} -m pytest --version --timeout ${TIME_LIMIT} --numprocesses=1 2>&1)" || {
-    echo "Error: PyNEST testing requested, but 'pytest' cannot be run."
-    echo "       Testing also requires the 'pytest-xdist' and 'pytest-timeout' extensions."
-    exit 1
-  }
-  PYTEST_VERSION="$(echo "${PYTEST_VERSION}" | cut -d' ' -f2)"
+      TIME_LIMIT=120  # seconds, for each of the Python tests
+      PYTEST_VERSION="$(${PYTHON} -m pytest --version --timeout ${TIME_LIMIT} --numprocesses=1 2>&1)" || {
+        echo "Error: PyNEST testing requested, but 'pytest' cannot be run."
+        echo "       Testing also requires the 'pytest-xdist' and 'pytest-timeout' extensions."
+        exit 1
+    }
+      PYTEST_VERSION="$(echo "${PYTEST_VERSION}" | cut -d' ' -f2)"
 fi
 
 if ! ${PYTHON} -c "import junitparser" >/dev/null 2>&1; then
-  echo "Error: Required Python package 'junitparser' not found."
-  exit 1
+    echo "Error: Required Python package 'junitparser' not found."
+    exit 1
 fi
 
 # source helpers to set environment variables and make functions available
@@ -124,7 +125,8 @@ TEST_OUTFILE="${REPORTDIR}/output.log"
 TEST_RETFILE="${REPORTDIR}/output.ret"
 TEST_RUNFILE="${REPORTDIR}/runtest.sh"
 
-get_build_info() {
+get_build_info ()
+{
   ${PYTHON} -c "import nest; print(nest.build_info['$1'])" --quiet
 }
 
@@ -132,29 +134,29 @@ HAVE_MPI="$(get_build_info have_mpi)"
 HAVE_OPENMP="$(get_build_info have_threads)"
 
 if test "${HAVE_MPI}" = "True"; then
-  MPI_LAUNCHER="$(get_build_info mpiexec)"
-  MPI_LAUNCHER_VERSION="$($MPI_LAUNCHER --version | head -n1)"
-  MPI_LAUNCHER_PREFLAGS="$(get_build_info mpiexec_preflags)"
-  # OpenMPI requires --oversubscribe to allow more processes than available cores
-  if [[ "${MPI_LAUNCHER_VERSION}" =~ "(OpenRTE)" ]] || [[ "${MPI_LAUNCHER_VERSION}" =~ "(Open MPI)" ]]; then
-    if [[ ! "$(get_build_info mpiexec_preflags)" =~ "--oversubscribe" ]]; then
-      MPI_LAUNCHER_PREFLAGS="${MPI_LAUNCHER_PREFLAGS} --oversubscribe"
+    MPI_LAUNCHER="$(get_build_info mpiexec)"
+    MPI_LAUNCHER_VERSION="$($MPI_LAUNCHER --version | head -n1)"
+    MPI_LAUNCHER_PREFLAGS="$(get_build_info mpiexec_preflags)"
+    # OpenMPI requires --oversubscribe to allow more processes than available cores
+    if [[ "${MPI_LAUNCHER_VERSION}" =~ "(OpenRTE)" ]] ||  [[ "${MPI_LAUNCHER_VERSION}" =~ "(Open MPI)" ]]; then
+	if [[ ! "$(get_build_info mpiexec_preflags)" =~ "--oversubscribe" ]]; then
+	    MPI_LAUNCHER_PREFLAGS="${MPI_LAUNCHER_PREFLAGS} --oversubscribe"
+	fi
     fi
-  fi
-  MPI_LAUNCHER_NUMPROC_FLAG="$(get_build_info mpiexec_numproc_flag)"
-  MPI_LAUNCHER_CMDLINE="${MPI_LAUNCHER} ${MPI_LAUNCHER_PREFLAGS} ${MPI_LAUNCHER_NUMPROC_FLAG}"
+    MPI_LAUNCHER_NUMPROC_FLAG="$(get_build_info mpiexec_numproc_flag)"
+    MPI_LAUNCHER_CMDLINE="${MPI_LAUNCHER} ${MPI_LAUNCHER_PREFLAGS} ${MPI_LAUNCHER_NUMPROC_FLAG}"
 fi
 
 # Under Mac OS X, suppress crash reporter dialogs. Restore old state at end.
 if test "x${INFO_OS}" = "xDarwin"; then
-  TEST_CRSTATE="$(defaults read com.apple.CrashReporter DialogType)" || true
-  echo "TEST_CRSTATE=$TEST_CRSTATE"
-  defaults write com.apple.CrashReporter DialogType server || echo "WARNING: Could not set CrashReporter DialogType!"
+    TEST_CRSTATE="$( defaults read com.apple.CrashReporter DialogType )" || true
+    echo "TEST_CRSTATE=$TEST_CRSTATE"
+    defaults write com.apple.CrashReporter DialogType server || echo "WARNING: Could not set CrashReporter DialogType!"
 fi
 
-print_paths() {
-  indent="$(printf '%23s')"
-  echo "$1" | sed "s/:/\n$indent/g" | sed '/^\s*$/d'
+print_paths () {
+    indent="`printf '%23s'`"
+    echo "$1" | sed "s/:/\n$indent/g" | sed '/^\s*$/d'
 }
 
 echo "================================================================================"
@@ -166,95 +168,92 @@ echo
 echo "  NEST version ....... $(get_build_info version)"
 echo "  PREFIX ............. $PREFIX"
 if test -n "${MUSIC}"; then
-  MUSIC_VERSION="$("${MUSIC}" --version | head -n1 | cut -d' ' -f2)"
-  echo "  MUSIC executable ... $MUSIC (version $MUSIC_VERSION)"
+    MUSIC_VERSION="$("${MUSIC}" --version | head -n1 | cut -d' ' -f2)"
+    echo "  MUSIC executable ... $MUSIC (version $MUSIC_VERSION)"
 fi
 if test -n "${PYTHON}"; then
-  PYTHON_VERSION="$("${PYTHON}" --version | cut -d' ' -f2)"
-  echo "  Python executable .. $PYTHON (version $PYTHON_VERSION)"
-  echo "  PYTHONPATH ......... $(print_paths ${PYTHONPATH:-})"
-  echo "  Pytest version ..... $PYTEST_VERSION"
-  echo "         timeout ..... $TIME_LIMIT s"
+    PYTHON_VERSION="$("${PYTHON}" --version | cut -d' ' -f2)"
+    echo "  Python executable .. $PYTHON (version $PYTHON_VERSION)"
+    echo "  PYTHONPATH ......... `print_paths ${PYTHONPATH:-}`"
+    echo "  Pytest version ..... $PYTEST_VERSION"
+    echo "         timeout ..... $TIME_LIMIT s"
 fi
 if test "${HAVE_MPI}" = "True"; then
-  echo "  Running MPI tests .. yes"
-  echo "         launcher .... $MPI_LAUNCHER"
-  echo "         version ..... $MPI_LAUNCHER_VERSION"
+    echo "  Running MPI tests .. yes"
+    echo "         launcher .... $MPI_LAUNCHER"
+    echo "         version ..... $MPI_LAUNCHER_VERSION"
 else
-  echo "  Running MPI tests .. no (compiled without MPI support)"
+    echo "  Running MPI tests .. no (compiled without MPI support)"
 fi
 echo "  TEST_BASEDIR ....... $TEST_BASEDIR"
 echo "  REPORTDIR .......... $REPORTDIR"
-echo "  PATH ............... $(print_paths ${PATH})"
+echo "  PATH ............... `print_paths ${PATH}`"
 echo
 echo "================================================================================"
 
 HEADLINE="NEST $(get_build_info version) testsuite log"
-echo >"${TEST_LOGFILE}" "$HEADLINE"
-echo >>"${TEST_LOGFILE}" "$(printf '%0.s=' $(seq 1 ${#HEADLINE}))"
-echo >>"${TEST_LOGFILE}" "Running tests from ${TEST_BASEDIR}"
+echo >  "${TEST_LOGFILE}" "$HEADLINE"
+echo >> "${TEST_LOGFILE}" "$(printf '%0.s=' $(seq 1 ${#HEADLINE}))"
+echo >> "${TEST_LOGFILE}" "Running tests from ${TEST_BASEDIR}"
 
 echo
 echo "Phase 7: Running PyNEST tests"
 echo "-----------------------------"
 
 if test "${PYTHON}"; then
-  PYNEST_TEST_DIR="${TEST_BASEDIR}/pytests"
-  XUNIT_NAME="07_pynesttests"
+    PYNEST_TEST_DIR="${TEST_BASEDIR}/pytests"
+    XUNIT_NAME="07_pynesttests"
 
-  # Run all tests except those in the mpi* and sli2py_mpi subdirectories because they cannot be run concurrently
-  XUNIT_FILE="${REPORTDIR}/${XUNIT_NAME}.xml"
-  env
-  set +e
-  # if test "${HAVE_MPI}" = "True"; then
-  #   ${MPI_LAUNCHER_CMDLINE} 1 ${PYTHON} -m pytest --verbose --timeout $TIME_LIMIT --junit-xml="${XUNIT_FILE}" \
-  #     --ignore="${PYNEST_TEST_DIR}/mpi" --ignore="${PYNEST_TEST_DIR}/sli2py_mpi" "${PYNEST_TEST_DIR}" 2>&1 | tee -a "${TEST_LOGFILE}"
-  # else
-  ${PYTHON} -m pytest --verbose --timeout $TIME_LIMIT --junit-xml="${XUNIT_FILE}" \
-    --ignore="${PYNEST_TEST_DIR}/mpi" --ignore="${PYNEST_TEST_DIR}/sli2py_mpi" "${PYNEST_TEST_DIR}" 2>&1 | tee -a "${TEST_LOGFILE}"
-  # fi
-
-  set -e
-
-  # Run tests in the sli2py_mpi subdirectory. The must be run without loading conftest.py.
-  if test "${HAVE_MPI}" = "True" && test "${HAVE_OPENMP}" = "true"; then
-    XUNIT_FILE="${REPORTDIR}/${XUNIT_NAME}_sli2py_mpi.xml"
+    # Run all tests except those in the mpi* and sli2py_mpi subdirectories because they cannot be run concurrently
+    XUNIT_FILE="${REPORTDIR}/${XUNIT_NAME}.xml"
     env
     set +e
-    "${PYTHON}" -m pytest --noconftest --verbose --timeout $TIME_LIMIT --junit-xml="${XUNIT_FILE}" --numprocesses=1 \
-      "${PYNEST_TEST_DIR}/sli2py_mpi" 2>&1 | tee -a "${TEST_LOGFILE}"
-    set -e
-  fi
-
-  # Run tests in the mpi/* subdirectories, with one subdirectory per number of processes to use
-  if test "${HAVE_MPI}" = "True"; then
-    if test "${MPI_LAUNCHER}"; then
-      # Loop over subdirectories whose names are the number of mpi procs to use
-      for numproc in $(
-        cd ${PYNEST_TEST_DIR}/mpi/
-        ls -d */ | tr -d '/'
-      ); do
-        XUNIT_FILE="${REPORTDIR}/${XUNIT_NAME}_mpi_${numproc}.xml"
-        PYTEST_ARGS="--verbose --timeout $TIME_LIMIT --junit-xml=${XUNIT_FILE} ${PYNEST_TEST_DIR}/mpi/${numproc}"
-
-        set +e
-        # Some doubling up of code here because trying to add the -m 'not requires...' to PYTEST_ARGS
-        # loses the essential quotes.
-        if "${DO_TESTS_SKIP_TEST_REQUIRING_MANY_CORES:-false}"; then
-          echo "Running ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS} -m 'not requires_many_cores'"
-          ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS} -m 'not requires_many_cores' 2>&1 | tee -a "${TEST_LOGFILE}"
-        else
-          echo "Running ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS}"
-          ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS} 2>&1 | tee -a "${TEST_LOGFILE}"
-        fi
-        set -e
-      done
+    if test "${HAVE_MPI}" = "True"; then
+	${MPI_LAUNCHER_CMDLINE} 1 ${PYTHON} -m pytest --verbose --timeout $TIME_LIMIT --junit-xml="${XUNIT_FILE}" \
+				--ignore="${PYNEST_TEST_DIR}/mpi" --ignore="${PYNEST_TEST_DIR}/sli2py_mpi" "${PYNEST_TEST_DIR}" 2>&1 | tee -a "${TEST_LOGFILE}"
+    else
+	${PYTHON} -m pytest --verbose --timeout $TIME_LIMIT --junit-xml="${XUNIT_FILE}" \
+				--ignore="${PYNEST_TEST_DIR}/mpi" --ignore="${PYNEST_TEST_DIR}/sli2py_mpi" "${PYNEST_TEST_DIR}" 2>&1 | tee -a "${TEST_LOGFILE}"
     fi
-  fi
+
+    set -e
+
+    # Run tests in the sli2py_mpi subdirectory. The must be run without loading conftest.py.
+    if test "${HAVE_MPI}" = "True" && test "${HAVE_OPENMP}" = "true" ; then
+	XUNIT_FILE="${REPORTDIR}/${XUNIT_NAME}_sli2py_mpi.xml"
+	env
+	set +e
+	"${PYTHON}" -m pytest --noconftest --verbose --timeout $TIME_LIMIT --junit-xml="${XUNIT_FILE}" --numprocesses=1 \
+		    "${PYNEST_TEST_DIR}/sli2py_mpi" 2>&1 | tee -a "${TEST_LOGFILE}"
+	set -e
+    fi
+
+    # Run tests in the mpi/* subdirectories, with one subdirectory per number of processes to use
+    if test "${HAVE_MPI}" = "True"; then
+        if test "${MPI_LAUNCHER}"; then
+	    # Loop over subdirectories whose names are the number of mpi procs to use
+            for numproc in $(cd ${PYNEST_TEST_DIR}/mpi/; ls -d */ | tr -d '/'); do
+                XUNIT_FILE="${REPORTDIR}/${XUNIT_NAME}_mpi_${numproc}.xml"
+                PYTEST_ARGS="--verbose --timeout $TIME_LIMIT --junit-xml=${XUNIT_FILE} ${PYNEST_TEST_DIR}/mpi/${numproc}"
+
+		set +e
+		# Some doubling up of code here because trying to add the -m 'not requires...' to PYTEST_ARGS
+		# loses the essential quotes.
+		if "${DO_TESTS_SKIP_TEST_REQUIRING_MANY_CORES:-false}"; then
+		    echo "Running ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS} -m 'not requires_many_cores'"
+                    ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS} -m 'not requires_many_cores' 2>&1 | tee -a "${TEST_LOGFILE}"
+		else
+		    echo "Running ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS}"
+                    ${MPI_LAUNCHER_CMDLINE} ${numproc} ${PYTHON} -m pytest ${PYTEST_ARGS} 2>&1 | tee -a "${TEST_LOGFILE}"
+		fi
+		set -e
+            done
+        fi
+    fi
 else
-  echo
-  echo "  Not running PyNEST tests because NEST was compiled without Python support."
-  echo
+    echo
+    echo "  Not running PyNEST tests because NEST was compiled without Python support."
+    echo
 fi
 
 echo
@@ -263,7 +262,7 @@ echo "-----------------------------------------"
 
 if command -v run_all_cpptests >/dev/null 2>&1; then
   set +e
-  CPP_TEST_OUTPUT=$(run_all_cpptests --logger=JUNIT,error,"${REPORTDIR}/08_cpptests.xml":HRF,error,stdout 2>&1)
+  CPP_TEST_OUTPUT=$( run_all_cpptests --logger=JUNIT,error,"${REPORTDIR}/08_cpptests.xml":HRF,error,stdout 2>&1 )
   set -e
   echo "${CPP_TEST_OUTPUT}" | tail -2
 else
@@ -279,8 +278,8 @@ python3 "$(dirname $0)/summarize_tests.py" "${REPORTDIR}"
 TESTSUITE_RESULT=$?
 
 # Mac OS X: Restore old crash reporter state
-if test "x${INFO_OS}" = xDarwin; then
-  defaults write com.apple.CrashReporter DialogType "${TEST_CRSTATE}" || echo "WARNING: Could not reset CrashReporter DialogType to '${TEST_CRSTATE}'!"
+if test "x${INFO_OS}" = xDarwin ; then
+    defaults write com.apple.CrashReporter DialogType "${TEST_CRSTATE}" || echo "WARNING: Could not reset CrashReporter DialogType to '${TEST_CRSTATE}'!"
 fi
 
 exit $TESTSUITE_RESULT

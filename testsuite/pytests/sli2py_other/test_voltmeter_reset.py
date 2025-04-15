00712080db00b6722cdf549aaf57f0417667ff5f
jessica-mitchell <mitchell20j@gmail.com>
2025-04-15 09:16:34 +0200

    Apply suggestions from code review
    Co-authored-by: Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>

--- testsuite/pytests/sli2py_other/test_voltmeter_reset.py
+++ testsuite/pytests/sli2py_other/test_voltmeter_reset.py
@@ -44,17 +44,20 @@ def test_voltmeter_reset():
     # Simulate and check initial recording
     nest.Simulate(10.5)
     events = vm.events
+    assert vm.n_events == 10
     assert len(events["times"]) == 10
     assert len(events["V_m"]) == 10
 
     # Reset voltmeter and check
     vm.set({"n_events": 0})
     events = vm.events
+    assert vm.n_events == 0
     assert len(events["times"]) == 0
     assert len(events["V_m"]) == 0
 
     # Simulate more and check recording
     nest.Simulate(5.0)
     events = vm.events
+    assert vm.n_events == 5
     assert len(events["times"]) == 5
     assert len(events["V_m"]) == 5

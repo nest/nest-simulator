00e6947a0627b35d41170111d60907e4299bf298
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2025-06-24 20:38:26 +0200

    Fix typo.

--- testsuite/pytests/sli2py_regressions/test_ticket_310.py
+++ testsuite/pytests/sli2py_regressions/test_ticket_310.py
@@ -13,7 +13,7 @@
 #
 # NEST is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-0  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License

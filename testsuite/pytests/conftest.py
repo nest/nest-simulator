f72cc2717ba8b79e211132294cd1bb1610aa77c2
Guido Trensch <19706694+gtrensch@users.noreply.github.com>
2025-07-27 21:33:21 +0200

    remove trailing spaces

--- testsuite/pytests/conftest.py
+++ testsuite/pytests/conftest.py
@@ -160,14 +160,14 @@ def have_plotting():
 @pytest.fixture(scope="session")
 def subprocess_compatible_mpi():
     """
-    Until at least OpenMPI 4.1.6, the following fails due to a bug in OpenMPI, 
+    Until at least OpenMPI 4.1.6, the following fails due to a bug in OpenMPI,
     from 5.0.7 is definitely safe.
     """
-    try: 
+    try:
         res = subprocess.run(["mpirun", "-np", "1", "echo"])
         return 0 == res.returncode
     except FileNotFoundError:
-        return False    
+        return False
 
 
 @pytest.fixture(autouse=True)

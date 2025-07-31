d58385288ae0c3d4f580da5a30fc4f0383a715c7
Finn Burkhardt <finn.burkhardt@web.de>
2025-07-31 17:45:57 +0200

    cleanup tests

--- testsuite/pytests/sli2py_stimulating/test_ou_noise_generator.py
+++ testsuite/pytests/sli2py_stimulating/test_ou_noise_generator.py
@@ -34,17 +34,9 @@ def prepare_kernel():
     nest.ResetKernel()
     nest.resolution = 0.1
 
-def burn_in_start(dt, tau, k=10.0):
-    """
-    Compute the array index corresponding to k*tau of warm-up.
-    dt   : simulation time step (ms)
-    tau  : OU time constant (ms)
-    k    : how many taus to discard (default 10)
-    """
+def burn_in_start(dt, tau, k=10):
+    """Return number of steps to discard for a k*tau burn-in."""
     return int(round((k * tau) / dt))
-
-
-
 
 def test_ou_noise_generator_set_parameters(prepare_kernel):
     params = {"mean": 210.0, "std": 60.0, "dt": 0.1}
@@ -62,19 +54,16 @@ def test_ou_noise_generator_incorrect_noise_dt(prepare_kernel):
     with pytest.raises(nest.kernel.NESTError, match="StepMultipleRequired"):
         nest.Create("ou_noise_generator", {"dt": 0.25})
 
-def test_ou_noise_generator_statistics(prepare_kernel):
+def test_ou_noise_mean_and_variance(prepare_kernel):
     # run for resolution dt=0.1 project to iaf_psc_alpha.
     # create 100 repetitions of 1000ms simulations
-    # collect membrane potential at end
-    #nest.rng_seed = 202
 
-    #oung = nest.Create("ou_noise_generator")
-    oung = nest.Create('ou_noise_generator', 1, {'mean':200.0, 'std': 60.0, 'tau':10, 'dt':0.1})
+    oung = nest.Create('ou_noise_generator', {'mean':0.0, 'std': 60.0, 'tau':1., 'dt':0.1})
     neuron = nest.Create("iaf_psc_alpha")
+    
     # we need to connect to a neuron otherwise the generator does not generate
     nest.Connect(oung, neuron)
-
-    mm = nest.Create('multimeter', 1, {'record_from':['I']})
+    mm = nest.Create('multimeter', 1, {'record_from':['I'], "interval": 0.1})
     nest.Connect(mm, oung, syn_spec={'weight': 1})
 
     # Simulate for 100 times
@@ -82,9 +71,7 @@ def test_ou_noise_generator_statistics(prepare_kernel):
     ou_current = np.empty(n_sims)
     for i in range(n_sims):
         nest.Simulate(1000.0)
-
         ou_current[i] = mm.get('events')['I'][-1]
-
 
     curr_mean = np.mean(ou_current)
     curr_var = np.var(ou_current)
@@ -96,67 +83,74 @@ def test_ou_noise_generator_statistics(prepare_kernel):
     assert np.abs(curr_var - expected_curr_var) < 0.2 * curr_var
 
 
-def test_ou_noise_generator_autocorrelation():
-    nest.rng_seed = 6789
-
-    dt = 1.0
+def test_ou_noise_generator_autocorrelation(prepare_kernel):
+    # verify lag-1 autocorr = exp(-dt/tau)
+    dt = 0.1
     tau = 1.0
 
-    oung = nest.Create('ou_noise_generator', 1, {'mean':0.0, 'std': 60.0, 'tau':tau,'dt': nest.resolution})
+    oung = nest.Create('ou_noise_generator', {'mean':0.0, 'std': 60.0, 'tau':tau,'dt': nest.resolution})
     neuron = nest.Create("iaf_psc_alpha")
+    
     # we need to connect to a neuron otherwise the generator does not generate
     nest.Connect(oung, neuron)
-    mm = nest.Create('multimeter', 1, {'record_from':['I']})
-    nest.Connect(mm, oung, syn_spec={'weight': 1})
+    mm = nest.Create('multimeter', 1, {'record_from':['I'], "interval": dt})
+    nest.Connect(mm, oung, syn_spec={'weight': 1 })
     nest.Simulate(10000.0)
-
-    
     ou_current = mm.get('events')['I']
 
-    ## drop first k*tau of data
-    cutoff = burn_in_start(dt=dt, tau=tau, k=10.0)
+    ## drop first k*tau
+    cutoff = burn_in_start(dt=dt, tau=tau)
     x = ou_current[cutoff:]
     x -= x.mean()
-
+    
     # empirical lag-1 autocorrelation
     emp_ac1 = np.dot(x[:-1], x[1:]) / ((len(x)-1)*x.var())
     # theoretical lag-1: exp(-dt/tau)
     theor_ac1 = np.exp(-dt / tau)
 
-
     assert abs(emp_ac1 - theor_ac1) < 0.05, \
         f"autocorr {emp_ac1:.3f} vs theoretical {theor_ac1:.3f}"
     
-def _crosscorr_between_two_neurons(V_mean, V_std, dt, tau_m, t_max):
-    nest.ResetKernel(); nest.resolution = dt
-    ou = nest.Create("ou_noise_generator", 1,
-                     {"mean": V_mean, "std": V_std, "tau": tau_m, "dt": dt})
+
+def test_cross_correlation(prepare_kernel):
+    # two neurons driven by the same OU noise should remain uncorrelated
+    dt, tau = 0.1, 1.0
+
+    ou = nest.Create("ou_noise_generator", 
+                     {"mean": 0.0, "std": 50.0, "tau": tau, "dt": dt})
     neurons = nest.Create("iaf_psc_alpha", 2,
-                          {"E_L": 0.0, "V_th": 1e9, "C_m": 1.0, "tau_m": tau_m})
+                          {"E_L": 0.0, "V_th": 1e9, "C_m": 1.0, "tau_m": tau})
     nest.Connect(ou, neurons)
-    mm = nest.Create("multimeter", 1, {"record_from": ["V_m"], "interval": dt})
+
+    mm = nest.Create("multimeter",
+                     params={"record_from": ["V_m"], "interval": dt})
     nest.Connect(mm, neurons)
-    nest.Simulate(t_max)
+
+    simtime = 50000.0
+    nest.Simulate(simtime)
+
     ev = mm.get("events")
     senders = np.asarray(ev["senders"], int)
-    times = np.asarray(ev["times"])
-    vms = np.asarray(ev["V_m"])
-    t_steps = int(round(t_max / dt)) + 1
-    V = np.zeros((t_steps, 2))
+    times   = np.asarray(ev["times"])
+    vms     = np.asarray(ev["V_m"])
+
+    # build time array
+    n_steps = int(round(simtime / dt)) + 1
+    V = np.zeros((n_steps, 2))
     for t, s, v in zip(times, senders, vms):
-        V[int(round(t / dt)), neurons.index(s)] = v
-    start = burn_in_start(dt, tau_m)
+        idx = int(round(t / dt))
+        col = neurons.index(s)
+        V[idx, col] = v
+
+    # discard burn-in
+    start = burn_in_start(dt, tau)
     X1 = V[start:, 0] - V[start:, 0].mean()
     X2 = V[start:, 1] - V[start:, 1].mean()
+
+    # cross-corr via FFT
     corr = fftconvolve(X1, X2[::-1], mode="full")
     corr /= np.sqrt(np.dot(X1, X1) * np.dot(X2, X2))
-    mid = corr.size // 2
-    return corr[mid:]
+    mid  = corr.size // 2
 
-
-def test_ou_noise_generator_cross_correlation(prepare_kernel):
-    nest.rng_seed = 2321
-    crosscorr = _crosscorr_between_two_neurons(
-        V_mean=0.0, V_std=60.0, dt=nest.resolution, tau_m=1.0, t_max=30_000.0
-    )
-    assert float(np.max(np.abs(crosscorr))) < 0.05
+    # maximum absolute cross-corr should be near zero
+    assert np.max(np.abs(corr[mid:])) < 0.05

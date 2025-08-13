bf810282836a792d09bc70fb44c1c78cc75f806a
janskaar <jewskaar@gmail.com>
2025-08-13 18:28:12 +0200

    flake8

--- testsuite/pytests/sli2py_neurons/test_iaf_psc_exp_ps_lossless.py
+++ testsuite/pytests/sli2py_neurons/test_iaf_psc_exp_ps_lossless.py
@@ -20,7 +20,8 @@
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Synopsis: (test_iaf_psc_exp_ps_lossless) run -> compares response to current step with analytical solution and tests lossless spike detection
+Synopsis: (test_iaf_psc_exp_ps_lossless) run -> compares response to current step with analytical 
+solution and tests lossless spike detection
 
 Description:
 test_iaf_psc_exp_ps_lossless.py is an overall test of the iaf_psc_exp_ps_lossless model connected

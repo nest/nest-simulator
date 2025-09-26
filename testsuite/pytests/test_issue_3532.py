0b521af64d8971350ae34fbed5db15c0d9457dde
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2025-09-26 23:06:26 +0200

    Add comments and remove unused code

--- testsuite/pytests/test_issue_3532.py
+++ testsuite/pytests/test_issue_3532.py
@@ -35,6 +35,8 @@ cases = [(compressed_spikes, n_nodes) for compressed_spikes in [False, True] for
 @pytest.fixture(params=cases, ids=[f"{compr}-{nn}" for compr, nn in cases])
 def network(request):
     compressed_spikes, n_nodes = request.param
+
+    random.seed(12345)
     nest.ResetKernel()
     nest.use_compressed_spikes = compressed_spikes
     nrns = nest.Create("parrot_neuron", n_nodes)
@@ -73,3 +75,42 @@ def test_disconnect_by_edge(network):
         c[i].disconnect()
 
     assert len(nest.GetConnections()) == 0
+
+
+@pytest.mark.parametrize("use_compressed", [False, True])
+@pytest.mark.parametrize("p_disconnect", [0.3, 0.9])
+def test_disconnect_random_connections(use_compressed, p_disconnect):
+    """
+    Test if disconnection works when connections have been created in random order.
+    """
+
+    num_neurons = 17
+    conn_reps = 3
+
+    random.seed(12345)
+    nest.ResetKernel()
+    nest.use_compressed_spikes = use_compressed
+
+    nrns = nest.Create("parrot_neuron", num_neurons)
+
+    # Create all-to-all connectivity, where each s-t pair is connected conn_reps times
+    # Ensure that connections are created in random order
+    gids = nrns.global_id
+    st_pairs = [(s, t) for s in gids for t in gids for _ in range(conn_reps)]
+    random.shuffle(st_pairs)
+    nest.Connect(*zip(*st_pairs), "one_to_one")
+
+    # Delete random sample of connections. As SynapseCollections do not allow slicing etc,
+    # we need to do this connection by connection. We also collect those that are not to
+    # be disconnected: these form the expectation for the test.
+    all_conns = nest.GetConnections()
+
+    not_disconn = []
+    for conn in all_conns:
+        if random.random() < p_disconnect:
+            nest.Disconnect(conn)
+        else:
+            not_disconn.append(tuple(conn.get(["source", "target"]).values()))
+
+    post_conns = nest.GetConnections()
+    assert sorted(not_disconn) == sorted(list(zip(*post_conns.get(["source", "target"]).values())))

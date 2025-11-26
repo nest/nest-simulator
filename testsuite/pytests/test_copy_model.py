33bbd3223285d7c9606de8e8d088a2388808b0ac
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2025-11-26 10:11:15 +0100

    Improve test

--- testsuite/pytests/test_copy_model.py
+++ testsuite/pytests/test_copy_model.py
@@ -106,8 +106,7 @@ class TestCopyModel:
         except KeyError:
             org_name = nest.GetDefaults(org_model)["synapse_model"]
 
-        # TODO-PYNEST-NG
-        with pytest.raises(nest.NESTError, match="is the name of an existing model and cannot be"):
+        with pytest.raises(nest.NESTErrors.NewModelNameExists):
             nest.CopyModel(org_model, org_model)
 
     @pytest.mark.parametrize("org_model", [nest.node_models[0], nest.synapse_models[0]])
@@ -118,6 +117,6 @@ class TestCopyModel:
 
         new_model = f"{org_model}_copy"
         nest.CopyModel(org_model, new_model)
-        # TODO-PYNEST-NG
-        with pytest.raises(nest.NESTError, match="is the name of an existing model and cannot be"):
+
+        with pytest.raises(nest.NESTErrors.NewModelNameExists):
             nest.CopyModel(org_model, new_model)

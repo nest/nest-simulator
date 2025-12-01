f2a9c32dbde6be3517933adffb55bdccb87fe1d9
Jessica Mitchell <j.mitchell@fz-juelich.de>
2025-12-01 10:18:44 +0100

    Port regression test 386 to Pytests

---
+++ testsuite/pytests/sli2py_regressions/test_ticket_386.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+# test_ticket_386.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+import nest
+import pytest
+
+"""
+Regression test for Ticket #386.
+
+Test ported from SLI regression test.
+Ensure that all models exposing recordables can be sampled by a multimeter at kernel resolution.
+
+Author: Hans Ekkehard Plesser, 2010-10-03
+"""
+
+
+RESOLUTION = 0.1
+
+
+def _models_supporting_recordables():
+    models = []
+    for model in nest.node_models:
+        defaults = nest.GetDefaults(model)
+        recordables = defaults.get("recordables")
+        if recordables:
+            models.append((model, tuple(recordables), "compartments" in defaults))
+    return models
+
+
+@pytest.mark.parametrize("model,recordables,has_compartments", _models_supporting_recordables())
+def test_ticket_386_multimeter_at_resolution(model, recordables, has_compartments):
+    """
+    Ensure multimeter recording at the kernel resolution works for models with recordables.
+    """
+
+    nest.ResetKernel()
+    nest.SetKernelStatus({"resolution": RESOLUTION})
+
+    neuron = nest.Create(model)
+
+    if has_compartments:
+        nest.SetStatus(neuron, {"compartments": {"parent_idx": -1}})
+
+    multimeter = nest.Create(
+        "multimeter",
+        params={
+            "record_from": list(recordables),
+            "interval": RESOLUTION,
+        },
+    )
+
+    nest.Connect(multimeter, neuron)
+
+    nest.Simulate(10.0)

bbd035b6f96b562c1dff8359ccaa7461b2d015af
Jessica Mitchell <j.mitchell@fz-juelich.de>
2025-12-02 12:29:47 +0100

    Convert unittest test_ppd_sup_generator to Pytest

---
+++ testsuite/pytests/sli2py_stimulating/test_ppd_sup_generator.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+#
+# test_ppd_sup_generator.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Name: testsuite::test_ppd_sup_generator - sli script for test of ppd_sup_generator output
+
+Synopsis: (test_ppd_sup_generator) run -> compare spike train statistics with expectations
+
+Description:
+
+ test_ppd_sup_generator is a collection of tests which require basic
+ functionality of the generator. It tests
+ 1) if the firing rate of a superposition is close to the preset one.
+ 2) if the coefficient of variation of a superposition agrees with theory
+ 3) if a fixed dead-time is respected absolutely in case of a single generated
+    process.
+ 4) if the coefficient of variation of a single process agrees with theory
+ 5) if the spike trains generated for two different targets differ
+
+ All of these tests are based on random number realizations, which is
+ necessarily so  since the model is stochastic. There is thus a finite
+ probability of test failure, even if everything is fine. The choice of the
+ variable err, which is the allowed relative deviation from the reference value,
+ can be used to make the test more or less strict. Increasing T inside the test
+ functions can also help to get more reliable statistics and a reduced
+ probability of false alarms.
+
+ The values are chosen to have a reasonable execution time. False alarm were
+ never observed yet. Since random numbers are preserved through repetitions of
+ the simulations, the test should work for sure as long as the random number
+ generation procedure of nest is not changed. If it is changed, failure of the
+ test is still very unlikely.
+
+ The intention of this script is to make sure that there are no gross errors in
+ the main functions of the ppd_sup_generator. It does not test the sinusoidal
+ hazard feature yet.
+
+Remarks:
+  This test script was adapted from test_pp_psc_delta.sli
+
+Test ported from SLI unittest.
+
+Author:  June 2011, Moritz Deger
+SeeAlso: ppd_sup_generator, testsuite::test_pp_psc_delta, testsuite::test_poisson_generator_ps
+"""
+
+import nest
+import numpy as np
+import pytest
+
+
+def test_ppd_sup_generator_sup_rate_and_cv():
+    """
+    Test superposition rate and coefficient of variation.
+    Tests:
+    1) if the firing rate of a superposition is close to the preset one.
+    2) if the coefficient of variation of a superposition agrees with theory
+    """
+    err = 0.2  # allowed relative deviation
+    dead_time = 25.0
+    rate = 10.0
+    T = 10000.0
+    n_proc = 5
+    h = 1.0
+
+    nest.ResetKernel()
+    nest.set(resolution=h)
+
+    psg = nest.Create(
+        "ppd_sup_generator",
+        params={"rate": rate, "dead_time": dead_time, "n_proc": n_proc, "frequency": 0.0, "relative_amplitude": 0.0},
+    )
+
+    sr = nest.Create("spike_recorder")
+
+    nest.Connect(psg, sr)
+    nest.Simulate(T)
+
+    spikes = sr.get("events", "times")
+
+    # rate_sim = size(spikes) / (T*1e-3)
+    rate_sim = len(spikes) / (T * 1e-3)
+
+    # rate_ana = rate * n_proc
+    rate_ana = rate * n_proc
+
+    # ratio = rate_sim / rate_ana
+    ratio = rate_sim / rate_ana
+
+    # Check that ratio is within error bounds
+    assert (1.0 - err) < ratio < (1.0 + err), f"Rate ratio {ratio} not within bounds [{1.0 - err}, {1.0 + err}]"
+
+    # Compute ISI (inter-spike intervals)
+    isi = np.diff(spikes)
+
+    # Compute moments of ISI to get mean and variance
+    isi_m1 = np.sum(isi)
+    isi_m2 = np.sum(isi**2)
+
+    # isi_mean = isi_m1 / len(isi)
+    # isi_var = isi_m2 / len(isi) - isi_mean**2
+    # cvsq = isi_var / isi_mean**2
+    isi_mean = isi_m1 / len(isi)
+    isi_var = isi_m2 / len(isi) - isi_mean**2
+    cvsq_sim = isi_var / (isi_mean**2)
+
+    # Theoretical CV**2, see Deger et al 2011, JCNS
+    mu = (1.0 / rate) * 1e3
+    cvfact1 = 1.0 / (1.0 + n_proc)
+    cvfact2 = n_proc - 1.0 + 2.0 * ((1.0 - dead_time / mu) ** (n_proc + 1))
+    cvsq_theo = cvfact1 * cvfact2
+
+    ratio_cvsq = cvsq_sim / cvsq_theo
+    assert (
+        (1.0 - err) <= ratio_cvsq <= (1.0 + err)
+    ), f"CV² ratio {ratio_cvsq} not within bounds [{1.0 - err}, {1.0 + err}]"
+
+
+def test_ppd_sup_generator_single_rate_and_dead_time():
+    """
+    Test single process rate and dead-time.
+    Tests:
+    3) if a fixed dead-time is respected absolutely in case of a single generated process.
+    4) if the coefficient of variation of a single process agrees with theory
+    """
+    err = 0.2  # allowed relative deviation
+    dead_time = 45.0
+    rate = 15.0
+    T = 100000.0
+    n_proc = 1
+    h = 1.0
+
+    nest.ResetKernel()
+    nest.set(resolution=h)
+
+    psg = nest.Create(
+        "ppd_sup_generator",
+        params={"rate": rate, "dead_time": dead_time, "n_proc": n_proc, "frequency": 0.0, "relative_amplitude": 0.0},
+    )
+
+    sr = nest.Create("spike_recorder")
+
+    nest.Connect(psg, sr)
+    nest.Simulate(T)
+
+    spikes = sr.get("events", "times")
+
+    # rate_sim = size(spikes) / (T*1e-3)
+    rate_sim = len(spikes) / (T * 1e-3)
+
+    # rate_ana = rate * n_proc
+    rate_ana = rate * n_proc
+
+    # ratio = rate_sim / rate_ana
+    ratio = rate_sim / rate_ana
+
+    # Check that ratio is within error bounds
+    assert (1.0 - err) < ratio < (1.0 + err), f"Rate ratio {ratio} not within bounds [{1.0 - err}, {1.0 + err}]"
+
+    # Compute ISI (inter-spike intervals)
+    isi = np.diff(spikes)
+
+    # Assert that min(isi) >= dead_time
+    assert np.min(isi) >= dead_time, f"Minimum ISI {np.min(isi)} is less than dead_time {dead_time}"
+
+    # Compute moments of ISI to get mean and variance
+    isi_m1 = np.sum(isi)
+    isi_m2 = np.sum(isi**2)
+
+    # isi_mean = isi_m1 / len(isi)
+    # isi_var = isi_m2 / len(isi) - isi_mean**2
+    # cvsq = isi_var / isi_mean**2
+    isi_mean = isi_m1 / len(isi)
+    isi_var = isi_m2 / len(isi) - isi_mean**2
+    cvsq_sim = isi_var / (isi_mean**2)
+
+    # Theoretical CV**2, see Deger et al 2011, JCNS
+    # SLI code: 1.0 rate div /mu Set -> mu = 1.0 / rate (in seconds)
+    #           mu dead_time sub /lam Set -> lam = mu - dead_time
+    #           1.0 lam div mu div 2 pow -> cvsq_theo
+    # Note: The SLI code has a unit mismatch (mu in seconds, dead_time in ms).
+    #       Converting dead_time from ms to seconds to match mu units.
+    #       The SLI expression "1.0 lam div mu div 2 pow" could be interpreted as:
+    #       - ((1.0 / lam) / mu) ** 2 = (1.0 / (lam * mu)) ** 2, OR
+    #       - (lam / mu) ** 2 (if parsed as lam div, then mu div, then 2 pow on the ratio)
+    #       Based on typical CV² values (~0.1-1.0), trying (lam/mu)**2:
+    mu = 1.0 / rate  # mean ISI in seconds (rate is in Hz)
+    dead_time_s = dead_time / 1000.0  # convert dead_time from ms to seconds
+    lam = mu - dead_time_s  # both in seconds
+    cvsq_theo = (lam / mu) ** 2
+
+    ratio_cvsq = cvsq_sim / cvsq_theo
+    assert (
+        (1.0 - err) <= ratio_cvsq <= (1.0 + err)
+    ), f"CV² ratio {ratio_cvsq} not within bounds [{1.0 - err}, {1.0 + err}]"
+
+
+def test_ppd_sup_generator_different_outputs():
+    """
+    Test that spike trains generated for two different targets differ.
+    Also checks that rates are correct.
+    """
+    err = 0.2  # allowed relative deviation
+    dead_time = 5.0
+    rate = 25.0
+    T = 10.0
+    n_proc = 1000
+    h = 0.01
+
+    nest.ResetKernel()
+    nest.set(resolution=h)
+
+    psg = nest.Create(
+        "ppd_sup_generator",
+        params={"rate": rate, "dead_time": dead_time, "n_proc": n_proc, "frequency": 0.0, "relative_amplitude": 0.0},
+    )
+
+    sr1 = nest.Create("spike_recorder")
+    sr2 = nest.Create("spike_recorder")
+
+    nest.Connect(psg, sr1)
+    nest.Connect(psg, sr2)
+    nest.Simulate(T)
+
+    # First check if the spike trains are different
+    spikes1 = sr1.get("events", "times")
+    spikes2 = sr2.get("events", "times")
+
+    assert not np.array_equal(spikes1, spikes2), "Spike trains should differ for different targets"
+
+    # Check rates
+    rate_sim1 = len(spikes1) / (T * 1e-3)
+    rate_sim2 = len(spikes2) / (T * 1e-3)
+
+    rate_ana = rate * n_proc
+
+    ratio1 = rate_sim1 / rate_ana
+    ratio2 = rate_sim2 / rate_ana
+
+    assert (1.0 - err) < ratio1 < (1.0 + err), f"Rate1 ratio {ratio1} not within bounds [{1.0 - err}, {1.0 + err}]"
+    assert (1.0 - err) < ratio2 < (1.0 + err), f"Rate2 ratio {ratio2} not within bounds [{1.0 - err}, {1.0 + err}]"

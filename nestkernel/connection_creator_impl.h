2c24500a6c9d6ae7d26a1842b150196bdf0474c4
Agnes Korcsak-Gorzo <40828647+akorgor@users.noreply.github.com>
2025-12-17 10:06:26 +0100

    Fix destructor warning by updating PoolWrapper destructor signature

--- nestkernel/connection_creator_impl.h
+++ nestkernel/connection_creator_impl.h
@@ -167,7 +167,7 @@ ConnectionCreator::PoolWrapper_< D >::PoolWrapper_()
 }
 
 template < int D >
-ConnectionCreator::PoolWrapper_< D >::~PoolWrapper_< D >()
+ConnectionCreator::PoolWrapper_< D >::~PoolWrapper_()
 {
   if ( masked_layer_ )
   {

2a1beb55752c1f448f708e5379921d9249584df9
Agnes Korcsak-Gorzo <40828647+akorgor@users.noreply.github.com>
2026-01-06 16:32:28 +0100

    Implement activation events

--- models/parrot_neuron.cpp
+++ models/parrot_neuron.cpp
@@ -61,6 +61,7 @@ parrot_neuron::update( Time const& origin, const long from, const long to )
 {
   for ( long lag = from; lag < to; ++lag )
   {
+    const long t = origin.get_steps() + lag;
     const unsigned long current_spikes_n = static_cast< unsigned long >( B_.n_spikes_.get_value( lag ) );
     if ( current_spikes_n > 0 )
     {
@@ -72,8 +73,16 @@ parrot_neuron::update( Time const& origin, const long from, const long to )
       // set the spike times, respecting the multiplicity
       for ( unsigned long i = 0; i < current_spikes_n; i++ )
       {
-        set_spiketime( Time::step( origin.get_steps() + lag + 1 ) );
+        set_spiketime( Time::step( t + 1 ) );
       }
+      last_event_time_ = t;
+    }
+    else if ( t - last_event_time_ >= activation_interval_ )
+    {
+      SpikeEvent se;
+      se.set_activation();
+      kernel().event_delivery_manager.send( *this, se, lag );
+      last_event_time_ = t;
     }
   }
 }

779795b0cbd0d8fb4d352a26d378ff5fb72c8a67
Agnes Korcsak-Gorzo <40828647+akorgor@users.noreply.github.com>
2026-02-03 10:35:33 +0100

    Add test to verify activation events are not recorded by spike recorder
    Co-authored-by: JesusEV <43375826+JesusEV@users.noreply.github.com>

---
+++ testsuite/pytests/test_activation_event_filtering.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# test_activation_event_filtering.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Test that connection types that do not support activation events filter them out.
+
+Connection types that do not support activation events (e.g., static_synapse) do not
+transmit activation events, preventing them from reaching recorders and corrupting
+spike train analyses.
+"""
+
+import nest
+import pytest
+
+
+def test_activation_events_not_transmitted():
+    """
+    Verify that static_synapse does not transmit activation events to spike recorder.
+    """
+    nest.ResetKernel()
+
+    spike_time = 10.0
+    sg = nest.Create("spike_generator", params=dict(spike_times=[spike_time]))
+    pt = nest.Create("parrot_neuron", params=dict(activation_interval=5.0))
+    sr = nest.Create("spike_recorder")
+
+    nest.Connect(sg, pt)
+    nest.Connect(pt, sr)
+
+    nest.Simulate(30.0)
+
+    spike_times = sr.get("events")["times"]
+
+    assert (
+        len(spike_times) == 1
+    ), f"Expected 1 spike, got {len(spike_times)}. Activation events may have been transmitted incorrectly."
+
+    expected_time = spike_time + nest.GetDefaults("static_synapse")["delay"]
+    assert spike_times[0] == pytest.approx(expected_time, abs=0.01)

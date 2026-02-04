e2482449c78a9c658c8f34bf100e475ebc4a7de1
Agnes Korcsak-Gorzo <40828647+akorgor@users.noreply.github.com>
2026-02-04 08:59:27 +0100

    Replace `insert` with `emplace_back` for efficiency

--- nestkernel/eprop_archiving_node_impl.h
+++ nestkernel/eprop_archiving_node_impl.h
@@ -66,7 +66,7 @@ EpropArchivingNode< HistEntryT >::initialize_update_history()
 
   if ( it_hist == update_history_.end() or it_hist->t_ != t_first_entry )
   {
-    update_history_.insert( it_hist, HistEntryEpropUpdate( t_first_entry, 1 ) );
+    update_history_.emplace_back( t_first_entry, 1 );
   }
   else
   {
@@ -100,7 +100,7 @@ EpropArchivingNode< HistEntryT >::write_update_to_history( const long t_previous
     }
     else
     {
-      update_history_.insert( it_hist_curr, HistEntryEpropUpdate( t_curr_update_shifted, 1 ) );
+      update_history_.emplace_back( t_curr_update_shifted, 1 );
     }
   }
 

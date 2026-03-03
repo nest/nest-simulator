65462407a580c884fa04c7764fa6ba0eed29d961
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2026-03-03 19:09:14 +0100

    Fix formatting.
    Co-authored-by: Dennis Terhorst <terhorstd@users.noreply.github.com>

--- pynest/nest/voltage_trace.py
+++ pynest/nest/voltage_trace.py
@@ -64,8 +64,7 @@ def from_file(fname, title=None, grayscale=False):
         line_style = ""
 
     if len(data.shape) == 1:
-        print("INFO: only found 1 column in the file. \
-            Assuming that only one neuron was recorded.")
+        print("INFO: only found 1 column in the file. Assuming that only one neuron was recorded.")
         plotid = plt.plot(data, line_style)
         plt.xlabel("Time (steps of length interval)")
 

172e82827b909c2045634c79d841e8e342e0566f
Jessica Mitchell <j.mitchell@fz-juelich.de>
2026-03-17 09:40:17 +0100

    update begindocumentation code

--- nestkernel/kernel_manager.h
+++ nestkernel/kernel_manager.h
@@ -45,135 +45,6 @@
 #include "compose.hpp"
 #include <fstream>
 
-/** @BeginDocumentation
- Name: kernel - Global properties of the simulation kernel.
-
- Description:
- Global properties of the simulation kernel.
-
- Parameters:
- The following parameters are available in the kernel status dictionary.
-
- kernel_status                         dicttype    - Get the complete kernel status (read only).
-
- Time and resolution
- biological_time                       doubletype  - The current simulation time (in ms).
- max_delay                             doubletype  - The maximum delay in the network, defaults to 0.1 (in ms).
- min_delay                             doubletype  - The minimum delay in the network, defaults to 0.1 (in ms).
- max_update_time                       doubletype  - Longest wall-clock time measured so far for a full update step (in
-                                                     s; read only).
- min_update_time                       doubletype  - Shortest wall-clock time measured so far for a full update step (in
-                                                     s; read only).
- ms_per_tic                            doubletype  - The number of milliseconds per tic, calculated by
-                                                     ms_per_tic = 1 / tics_per_ms (read_only).
- resolution                            doubletype  - The resolution of the simulation (in ms), defaults to 0.1.
- tics_per_ms                           doubletype  - The number of tics per millisecond, defaults to 1000.0.
- tics_per_step                         integertype - The number of tics per simulation time step, calculated by
-                                                     tics_per_step = resolution * tics_per_ms (read_only).
- to_do                                 integertype - The number of steps yet to be simulated (read only).
- T_max                                 doubletype  - The largest representable time value (in ms; read only).
- T_min                                 doubletype  - The smallest representable time value (in ms; read only).
- update_time_limit                     doubletype  - Maximum wall-clock time for one full update step (in s; default
-                                                     +∞). This can be used to terminate simulations that slow down
-                                                     significantly. Simulations may still get stuck if the slowdown
-                                                     occurs within a single update step.
-
- Parallel processing
- adaptive_target_buffers               booltype    - Whether MPI buffers for communication of connections resize on the
-                                                     fly, defaults to true.
- send_buffer_size_secondary_events     integertype - Size of MPI send buffers for communicating secondary events (in
-                                                     bytes; per MPI rank; for developers; read only).
- recv_buffer_size_secondary_events     integertype - Size of MPI recv buffers for communicating secondary events (in
-                                                     bytes; per MPI rank; for developers; read only).
- buffer_size_spike_data                integertype - Total size of MPI buffer for communication of spikes, defaults to
-                                                     2.
- buffer_size_target_data               integertype - Total size of MPI buffer for communication of connections, defaults
-                                                     to 2.
- local_num_threads                     integertype - The local number of threads, defaults to 1.
- max_buffer_size_target_data           integertype - Maximal size of MPI buffers for communication of connections,
-                                                     defaults to 16777216.
- num_processes                         integertype - The number of MPI processes (read only).
- off_grid_spiking                      booltype    - Whether to transmit precise spike times in MPI communication (read
-                                                     only).
- total_num_virtual_procs               integertype - The total number of virtual processes, defaults to 1.
- use_compressed_spikes                 booltype    - Whether to use spike compression; if a neuron has targets on
-                                                     multiple threads of a process, this switch makes sure that only a
-                                                     single packet is sent to the process instead of one packet per
-                                                     target thread (implies that connections will be sorted by source),
-                                                     defaults to true.
-
- Random number generators
- rng_seed                              integertype - Seed value used as basis of seeding of all random number generators
-                                                     managed by the kernel (\f$1 leq s \leq 2^{32}-1\f$).
- rng_type                              stringtype  - Name of random number generator type used by NEST, defaults to
-                                                     mt19937_64.
- rng_types                             arraytype   - List of available random number generator types (read only).
-
- Output
- data_path                             stringtype  - A path, where all data is written to, defaults to current
-                                                     directory.
- data_prefix                           stringtype  - A common prefix for all data files.
- overwrite_files                       booltype    - Whether to overwrite existing data files, defaults to false.
- print_time                            booltype    - Whether to print progress information during the simulation,
-                                                     defaults to false.
- recording_backends                    arraytype   - List of available backends for recording devices (read only).
-
- Network information
- connection_rules                      arraytype   - The list of available connection rules (read only).
- growth_curves                         arraytype   - The list of the available structural plasticity growth curves
-                                                     (read only).
- growth_factor_buffer_target_data      double      - If MPI buffers for communication of connections resize on the fly,
-                                                     grow them by this factor each round, defaults to 1.5.
- spike_buffer_grow_extra               double      - When spike exchange buffer is expanded, resize it to
-                                                     (1 + spike_buffer_grow_extra) * required_buffer_size,
-                                                     defaults to 0.5.
- spike_buffer_shrink_limit             double      - If largest number of spikes sent from any rank to any rank is
-                                                     less than spike_buffer_shrink_limit * buffer_size, then reduce
-                                                     buffer size. spike_buffer_shrink_limit == 0 means that buffers
-                                                     never shrink. See `spike_buffer_shrink_spare` for how the new
-                                                     buffer size is determined. Defaults to 0.2.
- spike_buffer_shrink_spare             double      - When buffer shrinks, set new size to
-                                                     (1 + spike_buffer_shrink_spare) * required_buffer_size`.
-                                                     See `spike_buffer_shrink_limit` for when buffers shrink.
-                                                     Defaults to 0.1.
- spike_buffer_resize_log               dict        - Information on spike buffer resizing as dictionary. It contains the
-                                                     times of the resizings (simulation clock in steps, always multiple
-                                                     of min_delay), global_max_spikes_sent, that is, the observed spike
-                                                     number that triggered the resize, and the new_buffer_size. Sizes
-                                                     are for the buffer section sent from one rank to another rank (read
- only). keep_source_table                     booltype    - Whether to keep source table after connection setup is
- complete, defaults to true. local_spike_counter                   integertype - Number of spikes fired by neurons on a
- given MPI rank during the most recent call to Simulate(). Only spikes from “normal” neurons are counted, not spikes
- generated by devices such as poisson_generator (read only). max_num_syn_models                    integertype - Maximal
- number of synapse models supported (read only). network_size                          integertype - The number of nodes
- in the network (read only). node_models                           arraytype   - The list of available node models
- (neurons and devices; read only). num_connections                       integertype - The number of connections in the
- network (read only; local only). stimulation_backends                  arraytype   - List of available backends for
- stimulation devices (read-only). structural_plasticity_synapses        dicttype    - Defines all synapses which are
- plastic for the structural plasticity algorithm. Each entry in the dictionary is composed of a synapse model, the
- presynaptic element and the postsynaptic element. structural_plasticity_update_interval integertype - Defines the time
- interval in ms at which the structural plasticity manager will make changes in the structure of the network (creation
-                                                     and deletion of plastic synapses), defaults to 10000.
- synapse_models                        arraytype   - The list of the available synapse models (read only).
-
- Waveform relaxation method (wfr)
- use_wfr                               booltype    - Whether to use waveform relaxation method, defaults to true.
- wfr_comm_interval                     doubletype  - Desired waveform relaxation communication interval, defaults to
-                                                     1.0.
- wfr_interpolation_order               integertype - Interpolation order of polynomial used in wfr iterations, defaults
-                                                     to 3.
- wfr_max_iterations                    integertype - Maximal number of iterations used for waveform relaxation, defaults
-                                                     to 15.
- wfr_tol                               doubletype  - Convergence tolerance of waveform relaxation method, defaults to
-                                                     0.0001.
-
- Miscellaneous
- dict_miss_is_error                    booltype    - Whether missed dictionary entries are treated as errors.
- build_info                   dicttype - Various information about the NEST build
- memory_size         integertype - Memory occupied by NEST process in kB (-1 if not available for OS)
- SeeAlso: Simulate, Node
-*/
-
 /**
  * Wrap debugging output code for easy enabling/disabling via -Dwith-full-logging=ON/OFF.
  */
@@ -186,6 +57,89 @@
 namespace nest
 {
 
+/**
+ * @brief Global properties of the simulation kernel.
+ *
+ * This class provides access to global properties of the simulation kernel.
+ *
+ * The following parameters are available in the kernel status dictionary:
+ *
+ * @par Time and resolution
+ * @param biological_time The current simulation time (in ms).
+ * @param max_delay The maximum delay in the network, defaults to 0.1 (in ms).
+ * @param min_delay The minimum delay in the network, defaults to 0.1 (in ms).
+ * @param max_update_time Longest wall-clock time measured so far for a full update step (in s; read only).
+ * @param min_update_time Shortest wall-clock time measured so far for a full update step (in s; read only).
+ * @param ms_per_tic The number of milliseconds per tic, calculated by ms_per_tic = 1 / tics_per_ms (read only).
+ * @param resolution The resolution of the simulation (in ms), defaults to 0.1.
+ * @param tics_per_ms The number of tics per millisecond, defaults to 1000.0.
+ * @param tics_per_step The number of tics per simulation time step, calculated by tics_per_step = resolution *
+ * tics_per_ms (read only).
+ * @param to_do The number of steps yet to be simulated (read only).
+ * @param T_max The largest representable time value (in ms; read only).
+ * @param T_min The smallest representable time value (in ms; read only).
+ * @param update_time_limit Maximum wall-clock time for one full update step (in s; default +infinity).
+ *
+ * @par Parallel processing
+ * @param adaptive_target_buffers Whether MPI buffers for communication of connections resize on the fly, defaults to
+ * true.
+ * @param buffer_size_spike_data Total size of MPI buffer for communication of spikes, defaults to 2.
+ * @param buffer_size_target_data Total size of MPI buffer for communication of connections, defaults to 2.
+ * @param local_num_threads The local number of threads, defaults to 1.
+ * @param num_processes The number of MPI processes (read only).
+ * @param off_grid_spiking Whether to transmit precise spike times in MPI communication (read only).
+ * @param total_num_virtual_procs The total number of virtual processes, defaults to 1.
+ * @param use_compressed_spikes Whether to use spike compression, defaults to true.
+ *
+ * @par Random number generators
+ * @param rng_seed Seed value used as basis of seeding of all random number generators managed by the kernel.
+ * @param rng_type Name of random number generator type used by NEST, defaults to mt19937_64.
+ * @param rng_types List of available random number generator types (read only).
+ *
+ * @par Output
+ * @param data_path A path where all data is written to, defaults to current directory.
+ * @param data_prefix A common prefix for all data files.
+ * @param overwrite_files Whether to overwrite existing data files, defaults to false.
+ * @param print_time Whether to print progress information during the simulation, defaults to false.
+ * @param recording_backends List of available backends for recording devices (read only).
+ *
+ * @par Network information
+ * @param connection_rules The list of available connection rules (read only).
+ * @param growth_curves The list of the available structural plasticity growth curves (read only).
+ * @param growth_factor_buffer_target_data If MPI buffers for communication of connections resize on the fly, grow them
+ * by this factor each round, defaults to 1.5.
+ * @param spike_buffer_grow_extra When spike exchange buffer is expanded, resize it to (1 + spike_buffer_grow_extra) *
+ * required_buffer_size, defaults to 0.5.
+ * @param spike_buffer_shrink_limit If largest number of spikes sent from any rank to any rank is less than
+ * spike_buffer_shrink_limit * buffer_size, then reduce buffer size. Defaults to 0.2.
+ * @param spike_buffer_resize_log Information on spike buffer resizing as dictionary (read only).
+ * @param keep_source_table Whether to keep source table after connection setup is complete, defaults to true.
+ * @param local_spike_counter Number of spikes fired by neurons on a given MPI rank during the most recent call to
+ * Simulate() (read only).
+ * @param max_num_syn_models Maximal number of synapse models supported (read only).
+ * @param network_size The number of nodes in the network (read only).
+ * @param node_models The list of available node models (neurons and devices; read only).
+ * @param num_connections The number of connections in the network (read only; local only).
+ * @param stimulation_backends List of available backends for stimulation devices (read only).
+ * @param structural_plasticity_synapses Defines all synapses which are plastic for the structural plasticity algorithm.
+ * @param structural_plasticity_update_interval Defines the time interval in ms at which the structural plasticity
+ * manager will make changes, defaults to 10000.
+ * @param synapse_models The list of the available synapse models (read only).
+ *
+ * @par Waveform relaxation method (wfr)
+ * @param use_wfr Whether to use waveform relaxation method, defaults to true.
+ * @param wfr_comm_interval Desired waveform relaxation communication interval, defaults to 1.0.
+ * @param wfr_interpolation_order Interpolation order of polynomial used in wfr iterations, defaults to 3.
+ * @param wfr_max_iterations Maximal number of iterations used for waveform relaxation, defaults to 15.
+ * @param wfr_tol Convergence tolerance of waveform relaxation method, defaults to 0.0001.
+ *
+ * @par Miscellaneous
+ * @param dict_miss_is_error Whether missed dictionary entries are treated as errors.
+ * @param build_info Various information about the NEST build.
+ * @param memory_size Memory occupied by NEST process in kB (-1 if not available for OS).
+ *
+ * @see Simulate, Node
+ */
 class KernelManager
 {
 private:

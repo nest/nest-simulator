8a4dd64339c267a494f6e781fd0791d385cb0195
Agnes Korcsak-Gorzo <40828647+akorgor@users.noreply.github.com>
2026-05-13 09:50:10 +0200

    Remove unnecessary condition
    Co-authored-by: Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>

--- models/pp_cond_exp_mc_urbanczik.h
+++ models/pp_cond_exp_mc_urbanczik.h
@@ -596,7 +596,7 @@ pp_cond_exp_mc_urbanczik::handles_test_event( DataLoggingRequest& dlr, size_t re
 {
   if ( receptor_type != 0 )
   {
-    if ( receptor_type < 0 or receptor_type >= SUP_CURR_RECEPTOR )
+    if ( receptor_type >= SUP_CURR_RECEPTOR )
     {
       throw UnknownReceptorType( receptor_type, get_name() );
     }
